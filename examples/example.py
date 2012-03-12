c1c609f21bed697742e34fd4aaf1ef1af02ef921
Travis E. Oliphant <teoliphant@gmail.com>
2012-03-12 03:44:22 -0500

    Add some documenation

---
+++ examples/example.py
@@ -0,0 +1,20 @@
+from math import sin, pi
+from numba.decorators import vectorize
+
+#@vectorize
+def sinc(x):
+    if x==0.0:
+        return 1.0
+    else:
+        return sin(x*pi)/(pi*x)
+
+from numba.translate import Translate
+t = Translate(sinc)
+t.translate()
+print t.mod
+res = t.make_ufunc()
+
+sinc = vectorize(sinc)
+
+from numpy import linspace
+print res(linspace(-5,5,50))
