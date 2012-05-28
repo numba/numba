2edec1c829710a72cbc23a14e3d1e0c133ee18bb
James Bergstra <james.bergstra@gmail.com>
2012-05-28 02:27:54 -0400

    working AD example

--- numba/ad.py
+++ numba/ad.py
@@ -7,13 +7,15 @@ import trace
 import opcode
 
 import numpy as np
+import theano
 
 from .utils import itercode
 
 
-class CallVM(object):
-    def __init__(self, func):
-        print 'CallVM', func
+class FrameVM(object):
+    def __init__(self, watcher, func):
+        print 'FrameVM', func
+        self.watcher = watcher
         self.func = func
         self.fco = func.func_code
         self.names = self.fco.co_names
@@ -27,14 +29,15 @@ class CallVM(object):
         self.rval = None
         self._myglobals = {}
         for name in self.names:
-            print 'name', name
+            #print 'name', name
             try:
                 self._myglobals[name] = self.func.func_globals[name]
             except KeyError:
                 try:
                     self._myglobals[name] = __builtin__.__getattribute__(name)
                 except AttributeError:
-                    print 'WARNING: name lookup failed', name
+                    #print 'WARNING: name lookup failed', name
+                    pass
 
         self._locals = [None]*len(self.fco.co_varnames)
         for i, name in enumerate(self.argnames):
@@ -43,7 +46,7 @@ class CallVM(object):
 
         for i, op, arg in itercode(self.costr):
             name = opcode.opname[op]
-            print 'OP: ', name
+            #print 'OP: ', name
             getattr(self, 'op_' + name)(i, op, arg)
 
         return self.rval
@@ -51,12 +54,26 @@ class CallVM(object):
     def op_BINARY_ADD(self, i, op, arg):
         arg2 = self.stack.pop(-1)
         arg1 = self.stack.pop(-1)
-        self.stack.append(arg1 + arg2)
+        r = arg1 + arg2
+        self.stack.append(r)
+        if (id(arg1) in self.watcher.svars 
+                or id(arg2) in self.watcher.svars):
+            s1 = self.watcher.svars.get(id(arg1), arg1)
+            s2 = self.watcher.svars.get(id(arg2), arg2)
+            self.watcher.svars[id(r)] = s1 + s2
+            #print 'added sym'
 
     def op_BINARY_MULTIPLY(self, i, op, arg):
         arg2 = self.stack.pop(-1)
         arg1 = self.stack.pop(-1)
-        self.stack.append(arg1 * arg2)
+        r = arg1 * arg2
+        self.stack.append(r)
+        if (id(arg1) in self.watcher.svars 
+                or id(arg2) in self.watcher.svars):
+            s1 = self.watcher.svars.get(id(arg1), arg1)
+            s2 = self.watcher.svars.get(id(arg2), arg2)
+            self.watcher.svars[id(r)] = s1 * s2
+            #print 'mul sym', id(r)
 
     def op_CALL_FUNCTION(self, i, op, arg):
         # XXX: does this work with kwargs?
@@ -67,35 +84,43 @@ class CallVM(object):
         recurse = True
         if func.__module__ and func.__module__.startswith('numpy'):
             recurse = False
-        print str(func)
         if 'built-in method' in str(func):
             recurse = False
         if recurse:
-            vm = CallVM(func)
+            vm = FrameVM(self.watcher, func)
             rval = vm.call(args, {})
         else:
+            #print 'running built-in', func, func.__name__, args
             rval = func(*args)
+            if any(id(a) in self.watcher.svars for a in args):
+                sargs = [self.watcher.svars.get(id(a), a)
+                        for a in args]
+                if func.__name__ == 'sum':
+                    #print 'sym sum', sargs
+                    self.watcher.svars[id(rval)] = theano.tensor.sum(*sargs)
+                else:
+                    raise NotImplementedError(func)
         self.stack.append(rval)
 
     def op_LOAD_GLOBAL(self, i, op, arg):
-        print 'LOAD_GLOBAL', self.names[arg]
+        #print 'LOAD_GLOBAL', self.names[arg]
         self.stack.append(self._myglobals[self.names[arg]])
 
     def op_LOAD_ATTR(self, i, op, arg):
-        print 'LOAD_ATTR', self.names[arg]
+        #print 'LOAD_ATTR', self.names[arg]
         TOS = self.stack[-1]
         self.stack[-1] = getattr(TOS, self.names[arg])
 
     def op_LOAD_CONST(self, i, op, arg):
-        print 'LOAD_CONST', self.constants[arg]
+        #print 'LOAD_CONST', self.constants[arg]
         self.stack.append(self.constants[arg])
 
     def op_LOAD_FAST(self, i, op, arg):
-        print 'LOAD_FAST', self.varnames[arg]
+        #print 'LOAD_FAST', self.varnames[arg]
         self.stack.append(self._locals[arg])
 
     def op_STORE_FAST(self, i, op, arg):
-        print 'STORE_FAST', self.varnames[arg]
+        #print 'STORE_FAST', self.varnames[arg]
         self._locals[arg] = self.stack.pop(-1)
 
     def op_RETURN_VALUE(self, i, op, arg):
@@ -105,8 +130,19 @@ class CallVM(object):
 class Watcher(object):
     def __init__(self, inputs):
         self.inputs = inputs
+        self.svars = {}
+        for var in inputs:
+            self.svars[id(var)] = theano.tensor.vector()
 
     def call(self, fn, *args, **kwargs):
-        vm = CallVM(fn)
+        vm = FrameVM(self, fn)
         return vm.call(args, kwargs)
+
+    def grad_fn(self, rval, ival):
+        sy = self.svars[id(rval)]
+        sx = self.svars[id(ival)]
+
+        dydx = theano.tensor.grad(sy, sx)
+
+        return theano.function([sx], dydx)
 
--- numba/tests/test_ad.py
+++ numba/tests/test_ad.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-from numba.ad import CallVM, Watcher
+from numba.ad import Watcher
 
 
 def sqr(x):
-    y = 2 * x
+    y = 2 * x  # irrelevant
     return x * x
 
 
 def compute_stuff(x):
     a = np.ones(3) + x
     b = 2 * a
-    c = sqr(b).sum()
+    #c = sqr(b).sum()  # XXX method binding is not implemented yet for AD
+    c = np.sum(sqr(b))
     return c
 
 
 def test_foo():
-
     x = np.zeros(3)
 
     w = Watcher([x])
@@ -28,13 +28,13 @@ def test_grad():
     x = np.zeros(3)
 
     w = Watcher([x])
+    assert id(x) in w.svars
     y = w.call(compute_stuff, x)
+    assert id(y) in w.svars
+    dy_dx_fn = w.grad_fn(y, x)
 
-    dy_dx_fn = Watcher.grad_fn(y, x)
+    print dy_dx_fn(x)
+    print dy_dx_fn(x + 1)
+    print dy_dx_fn(x + 2)
 
-    print dy_dx(x)
-    print dy_dx(x + 1)
-    print dy_dx(x + 2)
 
-if __name__ == '__main__':
-    sys.exit(main())
