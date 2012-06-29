b80ad3ac4562077c112cc60c557e609b49cb5559
Mark Florisson <markflorisson88@gmail.com>
2012-06-29 13:43:03 +0100

    Add submodule notice

--- numba/__init__.py
+++ numba/__init__.py
@@ -1,1 +1,11 @@
-from _numba_types import *
+import sys
+
+try:
+    from . import minivect
+except ImportError:
+    print >>sys.stderr, "Did you forget to update submodule minivect?"
+    print >>sys.stderr, "Run 'git submodule init' followed by 'git submodule update'"
+    raise
+
+
+from ._numba_types import *
