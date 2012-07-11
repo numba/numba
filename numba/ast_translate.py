e9de1e6a9bcae52910e90a992a9a3b115d6ebd20
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-07-11 12:09:33 -0700

    Minor fix

--- numba/ast_translate.py
+++ numba/ast_translate.py
@@ -417,10 +417,10 @@ class LLVMCodeGenerator(codevisitor.CodeGenerationBase):
 
     def generate_load_symbol(self, name):
         var = self.symtab[name]
-        if var.type.is_local:
+        if var.is_local:
             return self.builder.load(var.lvalue)
         else:
-            raise NotImplementedError
+            raise NotImplementedError(var)
 
     def generate_store_symbol(self, name):
         return self.symtab[name].lvalue
