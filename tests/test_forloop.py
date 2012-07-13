3341d39592f3fccd9097f61babb7e16c5780b028
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-07-13 12:51:41 -0700

    Improve test_forloop.py

--- tests/test_forloop.py
+++ tests/test_forloop.py
@@ -67,11 +67,12 @@ class TestForLoop(unittest.TestCase):
         self.assertEqual(result, for_loop_fn_0(test_data))
 
     def test_compiled_for_loop_fn_0_float32(self):
-        test_data = numpy.array([1., 2., 3.], dtype=numpy.float32)
+        test_data = numpy.array([1.2, 3.4, 5.6], dtype=numpy.float32)
         compiled_for_loop_fn = numba_compile(
             ret_type=numba.float32, arg_types=[numba.float32[:]])(for_loop_fn_0)
         result = compiled_for_loop_fn(test_data)
-        self.assertEqual(result, 6)
+        control_result = test_data.sum()
+        self.assertLess(abs(result-control_result)/control_result, 1e-7)
         self.assertEqual(result, for_loop_fn_0(test_data))
 
     def test_compiled_for_loop_fn_1(self):
