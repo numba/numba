98114750a3b5d996c4d850e084c4229a1b727fdc
Mark Florisson <markflorisson88@gmail.com>
2012-07-27 15:59:21 +0100

    Fix up merge

---
+++ examples/sum.py
@@ -0,0 +1,13 @@
+from numba import d
+from numba.decorators import numba_compile as jit
+
+def sum(arr):
+    M, N = arr.shape
+    result = 0.0
+    for i in range(M):
+        for j in range(N):
+            result += arr[i,j]
+    return result
+
+csum = jit(ret_type=d, arg_types=[d[:,:]])(sum)
+
---
+++ tests/test_types.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+# ______________________________________________________________________
+'''
+Test type mapping.
+'''
+# ______________________________________________________________________
+
+import numba
+from numba import *
+from numba.decorators import numba_compile
+
+import unittest
+
+# ______________________________________________________________________
+
+def test_int(arg):
+    if arg > 0:
+        result = 22
+    else:
+        result = 42
+    return result
+
+def test_long(arg):
+    if arg > 0:
+        result = 22
+    else:
+        result = 42
+    return result
+
+# ______________________________________________________________________
+
+class TestIf(unittest.TestCase):
+    def test_int(self):
+        func = numba_compile(ret_type=numba.int_,
+                                 arg_types=[numba.int_])(test_int)
+        self.assertEqual(func(-1), 42)
+        self.assertEqual(func(1), 22)
+
+    def test_long(self):
+        func = numba_compile(ret_type=numba.long_,
+                                 arg_types=[numba.long_])(test_long)
+        self.assertEqual(func(-1), 42)
+        self.assertEqual(func(1), 22)
+# ______________________________________________________________________
+
+if __name__ == "__main__":
+    unittest.main()
+
+# ______________________________________________________________________
+# End of test_if.py
+
