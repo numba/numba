91cfb1a046983725f179c70e3e5631521c97d45a
Maggie Mari <maggie.mari@continuum.io>
2012-08-21 15:47:06 -0500

    Changed 'llvmpy' to 'numba' in sphinx conf.py, added .gitignore

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'llvmpy'
+project = u'numba'
 copyright = u'2012, Mahadevan R (2008-2010), Continuum Analytics (2012)'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'llvmpydoc'
+htmlhelp_basename = 'numbadoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'llvmpy.tex', u'llvmpy Documentation',
+  ('index', 'numba.tex', u'numba Documentation',
    u'Mahadevan R (2008-2010), Continuum Analytics (2012)', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'llvmpy', u'llvmpy Documentation',
+    ('index', 'numba', u'numba Documentation',
      [u'Mahadevan R (2008-2010), Continuum Analytics (2012)'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'llvmpy', u'llvmpy Documentation',
-   u'Mahadevan R (2008-2010), Continuum Analytics (2012)', 'llvmpy', 'One line description of project.',
+  ('index', 'numba', u'numba Documentation',
+   u'Mahadevan R (2008-2010), Continuum Analytics (2012)', 'numba', 'One line description of project.',
    'Miscellaneous'),
 ]
 
