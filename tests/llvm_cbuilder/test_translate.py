ff1d949523d1f20e047ba56499d12081c4b1fd56
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-08-23 11:43:24 -0500

    Add llvm_cbuilder translate features:  * handle while loop  * handle for loop  * handle immediate values
    Todo: handle "break" and "continue"

--- tests/llvm_cbuilder/test_translate.py
+++ tests/llvm_cbuilder/test_translate.py
@@ -1,10 +1,13 @@
+
 from llvm.core import Module
 from llvm_cbuilder import *
 from llvm_cbuilder.translator import translate
 import llvm_cbuilder.shortnames as C
 import unittest, logging
 
-class Foo(CDefinition):
+#logging.basicConfig(level=logging.DEBUG)
+
+class FooIf(CDefinition):
     _name_ = 'foo'
     _retty_ = C.int
     _argtys_ = [('x', C.int),
@@ -19,17 +22,72 @@ class Foo(CDefinition):
                 return y - x
 
 
+class FooWhile(CDefinition):
+    _name_ = 'foo'
+    _retty_ = C.int
+    _argtys_ = [('x', C.int)]
+
+    def body(self, x):
+        y = self.var_copy(x)
+
+        @translate
+        def _():
+            while x > 0:
+                x -= 1
+                y += x
+            return y
+
+class FooForRange(CDefinition):
+    _name_ = 'foo'
+    _retty_ = C.int
+    _argtys_ = [('x', C.int)]
+
+    def body(self, x):
+        y = self.var(x.type, 0)
+
+        @translate
+        def _():
+            for i in range(x + 1):
+                y += i
+            return y
+
+
 class TestTranslate(unittest.TestCase):
-    def test_loopbreak(self):
+    def test_if(self):
         mod = Module.new(__name__)
-        lfoo = Foo()(mod)
+        lfoo = FooIf()(mod)
 
         print(mod)
         mod.verify()
 
         exe = CExecutor(mod)
         foo = exe.get_ctype_function(lfoo, 'int, int')
-        print(foo(10, 20))
+        self.assertEqual(foo(10, 20), 20 - 10)
+        self.assertEqual(foo(23, 17), 23 - 17)
+
+    def test_whileloop(self):
+        mod = Module.new(__name__)
+        lfoo = FooWhile()(mod)
+
+        print(mod)
+        mod.verify()
+
+        exe = CExecutor(mod)
+        foo = exe.get_ctype_function(lfoo, 'int')
+        self.assertEqual(foo(10), sum(range(10+1)))
+        self.assertEqual(foo(1324), sum(range(1324+1)))
+
+    def test_forloop(self):
+        mod = Module.new(__name__)
+        lfoo = FooForRange()(mod)
+
+        print(mod)
+        mod.verify()
+
+        exe = CExecutor(mod)
+        foo = exe.get_ctype_function(lfoo, 'int')
+        self.assertEqual(foo(10), sum(range(10+1)))
+        self.assertEqual(foo(1324), sum(range(1324+1)))
 
 if __name__ == '__main__':
     unittest.main()
