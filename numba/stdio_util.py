3208aa6fbaa9708241146eb2fcfb16105d838c59
Jon Riehl <jriehl@spaceship.com>
2012-09-19 12:56:40 -0500

    Adding utility to get C streams using ctypes instead of an extension module.

---
+++ numba/stdio_util.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+# ______________________________________________________________________
+
+import ctypes
+import ctypes.util
+
+# ______________________________________________________________________
+
+c_void_pp = ctypes.POINTER(ctypes.c_void_p)
+
+def get_libc ():
+    return ctypes.CDLL(ctypes.util.find_library('c'))
+
+def get_stdio_streams ():
+    '''
+    Returns file pointers (FILE *) as Python integers for the C stdio
+    stdin, stdout, and stderr streams.
+    '''
+    ret_val = None
+    if hasattr(ctypes.pythonapi, 'stdin'):
+        # Linux
+        _stdio_files = (ctypes.c_void_p.in_dll(ctypes.pythonapi, sym)
+                        for sym in ('stdin', 'stdout', 'stderr'))
+        ret_val = tuple(c_void_pp(file_p)[0] for file_p in _stdio_files)
+    elif hasattr(ctypes.pythonapi, '__stdinp'):
+        # OSX
+        _stdio_files = (ctypes.c_void_p.in_dll(ctypes.pythonapi, sym)
+                        for sym in ('__stdinp', '__stdoutp', '__stderrp'))
+        ret_val = tuple(c_void_pp(file_p)[0] for file_p in _stdio_files)
+    else:
+        libc = get_libc()
+        if hasattr(libc, '__getreent'):
+            # Cygwin
+            ret_val = tuple(ctypes.cast(libc.__getreent(), c_void_pp)[1:4])
+        else:
+            raise NotImplementedError("Unsupported platform, don't know how to "
+                                      "find pointers to stdio streams!")
+    return ret_val
+
+# ______________________________________________________________________
+
+def main ():
+    _, stdout, _ = get_stdio_streams()
+    PyObject_Print = ctypes.pythonapi.PyObject_Print
+    PyObject_Print.restype = ctypes.c_int
+    PyObject_Print.argtypes = ctypes.py_object, ctypes.c_void_p, ctypes.c_int
+    PyObject_Print(get_stdio_streams, stdout, 1)
+    PyObject_Print('\n\n', stdout, 1)
+
+# ______________________________________________________________________
+
+if __name__ == "__main__":
+    main()
+
+# ______________________________________________________________________
+# End of stdio_util.py
