3069dadc00ae5c43f409ce2409d0965a9bedb933
Mark Florisson <markflorisson88@gmail.com>
2012-09-28 15:42:15 +0100

    Add some imports to numbapro.vectorize

--- numbapro/vectorize/__init__.py
+++ numbapro/vectorize/__init__.py
@@ -2,10 +2,13 @@ from .basic import BasicVectorize
 from .parallel import ParallelVectorize
 from .stream import StreamVectorize
 
+from .gufunc import GUFuncVectorize, ASTGUFuncVectorize
+
 try:
     from .cuda import  CudaVectorize
+    from .gufunc import CUDAGUFuncVectorize
 except ImportError, e:
     CudaVectorize = BasicVectorize
+    CUDAGUFuncVectorize = GUFuncVectorize
 
 from .minivectorize import MiniVectorize, ParallelMiniVectorize
-from .gufunc import GUFuncVectorize
