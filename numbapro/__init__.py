20755a0a9332cbdad33b6b0df13a0e60789338ca
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-09-29 10:34:21 -0500

    Add numbapro.exercise for running tests that are not unittest. Note: CUDA test are automatically opt-out if the installation cannot import the CUDA libraries.

--- numbapro/__init__.py
+++ numbapro/__init__.py
@@ -27,6 +27,41 @@ def test(verbosity=2, failfast=False):
     runner = unittest.TextTestRunner(verbosity=verbosity, failfast=True, stream=sys.stdout)
     return runner.run(suite)
 
+def exercise():
+    '''Exercise test that are not unittest.TestCase
+    '''
+    import subprocess
+    import sys
+    import os
+    no_cuda_safe = '''
+        test_basic_vectorize.py
+        test_parallel_vectorize.py
+        test_stream_vectorize.py
+        test_gufunc.py
+        test_mini_vectorize.py
+        test_array_expressions.py
+        '''.split()
+
+    cuda_only = '''
+        test_cuda_vectorize.py
+        test_cuda_jit.py
+        '''.split()
+
+    tests = no_cuda_safe
+    try:
+        import numbapro._cudadispatch
+        tests += cuda_only
+    except ImportError: # no cuda?
+        pass
+
+    numbapro_dir = os.path.dirname(os.path.realpath(__file__))
+
+    for name in tests:
+        print('== Exercising %s' % name)
+        script = os.path.join(numbapro_dir, 'tests', name)
+        subprocess.check_call([sys.executable, script])
+    print('== Exercise completed successfully')
+
 def drop_in_gdb(addr=None, type='int'):
     import os, signal
     if addr is not None:
