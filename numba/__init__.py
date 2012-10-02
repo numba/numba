fe175a477942ccc4ab074dd422dcb2275c6d998f
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-10-02 17:02:30 -0500

    Slient numba logging.

--- numba/__init__.py
+++ numba/__init__.py
@@ -5,8 +5,8 @@ import logging
 #   https://github.com/numba/numba/issues/31
 # A good work around is to make your tests handle a debug flag, per
 # numba.tests.test_support.main().
-logging.basicConfig(level=logging.DEBUG,
-                    format="\n\033[1m%(levelname)s -- %(module)s:%(lineno)d:%(funcName)s\033[0m\n%(message)s")
+#logging.basicConfig(level=logging.DEBUG,
+#                    format="\n\033[1m%(levelname)s -- %(module)s:%(lineno)d:%(funcName)s\033[0m\n%(message)s")
 
 try:
     from . import minivect
