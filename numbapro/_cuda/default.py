4f9d8ea6e4fa3f03544b53c4663510d1293b887b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-10-09 16:27:50 -0500

    Default globals for driver, device and context

---
+++ numbapro/_cuda/default.py
@@ -0,0 +1,25 @@
+from .driver import Driver, Device, Context
+import logging, os
+logger = logging.getLogger(__name__)
+
+# Let there be a CUDA driver
+# Otherwise, raise ImportError
+driver = Driver()
+
+# Choose the first device unless the user has a specific choice
+# stored in file ~/.cuda_device
+device_number = 0
+try:
+    n = open(os.path.expanduser("~/.cuda_device")).read().strip()
+except IOError:
+    pass
+else:
+    try:
+        device_number = int(n)
+    except ValueError:
+        logger.error("Failed to parse ~/.cuda_device: %s" % n)
+
+device = Device(driver, device_number)
+context = Context(device)
+
+
