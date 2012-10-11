6488a821e6d77caed7e449248c4f084e3b4fb049
Mark Florisson <markflorisson88@gmail.com>
2012-10-11 20:39:26 +0100

    Test str builtin

---
+++ numba/tests/builtins/test_builtin_str.py
@@ -0,0 +1,23 @@
+"""
+>>> empty_str()
+''
+>>> str_convert(12.2)
+'12.2'
+"""
+
+import sys
+
+from numba import *
+
+@autojit(backend='ast')
+def empty_str():
+    x = str()
+    return x
+
+@autojit(backend='ast')
+def str_convert(x):
+    return str(x)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
