e0ef508936077d2e42a362bcf729f08c003de7a5
Mark Florisson <markflorisson88@gmail.com>
2012-10-11 21:51:12 +0100

    Test some more builtins

--- numba/tests/builtins/test_object_builtins.py
+++ numba/tests/builtins/test_object_builtins.py
@@ -5,6 +5,16 @@
 Traceback (most recent call last):
     ...
 NumbaError: locals() is not supported in numba functions
+>>> get_sum(3)
+6
+>>> eval_something("'hello'")
+'hello'
+>>> list(enumerate_list())
+[(0, 1), (1, 2), (2, 3)]
+>>> max_(20)
+20
+>>> min_(-2)
+-2
 """
 
 from numba import *
@@ -19,6 +29,26 @@ def get_globals():
 def get_locals():
     x = 2
     return locals()['x']
+
+@autojit(backend='ast')
+def get_sum(x):
+    return sum([1, 2, x])
+
+@autojit(backend='ast')
+def eval_something(s):
+    return eval(s)
+
+@autojit(backend='ast')
+def enumerate_list():
+    return enumerate([1, 2, 3])
+
+@autojit(backend='ast')
+def max_(x):
+    return max(1, 2.0, x, 14)
+
+@autojit(backend='ast')
+def min_(x):
+    return min(1, 2.0, x, 14)
 
 if __name__ == '__main__':
     import doctest
