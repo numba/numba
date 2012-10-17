3d158804d8641feca1ddb924861dfa6a45cf671a
Mark Florisson <markflorisson88@gmail.com>
2012-10-17 13:38:12 +0100

    Cleanup test_avg2d

--- numba/tests/test_avg2d.py
+++ numba/tests/test_avg2d.py
@@ -6,6 +6,7 @@ import numpy
 
 from numba import f8 as d
 from numba.decorators import jit, autojit
+from numba.tests import test_support
 
 import unittest
 import __builtin__
@@ -32,7 +33,7 @@ def avg2d_w_cast(arr, result):
 
 # ______________________________________________________________________
 
-class TestAvg2D (unittest.TestCase):
+class TestAvg2D(test_support.ByteCodeTestCase):
     def _do_test(self, _avg2d, compiled_fn):
         test_data = numpy.random.random((5,5))
         control_result = numpy.zeros((5,))
@@ -42,33 +43,13 @@ class TestAvg2D (unittest.TestCase):
         self.assertTrue((control_result == test_result).all())
 
     def test_avg2d(self):
-        compiled_fn = jit(argtypes = [d[:,:], d[:]])(avg2d)
+        compiled_fn = self.jit(argtypes = [d[:,:], d[:]])(avg2d)
         self._do_test(avg2d, compiled_fn)
 
-    def test_avg2d_ast(self):
-        compiled_fn = jit(argtypes = [d[:,:], d[:]], backend='ast')(avg2d)
-        self._do_test(avg2d, compiled_fn)
 
-    def test_avg2d_bytecode_function(self):
-        compiled_fn = autojit(backend='bytecode')(avg2d)
-        self._do_test(avg2d, compiled_fn)
-
-    def test_avg2d_ast_function(self):
-        compiled_fn = autojit(backend='ast')(avg2d)
-        self._do_test(avg2d, compiled_fn)
-
-    @unittest.skipUnless(hasattr(__builtin__, '__noskip__'),
-                         "Need support for float() builtin.")
+class TestAvg2DAST(test_support.ASTTestCase, TestAvg2D):
     def test_avg2d_w_cast(self):
-        compiled_fn = jit(argtypes = [d[:,:], d[:]])(avg2d_w_cast)
-        self._do_test(avg2d_w_cast, compiled_fn)
-
-    def test_avg2d_w_cast_ast(self):
-        compiled_fn = jit(argtypes = [d[:,:], d[:]], backend='ast')(avg2d_w_cast)
-        self._do_test(avg2d_w_cast, compiled_fn)
-
-    def test_avg2d_w_cast_function(self):
-        compiled_fn = autojit(backend='ast')(avg2d_w_cast)
+        compiled_fn = self.jit(argtypes = [d[:,:], d[:]])(avg2d_w_cast)
         self._do_test(avg2d_w_cast, compiled_fn)
 
 # ______________________________________________________________________
