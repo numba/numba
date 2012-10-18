071396ea7d11710b990ff19cd2b4e53556bf455e
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-10-18 17:15:52 -0500

    Improve timing in tests

--- numbapro/tests/timing.py
+++ numbapro/tests/timing.py
@@ -1,7 +1,17 @@
 from time import time, clock
-time_prec = abs(time() -  time())
-clock_prec = abs(clock() - clock())
+def _dummy():
+    j = 1
+    for i in range(100):
+        j += i
+    return 0
+
+time_prec = abs(time() - _dummy() - time())
+clock_prec = abs(clock() - _dummy() - clock())
 assert time_prec > 0 or clock_prec > 0, (time_prec, clock_prec)
 if clock_prec > time_prec:
     time = clock
 
+
+if __name__ == '__main__':
+    print 'time()', time_prec
+    print 'clock()', clock_prec
