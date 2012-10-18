8d612849f5f296e84dc76338ff7921fb1d1c4e7d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-10-18 17:03:41 -0500

    Naming convention needs to be compatible with every ABI.

--- numba/naming.py
+++ numba/naming.py
@@ -1,3 +1,13 @@
+import re
+
+_ptx_invalid_char = re.compile('[^a-zA-Z0-9_]')
+
+def _fix_naming(string):
+    def repl(m):
+        return '_%X_' % (ord(m.group(0)))
+    return _ptx_invalid_char.sub(repl, string)
+
 def specialized_mangle(func_name, types):
     type_strings = "_".join(str(t).replace(" ", "_") for t in types)
-    return "__numba_specialized_%s_%s" % (func_name, type_strings)
+    pre = "__numba_specialized_%s_%s" % (func_name, type_strings)
+    return _fix_naming(pre)
