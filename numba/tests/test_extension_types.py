c7ddfce466aabd631ec4c7e1a03a5673f89e29a6
Mark Florisson <markflorisson88@gmail.com>
2012-10-23 13:56:40 +0100

    Test native attributes refcounting

--- numba/tests/test_extension_types.py
+++ numba/tests/test_extension_types.py
@@ -1,63 +1,68 @@
-"""
->>> obj = MyExtension(10.0)
->>> obj.value
-10.0
->>> obj._numba_attrs.value
-10.0
->>> obj.setvalue(20.0)
->>> obj.getvalue()
-20.0
->>> obj.value
-20.0
->>> obj.getvalue.__name__
-'getvalue'
->>> obj.getvalue.__doc__
-'Return value'
->>> type(obj.getvalue.im_func)
-<type 'cython_function_or_method'>
-"""
+
 
 import sys
 
 from numba import extension_types
 from numba import *
 
-def test(struct_type, vtab_type):
-    """
-    outdated.
-
-    >> struct_type = struct([('x', int_), ('y', char), ('z', float_)])
-    >> vtab_type = struct([])
-    >> obj, attrs = test(struct_type, vtab_type)
-    Test
-    hello!
-    >> attrs.x, attrs.y, attrs.z
-    (0, 0, 0.0)
-    """
-
-    def init(self, value):
-        self.value = value
-
-    def repr(self):
-        return str(self.value)
-
-    dict = {
-        '__init__': init,
-        '__repr__': repr,
-    }
-
-    t = extension_types.create_new_extension_type('Test', (object,), dict,
-                                                  struct_type, vtab_type,
-                                                  [], [])
-    print t.__name__
-    print t('hello!')
-
-    obj = t(10)
-    return obj, obj._numba_attrs
+def format_str(msg, *values):
+    return msg % values
 
 @autojit
 class MyExtension(object):
+    """
+    >>> obj = MyExtension(10.0)
+    >>> obj.value
+    10.0
+    >>> obj._numba_attrs.value
+    10.0
+    >>> obj.setvalue(20.0)
+    >>> obj.getvalue()
+    20.0
+    >>> obj.value
+    20.0
+    >>> obj.getvalue.__name__
+    'getvalue'
+    >>> obj.getvalue.__doc__
+    'Return value'
+    >>> type(obj.getvalue.im_func)
+    <type 'cython_function_or_method'>
+    """
+
     @void(double)
+    def __init__(self, myfloat):
+        self.value = myfloat
+
+    def getvalue(self):
+        "Return value"
+        return self.value
+
+    @void(double)
+    def setvalue(self, value):
+        "Set value"
+        self.value = value
+
+    def __repr__(self):
+        return format_str('MyExtension%s', self.value)
+
+@autojit
+class ObjectAttrExtension(object):
+    """
+    >>> obj = ObjectAttrExtension(10.0)
+    >>> obj.value
+    10.0
+    >>> obj = ObjectAttrExtension('hello')
+    >>> obj.value
+    'hello'
+    >>> obj.setvalue(20.0)
+    >>> obj.getvalue()
+    20.0
+    >>> obj.value = MyExtension(10.0)
+    >>> obj.value
+    MyExtension10.0
+    """
+
+    @void(object_)
     def __init__(self, myfloat):
         self.value = myfloat
 
