dbcf161cf2f25c6777756e8b82c69fa1272dec4f
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-10-23 22:36:30 -0500

    remove byte code tests that uses autojit.  It still uses ast?

--- numba/tests/bytecode/test_all.py
+++ numba/tests/bytecode/test_all.py
@@ -12,13 +12,13 @@ from test_avg2d import TestAvg2D
 from test_cfg import TestCFG
 #from test_complex import TestComplex
 from test_cstring import TestBytecodeCString
-from test_extern_call import TestExternCall
+#from test_extern_call import TestExternCall
 from test_filter2d import TestFilter2d
 from test_forloop import TestForLoop
 from test_if import TestIf
 from test_indexing import TestIndexing
 from test_issues import TestIssues
-from test_mandelbrot import TestMandelbrot
+#from test_mandelbrot import TestMandelbrot
 from test_multiarray_api import TestMultiarrayAPI
 #from test_tuple import TestTuple # Not properly refcounted in bytecode translator!
 #from test_vectorize import TestVectorize
