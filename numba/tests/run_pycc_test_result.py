49ee400e6ef66addfc987d6604f41df4845bcf79
Jon Riehl <jriehl@spaceship.com>
2012-10-25 15:20:59 -0500

    Added code to automatically remove pycc generated test shared object in numba.tests.run_pycc_test_result.

--- numba/tests/run_pycc_test_result.py
+++ numba/tests/run_pycc_test_result.py
@@ -6,8 +6,11 @@ from numba.pycc import find_shared_ending
 from numba.pycc import pycc
 
 modulename = os.path.join(os.path.dirname(__file__), 'compile_with_pycc')
+cdll_modulename = modulename + find_shared_ending()
+if os.path.exists(cdll_modulename):
+    os.unlink(cdll_modulename)
 pycc.main(args=[modulename + '.py'])
-lib = CDLL(modulename + find_shared_ending())
+lib = CDLL(cdll_modulename)
 
 lib.mult.argtypes = [c_double, c_double]
 lib.mult.restype = c_double
@@ -25,4 +28,6 @@ res = lib.multf(987, 321)
 print 'lib.multf(987, 321) =', res
 assert res == 987 * 321
 
-
+del lib
+if os.path.exists(cdll_modulename):
+    os.unlink(cdll_modulename)
