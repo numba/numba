4123f349e2e6d6dc4e381d29f913d631fb7ddfbb
Travis E. Oliphant <teoliphant@gmail.com>
2012-11-01 18:02:43 -0500

    Add a few more examples.

---
+++ examples/findmulti.py
@@ -0,0 +1,9 @@
+from numba import jit
+
+@jit('i4(i4,f8,i4,f8[:])')
+def FindMult(i,u,p,U):
+    s = 0
+    for j in range(0-p, p+2):
+        if U[i+j] == u:
+            s = s + 1
+    return s
---
+++ examples/simpleadd.py
@@ -0,0 +1,14 @@
+from numba import jit, autojit
+
+@jit('f8(f8,f8)')
+def addv(a,b): return a+b
+
+@jit('f8(f8[:])')
+def sum1d(A):
+    n = A.size
+    s = 0.0
+    for i in range(n):
+        a = A[i]
+        s = addv(s, A[i])
+    return s
+
