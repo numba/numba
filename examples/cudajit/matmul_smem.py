0b86e14c580ab0fe1dedce9ffbb049d1c104724c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-11-05 18:03:50 -0600

    Add matrix multiplication example that uses shared memory. Need to work on it to support larger matrices.

--- examples/cudajit/matmul_smem.py
+++ examples/cudajit/matmul_smem.py
@@ -4,13 +4,15 @@ import numpy as np
 import math
 from timeit import default_timer as time
 
-
-bpg = 50
+bpg = 10
 tpb = 20
 n = bpg * tpb
 
 @cuda.jit(argtypes=[f4[:,:], f4[:,:], f4[:,:]])
 def cu_square_matrix_mul(A, B, C):
+    sA = cuda.shared.array(shape=(tpb, n), dtype=f4)
+    sB = cuda.shared.array(shape=(n, tpb), dtype=f4)
+    
     tx = cuda.threadIdx.x
     ty = cuda.threadIdx.y
     bx = cuda.blockIdx.x
@@ -21,19 +23,27 @@ def cu_square_matrix_mul(A, B, C):
     x = tx + bx * bw
     y = ty + by * bh
 
+    if x < n or y < n:
+        for i in range(bpg):
+            sA[ty, tx + i * tpb] = A[y, tx + i * tpb]
+            sB[ty + i * tpb, tx] = B[ty + i * tpb, x]
+
+
+    cuda.syncthreads()
+
     if x >= n or y >= n:
-        return
+        return # no thread can die before a syncthread
 
     C[y, x] = 0
     for i in range(n):
-        C[y, x] += A[y, i] * B[i, x]
+        C[y, x] += sA[ty, i] * sB[i, tx]
 
 
 A = np.array(np.random.random((n, n)), dtype=np.float32)
 B = np.array(np.random.random((n, n)), dtype=np.float32)
 C = np.empty_like(A)
 
-print "N = %d x %d" % (bpg * tpb, bpg * tpb)
+print "N = %d x %d" % (n, n)
 
 s = time()
 cu_square_matrix_mul[(bpg, bpg), (tpb, tpb)](A, B, C)
