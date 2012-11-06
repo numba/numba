Fix stride calculation for CUDA shared memory.

numbapro/_cuda/transform.py
@@ -36,7 +36,11 @@ class CudaSMemArrayCallNode(nodes.Node):
     _attributes = ('shape', 'variable')
     def __init__(self, context, shape, dtype):
         self.shape = shape
-        self.strides = [dtype.itemsize * s for s in list(self.shape[:-1]) + [1]]
+        tmp_strides = [dtype.itemsize]
+        for s in reversed(self.shape[1:]):
+            tmp_strides.append(tmp_strides[-1] * s)
+        self.strides = tuple(reversed(tmp_strides))
+
         self.elemcount = np.prod(self.shape)
         self.dtype = dtype
         type = minitypes.ArrayType(dtype=dtype,