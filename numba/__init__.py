40af69656b71cda7f775cface3478106f070ed35
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-11-07 14:50:12 -0600

    Update logging facility.  Don't overide root logger with basicConfig.

--- numba/__init__.py
+++ numba/__init__.py
@@ -5,8 +5,39 @@ import logging
 #   https://github.com/numba/numba/issues/31
 # A good work around is to make your tests handle a debug flag, per
 # numba.tests.test_support.main().
-logging.basicConfig(#level=logging.DEBUG,
-                    format="\n\033[1m%(levelname)s -- %(module)s:%(lineno)d:%(funcName)s\033[0m\n%(message)s")
+
+class _RedirectingHandler(logging.Handler):
+    '''
+    A log hanlder that applies its formatter and redirect the emission
+    to a parent handler.
+    '''
+    def set_handler(self, handler):
+        self.handler = handler
+
+    def emit(self, record):
+        # apply our own formatting
+        record.msg = self.format(record)
+        record.args = [] # clear the args
+        # use parent handler to emit record
+        self.handler.emit(record)
+
+def _config_logger():
+    root = logging.getLogger(__name__)
+    format = "\n\033[1m%(levelname)s -- "\
+             "%(module)s:%(lineno)d:%(funcName)s\033[0m\n%(message)s"
+    try:
+        parent_hldr = root.parent.handlers[0]
+    except IndexError: # parent handler is not initialized?
+        # build our own handler --- uses sys.stderr by default.
+        parent_hldr = logging.StreamHandler()
+    hldr = _RedirectingHandler()
+    hldr.set_handler(parent_hldr)
+    fmt = logging.Formatter(format)
+    hldr.setFormatter(fmt)
+    root.addHandler(hldr)
+    root.propagate = False # do not propagate to the root logger
+
+_config_logger()
 
 try:
     from . import minivect
