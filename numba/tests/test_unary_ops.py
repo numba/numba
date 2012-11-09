d3325c3c1489ad4659a138c5afd71c7fa0b18a28
Jon Riehl <jriehl@spaceship.com>
2012-11-09 13:27:45 -0600

    Adding (skipped) test to numba.tests.test_unary_ops based on user bug report.

--- numba/tests/test_unary_ops.py
+++ numba/tests/test_unary_ops.py
@@ -3,7 +3,10 @@
 from numba import *
 from . import test_support
 
+import numpy, math
+
 import unittest
+
 
 def unary_minus(x):
     return -x
@@ -18,6 +21,14 @@ def unary_not_pred(p):
 
 def unary_invert(x):
     return ~x
+
+def maxstar1d(a, b):
+    M = a.shape[0]
+    res = numpy.empty(M)
+    for i in range(M):
+        res[i] = numpy.max(a[i], b[i]) + numpy.log1p(
+            numpy.exp(-numpy.abs(a[i] - b[i])))
+    return res
 
 
 class TestUnaryOps(unittest.TestCase):
@@ -45,6 +56,14 @@ class TestUnaryOps(unittest.TestCase):
         self.assertEqual(test_fn(test_val), ~test_val)
         self.assertEqual(test_fn(test_val), unary_invert(test_val))
 
+    @test_support.checkSkipFlag("Object arithmetic not currently supported.")
+    def test_maxstar1d(self):
+        test_fn = jit('f8[:](f8[:],f8[:])')(maxstar1d)
+        test_a = numpy.random.random(10)
+        test_b = numpy.random.random(10)
+        self.assertTrue((test_fn(test_a, test_b) ==
+                         maxstar1d(test_a, test_b)).all())
+
 
 if __name__ == "__main__":
-    unittest.main()
+    test_support.main()
