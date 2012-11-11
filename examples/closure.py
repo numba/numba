b60eece9681e0d53960ea4abc8a40e7d364d5f40
Mark Florisson <markflorisson88@gmail.com>
2012-11-11 15:20:06 +0000

    Update closures example

--- examples/closure.py
+++ examples/closure.py
@@ -1,3 +1,10 @@
+"""
+Example for closures. Closures may be of arbitrary dept, and they keep
+the scope alive as long as the closure is alive. Only variables that are
+closed over (cell variables in the defining function, free variables in the
+closure), are kept alive. See also numba/tests/closures/test_closure.py
+"""
+
 from numba import autojit, jit, float_
 from numpy import linspace
 
@@ -7,6 +14,10 @@ def generate_power_func(n):
     def nth_power(x):
         return x ** n
 
+    # This is a native call
+    print nth_power(10)
+
+    # Return closure and keep all cell variables alive
     return nth_power
 
 for n in range(2, 5):
