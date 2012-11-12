3cb9bc792d3a5a62d9d16a787d02b5876cc3129a
Mark Florisson <markflorisson88@gmail.com>
2012-11-12 14:28:22 +0000

    Add diffusion array expression test

--- numbapro/tests/array_exprs/test_array_expressions.py
+++ numbapro/tests/array_exprs/test_array_expressions.py
@@ -95,6 +95,29 @@ def test_gufunc_array_expressions():
 #    b = vectorized_math.py_func(np.arange(100, dtype=np.float64))
 #    assert np.allclose(a, b)
 
+@autojit
+def diffuse_numbapro(iter_num):
+    u = np.zeros((Lx, Ly), dtype=np.float64)
+    temp_u = np.zeros_like(u)
+    temp_u[Lx / 2, Ly / 2] = 1000.0
+
+    for i in range(iter_num):
+        u[1:-1, 1:-1] = mu * (temp_u[2:, 1:-1] + temp_u[:-2, 1:-1] +
+                              temp_u[1:-1, 2:] + temp_u[1:-1, :-2] -
+                              4 * temp_u[1:-1, 1:-1])
+
+        temp = u
+        u = temp_u
+        temp_u = temp
+
+    return u
+
+mu = 0.1
+Lx, Ly = 101, 101
+
+def test_diffusion():
+    assert np.allclose(diffuse_numbapro(100), diffuse_numbapro.py_func(100))
+
 if __name__ == '__main__':
 #    test_vectorized_math()
     numba.nose_run()
