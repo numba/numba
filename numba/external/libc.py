4b575c9ef19d46d095ad1cb7c9d08f17283757b6
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-12-17 17:59:20 -0600

    Forgot to add libc.py

---
+++ numba/external/libc.py
@@ -0,0 +1,45 @@
+from external import ExternalFunction
+from numba import *
+
+class printf(ExternalFunction):
+    arg_types = [void.pointer()]
+    return_type = int32
+    is_vararg = True
+
+class puts(ExternalFunction):
+    arg_types = [c_string_type]
+    return_type = int32
+
+class labs(ExternalFunction):
+    arg_types = [long_]
+    return_type = long_
+
+class llabs(ExternalFunction):
+    arg_types = [longlong]
+    return_type = longlong
+
+class atoi(ExternalFunction):
+    arg_types = [c_string_type]
+    return_type = int_
+
+class atol(ExternalFunction):
+    arg_types = [c_string_type]
+    return_type = long_
+
+class atoll(ExternalFunction):
+    arg_types = [c_string_type]
+    return_type = longlong
+
+class atof(ExternalFunction):
+    arg_types = [c_string_type]
+    return_type = double
+
+class strlen(ExternalFunction):
+    arg_types = [c_string_type]
+    return_type = size_t
+
+__all__ = [k for k, v in globals().items()
+           if (v != ExternalFunction
+               and isinstance(v, type)
+               and issubclass(v, ExternalFunction))]
+
