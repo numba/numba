a56ffb9d7a60829ee6a849ae28df1053c8f699f3
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-01-03 13:03:35 -0600

    Enhance finalizer to handle multiple ownees per owner.

--- numbapro/_utils/finalizer.py
+++ numbapro/_utils/finalizer.py
@@ -10,34 +10,33 @@ class OwnerRef(weakref.ref):
     """A simple weakref.ref subclass, so attributes can be added."""
     pass
 
-
 def _run_finalizer(ref):
     """Internal weakref callback to run finalizers"""
-    del _finalize_refs[id(ref)]
-    finalizer = ref.finalizer
-    item = ref.item
-    try:
-        finalizer(item)
-    except Exception:
-        print>>sys.stderr, "Exception running {}:".format(finalizer)
-        traceback.print_exc()
-
+    del _finalize_refs[ref.owner]
+    for item, finalizer in ref.items:
+        try:
+            finalizer(item)
+        except Exception:
+            print>>sys.stderr, "Exception running {}:".format(finalizer)
+            traceback.print_exc()
 
 _finalize_refs = {}
-
 
 def track(owner, item, finalizer):
     """Register an object for finalization.
 
-    ``owner`` is the the object which is responsible for ``item``.
-    ``finalizer`` will be called with ``item`` as its only argument when
-    ``owner`` is destroyed by the garbage collector.
-    """
-    ref = OwnerRef(owner, _run_finalizer)
-    ref.owner = weakref.ref(owner)
-    ref.item = item
-    ref.finalizer = finalizer
-    _finalize_refs[id(ref)] = ref
+        ``owner`` is the the object which is responsible for ``item``.
+        ``finalizer`` will be called with ``item`` as its only argument when
+        ``owner`` is destroyed by the garbage collector.
+        """
+    if id(owner) in _finalize_refs:
+        ref = _finalize_refs[id(owner)]
+    else:
+        ref = OwnerRef(owner, _run_finalizer)
+        ref.owner = id(owner)
+        ref.items = []
+    ref.items.append((item, finalizer))
+    _finalize_refs[id(owner)] = ref
 
 class OwnerMixin(object):
     def _finalizer_track(self, item):
