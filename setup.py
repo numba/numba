25d6a659e8460c8452b83ae2b2b806a11a5ed6da
Mark Florisson <markflorisson88@gmail.com>
2013-01-12 22:30:26 -0600

    Remove python 3.2 pypi classifier (Issue #78)

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.2",
+        # "Programming Language :: Python :: 3.2",
         "Topic :: Utilities",
     ],
     description = "compiling Python code using LLVM",
