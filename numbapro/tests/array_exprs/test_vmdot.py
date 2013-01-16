d576fdf05f3024e08b7513943172307929e88cf3
Mark Florisson <markflorisson88@gmail.com>
2013-01-15 19:14:04 -0600

    Fix test vmdot

--- numbapro/tests/array_exprs/test_vmdot.py
+++ numbapro/tests/array_exprs/test_vmdot.py
@@ -1,18 +1,38 @@
+import time
+
 import numbapro
 import numba
 from numba import *
 import numpy as np
 
 # Bug reported by Jeremiah L. Lowin
+
+def timer(pyfunc, numbafunc, *args, **kwargs):
+    t1 = time.time()
+    pyresult = pyfunc(*args, **kwargs)
+    t2 = time.time()
+    print ''
+    print 'python function took: {0}'.format(t2-t1)
+
+    t3 = time.time()
+    numbaresult = numbafunc(*args, **kwargs)
+    t4 = time.time()
+    print 'numba function took: {0}'.format(t4-t3)
+    print 'speedup: {0}x'.format(np.round((t2-t1) / (t4-t3),2))
+
+    if not np.allclose(pyresult, numbaresult):
+        print '======='
+        print 'WARNING: python and numba results are different!!'
+        print '======='
+
+    print ''
+
 def vmdot(x, w):
     out = np.empty((x.shape[0], w.shape[1]))
-    print "out", numba.typeof(out)
-#    for i in range(x.shape[0]):
-    i = 0
-    dot_prod = np.dot(x[i], w)
-    print "dot_prod", numba.typeof(dot_prod)
-    #print "np.exp", numba.typeof(np.exp(-1 * dot_prod))
-    out[i] = np.exp(-1 * dot_prod)
+    for i in range(x.shape[0]):
+        dot_prod = np.dot(x[i], w)
+        out[i] = np.exp(-1 * dot_prod)
+
     return out
 
 def test_vmdot():
@@ -20,15 +40,7 @@ def test_vmdot():
                       argtypes=[double[:,:], double[:,:]])(vmdot)
     x = np.random.random((1000, 1000))
     w = np.random.random((1000, 1000)) / 1000.
-    print numba_vmdot(x, w)
-    #timer(vmdot, numba_vmdot, x, w) # fails without error -- different results
-
-@autojit
-def func(a, b):
-    a[:] = b[:]
+    timer(vmdot, numba_vmdot, x, w) # fails without error -- different results
 
 if __name__ == '__main__':
-#    a = np.arange(10, dtype=np.float32)
-#    b = np.arange(10, dtype=np.complex128)
-#    func(a, b)
     test_vmdot()
