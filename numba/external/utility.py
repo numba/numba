d0a5e2901a9bcb516fd9bef8f01eb9545d20bdc6
Mark Florisson <markflorisson88@gmail.com>
2013-01-26 16:36:16 -0600

    Allow easy loading of utility functions as AST nodes and LLVM values

--- numba/external/utility.py
+++ numba/external/utility.py
@@ -3,10 +3,13 @@ import llvm.core
 from numba import Py_uintptr_t
 from numba import nodes
 from numba.external import external
+from numba.external.utilities import utilities
 
 class UtilityFunction(external.ExternalFunction):
     """
     A utility function written in a native language.
+
+    funcaddr: the integer address of the C utility function
     """
 
     def __init__(self, funcaddr, return_type=None, arg_types=None, **kwargs):
@@ -21,4 +24,14 @@ class UtilityFunction(external.ExternalFunction):
 
     def ast_node(self, context):
         lvalue = self.llvm_value(context)
-        nodes.LLVMValueRefNode(self.signature, lvalue)
+        return nodes.LLVMValueRefNode(self.signature, lvalue)
+
+    @classmethod
+    def load(cls, func_name):
+        """
+        Load a utility function by name from the
+        numba.external.utilities.utilities module.
+        """
+        # Get the integer address of C utility function
+        func_addr = getattr(utilities, func_name)
+        return cls(func_addr)
