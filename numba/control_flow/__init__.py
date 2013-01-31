acda2de1d6b317308a4a4f75d707774f06f16062
Mark Florisson <markflorisson88@gmail.com>
2013-01-31 17:07:24 -0600

    Add DeleteStatement to control flow package

--- numba/control_flow/__init__.py
+++ numba/control_flow/__init__.py
@@ -1,3 +1,4 @@
 from numba.control_flow.control_flow import (ControlBlock, ControlFlowAnalysis,
                                              FuncDefExprNode)
 from numba.control_flow.cfstats import *
+from numba.control_flow.delete_cfnode import DeleteStatement
