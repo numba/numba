890fc1ca6a71f5075aaf1ecd4d173d74df054eb5
Mark Florisson <markflorisson88@gmail.com>
2013-02-07 09:45:52 -0600

    Add llvmwrapper module

---
+++ numba/codegen/llvmwrapper.py
@@ -0,0 +1,56 @@
+"""
+Module that creates wrapper around llvm functions. The wrapper is callable
+from Python.
+"""
+
+import ctypes
+
+import llvm
+
+from numba import *
+from numba import extension_types
+from numba.functions import keep_alive
+
+def _create_methoddef(py_func, func_name, func_doc, func_pointer):
+    # struct PyMethodDef {
+    #     const char  *ml_name;   /* The name of the built-in function/method */
+    #     PyCFunction  ml_meth;   /* The C function that implements it */
+    #     int      ml_flags;      /* Combination of METH_xxx flags, which mostly
+    #                                describe the args expected by the C func */
+    #     const char  *ml_doc;    /* The __doc__ attribute, or NULL */
+    # };
+    PyMethodDef = struct([('name', c_string_type),
+                          ('method', void.pointer()),
+                          ('flags', int_),
+                          ('doc', c_string_type)])
+    c_PyMethodDef = PyMethodDef.to_ctypes()
+
+    PyCFunction_NewEx = ctypes.pythonapi.PyCFunction_NewEx
+    PyCFunction_NewEx.argtypes = [ctypes.POINTER(c_PyMethodDef),
+                                  ctypes.py_object,
+                                  ctypes.c_void_p]
+    PyCFunction_NewEx.restype = ctypes.py_object
+
+    # It is paramount to put these into variables first, since every
+    # access may return a new string object!
+    keep_alive(py_func, func_name)
+    keep_alive(py_func, func_doc)
+
+    methoddef = c_PyMethodDef()
+    methoddef.name = func_name
+    methoddef.doc = func_doc
+    methoddef.method = ctypes.c_void_p(func_pointer)
+    methoddef.flags = 1 # METH_VARARGS
+
+    return methoddef
+
+def numbafunction_new(py_func, func_name, func_doc, module_name, func_pointer,
+                      wrapped_lfunc_pointer, wrapped_signature):
+    methoddef = _create_methoddef(py_func, func_name, func_doc, func_pointer)
+    keep_alive(py_func, methoddef)
+    keep_alive(py_func, module_name)
+
+    wrapper = extension_types.create_function(methoddef, py_func,
+                                              wrapped_lfunc_pointer,
+                                              wrapped_signature, module_name)
+    return methoddef, wrapper
--- numba/codegen/translate.py
+++ numba/codegen/translate.py
@@ -15,7 +15,7 @@ from numba import *
 
 from numba.codegen.debug import *
 from numba.codegen.codeutils import llvm_alloca
-from numba.codegen import coerce, complexsupport, refcounting
+from numba.codegen import coerce, complexsupport, refcounting, llvmwrapper
 
 from numba import visitors, nodes, llvm_types, utils, function_util
 from numba.minivect import minitypes, llvm_codegen
@@ -27,50 +27,6 @@ from numba.functions import keep_alive
 from numba.control_flow import ssa
 
 _int32_zero = lc.Constant.int(_int32, 0)
-
-def _create_methoddef(py_func, func_name, func_doc, func_pointer):
-    # struct PyMethodDef {
-    #     const char  *ml_name;   /* The name of the built-in function/method */
-    #     PyCFunction  ml_meth;   /* The C function that implements it */
-    #     int      ml_flags;      /* Combination of METH_xxx flags, which mostly
-    #                                describe the args expected by the C func */
-    #     const char  *ml_doc;    /* The __doc__ attribute, or NULL */
-    # };
-    PyMethodDef = struct([('name', c_string_type),
-                          ('method', void.pointer()),
-                          ('flags', int_),
-                          ('doc', c_string_type)])
-    c_PyMethodDef = PyMethodDef.to_ctypes()
-
-    PyCFunction_NewEx = ctypes.pythonapi.PyCFunction_NewEx
-    PyCFunction_NewEx.argtypes = [ctypes.POINTER(c_PyMethodDef),
-                                  ctypes.py_object,
-                                  ctypes.c_void_p]
-    PyCFunction_NewEx.restype = ctypes.py_object
-
-    # It is paramount to put these into variables first, since every
-    # access may return a new string object!
-    keep_alive(py_func, func_name)
-    keep_alive(py_func, func_doc)
-
-    methoddef = c_PyMethodDef()
-    methoddef.name = func_name
-    methoddef.doc = func_doc
-    methoddef.method = ctypes.c_void_p(func_pointer)
-    methoddef.flags = 1 # METH_VARARGS
-
-    return methoddef
-
-def numbafunction_new(py_func, func_name, func_doc, module_name, func_pointer,
-                      wrapped_lfunc_pointer, wrapped_signature):
-    methoddef = _create_methoddef(py_func, func_name, func_doc, func_pointer)
-    keep_alive(py_func, methoddef)
-    keep_alive(py_func, module_name)
-
-    wrapper = extension_types.create_function(methoddef, py_func,
-                                              wrapped_lfunc_pointer,
-                                              wrapped_signature, module_name)
-    return methoddef, wrapper
 
 
 _compare_mapping_float = {'>':lc.FCMP_OGT,
@@ -651,7 +607,7 @@ class LLVMCodeGenerator(visitors.NumbaVisitor,
 
         # Return a PyCFunctionObject holding the wrapper
         func_pointer = t.lfunc_pointer
-        methoddef, wrapper = numbafunction_new(
+        methoddef, wrapper = llvmwrapper.numbafunction_new(
                 self.func, self.func_name, self.func_doc, self.module_name,
                 func_pointer, self.lfunc_pointer, self.func_signature)
 
