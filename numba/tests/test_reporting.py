84a7979f5d14d19154c2b072da67f892c0f286ce
Mark Florisson <markflorisson88@gmail.com>
2013-02-18 18:36:31 +0000

    Test error reporting with decorated functions

--- numba/tests/test_reporting.py
+++ numba/tests/test_reporting.py
@@ -1,25 +1,3 @@
-"""
->>> compile_func1()
---------------------- Numba Encountered Errors or Warnings ---------------------
-<BLANKLINE>
-    if x:
--------^
-Error 28:7: No global named 'x'
-<BLANKLINE>
---------------------------------------------------------------------------------
-exception: 28:7: No global named 'x'
-
->>> compile_func2()
---------------------- Numba Encountered Errors or Warnings ---------------------
-<BLANKLINE>
-    print 10[20]
-----------^
-Error 41:10: object of type int cannot be indexed
-<BLANKLINE>
---------------------------------------------------------------------------------
-exception: 41:10: object of type int cannot be indexed
-"""
-
 from numba import *
 from numba import error
 
@@ -36,6 +14,18 @@ def compile_func1():
     except error.NumbaError, e:
         print "exception:", e
 
+__doc__ = """
+>>> compile_func1()
+--------------------- Numba Encountered Errors or Warnings ---------------------
+<BLANKLINE>
+    if x:
+-------^
+Error 6:7: No global named 'x'
+<BLANKLINE>
+--------------------------------------------------------------------------------
+exception: 6:7: No global named 'x'
+"""
+
 #@autojit
 def func2():
     print 10[20]
@@ -46,8 +36,40 @@ def compile_func2():
     except error.NumbaError, e:
         print "exception:", e
 
+__doc__ += """>>> compile_func2()
+--------------------- Numba Encountered Errors or Warnings ---------------------
+<BLANKLINE>
+    print 10[20]
+----------^
+Error 31:10: object of type int cannot be indexed
+<BLANKLINE>
+--------------------------------------------------------------------------------
+exception: 31:10: object of type int cannot be indexed
+"""
+
+@autojit # this often messes up line numbers
+def func_decorated():
+    print 10[20]
+
+def compile_func3():
+    try:
+        func_decorated()
+    except error.NumbaError, e:
+        print "exception:", e
+
+__doc__ += """
+>>> compile_func3()
+--------------------- Numba Encountered Errors or Warnings ---------------------
+<BLANKLINE>
+    print 10[20]
+----------^
+Error 52:10: object of type int cannot be indexed
+<BLANKLINE>
+--------------------------------------------------------------------------------
+exception: 52:10: object of type int cannot be indexed
+"""
 
 if __name__ == '__main__':
-    compile_func1()
-#    import doctest
-#    doctest.testmod()
+#    compile_func3()
+    import doctest
+    doctest.testmod()
