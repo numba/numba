50fb13a9d4ff65ca570e8cfbac80f9991c7349e5
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-02-19 16:39:41 -0600

    Add tests for issues #117 and #118.

---
+++ numba/tests/issues/test_issue_118.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numba import autojit
+
+@autojit
+def get_jenks_breaks(data, lower_class_limits, n_classes):
+    k = int(len(data) - 1)
+    for countNum in range(n_classes):
+        # problem inferring `k` in slice
+        k = int(lower_class_limits[k, countNum] - 1)
+
+def test():
+    n_classes = 5
+    data = np.ones(10)
+    lower_class_limits = np.empty((data.size + 1, n_classes + 1))
+    #
+    #        File "/Users/sklam/dev/numba/numba/type_inference/infer.py", line 1055, in visit_Subscript
+    #    if slice_type.variable.type.is_unresolved:
+    #        File "/Users/sklam/dev/numba/numba/minivect/minitypes.py", line 492, in __getattr__
+    #            return getattr(type(self), attr)
+    #AttributeError: type object 'TupleType' has no attribute 'variable'
+    print get_jenks_breaks(data, lower_class_limits, n_classes)
+
+if __name__ == '__main__':
+    test()
