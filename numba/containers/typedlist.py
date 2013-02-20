10339eab06cafaa9812e3c56511b515fb171ce58
Mark Florisson <markflorisson88@gmail.com>
2013-02-20 15:56:51 +0000

    Implement typed tuples (Issue #99)

--- numba/containers/typedlist.py
+++ numba/containers/typedlist.py
@@ -1,6 +1,7 @@
 import numba as nb
 from numba import *
 from numba import typesystem
+from numba.containers import orderedcontainer
 
 import numpy as np
 
@@ -14,11 +15,25 @@ def notimplemented(msg):
 _list_cache = {}
 
 def typedlist(item_type, iterable=None, _list_cache=_list_cache):
+    """
+    >>> typedlist(int_)
+    []
+    >>> tlist = typedlist(int_, range(10))
+    >>> tlist
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> tlist[5]
+    5L
+
+    >>> typedlist(float_, range(10))
+    [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+    """
     if item_type in _list_cache:
         return _list_cache[item_type](item_type, item_type.get_dtype(), iterable)
 
     item_type_t = typesystem.CastType(item_type)
     dtype_t = typesystem.NumpyDtypeType(item_type)
+
+    methods = orderedcontainer.container_methods(item_type, notimplemented)
 
     @jit(warn=False)
     class typedlist(object):
@@ -38,39 +53,12 @@ def typedlist(item_type, iterable=None, _list_cache=_list_cache):
 
         # TODO: Jit __getitem__/__setitem__ of numba extension types
 
-        @item_type(Py_ssize_t)
-        def __getitem__(self, key):
-            if not (0 <= key < self.size):
-                # TODO: Implement raise !
-                # raise IndexError(key)
-                [][key] # tee hee
-
-            return self.buf[key]
-
-        @void(Py_ssize_t, item_type)
-        def __setitem__(self, key, value):
-            if not (0 <= key < self.size):
-                # TODO: Implement raise !
-                # raise IndexError(key)
-                [][key]
-
-            self.buf[key] = value
-
-        @void(item_type)
-        def append(self, value):
-            size = self.size
-            if size >= self.buf.shape[0]:
-                self.buf.resize(int(size * GROW), refcheck=False)
-
-            self.buf[size] = value
-            self.size = size + 1
-
-        @void(object_)
-        def extend(self, iterable):
-            # TODO: something fast for common cases (e.g. typedlist,
-            #                                        np.ndarray, etc)
-            for obj in iterable:
-                self.append(obj)
+        __getitem__ = methods['getitem']
+        __setitem__ = methods['setitem']
+        append = methods['append']
+        extend = methods['extend']
+        index = methods['index']
+        count = methods['count']
 
         @item_type()
         def pop(self):
@@ -83,31 +71,6 @@ def typedlist(item_type, iterable=None, _list_cache=_list_cache):
                 self.buf.resize(int(SHRINK * size), refcheck=False)
 
             return item
-
-        @Py_ssize_t(item_type)
-        def index(self, value):
-            self, value
-            notimplemented("index")
-            # TODO: comparison of complex numbers (#121)
-            # buf = self.buf
-            # for i in range(self.size):
-            #     if buf[i] == value:
-            #         return i
-
-            # [].index(value) # raise ValueError
-
-        @Py_ssize_t(item_type)
-        def count(self, value):
-            self, value
-            notimplemented("count")
-            # TODO: comparison of complex numbers (#121)
-            # count = 0
-            # buf = self.buf
-            # for i in range(self.size):
-            #     # TODO: promotion of (bool_, int_)
-            #     count += Py_ssize_t(buf[i] == value)
-
-            # return count
 
         @void(Py_ssize_t, item_type)
         def insert(self, index, value):
@@ -148,10 +111,5 @@ def typedlist(item_type, iterable=None, _list_cache=_list_cache):
 
 
 if __name__ == "__main__":
-    result = typedlist(int_)
-    for i in range(50):
-        result.append(i)
-
-    print result.buf
-    print result
-    print result[100]
+    import doctest
+    doctest.testmod()
