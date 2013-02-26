ff49edc751d52229985c7772a838701ccbc7a1f5
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-02-25 18:31:51 -0600

    Update CUDA driver and NVVM

---
+++ numbapro/cudapipeline/error.py
@@ -0,0 +1,11 @@
+class CudaDriverError(Exception):
+    pass
+
+class CudaSupportError(ImportError):
+    pass
+
+class NvvmError(Exception):
+    pass
+
+class NvvmSupportError(ImportError):
+    pass
