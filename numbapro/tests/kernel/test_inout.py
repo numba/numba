9ab36443c145ad34c0c3062d155203d78acfc84d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-03-05 16:16:15 -0600

    Add test for inout marker

---
+++ numbapro/tests/kernel/test_inout.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numbapro.parallel.kernel import CU
+
+def incr(tid, A):
+    A[tid] += 1
+
+def test():
+    A = np.arange(10)    
+    A0 = A.copy()
+
+    cu = CU('gpu')    
+    dA = cu.inout(A)
+    cu.enqueue(incr, ntid=A.size, args=(A,))
+
+    print ' A', A
+    print 'A0', A0 
+    assert all(A == A0 + 1)    
+
+if __name__ == '__main__':
+    test()
+
