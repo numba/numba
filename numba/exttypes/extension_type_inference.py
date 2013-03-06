5758059c478af7254ce8ef00e62de66aace95db9
Mark Florisson <markflorisson88@gmail.com>
2013-03-06 12:02:31 +0000

    Make method signature processing and error issuing a bit more pluggable

--- numba/exttypes/extension_type_inference.py
+++ numba/exttypes/extension_type_inference.py
@@ -52,6 +52,7 @@ import warnings
 import inspect
 
 import numba
+from numba import *
 from numba import pipeline, error, symtab
 from numba import typesystem
 from numba.exttypes import extension_types
@@ -63,32 +64,36 @@ logger = logging.getLogger(__name__)
 # Parse method signatures
 #------------------------------------------------------------------------
 
+class Method(object):
+    """
+    py_func: the python 'def' function
+    """
+
+    def __init__(self, py_func, name, signature, is_class, is_static):
+        self.py_func = py_func
+        # Name of this function, py_func.__name__ is inherently unreliable
+        self.name = name
+        self.signature = signature
+        py_func.live_objects = []
+        self.is_class = is_class
+        self.is_static = is_static
+
+    def result(self, py_func):
+        if self.is_class:
+            return classmethod(py_func)
+        elif self.is_static:
+            return staticmethod(py_func)
+        else:
+            return py_func
+
 def validate_method(py_func, sig, is_static):
     assert isinstance(py_func, types.FunctionType)
+
     nargs = py_func.__code__.co_argcount - 1 + is_static
     if len(sig.args) != nargs:
         raise error.NumbaError(
             "Expected %d argument types in function "
             "%s (don't include 'self')" % (nargs, py_func.__name__))
-
-def get_signature(ext_type, is_class, is_static, sig):
-    """
-    Create a signature given the user-specified signature. E.g.
-
-        class Foo(object):
-            @void()                 # becomes: void(ext_type(Foo))
-            def method(self): ...
-    """
-    if is_static:
-        leading_arg_types = ()
-    elif is_class:
-        leading_arg_types = (numba.object_,)
-    else:
-        leading_arg_types = (ext_type,)
-
-    argtypes = leading_arg_types + sig.args
-    restype = sig.return_type
-    return minitypes.FunctionType(return_type=restype, args=argtypes)
 
 def get_classmethod_func(func):
     """
@@ -103,65 +108,146 @@ def get_classmethod_func(func):
         assert isinstance(func, staticmethod)
         return func.__get__(object())
 
-def process_signature(ext_type, method, default_signature,
-                      is_static=False, is_class=False):
+class MethodMaker(object):
     """
-    Verify a method signature.
-
-    Returns a Method object and the resolved signature.
+    Creates Methods from python functions and validates user-declared
+    signatures.
     """
-    while True:
-        if isinstance(method, types.FunctionType):
-            # Process function
-            if default_signature is None:
-                # TODO: support deferred type inference and autojit methods
-                raise error.NumbaError(
-                    "Method '%s' does not have signature" % (method.__name__,))
-            validate_method(method, default_signature or numba.object_(),
-                            is_static)
-            if default_signature is None:
-                default_signature = minitypes.FunctionType(return_type=None,
-                                                           args=[])
-            sig = get_signature(ext_type, is_class, is_static, default_signature)
-            method = Method(method, is_class, is_static)
-            return method, sig.return_type, sig.args
 
-        elif isinstance(method, minitypes.Function):
-            # @double(...)
-            # def func(self, ...): ...
-            default_signature = method.signature
-            method = method.py_func
+    def __init__(self, ext_type):
+        self.ext_type = ext_type
 
+    def no_signature(self, method):
+        "Called when no signature is found for the method"
+
+    def default_signature(self, method, method_name):
+        "Retrieve the default method signature for the given method"
+
+    def make_method_type(self, method):
+        "Create a method type for the given Method and declared signature"
+
+
+class JitMethodMaker(MethodMaker):
+
+    def no_signature(self, method):
+        raise error.NumbaError(
+            "Method '%s' does not have signature" % (method.__name__,))
+
+    def validate_init_method(self, init_method):
+        if inspect.getargspec(init_method).args:
+            warnings.warn(
+                "Constructor for class '%s' has no signature, "
+                "assuming arguments have type 'object'" %
+                self.ext_type.py_class.__name__)
+
+    def default_signature(self, method, method_name):
+        if (method_name == '__init__' and
+                isinstance(method, types.FunctionType)):
+            self.validate_init_method(method)
+
+            argtypes = [numba.object_] * (method.__code__.co_argcount - 1)
+            default_signature = numba.void(*argtypes)
+            return default_signature
         else:
-            # Process staticmethod and classmethod
-            if isinstance(method, staticmethod):
-                is_static = True
-            elif isinstance(method, classmethod):
-                is_class = True
+            return None
+
+    def make_method_type(self, method):
+        restype = method.signature.return_type
+        argtypes = method.signature.args
+        signature = typesystem.ExtMethodType(
+                    return_type=restype, args=argtypes, name=method.name,
+                    is_class=method.is_class, is_static=method.is_static)
+        return signature
+
+
+class MethodSignatureProcessor(object):
+    """
+    Processes signatures of extension types.
+    """
+
+    def __init__(self, class_dict, ext_type, method_maker):
+        self.class_dict = class_dict
+        self.ext_type = ext_type
+        self.method_maker = method_maker
+
+    def get_signature(self, is_class, is_static, sig):
+        """
+        Create a signature given the user-specified signature. E.g.
+
+            class Foo(object):
+                @void()                 # becomes: void(ext_type(Foo))
+                def method(self): ...
+        """
+        if is_static:
+            leading_arg_types = ()
+        elif is_class:
+            leading_arg_types = (numba.object_,)
+        else:
+            leading_arg_types = (self.ext_type,)
+
+        argtypes = leading_arg_types + sig.args
+        restype = sig.return_type
+        return minitypes.FunctionType(return_type=restype, args=argtypes)
+
+    def process_signature(self, method, method_name, default_signature,
+                          is_static=False, is_class=False):
+        """
+        Verify a method signature.
+
+        Returns a Method object and the resolved signature.
+        """
+        while True:
+            if isinstance(method, types.FunctionType):
+                # Process function
+                if default_signature is None:
+                    self.method_maker.no_signature(method)
+
+                validate_method(method, default_signature or object_(),
+                                is_static)
+                if default_signature is None:
+                    default_signature = minitypes.FunctionType(return_type=None,
+                                                               args=[])
+                sig = self.get_signature(is_class, is_static, default_signature)
+                method = Method(method, method_name, sig, is_class, is_static)
+                return method
+
+            elif isinstance(method, minitypes.Function):
+                # @double(...)
+                # def func(self, ...): ...
+                default_signature = method.signature
+                method = method.py_func
+
             else:
-                return None, None, None
+                # Process staticmethod and classmethod
+                if isinstance(method, staticmethod):
+                    is_static = True
+                elif isinstance(method, classmethod):
+                    is_class = True
+                else:
+                    return None
 
-            method = get_classmethod_func(method)
+                method = get_classmethod_func(method)
 
-class Method(object):
-    """
-    py_func: the python 'def' function
-    """
+    def get_method_signatures(self):
+        """
+        Return [Method] for each decorated method in the class
+        """
+        methods = []
 
-    def __init__(self, py_func, is_class, is_static):
-        self.py_func = py_func
-        py_func.live_objects = []
-        self.is_class = is_class
-        self.is_static = is_static
-        self.name = py_func.__name__
+        for method_name, method in self.class_dict.iteritems():
+            default_signature = self.method_maker.default_signature(method,
+                                                                    method_name)
 
-    def result(self, py_func):
-        if self.is_class:
-            return classmethod(py_func)
-        elif self.is_static:
-            return staticmethod(py_func)
-        else:
-            return py_func
+            method = self.process_signature(method, method_name,
+                                            default_signature)
+            if method is None:
+                continue
+
+            method_type = self.method_maker.make_method_type(method)
+            methods.append((method, method_type))
+
+        return methods
+
 
 #------------------------------------------------------------------------
 # Populate Extension Type with Methods
@@ -174,28 +260,11 @@ def process_method_signatures(class_dict, ext_type):
         * Verify signatures
         * Populate ext_type with method signatures (ExtMethodType)
     """
-    for method_name, method in class_dict.iteritems():
-        default_signature = None
-        if (method_name == '__init__' and
-                isinstance(method, types.FunctionType)):
-            if inspect.getargspec(method).args:
-                warnings.warn(
-                    "Constructor for class '%s' has no signature, "
-                    "assuming arguments have type 'object'" %
-                                        ext_type.py_class.__name__)
-            argtypes = [numba.object_] * (method.__code__.co_argcount - 1)
-            default_signature = numba.void(*argtypes)
-
-        method, restype, argtypes = process_signature(ext_type, method,
-                                                      default_signature)
-        if method is None:
-            continue
-
-        signature = typesystem.ExtMethodType(
-                    return_type=restype, args=argtypes, name=method.name,
-                    is_class=method.is_class, is_static=method.is_static)
-        ext_type.add_method(method_name, signature)
-        class_dict[method_name] = method
+    method_maker = JitMethodMaker(ext_type)
+    processor = MethodSignatureProcessor(class_dict, ext_type, method_maker)
+    for method, method_type in processor.get_method_signatures():
+        ext_type.add_method(method.name, method_type)
+        class_dict[method.name] = method
 
 def _type_infer_method(env, ext_type, method, method_name, class_dict, flags):
     if method_name not in ext_type.methoddict:
