10c647ec7ee3d4cd05bcad94e012bcac59c6e759
Mark Florisson <markflorisson88@gmail.com>
2013-03-07 23:16:19 +0000

    Fix division in cbuilder utilities (don't use future division)

--- numba/support/numpy_support/sliceutils.py
+++ numba/support/numpy_support/sliceutils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function, division, absolute_import
+from __future__ import print_function, absolute_import
 from llvm_cbuilder import *
 from llvm_cbuilder import shortnames as C
 
