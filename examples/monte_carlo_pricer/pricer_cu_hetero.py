dd812f8a3205a2a40baf8a33ff9f9016eee70683
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-03-10 21:11:26 -0500

    Improve hetero version of mc pricer

--- examples/monte_carlo_pricer/pricer_cu_hetero.py
+++ examples/monte_carlo_pricer/pricer_cu_hetero.py
@@ -5,20 +5,33 @@ with heterogeneous targets.
 
 from contextlib import closing, nested
 import numpy as np
-from math import sqrt, exp
+from math import sqrt, exp, pi
 from timeit import default_timer as timer
-from numbapro import CU
+from numbapro import CU, uint32
 from numbapro.parallel.kernel import builtins
 #from matplotlib import pyplot
 
 def step(tid, paths, dt,  prices, c0, c1, noises):
     paths[tid] = prices[tid] * np.exp(c0 * dt + c1 * noises[tid])
 
+A = 1664525
+C = 1013904223
+def step_cpu(tid, paths, dt,  prices, c0, c1, seeds):
+    seed = uint32(seeds[tid])
+    rand = uint32(A) * seed + uint32(C)
+    seeds[tid] = rand
+    x = 4. / rand
+    noise = 1. / np.sqrt(2 * pi) * np.exp(-(x * x / 2.))
+    paths[tid] = prices[tid] * np.exp(c0 * dt + c1 * noise)
+
 def monte_carlo_pricer(paths, dt, interest, volatility):
     n = paths.shape[0]
     half = n // 2
     cud = CU('gpu')
     cuh = CU('cpu')
+
+    seeds = np.random.random(n)
+
 
     with nested(closing(cud), closing(cuh)):
 
@@ -39,20 +52,17 @@ def monte_carlo_pricer(paths, dt, interest, volatility):
             cud.enqueue(builtins.random.normal,
                        ntid=half,
                        args=(d_normdist,))
-            
-            normdist = np.random.normal(size=half)
-            h_normdist = cuh.input(normdist)
+            h_seeds = cuh.input(seeds)
             # calculate next step
-
             # GPU
             cud.enqueue(step,
                        ntid=half,
                        args=(d_paths, dt, d_last_paths, c0, c1, d_normdist))
 
             # CPU
-            cuh.enqueue(step,
+            cuh.enqueue(step_cpu,
                         ntid=half,
-                        args=(h_paths, dt, h_last_paths, c0, c1, h_normdist))
+                        args=(h_paths, dt, h_last_paths, c0, c1, h_seeds))
 
             d_last_paths = d_paths
             h_last_paths = h_paths
