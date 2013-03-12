cae29cdac8d8c11b5d9a4e63091c475c75f91907
Mark Florisson <markflorisson88@gmail.com>
2013-03-12 22:24:19 +0000

    Fix some simple traits

--- numba/traits.py
+++ numba/traits.py
@@ -10,13 +10,15 @@ Minimal traits implementation:
 
 import inspect
 
-from numba.utils import TypedProperty
+# from numba.utils import TypedProperty
 
 def traits(cls):
     "@traits class decorator"
     for name, py_func in vars(cls).iteritems():
         if isinstance(py_func, TraitBase):
             py_func.set_attr_name(name)
+
+    return cls
 
 class TraitBase(object):
     "Base class for traits"
@@ -33,16 +35,14 @@ class Delegate(TraitBase):
     Delegate to some other object.
     """
 
-    def __init__(self, value, delegate_method_name=None, doc=None):
+    def __init__(self, value, delegate_attr_name=None, doc=None):
         super(Delegate, self).__init__(value, doc=doc)
-        self.delegate_attr_name = delegate_method_name
-
-    def set_attr_name(self, name):
-        if self.delegate_attr_name is None:
-            self.delegate_attr_name = name
+        self.delegate_attr_name = delegate_attr_name
 
     def __get__(self, instance, owner):
-        return getattr(instance, self.delegate_attr_name)
+        obj = getattr(instance, self.value)
+        return getattr(obj, self.delegate_attr_name or self.attr_name)
 
     def __set__(self, instance, value):
-        return getattr(instance, self.delegate_attr_name, value)
+        obj = getattr(instance, self.value)
+        return setattr(obj, self.delegate_attr_name or self.attr_name, value)
