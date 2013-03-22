6adc6d0e9fe510112bfe884954416a701fe382be
Mark Florisson <markflorisson88@gmail.com>
2013-03-22 08:32:16 +0000

    Expose 'python' and 'nopython' to pure python

--- numba/tests/test_mandelbrot.py
+++ numba/tests/test_mandelbrot.py
@@ -9,7 +9,7 @@
 from numba import *
 import numpy as np
 
-@autojit
+@autojit(nopython=True)
 def mandel(x, y, max_iters):
     """
     Given the real and imaginary parts of a complex number,
@@ -28,17 +28,18 @@ def mandel(x, y, max_iters):
 
 @autojit
 def create_fractal(min_x, max_x, min_y, max_y, image, iters):
-    height = image.shape[0]
-    width = image.shape[1]
+    with nopython:
+        height = image.shape[0]
+        width = image.shape[1]
 
-    pixel_size_x = (max_x - min_x) / width
-    pixel_size_y = (max_y - min_y) / height
-    for x in range(width):
-        real = min_x + x * pixel_size_x
-        for y in range(height):
-            imag = min_y + y * pixel_size_y
-            color = mandel(real, imag, iters)
-            image[y, x] = color
+        pixel_size_x = (max_x - min_x) / width
+        pixel_size_y = (max_y - min_y) / height
+        for x in range(width):
+            real = min_x + x * pixel_size_x
+            for y in range(height):
+                imag = min_y + y * pixel_size_y
+                color = mandel(real, imag, iters)
+                image[y, x] = color
 
     return image
 
