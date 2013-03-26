c9e44193b960379ceb335609735fbd492d9c06a3
Jon Riehl <jriehl@spaceship.com>
2013-03-26 16:21:12 -0500

    Added check for backwards compatibility with 2.6 unittest.

--- numba/asdl/tests/test_bad.py
+++ numba/asdl/tests/test_bad.py
@@ -2,26 +2,39 @@
 from __future__ import print_function, division, absolute_import
 # Use Python.asdl to test bad ast.
 
-import unittest, ast
+import unittest, ast, sys
 
 from numba.asdl.tests import support
 
 class TestBad(support.SchemaTestCase):
+    def make_verification_callable(self, the_ast):
+        def _verify():
+            self.schema.verify(the_ast)
+        return _verify
+
     def test_module_missing_body(self):
         the_ast = ast.Module()
-        with self.capture_error() as cap:
-            self.schema.verify(the_ast)
-        self.assertEqual(str(cap.exception),
-                         "At Module.body: Missing field")
+        if sys.version_info < (2,7):
+            self.assertRaises(support.schema.SchemaError,
+                              self.make_verification_callable(the_ast))
+        else:
+            with self.capture_error() as cap:
+                self.schema.verify(the_ast)
+            self.assertEqual(str(cap.exception),
+                             "At Module.body: Missing field")
 
     def test_missing_expr_context(self):
         a_name = ast.Name(id='x') # missing expr_context
         a_binop = ast.BinOp(left=a_name, right =a_name, op=ast.Add())
         the_ast = ast.Expr(value=a_binop)
-        with self.capture_error() as cap:
-            self.schema.verify(the_ast)
-        self.assertEqual(str(cap.exception),
-                         "At Name.ctx: Missing field")
+        if sys.version_info < (2,7):
+            self.assertRaises(support.schema.SchemaError,
+                              self.make_verification_callable(the_ast))
+        else:
+            with self.capture_error() as cap:
+                self.schema.verify(the_ast)
+            self.assertEqual(str(cap.exception),
+                             "At Name.ctx: Missing field")
 
     def test_wrong_arg(self):
         bad = ast.Raise() # doesn't matter what is inside
@@ -30,17 +43,25 @@ class TestBad(support.SchemaTestCase):
                                   args=args,
                                   body=[],
                                   decorator_list=[])
-        with self.capture_error() as cap:
-            self.schema.verify(the_ast)
-        self.assertEqual(str(cap.exception),
-                        "At arguments.args[0]: Cannot be a Raise")
+        if sys.version_info < (2,7):
+            self.assertRaises(support.schema.SchemaError,
+                              self.make_verification_callable(the_ast))
+        else:
+            with self.capture_error() as cap:
+                self.schema.verify(the_ast)
+            self.assertEqual(str(cap.exception),
+                            "At arguments.args[0]: Cannot be a Raise")
 
     def test_return_missing_lineno(self):
         the_ast = ast.Return(col_offset=0)
-        with self.capture_error() as cap:
-            self.schema.verify(the_ast)
-        self.assertEqual(str(cap.exception),
-                         "At Return.lineno: Missing field")
+        if sys.version_info < (2,7):
+            self.assertRaises(support.schema.SchemaError,
+                              self.make_verification_callable(the_ast))
+        else:
+            with self.capture_error() as cap:
+                self.schema.verify(the_ast)
+            self.assertEqual(str(cap.exception),
+                             "At Return.lineno: Missing field")
 
 
 if __name__ == '__main__':
