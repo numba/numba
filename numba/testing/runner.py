6e4a9fb7b09b8dbb2fb88a9fa76b8d89fa61c57d
Mark Florisson <markflorisson88@gmail.com>
2013-04-12 17:12:09 +0100

    Align for longer test file names

--- numba/testing/runner.py
+++ numba/testing/runner.py
@@ -171,7 +171,7 @@ class TestRunner(object):
     def run(self, modname):
         self.ran += 1
         if not self.print_failures_only:
-            sys.stdout.write("running %-61s" % (modname,))
+            sys.stdout.write("running %-70s" % (modname,))
 
         process = subprocess.Popen([sys.executable, '-m', modname],
                                    stdout=subprocess.PIPE,
