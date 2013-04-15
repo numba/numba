ac40e8a936f35757a43769f00fdef84a40919829
Mark Florisson <markflorisson88@gmail.com>
2013-04-15 14:40:35 +0100

    Add "loop until fail" option to test runner (--loop)

--- runtests.py
+++ runtests.py
@@ -5,4 +5,12 @@ from __future__ import print_function, division, absolute_import
 import sys
 import numba
 
-sys.exit(0 if numba.test(sys.argv[1:]) == 0 else 1)
+# TODO: Use argparse
+if '--loop' in sys.argv:
+    whitelist = [arg for arg in sys.argv[1:] if arg != '--loop']
+    while True:
+        exit_status = numba.test(whitelist)
+        if exit_status != 0:
+            sys.exit(exit_status)
+else:
+    sys.exit(numba.test(sys.argv[1:]))
