2026bb6451c34d52deec52900a2889a7a9f3001a
Mark Florisson <markflorisson88@gmail.com>
2013-04-18 18:07:01 +0100

    Add grammer for normalized IR and build script

--- numba/ir/build.py
+++ numba/ir/build.py
@@ -1,1 +1,13 @@
-__author__ = 'mark'
+import os
+from .generator import build
+
+root = os.path.dirname(os.path.abspath(__file__))
+features = ['ast', 'transformer', 'visitor']
+
+def build_normalized():
+    fn = os.path.join(root, "Normalized.asdl")
+    outdir = os.path.join(root, "normalized")
+    build.build_package(fn, features, outdir)
+
+if __name__ == '__main__':
+    build_normalized()
