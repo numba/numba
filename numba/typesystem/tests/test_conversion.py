f347ed3cc85ef0a9666fda07a27578e3a331f2f6
Mark Florisson <markflorisson88@gmail.com>
2013-06-08 09:42:32 +0100

    Print converted type in type conversion test

--- numba/typesystem/tests/test_conversion.py
+++ numba/typesystem/tests/test_conversion.py
@@ -20,7 +20,8 @@ def convert(ts1, ts2, conversion_type, typenames):
     for typename in typenames:
         t1 = getattr(ts1, tyname(typename))
         t2 = getattr(ts2, tyname(typename))
-        assert ts.convert(conversion_type, t1) == t2, (t1, t2)
+        converted = ts.convert(conversion_type, t1)
+        assert converted == t2, (t1, str(converted), str(t2))
 
 #-------------------------------------------------------------------
 # Numba -> LLVM
