e399b2c87c0b36676975f62f0c5956e086c2e47b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-06-21 17:56:31 -0500

    Add test for compiling montecarlo

--- numbapro/cudapy/tests/test_mandel.py
+++ numbapro/cudapy/tests/test_mandel.py
@@ -6,6 +6,8 @@ from .support import testcase, main
 
 @testcase
 def test_mandel():
+    '''Just make sure we can compile this
+    '''
     @cuda.jit('uint8(uint32, f8, f8, f8, f8, uint32, uint32, uint32)',
               device=True)
     def mandel(tid, min_x, max_x, min_y, max_y, width, height, iters):
