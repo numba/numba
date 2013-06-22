96b0b2baf4368727ea0aaa2ccc7c567255552d82
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-06-22 16:41:53 -0500

    Cleanup

--- setup.py
+++ setup.py
@@ -3,87 +3,20 @@ from fnmatch import fnmatchcase
 from distutils.core import setup, Extension
 from distutils.util import convert_path
 
-from numba import minivect, vectorize
-
-import numpy
-from Cython.Distutils import build_ext
-from Cython.Distutils.extension import Extension as CythonExtension
-
 import versioneer
-
 
 versioneer.versionfile_source = 'numbapro/_version.py'
 versioneer.versionfile_build = 'numbapro/_version.py'
 versioneer.tag_prefix = ''
 versioneer.parentdir_prefix = 'numbapro-'
 
-# NOTE: On OSX 10.8.2, XCode 4.2 you need -D_FORTIFY_SOURCE=0
-#       http://comments.gmane.org/gmane.comp.compilers.llvm.devel/37989
-#       This is a problem with GCC builtin.
-#       And you need to export CC=gcc, but there is still problem at linkage,
-#       where it uses clang.  I am solving that manually by copying the
-#       the commands and swap out clang with gcc.
-OMP_ARGS = ['-fopenmp']
-OMP_LINK = OMP_ARGS
-
-miniutils = minivect.get_include() + "/miniutils"
-miniutils_dep = miniutils + ".pyx"
-miniutils_header_dep = miniutils + ".h"
-
 ext_modules = [
-#    Extension(
-#        name = "numba.vectorize._internal",
-#        sources = ["numba_vectorize/_internal.c",
-#                   "numba_vectorize/_ufunc.c",
-#                   "numba_vectorize/_gufunc.c"],
-#        include_dirs = [numpy.get_include(), minivect.get_include()],
-#        depends = ["numba_vectorize/_internal.h", miniutils_header_dep],
-#    ),
     Extension(
         name = 'numbapro._utils.mviewbuf',
         sources = ['numbapro/_utils/mviewbuf.c'],
         # extra_compile_args = ['-pedantic', '-ansi'], # for checking C90
     ),
-
-
-#    CythonExtension(
-#        name = "numbapro._minidispatch",
-#        sources = ["numbapro/_minidispatch.pyx"],
-#        depends = [miniutils_dep, "numbapro/dispatch.pxd"],
-#        include_dirs = [numpy.get_include(), minivect.get_include()],
-#        cython_include_dirs = [minivect.get_include()],
-#        cython_gdb=True,
-#    ),
-#
-#    CythonExtension(
-#        name = "numbapro.dispatch",
-#        sources = ["numbapro/dispatch.pyx"],
-#        include_dirs = [numpy.get_include(), vectorize.get_include()],
-#        depends = [miniutils_dep, "numbapro/dispatch.pxd"],
-#        extra_compile_args = OMP_ARGS + ['-D_FORTIFY_SOURCE=0'],
-#        extra_link_args = OMP_LINK,
-#        cython_gdb=True,
-#    ),
-#
-#    CythonExtension(
-#        name = "numbapro.parallel.kernel.cpuimpl._cpuscheduler",
-#        sources = ["numbapro/parallel/kernel/cpuimpl/_cpuscheduler.pyx",
-#                   "numbapro/parallel/kernel/cpuimpl/cpuscheduler.c"],
-#        depends = ["numbapro/parallel/kernel/cpuimpl/cpuscheduler.pxd"],
-#        extra_compile_args = ['-D_FORTIFY_SOURCE=0'],
-##        cython_gdb=True,
-#    ),
-
-#    CythonExtension(
-#        name = "numbapro._cudadispatchlib",
-#        sources = ["numbapro/_cudadispatchlib.pyx", "numbapro/_cuda.c"],
-#        include_dirs = [numpy.get_include(), "cuda_toolkit"],
-#        depends = ["numbapro/_cuda.h", "numbapro/cuda.pxd",
-#                   "numbapro/dispatch.pxd",],
-#        cython_gdb=True,
-#    )
 ]
-
 
 def find_packages(where='.', exclude=()):
     out = []
@@ -103,7 +36,6 @@ def find_packages(where='.', exclude=()):
 
 
 cmdclass = versioneer.get_cmdclass()
-cmdclass['build_ext'] = build_ext
 
 setup(
     name = "numbapro",
@@ -116,6 +48,5 @@ setup(
     ext_modules = ext_modules,
     packages = find_packages(),
     cmdclass = cmdclass,
-    package_data ={'numbapro.parallel.kernel.cpuimpl': ['atomic.bc']},
 )
 
