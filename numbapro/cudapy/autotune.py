a1ce8e60aae4d87c9b8f490c4072b4dab8103c90
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-07-10 15:27:08 -0500

    occupancy calc

--- numbapro/cudapy/autotune.py
+++ numbapro/cudapy/autotune.py
@@ -2,9 +2,147 @@
 - Parse jit compile info
 - Compute warp occupany histogram
 '''
-
-
+from __future__ import division
+import math
 import re
+
+
+#------------------------------------------------------------------------------
+# warp occupancy calculator
+
+LIMITS_CC_20 = {
+    'thread_per_warp': 32,
+    'warp_per_sm'    : 48,
+    'thread_per_sm'  : 1536,
+    'block_per_sm'   : 8,
+    'registers'      : 32768,
+    'reg_alloc_unit' : 64,
+    'reg_alloc_gran' : 'warp',
+    'reg_per_thread' : 63,
+    'smem_per_sm'    : 49152,
+    'smem_alloc_unit': 128,
+    'warp_alloc_gran': 2,
+    'max_block_size' : 1024,
+}
+
+
+LIMITS_CC_21 = LIMITS_CC_20
+
+LIMITS_CC_30 = {
+    'thread_per_warp': 32,
+    'warp_per_sm'    : 64,
+    'thread_per_sm'  : 2048,
+    'block_per_sm'   : 16,
+    'registers'      : 65535,
+    'reg_alloc_unit' : 256,
+    'reg_alloc_gran' : 'warp',
+    'reg_per_thread' : 63,
+    'smem_per_sm'    : 49152,
+    'smem_alloc_unit': 256,
+    'warp_alloc_gran': 4,
+    'max_block_size' : 1024,
+}
+
+LIMITS_CC_35 = LIMITS_CC_30.copy()
+LIMITS_CC_35.update({
+    'reg_per_thread' : 255,
+})
+
+PHYSICAL_LIMITS = {
+    (2, 0): LIMITS_CC_20,
+    (2, 1): LIMITS_CC_21,
+    (3, 0): LIMITS_CC_30,
+    (3, 5): LIMITS_CC_35,
+}
+
+
+def ceil(x, s=1):
+    return s * math.ceil(x / s)
+
+def floor(x, s=1):
+    return s * math.floor(x / s)
+
+def warp_occupancy(info, cc, smem_config=48 * 2**10):
+    '''Returns a dictionary of {threadperblock: occupancy, factor}
+    
+    Only threadperblock of multiple of warpsize is used.
+    Only threadperblock of non-zero occupancy is returned.
+    '''
+    ret = {}
+    limits = PHYSICAL_LIMITS[cc]
+    warpsize = limits['thread_per_warp']
+    max_thread = limits['max_block_size']
+
+    for tpb in range(warpsize, max_thread, warpsize):
+        result = compute_warp_occupancy(tpb=tpb,
+                                        reg=info.get('reg', 0),
+                                        smem=info.get('shared', 0),
+                                        smem_config=smem_config,
+                                        limits=limits)
+        if result:
+            ret[tpb] = result
+    return ret
+
+def compute_warp_occupancy(tpb, reg, smem, smem_config, limits):
+    assert limits['reg_alloc_gran'] == 'warp', \
+                "assume warp register allocation granularity"
+    limit_block_per_sm = limits['block_per_sm']
+    limit_warp_per_sm = limits['warp_per_sm']
+    limit_thread_per_warp = limits['thread_per_warp']
+    limit_reg_per_thread = limits['reg_per_thread']
+    limit_total_regs = limits['registers']
+    limit_total_smem = min(limits['smem_per_sm'], smem_config)
+    my_smem_alloc_unit = limits['smem_alloc_unit']
+    reg_alloc_unit = limits['reg_alloc_unit']
+    warp_alloc_gran = limits['warp_alloc_gran']
+
+    my_warp_per_block = ceil(tpb / limit_thread_per_warp)
+    my_reg_count = reg
+    my_reg_per_block = my_warp_per_block
+    my_smem = smem
+    my_smem_per_block = ceil(my_smem, my_smem_alloc_unit)
+
+    # allocated resource
+    limit_blocks_due_to_warps = min(limit_block_per_sm,
+                                    floor(limit_warp_per_sm / my_warp_per_block))
+
+
+    c39 = floor(limit_total_regs / ceil(my_reg_count * limit_thread_per_warp,
+                                        reg_alloc_unit),
+                warp_alloc_gran)
+
+    limit_blocks_due_to_regs = (0
+                                if my_reg_count > limit_reg_per_thread
+                                else (floor(c39 / my_reg_per_block)
+                                      if my_reg_count > 0
+                                      else limit_block_per_sm))
+
+    limit_blocks_due_to_smem = (floor(limit_total_smem /
+                                      my_smem_per_block)
+                                if my_smem_per_block > 0
+                                else limit_block_per_sm)
+
+    # occupancy
+    active_block_per_sm = min(limit_blocks_due_to_smem,
+                              limit_blocks_due_to_warps,
+                              limit_blocks_due_to_regs)
+                    
+    if active_block_per_sm == limit_blocks_due_to_warps:
+        factor = 'warps'
+    elif active_block_per_sm == limit_blocks_due_to_regs:
+        factor = 'regs'
+    else:
+        factor = 'smem'
+
+
+    active_warps_per_sm = active_block_per_sm * my_warp_per_block
+    #active_threads_per_sm = active_warps_per_sm * limit_thread_per_warp
+
+    occupancy = active_warps_per_sm / limit_warp_per_sm
+    return occupancy, factor
+
+#------------------------------------------------------------------------------
+# compile info parsing
 
 def _sw(s):
     return s.replace(' ', r'\s+')
--- numbapro/cudapy/tests/test_autotune.py
+++ numbapro/cudapy/tests/test_autotune.py
@@ -1,22 +1,15 @@
 from .support import testcase, main
 from numbapro.cudapy import autotune
 
-@testcase
-def test_autotune_parsing_1():
-    sample = '''
+
+SAMPLE1 = '''
 ptxas info    : 0 bytes gmem
 ptxas info    : Compiling entry function 'foo' for 'sm_30'
 ptxas info    : Function properties for foo
     0 bytes stack frame, 0 bytes spill stores, 0 bytes spill loads
 ptxas info    : Used 14 registers, 336 bytes cmem[0]
 '''
-    info = autotune.parse_compile_info(sample)
-    assert info['foo']['stack'] == 0
-    assert info['foo']['reg'] == 14
-
-@testcase
-def test_autotune_parsing_2():
-    sample='''
+SAMPLE2 = '''
 ptxas info    : 0 bytes gmem
 ptxas info    : Compiling entry function 'foo' for 'sm_30'
 ptxas info    : Function properties for foo
@@ -29,13 +22,55 @@ info    : 0 bytes gmem
 info    : Function properties for 'foo':
 info    : used 19 registers, 8 stack, 0 bytes smem, 336 bytes cmem[0], 0 bytes lmem
     '''
-    info = autotune.parse_compile_info(sample)
+
+
+@testcase
+def test_autotune_parsing_1():
+    info = autotune.parse_compile_info(SAMPLE1)
+    assert info['foo']['stack'] == 0
+    assert info['foo']['reg'] == 14
+
+@testcase
+def test_autotune_parsing_2():
+    sample=    info = autotune.parse_compile_info(SAMPLE2)
     assert info['foo']['stack'] == 8
     assert info['foo']['reg'] == 19
     assert info['foo']['shared'] == 0
     assert info['foo']['local'] == 0
 
     assert info['bar']['stack'] == 0
+
+
+def round100(x):
+    return round(x * 100)
+
+@testcase
+def test_autotune_occupancy_1():
+    info = autotune.parse_compile_info(SAMPLE1)
+    result = autotune.warp_occupancy(info['foo'], cc=(2, 0))
+    assert round100(result[256][0]) == 100
+    assert round100(result[288][0]) == 94
+    assert round100(result[320][0]) == 83
+    assert round100(result[352][0]) == 92
+    assert round100(result[416][0]) == 81
+    assert round100(result[448][0]) == 88
+    assert round100(result[480][0]) == 94
+    assert round100(result[512][0]) == 100
+    assert round100(result[544][0]) == 71
+
+@testcase
+def test_autotune_occupancy_2():
+    info = autotune.parse_compile_info(SAMPLE1)
+    result = autotune.warp_occupancy(info['foo'], cc=(3, 5))
+    assert round100(result[256][0]) == 100
+    assert round100(result[288][0]) == 98
+    assert round100(result[320][0]) == 94
+    assert round100(result[352][0]) == 86
+    assert round100(result[416][0]) == 81
+    assert round100(result[448][0]) == 88
+    assert round100(result[480][0]) == 94
+    assert round100(result[512][0]) == 100
+    assert round100(result[544][0]) == 80
 
 
 if __name__ == '__main__':
