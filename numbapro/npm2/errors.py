e9a03aa946b36c9464cddd4bb235ceea80ccdd23
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-07-16 16:56:39 -0500

    better nested error

--- numbapro/npm2/errors.py
+++ numbapro/npm2/errors.py
@@ -9,16 +9,18 @@ def error_context(lineno=-1, during=None):
     try:
         yield
     except Exception, e:
-        msg = ['']
+        msg = []
         if lineno >= 0:
             msg.append('At line %d:' % lineno)
         if during:
             msg.append('During: %s' % during)
 
         if isinstance(e, AssertionError):
-            msg.append('\tInternal error: %s' % e)
+            msg.append('Internal error: %s' % e)
+        elif isinstance(e, CompilerError):
+            msg.append(str(e))
         else:
-            msg.append('\t%s: %s' % (type(e).__name__, e))
+            msg.append('%s: %s' % (type(e).__name__, e))
 
         exc = CompilerError('\n'.join(msg))
         raise exc, None, sys.exc_info()[2]
