9e1aadef0b403fdee072537995a46f554b3bba2f
Mark Florisson <markflorisson88@gmail.com>
2013-07-26 15:36:01 +0100

    Enable #297 enumerate promotion test after merge

--- numba/tests/issues/test_issue_297.py
+++ numba/tests/issues/test_issue_297.py
@@ -16,18 +16,18 @@ def test1(arr):
 
     return u + v
 
-# @jit(argtypes=(int_[:],))
-# def test2(arr):
-#     s = 0
-#     for i, x in enumerate(arr):
-#         s += i*x
-#
-#     s2 = 0
-#     for i2, x2 in enumerate(arr, 1):
-#         s2 += i2*x2
-#     return s+s2
+@jit(argtypes=(int_[:],))
+def test2(arr):
+    s = 0
+    for i, x in enumerate(arr):
+        s += i*x
+
+    s2 = 0
+    for i2, x2 in enumerate(arr, 1):
+        s2 += i2*x2
+    return s+s2
 
 if __name__ == '__main__':
     arr = numpy.arange(1, 4)
     assert test1(arr) == test1.py_func(arr)
-    # assert test2(arr) == test2.py_func(arr)
+    assert test2(arr) == test2.py_func(arr)
