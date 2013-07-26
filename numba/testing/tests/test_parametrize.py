b14490dcf14442c4ea9126ca0040933bb506a9db
Mark Florisson <markflorisson88@gmail.com>
2013-07-26 14:19:31 +0100

    Patch for issue #299 (thanks to @eltjpm)

--- numba/testing/tests/test_parametrize.py
+++ numba/testing/tests/test_parametrize.py
@@ -6,8 +6,8 @@ def func(arg):
 
 assert func_testcase.__name__ == 'func'
 
-assert hasattr(func_testcase, 'func_0')
-assert hasattr(func_testcase, 'func_1')
+assert hasattr(func_testcase, 'test_func_0')
+assert hasattr(func_testcase, 'test_func_1')
 
-assert func_testcase('func_0').func_0() == 'foo'
-assert func_testcase('func_1').func_1() == 'bar'
+assert func_testcase('test_func_0').test_func_0() == 'foo'
+assert func_testcase('test_func_1').test_func_1() == 'bar'
