fab7bff7717d8f785f46a515493287ee58018fc7
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-08-07 10:24:56 -0500

    test for #41

---
+++ numbapro/cudavec/tests/test_cuda_vectorize_scalar_arg.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numbapro import vectorize
+from numbapro import cuda, int32, float32, float64
+from timeit import default_timer as time
+from .support import testcase, main
+
+sig = [float64(float64, float64)]
+
+@vectorize(sig, target='gpu')
+def vector_add(a, b):
+    return a + b
+
+@testcase
+def test_vectorize_scalar_arg():
+    A = np.arange(10, dtype=np.float64)
+    dA = cuda.to_device(A)
+    vector_add(1.0, dA)
+
+@testcase
+def test_vectorize_all_scalars():
+    vector_add(1.0, 1.0)
+
+if __name__ == '__main__':
+    main()
