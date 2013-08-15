e8d3b7d1d38dfa9791798d65fc22a76eb3091615
majidaldo <majidaldosari-github@yahoo.com>
2013-08-15 17:50:42 -0500

    changed mac compiler cmd for conda recipe

--- buildscripts/condarecipe/build.sh
+++ buildscripts/condarecipe/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ `uname` == Darwin ]; then
-    export CC=gcc-4.2
+    #export CC=gcc-4.2
+	CC=gcc
     if [ $CONDA_PY == 26 ]; then
-        $REPLACE gcc gcc-4.2 $PREFIX/lib/python2.6/config/Makefile
+        #$REPLACE gcc gcc-4.2 $PREFIX/lib/python2.6/config/Makefile
+		$REPLACE gcc gcc $PREFIX/lib/python2.6/config/Makefile
     fi
 fi
 
