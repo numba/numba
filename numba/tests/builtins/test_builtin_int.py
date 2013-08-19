4d2f11843334d20383bf32a097ad526fed7e020c
Mark Florisson <markflorisson88@gmail.com>
2013-08-19 12:32:53 +0100

    Small additions and fixes to tests (thanks to @eltjpm, #314)

--- numba/tests/builtins/test_builtin_int.py
+++ numba/tests/builtins/test_builtin_int.py
@@ -1,26 +1,23 @@
 """
->>> int() == 0
-True
->>> convert_int(2.5) == 2
-True
->>> convert_to_int('FF', 16) == 255
-True
+>>> empty_int()
+0
+>>> convert_int(2.5)
+2
+>>> convert_to_int('FF', 16)
+255
 """
 
-import sys
+from numba import autojit
 
-from numba import *
-
-@autojit(backend='ast')
+@autojit
 def empty_int():
-    x = float()
-    return x
+    return int()
 
-@autojit(backend='ast')
+@autojit
 def convert_int(x):
     return int(x)
 
-@autojit(backend='ast')
+@autojit
 def convert_to_int(s, base):
     return int(s, base)
 
--- numba/tests/builtins/test_builtin_pow.py
+++ numba/tests/builtins/test_builtin_pow.py
@@ -1,49 +1,74 @@
 """
->>> pow3(2,3,5) == 3
+>>> pow3(2,3,5)
+3
+>>> pow3(3,3,5)
+2
+ 
+>>> pow3_const()
+3
+ 
+>>> pow2(2,3)
+8
+>>> pow2(3,3)
+27
+>>> pow2(3.0,3)
+27.0
+>>> pow2(3,3.0)
+27.0
+>>> pow2(3.0,3.0)
+27.0
+>>> pow2(1.5, 2)
+2.25
+>>> pow2(1.5, 1.5) == pow(1.5, 1.5)
 True
->>> pow3(3,3,5) == 2
+ 
+>>> pow_op(3,3)
+27
+>>> pow_op(3.0,3)
+27.0
+>>> pow_op(3,3.0)
+27.0
+>>> pow_op(3.0,3.0)
+27.0
+>>> pow_op(1.5, 2)
+2.25
+>>> pow_op(1.5, 1.5) == pow(1.5, 1.5)
 True
 
->>> pow3_const() == 3
-True
-
->>> pow2(2,3) == 8
-True
->>> pow2(3,3) == 27
-True
-
->>> pow2_const() == 8
-True
+>>> pow2_const()
+8
 
 >>> c1, c2 = 1.2 + 4.1j, 0.6 + 0.5j
->>> allclose(pow2(c1, c2), pow(c1, c2))
+>>> pow2(c1, c2) == pow(c1, c2)
 True
 
 >>> d1, d2 = 4.2, 5.1
->>> allclose(pow2(d1, d2), pow(d1, d2))
+>>> pow2(d1, d2) == pow(d1, d2)
 True
 """
-
-from numpy import allclose
-from numba import *
-
+ 
+from numba import autojit
+ 
 @autojit
 def pow3(a,b,c):
     return pow(a,b,c)
-
+ 
 @autojit
 def pow3_const():
     return pow(2,3,5)
-
+ 
 @autojit(nopython=True)
 def pow2(a,b):
     return pow(a,b)
-
+ 
+@autojit(nopython=True)
+def pow_op(a,b):
+    return a**b
+ 
 @autojit(nopython=True)
 def pow2_const():
     return pow(2,3)
-
+ 
 if __name__ == '__main__':
-#    import logging; logging.getLogger().setLevel(0)
     import numba
     numba.testing.testmod()
