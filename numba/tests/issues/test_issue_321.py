241a8788d0dfdc899a84c10b1d65409bdfeba822
Mark Florisson <markflorisson88@gmail.com>
2013-08-21 18:00:59 +0100

    Update #321 for python 2.6

--- numba/tests/issues/test_issue_321.py
+++ numba/tests/issues/test_issue_321.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import
+import sys
 from numba import *
 
-f = autojit(lambda x: x * x)
-assert f(10) == 100
+if sys.version_info[:2] > (2, 6):
+    f = autojit(lambda x: x * x)
+    assert f(10) == 100
