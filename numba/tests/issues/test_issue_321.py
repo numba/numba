d4654aba1f2ba732066c0cce3ddbc655a601a9d8
Ilan Schnell <ilanschnell@gmail.com>
2013-08-29 11:23:59 -0500

    only run test_issue_321 if meta is avaialble

--- numba/tests/issues/test_issue_321.py
+++ numba/tests/issues/test_issue_321.py
@@ -3,6 +3,11 @@ from __future__ import print_function, division, absolute_import
 import sys
 from numba import *
 
-if sys.version_info[:2] > (2, 6):
+try:
+    import meta
+except ImportError:
+    meta = None
+
+if meta and sys.version_info[:2] > (2, 6):
     f = autojit(lambda x: x * x)
     assert f(10) == 100
