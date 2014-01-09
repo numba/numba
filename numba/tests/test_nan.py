1cdb3544597627c838ef6085d7ba08a23dc5e218
Jay Bourque <jay.bourque@continuum.io>
2014-01-09 16:42:37 -0600

    Add tests for NaN

---
+++ numba/tests/test_nan.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+import unittest
+import numpy as np
+from numba.compiler import compile_isolated, Flags
+from numba import types, utils
+from numba.tests import usecases
+
+enable_pyobj_flags = Flags()
+enable_pyobj_flags.set("enable_pyobject")
+
+force_pyobj_flags = Flags()
+force_pyobj_flags.set("force_pyobject")
+
+
+def isnan(x):
+    return x != x
+
+def isequal(x):
+    return x == x
+
+
+class TestNaN(unittest.TestCase):
+
+    def test_nans(self):
+        pyfunc = isnan
+        cr = compile_isolated(pyfunc, (types.float64,))
+        cfunc = cr.entry_point
+
+        self.assertTrue(cfunc(float('nan')))
+        self.assertFalse(cfunc(1.0))
+
+        pyfunc = isequal
+        cr = compile_isolated(pyfunc, (types.float64,))
+        cfunc = cr.entry_point
+
+        self.assertFalse(cfunc(float('nan')))
+        self.assertTrue(cfunc(1.0))
+
+if __name__ == '__main__':
+    unittest.main()
+
