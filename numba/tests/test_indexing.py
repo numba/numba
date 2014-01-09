66a67f88b429d779e9e60070bfe4c692bd374130
Jay Bourque <jay.bourque@continuum.io>
2014-01-08 19:00:39 -0600

    Add array indexing tests

---
+++ numba/tests/test_indexing.py
@@ -0,0 +1,168 @@
+from __future__ import print_function
+import unittest
+import numpy as np
+from numba.compiler import compile_isolated, Flags
+from numba import types, utils
+from numba.tests import usecases
+
+enable_pyobj_flags = Flags()
+enable_pyobj_flags.set("enable_pyobject")
+
+force_pyobj_flags = Flags()
+force_pyobj_flags.set("force_pyobject")
+
+
+def slicing_1d_usecase(a, start, stop, step):
+    return a[start:stop:step]
+
+def slicing_2d_usecase(a, start1, stop1, step1, start2, stop2, step2):
+    return a[start1:stop1:step1,start2:stop2:step2]
+
+def integer_indexing_1d_usecase(a, i):
+    return a[i]
+
+def integer_indexing_2d_usecase(a, i1, i2):
+    return a[i1,i2]
+
+def ellipse_usecase(a):
+    return a[...,0]
+
+def none_index_usecase(a):
+    return a[None]
+
+def fancy_index_usecase(a, index):
+    return a[index]
+
+def boolean_indexing_usecase(a, mask):
+    return a[mask]
+
+
+class TestIndexing(unittest.TestCase):
+
+    def test_1d_slicing(self):
+        pyfunc = slicing_1d_usecase
+        arraytype = types.Array(types.int32, 1, 'C')
+        cr = compile_isolated(pyfunc,
+            (arraytype, types.int32, types.int32, types.int32))
+        cfunc = cr.entry_point
+
+        a = np.arange(10, dtype='i4')
+        self.assertTrue((pyfunc(a, 0, 10, 1) == cfunc(a, 0, 10, 1)).all())
+        self.assertTrue((pyfunc(a, 2, 3, 1) == cfunc(a, 2, 3, 1)).all())
+        self.assertTrue((pyfunc(a, 10, 0, 1) == cfunc(a, 10, 0, 1)).all())
+        self.assertTrue((pyfunc(a, 0, 10, -1) == cfunc(a, 0, 10, -1)).all())
+        self.assertTrue((pyfunc(a, 0, 10, 2) == cfunc(a, 0, 10, 2)).all())
+
+    def test_2d_slicing(self):
+        pyfunc = slicing_1d_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        cr = compile_isolated(pyfunc,
+            (arraytype, types.int32, types.int32, types.int32))
+        cfunc = cr.entry_point
+
+        a = np.arange(100, dtype='i4').reshape(10, 10)
+        self.assertTrue((pyfunc(a, 0, 10, 1) == cfunc(a, 0, 10, 1)).all())
+        self.assertTrue((pyfunc(a, 2, 3, 1) == cfunc(a, 2, 3, 1)).all())
+        self.assertTrue((pyfunc(a, 10, 0, 1) == cfunc(a, 10, 0, 1)).all())
+        self.assertTrue((pyfunc(a, 0, 10, -1) == cfunc(a, 0, 10, -1)).all())
+        self.assertTrue((pyfunc(a, 0, 10, 2) == cfunc(a, 0, 10, 2)).all())
+
+        pyfunc = slicing_2d_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        cr = compile_isolated(pyfunc,
+            (arraytype, types.int32, types.int32, types.int32,
+            types.int32, types.int32, types.int32))
+        cfunc = cr.entry_point
+
+        self.assertTrue((pyfunc(a, 0, 10, 1, 0, 10, 1) ==
+                         cfunc(a, 0, 10, 1, 0, 10, 1)).all())
+        self.assertTrue((pyfunc(a, 2, 3, 1, 2, 3, 1) ==
+                         cfunc(a, 2, 3, 1, 2, 3, 1)).all())
+        self.assertTrue((pyfunc(a, 10, 0, 1, 10, 0, 1) ==
+                         cfunc(a, 10, 0, 1, 10, 0, 1)).all())
+        self.assertTrue((pyfunc(a, 0, 10, -1, 0, 10, -1) ==
+                         cfunc(a, 0, 10, -1, 0, 10, -1)).all())
+        self.assertTrue((pyfunc(a, 0, 10, 2, 0, 10, 2) ==
+                         cfunc(a, 0, 10, 2, 0, 10, 2)).all())
+
+    def test_1d_integer_indexing(self):
+        pyfunc = integer_indexing_1d_usecase
+        arraytype = types.Array(types.int32, 1, 'C')
+        cr = compile_isolated(pyfunc, (arraytype, types.int32))
+        cfunc = cr.entry_point
+        
+        a = np.arange(10, dtype='i4')
+        self.assertEqual(pyfunc(a, 0), cfunc(a, 0))
+        self.assertEqual(pyfunc(a, 9), cfunc(a, 9))
+        self.assertEqual(pyfunc(a, -1), cfunc(a, -1))
+
+    def test_2d_integer_indexing(self):
+        pyfunc = integer_indexing_1d_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        cr = compile_isolated(pyfunc, (arraytype, types.int32))
+        cfunc = cr.entry_point
+        
+        a = np.arange(100, dtype='i4').reshape(10, 10)
+        self.assertTrue((pyfunc(a, 0) == cfunc(a, 0)).all())
+        self.assertTrue((pyfunc(a, 9) == cfunc(a, 9)).all())
+        self.assertTrue((pyfunc(a, -1) == cfunc(a, -1)).all())
+
+        pyfunc = integer_indexing_2d_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        cr = compile_isolated(pyfunc, (arraytype, types.int32, types.int32))
+        cfunc = cr.entry_point
+
+        self.assertEqual(pyfunc(a, 0, 0), cfunc(a, 0, 0))
+        self.assertEqual(pyfunc(a, 9, 9), cfunc(a, 9, 9))
+        self.assertEqual(pyfunc(a, -1, -1), cfunc(a, -1, -1))
+
+    def test_ellipse(self):
+        pyfunc = ellipse_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        cr = compile_isolated(pyfunc, (arraytype,))
+        cfunc = cr.entry_point
+        
+        a = np.arange(100, dtype='i4').reshape(10, 10)
+        self.assertTrue((pyfunc(a) == cfunc(a)).all())
+
+    def test_none_index(self):
+        pyfunc = none_index_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        cr = compile_isolated(pyfunc, (arraytype,))
+        cfunc = cr.entry_point
+        
+        a = np.arange(100, dtype='i4').reshape(10, 10)
+        self.assertTrue((pyfunc(a) == cfunc(a)).all())
+
+    def test_fancy_index(self):
+        pyfunc = fancy_index_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        indextype = types.Array(types.int32, 1, 'C')
+        cr = compile_isolated(pyfunc, (arraytype, indextype))
+        cfunc = cr.entry_point
+        
+        a = np.arange(100, dtype='i4').reshape(10, 10)
+        index = np.array([], dtype='i4')
+        self.assertTrue((pyfunc(a, index) == cfunc(a, index)).all())
+        index = np.array([0], dtype='i4')
+        self.assertTrue((pyfunc(a, index) == cfunc(a, index)).all())
+        index = np.array([1,2], dtype='i4')
+        self.assertTrue((pyfunc(a, index) == cfunc(a, index)).all())
+        index = np.array([-1], dtype='i4')
+        self.assertTrue((pyfunc(a, index) == cfunc(a, index)).all())
+
+    def test_boolean_indexing(self):
+        pyfunc = boolean_indexing_usecase
+        arraytype = types.Array(types.int32, 2, 'C')
+        masktype = types.Array(types.boolean, 1, 'C')
+        cr = compile_isolated(pyfunc, (arraytype, masktype))
+        cfunc = cr.entry_point
+        
+        a = np.arange(100, dtype='i4').reshape(10, 10)
+        mask = np.array([True, False, True])
+        self.assertTrue((pyfunc(a, mask) == cfunc(a, mask)).all())
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
