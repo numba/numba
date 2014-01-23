9a87ddbeac35d0efcb128f8c45a5ce46169141f1
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-01-22 20:04:28 -0600

    Change DEBUG_JIT environ name

--- numba/config.py
+++ numba/config.py
@@ -5,7 +5,8 @@ import os
 # Debug flag to control compiler debug print
 DEBUG = int(os.environ.get("NUMBA_DEBUG", '0'))
 
-DEBUG_JIT = int(os.environ.get("NUMBA_DEBUG", '0'))
+# JIT Debug flag to trigger IR instruction print
+DEBUG_JIT = int(os.environ.get("NUMBA_DEBUG_JIT", '0'))
 
 # Optimization level
 OPT = int(os.environ.get("NUMBA_OPT", '2'))
