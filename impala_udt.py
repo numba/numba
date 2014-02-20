e14a92e26fe3a8fd14617a57dbf3d4630ba1e50b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-02-20 14:59:10 -0600

    Add simple test to exercise DoubleVal, TinyIntVal and BooleanVal

--- impala_udt.py
+++ impala_udt.py
@@ -2,7 +2,8 @@
 A simple demonstration of Impala UDF generation.
 """
 
-from numba.ext.impala import udf, IntVal, FunctionContext
+from numba.ext.impala import (udf, IntVal, FunctionContext, BooleanVal,
+                              DoubleVal, TinyIntVal)
 
 @udf(IntVal(FunctionContext, IntVal, IntVal))
 def add_udf(context, arg1, arg2):
@@ -13,3 +14,14 @@ def add_udf(context, arg1, arg2):
 # Simply print the module IR
 print(add_udf.llvm_module)
 
+
+@udf(BooleanVal(FunctionContext, DoubleVal, TinyIntVal))
+def exercise_double_tinyint_bool(context, arg1, arg2):
+    if arg1.is_null or arg2.is_null:
+        return BooleanVal.null
+
+    lt = arg1.val < arg2.val
+    return BooleanVal(lt)
+
+
+print(exercise_double_tinyint_bool.llvm_module)
