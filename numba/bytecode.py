bc0329ddd5b8499c231b5eb99b3915f873d5ae4d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-03-04 11:39:26 -0600

    Add comment

--- numba/bytecode.py
+++ numba/bytecode.py
@@ -14,6 +14,11 @@ opcode_info = namedtuple('opcode_info', ['argsize'])
 
 
 def get_function_object(obj):
+    """
+    Objects that wraps function should provide a "__numba__" magic attribute
+    that contains a name of an attribute that contains the actual python
+    function object.
+    """
     attr = getattr(obj, "__numba__", None)
     if attr:
         return getattr(obj, attr)
