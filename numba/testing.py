4233556913724b8afb01e6da69f7c03b7cf7144a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-04-04 15:56:15 -0500

    Improve runtests

--- numba/testing.py
+++ numba/testing.py
@@ -1,4 +1,13 @@
 from __future__ import print_function, division, absolute_import
+import sys
+
+if sys.version_info[0] >= 3:
+    from io import StringIO
+else:
+    try:
+        from cStringIO import StringIO
+    except ImportError:
+        from StringIO import StringIO
 
 
 def discover_tests(startdir):
@@ -25,6 +34,19 @@ def test():
     return run_tests(suite).wasSuccessful()
 
 
+def _flatten_suite(test):
+    """Expand suite into list of tests
+    """
+    from numba.unittest_support import TestSuite
+    if isinstance(test, TestSuite):
+        tests = []
+        for x in test:
+            tests.extend(_flatten_suite(x))
+        return tests
+    else:
+        return [test]
+
+
 def multitest():
     """
     Run tests in multiple processes.
@@ -35,16 +57,35 @@ def multitest():
     loader = unittest.TestLoader()
     startdir = "numba.tests"
     suites = loader.discover(startdir)
-
+    tests = _flatten_suite(suites)
+    # Distribute tests to multiple processes
     pool = mp.Pool(processes=mp.cpu_count())
-    results = pool.map(_multiruntest, suites)
-    return all(results)
+    results = pool.imap_unordered(_multiruntest, tests)
+
+    errct = 0
+    for ok, out in results:
+        if not ok:
+            print()
+            print("=== Error ===")
+            print(out)
+            errct += 1
+        else:
+            print('.', end='', flush=True)
+
+    print()
+    if errct == 0:
+        print("All passed!")
+        return True
+    else:
+        print("Error %d/%d" % (errct, len(tests)))
+        return False
 
 
 def _multiruntest(suite):
     import numba.unittest_support as unittest
-
-    runner = unittest.TextTestRunner(descriptions=False, verbosity=0,
-                                     buffer=True)
-    result = runner.run(suite)
-    return result.wasSuccessful()
+    stream = StringIO()
+    with stream:
+        runner = unittest.TextTestRunner(descriptions=False, verbosity=3,
+                                         buffer=True, stream=stream)
+        result = runner.run(suite)
+        return result.wasSuccessful(), stream.getvalue()
