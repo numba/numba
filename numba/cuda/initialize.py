fcdcc8a2f5e45e1b74c55d584f31814d964cc35a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-04-30 13:10:03 -0500

    Better let numbapro initialize ufunc and gufunc support

--- numba/cuda/initialize.py
+++ numba/cuda/initialize.py
@@ -1,27 +1,10 @@
 from __future__ import absolute_import, print_function
 from numba.targets.registry import target_registry
-from numba.npyufunc import Vectorize, GUVectorize
-
 
 def init_jit():
     from numba.cuda.dispatcher import CUDADispatcher
     return CUDADispatcher
 
-
-def init_vectorize():
-    from numbapro.cudavec.vectorizers import CudaVectorize
-    return CudaVectorize
-
-
-def init_guvectorize():
-    from numbapro.cudavec.vectorizers import CudaGUFuncVectorize
-    return CudaGUFuncVectorize
-
-
 def initialize_all():
     target_registry.ondemand['gpu'] = init_jit
     target_registry.ondemand['cuda'] = init_jit
-    Vectorize.target_registry.ondemand['gpu'] = init_vectorize
-    Vectorize.target_registry.ondemand['cuda'] = init_vectorize
-    GUVectorize.target_registry.ondemand['gpu'] = init_guvectorize
-    GUVectorize.target_registry.ondemand['cuda'] = init_guvectorize
