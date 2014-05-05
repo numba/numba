cf3dad43dfe34c49205ee8a8cdad9b697d9f0ae2
Oscar Villellas <oscar.villellas@continuum.io>
2014-05-05 11:33:08 +0200

    python 3 fix of apple_hello_world sample
    Better use "array.nbytes" that relying on len(array.data). As
    array.data is a memory view in python3 that returns number of
    elements instead of byte length (as it happen on 2.7 where data
    is a buffer.

--- numba/ocl/ocldrv/samples/apple_hello_world.py
+++ numba/ocl/ocldrv/samples/apple_hello_world.py
@@ -9,7 +9,7 @@ import numpy as np
 
 DATA_SIZE = 1024
 
-opencl_source = """
+opencl_source = b"""
 __kernel void square(__global float* input, __global float* output, const unsigned int count)
 {
     int i = get_global_id(0);
@@ -19,6 +19,8 @@ __kernel void square(__global float* input, __global float* output, const unsign
 """
 
 data = np.random.random_sample(DATA_SIZE).astype(np.float32)
+results = np.empty_like(data)
+
 platform = cl.platforms[0]
 device = platform.devices[-1]
 
@@ -27,9 +29,9 @@ q = ctxt.create_command_queue(device)
 program = ctxt.create_program_from_source(opencl_source)
 program.build()
 kernel = program.create_kernel(b"square")
-input = ctxt.create_buffer(len(data.data))
-output = ctxt.create_buffer(len(data.data))
-q.enqueue_write_buffer(input, 0, len(data.data), data.ctypes.data)
+input = ctxt.create_buffer(data.nbytes)
+output = ctxt.create_buffer(results.nbytes)
+q.enqueue_write_buffer(input, 0, data.nbytes, data.ctypes.data)
 kernel.set_arg(0, input)
 kernel.set_arg(1, output)
 kernel.set_arg(2, DATA_SIZE)
@@ -37,6 +39,5 @@ local_sz = kernel.get_work_group_size_for_device(device)
 global_sz = DATA_SIZE
 q.enqueue_nd_range_kernel(kernel, 1, [global_sz], [local_sz])
 q.finish()
-results = np.empty_like(data)
-q.enqueue_read_buffer(output, 0, len(results.data), results.ctypes.data)
-print("Computed '{0}/{1}' correct values.".format(np.sum(results == data*data), DATA_SIZE))
+q.enqueue_read_buffer(output, 0, results.nbytes, results.ctypes.data)
+print("Computed '{0}/{1}' correct values.\n".format(np.sum(results == data*data), DATA_SIZE))
