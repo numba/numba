0f4ac4ad23a813e24d225c6c0d9b7ee183947ea1
Oscar Villellas <oscar.villellas@continuum.io>
2014-05-27 16:48:20 +0200

    added more tests to oil

---
+++ numba/ocl/tests/ocldrv/test_ocl_auto_context.py
@@ -0,0 +1,21 @@
+from __future__ import print_function, absolute_import
+import numpy as np
+from numba import ocl
+import unittest
+
+
+class TestCudaAutoContext(unittest.TestCase):
+    def test_auto_context(self):
+        """
+        This caused problems in the CUDA driver at some point.
+        Make sure that there is no regression in OpenCL
+        """
+        A = np.arange(10, dtype=np.float32)
+        newA = np.empty_like(A)
+        dA = ocl.to_device(A)
+
+        dA.copy_to_host(newA)
+        self.assertTrue(np.allclose(A, newA))
+
+if __name__ == '__main__':
+    unittest.main()
