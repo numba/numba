ac909ad8449654f40153b041165517ffeeb8c393
Scott Chadde <scott.chadde@gmail.com>
2014-06-03 09:21:50 -0500

    added ufunc trig unit tests

--- numba/array/examples.py
+++ numba/array/examples.py
@@ -6,8 +6,8 @@ from numba import vectorize
 
 def simple_example():
 
-    a1 = numbarray.Array(data=np.arange(10000000, dtype='f8'))
-    a2 = numbarray.Array(data=np.arange(10000000, dtype='f8'))
+    a1 = numbarray.Array(data=np.arange(100, dtype='f8'))
+    a2 = numbarray.Array(data=np.arange(100, dtype='f8'))
 
     result = a1 + a2 + a1*2 + a2*3
 
@@ -43,10 +43,10 @@ def deferred_data_example():
 
     # force eval with concrete data
     print result.eval(a1=np.arange(10, dtype='i8'), a2=np.arange(10, dtype='i8'))
-    
+
     # force eval with concrete data
     print result.eval(a1=np.arange(10, dtype='f8'), a2=np.arange(10, dtype='f8'))
-    
+
 
 def python_mode_example():
 
--- numba/array/test_ufuncs.py
+++ numba/array/test_ufuncs.py
@@ -2,7 +2,7 @@ import numba.unittest_support as unittest
 import numba.array as numbarray
 import numpy as np
 from numba.config import PYVERSION
-
+from math import pi
 use_python = True
 
 
@@ -31,7 +31,69 @@ class TestUFuncs(unittest.TestCase):
 
         self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
 
+    def test_unary_sin_ufunc(self):
+
+        a = (pi / 2) * numbarray.ones(10)
+        b = (pi / 2) * np.ones(10)
+
+        result = numbarray.sin(a)
+        result.eval()  # eval is deferred so w/o (or print) this the assert fails
+        #print result
+        expected = np.sin(b)
+        self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
+
+    def test_unary_cos_ufunc(self):
+
+        a = numbarray.zeros(10)
+        b = np.zeros(10)
+
+        result = numbarray.cos(a)
+        result.eval()
+        expected = np.cos(b)
+        self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
+
+    def test_unary_tan_ufunc(self):
+
+        # The (pi / 4) fails
+        #a = (pi / 4) * numbarray.ones(10)
+        a = numbarray.zeros(10)
+        #b = (pi / 4) * np.ones(10)
+        b = np.zeros(10)
+        result = numbarray.tan(a)
+        result.eval()
+        expected = np.tan(b)
+        self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
+
+    def test_unary_arcsin_ufunc(self):
+
+        a = numbarray.zeros(10)
+        b = np.zeros(10)
+        result = numbarray.arcsin(a)
+        result.eval()
+        expected = np.arcsin(b)
+        self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
+
+    def test_unary_arccos_ufunc(self):
+
+        a = numbarray.ones(10)
+
+        b = np.ones(10)
+        result = numbarray.arccos(a)
+        result.eval()
+        print type(result)
+        expected = np.arccos(b)
+        self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
+    
+    def test_unary_arctan_ufunc(self):
+
+        a = numbarray.ones(10)
+        b = np.ones(10)
+        result = numbarray.arctan(a)
+        result.eval()
+        print type(result)
+        expected = np.arctan(b)
+        self.assertTrue(np.all(result.eval(use_python=use_python) == expected))
+     
 
 if __name__ == '__main__':
     unittest.main()
-
