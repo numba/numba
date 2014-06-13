f03caebe2b8ae6f4f1261b246be52a1333b273be
Jay Bourque <jay.bourque@continuum.io>
2014-06-13 14:14:54 -0500

    Add comparison operators

--- numba/typing/operatordecl.py
+++ numba/typing/operatordecl.py
@@ -26,6 +26,14 @@ class Operator_binary(ConcreteTemplate):
         signature(types.float64, types.float64, types.float64),
     ]
 
+class Operator_comparison(ConcreteTemplate):
+    cases = [
+        signature(types.bool_, types.int64, types.int64),
+        signature(types.bool_, types.uint64, types.uint64),
+        signature(types.bool_, types.float32, types.float32),
+        signature(types.bool_, types.float64, types.float64),
+    ]
+
 def create_resolve_method(op):
     def resolve_op(self, mod):
         return types.Function(op)
@@ -41,11 +49,18 @@ for op in unary_operators:
 
 
 binary_operators = ['add', 'sub', 'mul', 'div', 'floordiv', 'truediv', 'mod', 'pow',
-    'eq', 'ne', 'lt', 'le', 'gt', 'ge', 'and_', 'or_', 'xor',
     'lshift', 'rshift']
 
 for op in binary_operators:
     op_type = type('Operator_' + op, (Operator_binary,), {'key':getattr(operator, op)})
+    setattr(OperatorModuleAttribute, 'resolve_' + op,  create_resolve_method(op_type))
+    builtin_global(getattr(operator, op), types.Function(op_type))
+
+
+comparison_operators = ['eq', 'ne', 'lt', 'le', 'gt', 'ge', 'and_', 'or_', 'xor']
+
+for op in comparison_operators:
+    op_type = type('Operator_' + op, (Operator_comparison,), {'key':getattr(operator, op)})
     setattr(OperatorModuleAttribute, 'resolve_' + op,  create_resolve_method(op_type))
     builtin_global(getattr(operator, op), types.Function(op_type))
 
