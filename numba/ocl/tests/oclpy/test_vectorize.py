0a9bc9efa49d802dde68c2147be37cd5caf1970c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-01 13:14:47 -0500

    OCL vectorize

--- numba/ocl/tests/oclpy/test_vectorize.py
+++ numba/ocl/tests/oclpy/test_vectorize.py
@@ -16,14 +16,33 @@ class TestVectorize(unittest.TestCase):
         b = np.arange(a.size, dtype="int32")
         c = np.zeros_like(a)
 
+        # All device
         dev_a = ocl.to_device(a)
         dev_b = ocl.to_device(b)
         dev_c = ocl.to_device(c)
         vsum(dev_a, dev_b, out=dev_c)
         dev_c.copy_to_host(c)
 
-        print(c)
         self.assertTrue(np.all(a + b == c))
+
+        c.fill(0)  # reset
+
+        # All host
+        vsum(a, b, out=c)   # out provided
+        self.assertTrue(np.all(a + b == c))
+
+        c = vsum(a, b)      # out not provided
+        self.assertTrue(np.all(a + b == c))
+
+        # Mixed device and host
+        c = vsum(dev_a, b)      # out not provided
+        self.assertTrue(np.all(a + b == c))
+
+        # With scalar
+        # Mixed device and host
+        c = vsum(dev_a, 1)      # out not provided
+        
+        self.assertTrue(np.all(a + 1 == c))
 
 
 if __name__ == '__main__':
--- numba/ocl/ufunc.py
+++ numba/ocl/ufunc.py
@@ -2,7 +2,7 @@ from __future__ import print_function, absolute_import
 import warnings
 import numpy as np
 from numba import sigutils, ocl
-from numba.utils import IS_PY3
+from numba.utils import IS_PY3, longint
 from numba.ocl.ocldrv import oclarray
 
 if IS_PY3:
@@ -30,7 +30,7 @@ class OclVectorize(object):
     def __init__(self, func, targetoptions={}):
         assert not targetoptions
         self.pyfunc = func
-        self.kernelmap = {}  # { arg_dtype: (return_dtype), cudakernel }
+        self.kernelmap = {}  # { arg_dtype: (return_dtype), kernel }
 
     def add(self, sig=None, argtypes=None, restype=None):
         # Handle argtypes
@@ -49,7 +49,7 @@ class OclVectorize(object):
         args, return_type = sigutils.normalize_signature(sig)
         sig = return_type(*args)
 
-        cudevfn = ocl.jit(sig, device=True, inline=True)(self.pyfunc)
+        ocldevfn = ocl.jit(sig, device=True, inline=True)(self.pyfunc)
 
         # generate outer loop as kernel
         args = ['a%d' % i for i in range(len(sig.args))]
@@ -60,7 +60,7 @@ class OclVectorize(object):
         kernelsource = vectorizer_stager_source % fmts
         glbl = self.pyfunc.__globals__
         glbl.update({'__ocl__': ocl,
-                     '__core__': cudevfn})
+                     '__core__': ocldevfn})
 
         _exec(kernelsource, glbl)
 
@@ -85,41 +85,6 @@ class OclUFuncDispatcher(object):
     def __init__(self, types_to_retty_kernels):
         self.functions = types_to_retty_kernels
 
-    @property
-    def max_blocksize(self):
-        try:
-            return self.__max_blocksize
-        except AttributeError:
-            return 2 ** 30 # a very large number
-
-    @max_blocksize.setter
-    def max_blocksize(self, blksz):
-        self.__max_blocksize = blksz
-
-    @max_blocksize.deleter
-    def max_blocksize(self, blksz):
-        del self.__max_blocksize
-
-    def _prepare_inputs(self, args):
-        # prepare broadcasted contiguous arrays
-        # TODO: Allow strided memory (use mapped memory + strides?)
-        # TODO: don't perform actual broadcasting, pass in strides
-        #        args = [np.ascontiguousarray(a) for a in args]
-
-        return np.broadcast_arrays(*args)
-
-    def _adjust_dimension(self, broadcast_arrays):
-        '''Reshape the broadcasted arrays so that they are all 1D arrays.
-        Uses ndarray.ravel() to flatten.  It only copy if necessary.
-        '''
-        for i, ary in enumerate(broadcast_arrays):
-            if ary.ndim > 1:  # flatten multi-dimension arrays
-                broadcast_arrays[i] = ary.ravel()  # copy if necessary
-        return broadcast_arrays
-
-    def _allocate_output(self, broadcast_arrays, result_dtype):
-        return np.empty(shape=broadcast_arrays[0].shape, dtype=result_dtype)
-
     def __call__(self, *args, **kws):
         """
         *args: numpy arrays or DeviceArrayBase (created by ocl.to_device).
@@ -131,69 +96,208 @@ class OclUFuncDispatcher(object):
                       depending on the input arguments.  Type must match
                       the input arguments.
         """
-        accepted_kws = 'stream', 'out'
-        unknown_kws = [k for k in kws if k not in accepted_kws]
-        assert not unknown_kws, ("Unknown keyword args %s" % unknown_kws)
-
-        stream = kws.get('stream', 0)
+        return OclUFuncCallResolution.call(self.functions, args, kws)
 
 
-        # convert arguments to ndarray if they are not
-        args = list(args) # convert to list
-        has_device_array_arg = any(oclarray.is_ocl_ndarray(v)
-                                   for v in args)
+class UFuncCallResolution(object):
+    """
+    Prepare ufunc arguments for vectorize.
+    """
+    DEFAULT_STREAM = None
+    SUPPORT_DEVICE_SLICING = False
 
-        for i, arg in enumerate(args):
-            if not isinstance(arg, np.ndarray) and \
-                    not oclarray.is_ocl_ndarray(arg):
-                args[i] = np.asarray(arg)
+    def __init__(self, typemap, args):
+        self.typemap = typemap
+        self.args = args
+        nargs = len(self.args)
+        self.argtypes = [None] * nargs
+        self.scalarpos = []
+        self.signature = None
+        self.arrays = [None] * nargs
 
-        # get the dtype for each argument
-        def _get_dtype(x):
-            try:
-                return x.dtype
-            except AttributeError:
-                return np.dtype(type(x))
+    def _fill_arrays(self):
+        """
+        Get all arguments in array form
+        """
+        for i, arg in enumerate(self.args):
+            if isinstance(arg, np.ndarray):
+                self.arrays[i] = arg
+            elif self.is_device_array(arg):
+                self.arrays[i] = arg
+            # elif isinstance(arg, (int, longint, float, complex)):
+            elif isinstance(arg, (int, float, complex)):
+                # Is scalar
+                self.scalarpos.append(i)
+            else:
+                raise TypeError("argument #%d has invalid type" % (i + 1,))
 
-        dtypes = tuple(_get_dtype(a) for a in args)
+    def _fill_argtypes(self):
+        for i, ary in enumerate(self.arrays):
+            if ary is not None:
+                self.argtypes[i] = ary.dtype
 
-        # find the fitting function
-        result_dtype, ocl_func = self._get_function_by_dtype(dtypes)
+    def _guess_signature(self):
+        # Find all matching signature, ignoring missing argtype
+        matches = []
+        for formaltys in self.typemap:
+            all_matches = all(actual is None or formal == actual
+                              for formal, actual in
+                              zip(formaltys, self.argtypes))
+            if all_matches:
+                matches.append(formaltys)
 
-        output = kws['out']
-        kargs = list(args) + [output]
-        ocl_func.configure(output.shape[0])(*kargs)
+        if not matches:
+            raise TypeError("No matching version")
+
+        if len(matches) > 1:
+            raise TypeError("Failed to resolve ufunc due to ambiguous "
+                            "signature. Too many untyped scalars. "
+                            "Use numpy dtype object to type tag.")
+
+        self.argtypes = matches[0]
+
+    def _get_actual_args(self):
+        for i in self.scalarpos:
+            self.arrays[i] = np.array([self.args[i]], dtype=self.argtypes[i])
+
+        return self.arrays
+
+    def _broadcast(self, arys):
+        # Get the biggest shape
+        nds = [a.ndim for a in arys]
+        biggest = np.argmax(nds)
+        shape = arys[biggest].shape
+
+        for i, ary in enumerate(arys):
+            if ary.shape == shape:
+                pass
+
+            elif ary.shape[0] == 1:
+                if self.is_device_array(ary):
+                    arys[i] = self.broadcast_device(ary)
+
+                else:
+                    missingdim = len(shape) - len(ary.shape) + 1
+                    strides = (0,) * missingdim + ary.strides[1:]
+                    strided = np.lib.stride_tricks.as_strided(ary,
+                                                              shape=shape,
+                                                              strides=strides)
+                    arys[i] = self.force_array_layout(strided)
+
+            else:
+                raise ValueError("arg #%d cannot be broadcasted" % (i + 1))
+
+        return arys
+
+    def get_arguments(self):
+        self._fill_arrays()
+        self._guess_signature()
+        arys = self._get_actual_args()
+        return self._broadcast(arys)
+
+    def get_function(self):
+        return self.typemap[self.argtypes]
+
+    def is_device_array(self, obj):
+        return False
+
+    def broadcast_device(self, ary):
+        raise NotImplementedError("broadcasting on device is not supported")
+
+    def force_array_layout(self, ary):
+        return ary
+
+    @classmethod
+    def call(cls, typemap, args, kws):
+        # Handle keywords
+        stream = kws.pop('stream', cls.DEFAULT_STREAM)
+        out = kws.pop('out', None)
+
+        if kws:
+            warnings.warn("unrecognized keywords: %s" % ', '.join(kws))
+
+        # Begin call resolution
+        cr = cls(typemap, args)
+        args = cr.get_arguments()
+        resty, func = cr.get_function()
+
+        if args[0].ndim > 1:
+            if not cr.SUPPORT_DEVICE_SLICING:
+                raise NotImplementedError("Support 1D array only.")
+            else:
+                raise NotImplementedError
+
+        # Prepare argument on the device
+        devarys = []
+        all_device = True
+        for a in args:
+            if cr.is_device_array(a):
+                devarys.append(a)
+            else:
+                dev_a = cr.to_device(a)
+                devarys.append(dev_a)
+                all_device = False
+
+        # Launch
+        shape = args[0].shape
+        if out is None:
+            # No output is provided
+            devout = cr.device_array(shape, resty)
+
+            devarys.extend([devout])
+            cr.launch(func, shape[0], stream, devarys)
+
+            if all_device:
+                # If all arguments are on device,
+                # Keep output on the device
+                return devout
+            else:
+                # Otherwise, transfer output back to host
+                return devout.copy_to_host()
+
+        elif cr.is_device_array(out):
+            # If output is provided and it is a device array,
+            # Return device array
+            devout = out
+            devarys.extend([devout])
+            cr.launch(func, shape[0], stream, devarys)
+            return devout
+
+        else:
+            # If output is provided and it is a host array,
+            # Return host array
+            assert out.shape == shape
+            assert out.dtype == resty
+            devout = cr.device_array(shape, resty)
+            devarys.extend([devout])
+            cr.launch(func, shape[0], stream, devarys)
+            return devout.copy_to_host(out)
+
+    def to_device(self, hostary):
+        raise NotImplementedError
+
+    def device_array(self, shape, dtype):
+        raise NotImplementedError
+
+    def launch(self, func, count, stream, args):
+        raise NotImplementedError
 
 
-    def _determine_output_shape(self, broadcast_arrays):
-        return broadcast_arrays[0].shape
+class OclUFuncCallResolution(UFuncCallResolution):
+    DEFAULT_STREAM = None
+    ARRAY_ORDER = 'C'
 
-    def _get_function_by_dtype(self, dtypes):
-        try:
-            result_dtype, cuda_func = self.functions[dtypes]
-            return result_dtype, cuda_func
-        except KeyError:
-            raise TypeError("Input dtypes not supported by ufunc %s" %
-                            (dtypes,))
+    def is_device_array(self, obj):
+        return oclarray.is_ocl_ndarray(obj)
 
-    def _determine_element_count(self, broadcast_arrays):
-        return np.prod(broadcast_arrays[0].shape)
+    def to_device(self, hostary):
+        return ocl.to_device(hostary)
 
-    def _arguments_requirement(self, args):
-        # get shape of all array
-        array_shapes = []
-        for i, a in enumerate(args):
-            if a.strides[0] != 0:
-                array_shapes.append((i, a.shape[0]))
+    def launch(self, func, count, stream, args):
+        func.configure(count, stream=stream)(*args)
 
-        _, ms = array_shapes[0]
-        for i, s in array_shapes[1:]:
-            if ms != s:
-                raise ValueError("arg %d should have length %d" % ms)
+    def device_array(self, shape, dtype):
+        return ocl.device_array(shape=shape, dtype=dtype)
 
-    def _determine_dimensions(self, n, max_thread):
-        # determine grid and block dimension
-        thread_count = int(min(max_thread, n))
-        block_count = int((n + max_thread - 1) // max_thread)
-        return block_count, thread_count
-
+    def force_array_layout(self, ary):
+        return np.ascontiguousarray(ary)
