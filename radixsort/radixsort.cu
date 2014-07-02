8847c8131f5209892d70d55dc26fca34c5d7fb18
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-02 17:40:34 -0500

    Radixsort building block completed

--- radixsort/radixsort.cu
+++ radixsort/radixsort.cu
@@ -1,10 +1,15 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <cub/block/block_load.cuh>
+#include <cub/block/block_store.cuh>
+#include <cub/block/block_scan.cuh>
+
 #define CUDA_SAFE(X) {if((X) != cudaSuccess) {fprintf(stderr, "cuda error: line %d\n", __LINE__); exit(1);}}
 #define ASSERT_CUDA_LAST_ERROR() CUDA_SAFE(cudaPeekAtLastError());
 
-enum { BUCKETSIZE = 16 };
+enum { BUCKETSIZE = 256 };
 
 __device__
 unsigned global_id() {
@@ -16,13 +21,13 @@ unsigned local_id() {
     return threadIdx.x;
 }
 
-
 /**
 blocksize >= BUCKETSIZE
 */
 __global__
 void
-gpu_count_occurence(unsigned char input[], unsigned freq[], unsigned count)
+gpu_count_occurrence(unsigned char input[], unsigned freq[], unsigned count,
+                     unsigned stride, unsigned offset)
 {
     __shared__ unsigned cache[BUCKETSIZE];
     unsigned i = global_id();
@@ -40,7 +45,7 @@ gpu_count_occurence(unsigned char input[], unsigned freq[], unsigned count)
     __syncthreads();
 
     // Compute local frequency
-    atomicAdd(&cache[input[i]], 1);
+    atomicAdd(&cache[input[i * stride + offset]], 1);
 
     __syncthreads();
 
@@ -54,39 +59,38 @@ gpu_count_occurence(unsigned char input[], unsigned freq[], unsigned count)
 Launch BUCKETSIZE blocksize
 */
 __global__
-void 
-gpu_scan(unsigned freq[], unsigned offset[])
+void
+gpu_scan(unsigned freq[])
 {
-    __shared__ unsigned cache[BUCKETSIZE];
-    unsigned tid = local_id();
-
-    // Initialize cache
-    if (tid < BUCKETSIZE) {
-        if (tid == 0) cache[0] = 0;
-        else {
-            cache[tid] = freq[tid - 1];
-        }
-    }
+    using namespace cub;
+    enum { BLOCK_THREADS = BUCKETSIZE, ITEMS_PER_THREAD = 1 };
+    typedef BlockLoad<unsigned*, BLOCK_THREADS, ITEMS_PER_THREAD> BlockLoadT;
+    typedef BlockStore<unsigned*, BLOCK_THREADS, ITEMS_PER_THREAD> BlockStoreT;
+    typedef BlockScan<unsigned, BLOCK_THREADS> BlockScanT;
+    // Shared memory
+    __shared__ union
+    {
+        typename BlockLoadT::TempStorage    load;
+        typename BlockStoreT::TempStorage   store;
+        typename BlockScanT::TempStorage    scan;
+    } temp_storage;
+    // Per-thread tile data
+    unsigned data[ITEMS_PER_THREAD];
+    // Load items into a blocked arrangement
+    BlockLoadT(temp_storage.load).Load(freq, data);
+    // Barrier for smem reuse
     __syncthreads();
 
-    // Perform scan on cache
-    unsigned round = 0;
-    unsigned cond = BUCKETSIZE >> 1;
-    while (cond) {
-        unsigned step = 1 << round;
-        if ( tid + step  < BUCKETSIZE ) {
-            unsigned left = cache[tid];
-            __syncthreads();
+    // Compute exclusive prefix sum
+    unsigned aggregate;
+    BlockScanT(temp_storage.scan).ExclusiveSum(data, data, aggregate);
 
-            cache[tid + step] += left;
-            __syncthreads();
-        }
-        cond >>= 1;
-        round += 1;
-    }
+    // Barrier for smem reuse
+    __syncthreads();
 
-    // Copy result in cache to global memory
-    offset[tid] = cache[tid];
+    // Store items from a blocked arrangement
+    BlockStoreT(temp_storage.store).Store(freq, data);
+
 }
 
 
@@ -94,8 +98,9 @@ gpu_scan(unsigned freq[], unsigned offset[])
 Compute index base on bucket offset and input data.
 */
 __global__
-void 
-gpu_final_pos(unsigned char input[], unsigned offset[], unsigned indices[], unsigned count)
+void
+gpu_final_pos(unsigned char input[], unsigned starts[], unsigned indices[],
+              unsigned count, unsigned stride, unsigned offset)
 {
     __shared__ unsigned bucket_offset;
     __shared__ unsigned cached_offset[BUCKETSIZE];
@@ -106,12 +111,12 @@ gpu_final_pos(unsigned char input[], unsigned offset[], unsigned indices[], unsi
     // Preload
     if (tid == 0) {
         // First thread initialize bucket_offset
-        bucket_offset = 0; 
+        bucket_offset = 0;
     }
 
     if (tid < BUCKETSIZE) {
         // Cache offset memory into shared memory
-        cached_offset[tid] = offset[tid];
+        cached_offset[tid] = starts[tid];
     }
 
     __syncthreads();
@@ -120,7 +125,7 @@ gpu_final_pos(unsigned char input[], unsigned offset[], unsigned indices[], unsi
     while (input_offset < count) {
         unsigned idx = input_offset + tid;
         if ( idx < count ) {
-            unsigned char val = input[idx];    
+            unsigned char val = input[idx * stride + offset];
             if (val == bucket_idx) {    // is in this bucket
                 unsigned base = cached_offset[val];
                 unsigned off = atomicAdd(&bucket_offset, 1);
@@ -134,62 +139,109 @@ gpu_final_pos(unsigned char input[], unsigned offset[], unsigned indices[], unsi
     }
 }
 
+template<typename Ty>
+struct copy {
+    __device__
+    static
+    void as(void *output, void *input, unsigned *indices, unsigned i) {
+        ((Ty*)output)[indices[i]] = ((Ty*)input)[i];
+    }
+};
+
 __global__
-void 
-gpu_scatter(unsigned char input[], unsigned indices[], unsigned char output[], unsigned count)
+void
+gpu_scatter(void *input, unsigned indices[], void* output, unsigned count, unsigned bytesize)
 {
     unsigned i = global_id();
     if ( i < count ) {
-        output[indices[i]] = input[i];
+        switch(bytesize) {
+        case 1:
+            copy<uint8_t>::as(output, input, indices, i);
+            break;
+        case 2:
+            copy<uint16_t>::as(output, input, indices, i);
+            break;
+        case 4:
+            copy<uint32_t>::as(output, input, indices, i);
+            break;
+        case 8:
+            copy<uint64_t>::as(output, input, indices, i);
+            break;
+        }
+
     }
 }
 
-enum {COUNT = 1024};
-
-unsigned 
+unsigned
 forall_gridsize_given_blocksize(unsigned count, unsigned blocksz) {
     return (count + (blocksz - 1)) / blocksz;
 }
 
 void
-count_occurence(unsigned char *dev_data, unsigned *dev_freq, unsigned count) {
+count_occurrence(unsigned char *dev_data, unsigned *dev_freq, unsigned count,
+                 unsigned stride, unsigned offset) {
     unsigned blocksz = 512;
     unsigned gridsz = forall_gridsize_given_blocksize(count, blocksz);
-    gpu_count_occurence<<<gridsz, blocksz>>>(dev_data, dev_freq, count);
+    gpu_count_occurrence<<<gridsz, blocksz>>>(dev_data, dev_freq, count,
+                                              stride, offset);
     ASSERT_CUDA_LAST_ERROR();
 }
 
-void 
-scan(unsigned *dev_freq, unsigned *dev_offset) {
-    gpu_scan<<<1, BUCKETSIZE>>>(dev_freq, dev_offset);
-    ASSERT_CUDA_LAST_ERROR();   
+void
+scan(unsigned *dev_freq) {
+    gpu_scan<<<1, BUCKETSIZE>>>(dev_freq);
+    ASSERT_CUDA_LAST_ERROR();
 }
 
-void 
-final_pos(unsigned char *dev_data, unsigned *dev_offset, unsigned *dev_indices, unsigned count) {
-    gpu_final_pos<<<BUCKETSIZE, 512>>>(dev_data, dev_offset, dev_indices, count);
+void
+final_pos(unsigned char *dev_data, unsigned *dev_offset, unsigned *dev_indices,
+          unsigned count, unsigned stride, unsigned offset) {
+    gpu_final_pos<<<BUCKETSIZE, 512>>>(dev_data, dev_offset, dev_indices, count,
+                                       stride, offset);
     ASSERT_CUDA_LAST_ERROR();
-}   
+}
 
-void scatter(unsigned char *dev_data, unsigned *dev_indices, unsigned char *dev_sorted, unsigned count) {
+void scatter(unsigned char *dev_data, unsigned *dev_indices, unsigned char *dev_sorted, unsigned count, unsigned bytesize) {
     unsigned blocksz = 512;
     unsigned gridsz = forall_gridsize_given_blocksize(count, blocksz);
-    gpu_scatter<<<gridsz, blocksz>>>(dev_data, dev_indices, dev_sorted, count);
+    gpu_scatter<<<gridsz, blocksz>>>(dev_data, dev_indices, dev_sorted, count, bytesize);
     ASSERT_CUDA_LAST_ERROR();
 }
+
+void
+radixsort(unsigned char *dev_data,
+          unsigned      *dev_freq,
+          unsigned      *dev_indices,
+          unsigned char *dev_sorted,
+          unsigned       count,
+          unsigned       elemsize,
+          unsigned       offset)
+{
+    cudaMemset(dev_freq, 0, sizeof(unsigned)*BUCKETSIZE);
+    count_occurrence(dev_data, dev_freq, count, elemsize, offset);
+    scan(dev_freq);
+    final_pos(dev_data, dev_freq, dev_indices, count, elemsize, offset);
+    scatter(dev_data, dev_indices, dev_sorted, count, elemsize);
+}
+
+
+enum {COUNT = 300};
 
 int main() {
-    unsigned char data[COUNT] = {0};
-    const unsigned count = sizeof(data) / sizeof(unsigned char);
+    typedef int data_type;
+    data_type data[COUNT] = {0};
+    const unsigned elemsize = sizeof(data_type);
+    const unsigned offset = 0;
+
+    const unsigned count = sizeof(data) / sizeof(data_type);
 
     for(unsigned i=0; i<count; ++i) {
-        data[i] = (i % BUCKETSIZE);
+        data[i] = i;
     }
 
     unsigned data_size = sizeof(data);
 
     unsigned freq[BUCKETSIZE];
-    unsigned offset[BUCKETSIZE];
     unsigned indices[count];
     memset(freq, 0, sizeof(freq));
     memset(indices, 0, sizeof(indices));
@@ -198,35 +250,39 @@ int main() {
     unsigned indices_size = sizeof(indices);
 
     unsigned char *dev_data, *dev_sorted;
-    unsigned *dev_freq, *dev_offset, *dev_indices;
+    unsigned *dev_freq, *dev_indices;
 
     CUDA_SAFE(cudaMalloc(&dev_data, data_size));
     CUDA_SAFE(cudaMalloc(&dev_freq, freq_size));
-    CUDA_SAFE(cudaMalloc(&dev_offset, freq_size));
     CUDA_SAFE(cudaMalloc(&dev_indices, indices_size));
     CUDA_SAFE(cudaMalloc(&dev_sorted, data_size));
 
     CUDA_SAFE(cudaMemcpy(dev_data, data, data_size, cudaMemcpyHostToDevice));
-    CUDA_SAFE(cudaMemcpy(dev_freq, freq, freq_size, cudaMemcpyHostToDevice));
+    //CUDA_SAFE(cudaMemcpy(dev_freq, freq, freq_size, cudaMemcpyHostToDevice));
 
-    count_occurence(dev_data, dev_freq, count);
-    scan(dev_freq, dev_offset);
-    final_pos(dev_data, dev_offset, dev_indices, count);
-    scatter(dev_data, dev_indices, dev_sorted, count);
+    radixsort(dev_data, dev_freq, dev_indices, dev_sorted, count, elemsize,
+              1);
 
-    // CUDA_SAFE(cudaMemcpy(freq, dev_freq, freq_size, cudaMemcpyDeviceToHost));
-    CUDA_SAFE(cudaMemcpy(offset, dev_offset, freq_size, cudaMemcpyDeviceToHost));
-    CUDA_SAFE(cudaMemcpy(indices, dev_indices, indices_size, cudaMemcpyDeviceToHost));
+    CUDA_SAFE(cudaMemcpy(dev_data, dev_sorted, data_size, cudaMemcpyDeviceToDevice));
+    radixsort(dev_data, dev_freq, dev_indices, dev_sorted, 256, elemsize,
+              0);
+
+    CUDA_SAFE(cudaMemcpy(dev_data, dev_sorted, data_size, cudaMemcpyDeviceToDevice));
+    radixsort(dev_data + 4*256, dev_freq, dev_indices, dev_sorted + 4*256, 300 - 256, elemsize,
+              0);
+
+    CUDA_SAFE(cudaMemcpy(freq, dev_freq, freq_size, cudaMemcpyDeviceToHost));
+    // CUDA_SAFE(cudaMemcpy(indices, dev_indices, indices_size, cudaMemcpyDeviceToHost));
     CUDA_SAFE(cudaMemcpy(data, dev_sorted, data_size, cudaMemcpyDeviceToHost));
 
     puts("offset");
     for(unsigned i=0; i<BUCKETSIZE; ++i) {
-        printf("[%u] = %u\n", i, offset[i]);
+        printf("[%u] = %u\n", i, freq[i]);
     }
-    puts("final pos");
-    for(unsigned i=0; i<count; ++i) {
-        printf("[%u] = %u\n", i, indices[i]);
-    }
+    // puts("final pos");
+    // for(unsigned i=0; i<count; ++i) {
+    //     printf("[%u] = %u\n", i, indices[i]);
+    // }
     puts("sorted");
     for(unsigned i=0; i<count; ++i) {
         printf("[%u] = %u\n", i, data[i]);
