64fe7d4b269a99baf3fe5ed1ce3e247ba8e3f08e
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-07 12:37:10 -0500

    Fix import when no CUDA

--- numbapro/_cuda.py
+++ numbapro/_cuda.py
@@ -1,34 +1,34 @@
 from numbapro import cuda
-import numbapro.cudapy  # import time sideeffect
-from numba.cuda.cudadrv.driver import FuncAttr
+if cuda.is_available:
+    import numbapro.cudapy  # import time sideeffect
+    from numba.cuda.cudadrv.driver import FuncAttr
 
 
+    def calc_occupancy(cc, reg, smem=0, smem_config=None):
+        """Occupancy calculator
 
-def calc_occupancy(cc, reg, smem=0, smem_config=None):
-    """Occupancy calculator
+        Args
+        ----
+        - cc
+            compute capability as a tuple-2 of ints.
+        - reg
+            register used per thread.
+        - smem
+            shared memory used per block.
+        - smem_config
+            (optional) smem configuration
 
-    Args
-    ----
-    - cc
-        compute capability as a tuple-2 of ints.
-    - reg
-        register used per thread.
-    - smem
-        shared memory used per block.
-    - smem_config
-        (optional) smem configuration
+        Returns
+        -------
+        returns an AutoTuner object
 
-    Returns
-    -------
-    returns an AutoTuner object
+        """
+        from .cudadrv import autotune
 
-    """
-    from .cudadrv import autotune
+        usage = FuncAttr(regs=reg, shared=smem, local=0, const=0,
+                         maxthreads=cuda.get_current_device().MAX_THREADS_PER_BLOCK)
+        at = autotune.AutoTuner(cc=cc, info=usage, smem_config=smem_config)
+        return at
 
-    usage = FuncAttr(regs=reg, shared=smem, local=0, const=0,
-                     maxthreads=cuda.get_current_device().MAX_THREADS_PER_BLOCK)
-    at = autotune.AutoTuner(cc=cc, info=usage, smem_config=smem_config)
-    return at
-
-# Install extensions
-cuda.calc_occupancy = calc_occupancy
+    # Install extensions
+    cuda.calc_occupancy = calc_occupancy
