3c1f7e607407852ce9c83b41e945064b5d176cc1
Antoine Pitrou <antoine.pitrou@continuum.io>
2014-07-08 14:26:27 -0400

    Add tests for the IR of some constructs. The tests are easy to write (just dump the IR) but easy to break :-)

--- numba/ir.py
+++ numba/ir.py
@@ -289,7 +289,7 @@ class Const(object):
         self.loc = loc
 
     def __repr__(self):
-        return 'const(%s, %s)' % (type(self.value), self.value)
+        return 'const(%s, %s)' % (type(self.value).__name__, self.value)
 
 
 class Global(object):
@@ -449,7 +449,7 @@ class Block(object):
 
     def dump(self, file=sys.stdout):
         for inst in self.body:
-            print('  ', inst, file=file)
+            print('   ', inst, file=file)
 
     @property
     def terminator(self):
--- numba/tests/test_ir.py
+++ numba/tests/test_ir.py
@@ -1,6 +1,56 @@
 from __future__ import print_function
+
+import textwrap
+
 import numba.unittest_support as unittest
-from numba import ir
+from numba import bytecode, interpreter, ir
+from numba.utils import PYVERSION, StringIO
+
+
+def var_swapping(a, b, c, d, e):
+    """
+    label 0:
+        a.1 = a
+        b.1 = b
+        c.1 = c
+        d.1 = d
+        e.1 = e
+        a.2 = b.1
+        b.2 = a.1
+        c.2 = e.1
+        d.2 = c.1
+        e.2 = d.1
+        $0.8 = a.2 + b.2
+        $0.10 = $0.8 + c.2
+        $0.12 = $0.10 + d.2
+        $0.14 = $0.12 + e.2
+        return $0.14
+    """
+    a, b = b, a
+    c, d, e = e, c, d
+    return a + b + c + d + e
+
+def var_propagate1(a, b):
+    """
+    label 0:
+        a.1 = a
+        b.1 = b
+        $0.3 = a.1 > b.1
+        branch $0.3, 12, 18
+    label 12:
+        $phi21.2 = a.1
+        jump 21
+    label 18:
+        $phi21.2 = b.1
+        jump 21
+    label 21:
+        $const21.1 = const(int, 5)
+        $21.3 = $phi21.2 + $const21.1
+        c = $21.3
+        return c
+    """
+    c = (a if a > b else b) + 5
+    return c
 
 
 class TestIR(unittest.TestCase):
@@ -35,7 +85,40 @@ class TestIR(unittest.TestCase):
             self.fail("Expecting an %s" % ir.RedefinedError)
 
 
+class TestIRDump(unittest.TestCase):
+    """
+    Exercise the IR dump of some constructs.  These tests are fragile
+    (may need to be updated when details of IR generation change, may
+    need to be skipped for some Python versions) but help find out
+    regressions.
+    """
+
+    def get_ir(self, pyfunc):
+        bc = bytecode.ByteCode(func=pyfunc)
+        interp = interpreter.Interpreter(bc)
+        interp.interpret()
+        return interp
+
+    def check_ir_dump(self, pyfunc):
+        interp = self.get_ir(pyfunc)
+        out = StringIO()
+        interp.dump(file=out)
+        expected = textwrap.dedent(pyfunc.__doc__).strip().splitlines()
+        got = out.getvalue().strip().splitlines()
+        self.assertEqual(got, expected,
+                         "dump might need to be refreshed; here is the "
+                         "actual dump:\n%s\n" % (out.getvalue()))
+
+    def test_var_swapping(self):
+        # This exercises removal of unused temporaries.
+        self.check_ir_dump(var_swapping)
+
+    @unittest.skipIf(PYVERSION < (2, 7),
+                     "2.6 bytecode gives slightly different IR dump")
+    def test_var_propagate1(self):
+        # This exercises generation of phi nodes.
+        self.check_ir_dump(var_propagate1)
+
 
 if __name__ == '__main__':
     unittest.main()
-
