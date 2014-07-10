1df1136b5eaa3fd5ffffcf77eec22dc47d13daa0
Oscar Villellas <oscar.villellas@continuum.io>
2014-07-10 11:18:50 +0200

    a bit of reorganization and dead-code removal

--- numba/targets/npyimpl.py
+++ numba/targets/npyimpl.py
@@ -138,44 +138,6 @@ def _prepare_argument(ctxt, bld, inp, tyinp, where='input operand'):
         raise TypeError('unknown type for {0}'.format(where))
 
 
-# _externs will be used to register ufuncs.
-# each tuple contains the ufunc to be translated. That ufunc will be converted to
-# an equivalent loop that calls the function in the npymath support module (registered
-# as external function as "numba.npymath."+func
-_externs = [
-    (numpy.exp, "exp"),
-    (numpy.exp2, "exp2"),
-    (numpy.expm1, "expm1"),
-    (numpy.log, "log"),
-    (numpy.log2, "log2"),
-    (numpy.log10, "log10"),
-    (numpy.log1p, "log1p"),
-    (numpy.deg2rad, "deg2rad"),
-    (numpy.rad2deg, "rad2deg"),
-    (numpy.sin, "sin"),
-    (numpy.cos, "cos"),
-    (numpy.tan, "tan"),
-    (numpy.sinh, "sinh"),
-    (numpy.cosh, "cosh"),
-    (numpy.tanh, "tanh"),
-    (numpy.arcsin, "asin"),
-    (numpy.arccos, "acos"),
-    (numpy.arctan, "atan"),
-    (numpy.arcsinh, "asinh"),
-    (numpy.arccosh, "acosh"),
-    (numpy.arctanh, "atanh"),
-    (numpy.sqrt, "sqrt"),
-    (numpy.floor, "floor"),
-    (numpy.ceil, "ceil"),
-    (numpy.trunc, "trunc"),
-]
-
-_externs_2 = [
-    (numpy.arctan2, "atan2"),
-    (numpy.logaddexp, "logaddexp"),
-    (numpy.logaddexp2, "logaddexp2"),
-]
-
 def npy_math_extern(fn, fnty):
     setattr(npy, fn, fn)
     fn_sym = eval("npy."+fn)
@@ -207,82 +169,6 @@ def npy_math_extern(fn, fnty):
             cast_vals = args if ty_dst == ty_src else [context.cast(builder, val, ty_src, ty_dst) for val in args]
             sig = typing.signature(*[ty_dst]*(len(cast_vals)+1))
             return ref_impl(context, builder, sig, cast_vals)
-
-
-def numpy_binary_ufunc(context, builder, sig, args, funckey, divbyzero=False,
-                       asfloat=False, true_divide=False):
-    [tyinp1, tyinp2, tyout] = sig.args
-    [inp1, inp2, out] = args
-
-    i1ary = _prepare_argument(context, builder, inp1, tyinp1)
-    i2ary = _prepare_argument(context, builder, inp2, tyinp2)
-    oary  = _prepare_argument(context, builder, out, tyout)
-
-    # based only on the first operand?
-    promote_type = types.float64 if asfloat else _default_promotion_for_type(i1ary.base_type)
-    result_type = promote_type
-
-    # Temporary hack for __ftol2 llvm bug. Don't allow storing
-    # float results in uint64 array on windows.
-    if result_type in types.real_domain and \
-            tyout.dtype is types.uint64 and \
-            sys.platform.startswith('win32'):
-        raise TypeError('Cannot store result in uint64 array')
-
-    sig = typing.signature(result_type, promote_type, promote_type)
-
-    fnwork = context.get_function(funckey, sig)
-    intpty = context.get_value_type(types.intp)
-
-    inp1_indices = i1ary.create_iter_indices() if i1ary else None
-    inp2_indices = i2ary.create_iter_indices() if i2ary else None
-
-    loopshape = oary.shape
-    with cgutils.loop_nest(builder, loopshape, intp=intpty) as indices:
-        inp1_indices.update_indices(indices, '1')
-        inp2_indices.update_indices(indices, '2')
-
-        x = i1ary.load_data(inp1_indices.as_values())
-        y = i2ary.load_data(inp2_indices.as_values())
-
-        if divbyzero:
-            # Handle division
-            iszero = cgutils.is_scalar_zero(builder, y)
-            with cgutils.ifelse(builder, iszero, expect=False) as (then,
-                                                                   orelse):
-                with then:
-                    # Divide by zero
-                    if ((i1ary.base_type in types.real_domain or
-                            i2ary.base_type in types.real_domain) or
-                            not numpy_support.int_divbyzero_returns_zero) or \
-                            true_divide:
-                        # If y is float and is 0 also, return Nan; else
-                        # return Inf
-                        outltype = context.get_data_type(result_type)
-                        shouldretnan = cgutils.is_scalar_zero(builder, x)
-                        nan = Constant.real(outltype, float("nan"))
-                        inf = Constant.real(outltype, float("inf"))
-                        tempres = builder.select(shouldretnan, nan, inf)
-                        res = context.cast(builder, tempres, result_type,
-                                           tyout.dtype)
-                    elif tyout.dtype in types.signed_domain and \
-                            not numpy_support.int_divbyzero_returns_zero:
-                        res = Constant.int(context.get_data_type(tyout.dtype),
-                                           0x1 << (y.type.width-1))
-                    else:
-                        res = Constant.null(context.get_data_type(tyout.dtype))
-
-                    oary.store_data(indices, res)
-                with orelse:
-                    # Normal
-                    d_x = context.cast(builder, x, i1ary.base_type, promote_type)
-                    d_y = context.cast(builder, y, i2ary.base_type, promote_type)
-                    tempres = fnwork(builder, [d_x, d_y])
-                    res = context.cast(builder, tempres, result_type, tyout.dtype)
-                    oary.store_data(indices, res)
-        else:
-            raise LoweringError("codepath no longer supported")
-    return out
 
 
 def numpy_ufunc_kernel(context, builder, sig, args, kernel_class):
@@ -439,26 +325,6 @@ def _division(operator, legacy_divide=False):
 ################################################################################
 # Helper functions that register the ufuncs
 
-def register_binary_ufunc(ufunc, operator, asfloat=False, divbyzero=False,
-                          true_divide=False):
-
-    def binary_ufunc(context, builder, sig, args):
-        return numpy_binary_ufunc(context, builder, sig, args, operator,
-                                  asfloat=asfloat, divbyzero=divbyzero,
-                                  true_divide=true_divide)
-
-    register(implement(ufunc, types.Kind(types.Array), types.Kind(types.Array),
-        types.Kind(types.Array))(binary_ufunc))
-    for ty in types.number_domain:
-        register(implement(ufunc, ty, types.Kind(types.Array),
-            types.Kind(types.Array))(binary_ufunc))
-        register(implement(ufunc, types.Kind(types.Array), ty,
-            types.Kind(types.Array))(binary_ufunc))
-    for ty1, ty2 in itertools.product(types.number_domain, types.number_domain):
-        register(implement(ufunc, ty1, ty2,
-            types.Kind(types.Array))(binary_ufunc))
-
-
 def register_unary_ufunc_kernel(ufunc, kernel):
     def unary_ufunc(context, builder, sig, args):
         return numpy_ufunc_kernel(context, builder, sig, args, kernel)
@@ -497,6 +363,38 @@ _float_unary_sig = typing.signature(types.float64, types.float64)
 _float_binary_sig = typing.signature(types.float64, types.float64, types.float64)
 
 
+# _externs will be used to register ufuncs.
+# each tuple contains the ufunc to be translated. That ufunc will be converted to
+# an equivalent loop that calls the function in the npymath support module (registered
+# as external function as "numba.npymath."+func
+_externs = [
+    (numpy.exp, "exp"),
+    (numpy.exp2, "exp2"),
+    (numpy.expm1, "expm1"),
+    (numpy.log, "log"),
+    (numpy.log2, "log2"),
+    (numpy.log10, "log10"),
+    (numpy.log1p, "log1p"),
+    (numpy.deg2rad, "deg2rad"),
+    (numpy.rad2deg, "rad2deg"),
+    (numpy.sin, "sin"),
+    (numpy.cos, "cos"),
+    (numpy.tan, "tan"),
+    (numpy.sinh, "sinh"),
+    (numpy.cosh, "cosh"),
+    (numpy.tanh, "tanh"),
+    (numpy.arcsin, "asin"),
+    (numpy.arccos, "acos"),
+    (numpy.arctan, "atan"),
+    (numpy.arcsinh, "asinh"),
+    (numpy.arccosh, "acosh"),
+    (numpy.arctanh, "atanh"),
+    (numpy.sqrt, "sqrt"),
+    (numpy.floor, "floor"),
+    (numpy.ceil, "ceil"),
+    (numpy.trunc, "trunc"),
+]
+
 for sym, name in _externs:
     npy_math_extern(name, _float_unary_function_type)
     register_unary_ufunc_kernel(sym, _function_with_cast(getattr(npy, name), _float_unary_sig))
@@ -521,6 +419,13 @@ if not PYVERSION >= (3, 0):
 register_binary_ufunc_kernel(numpy.floor_divide, _division('//'))
 register_binary_ufunc_kernel(numpy.true_divide, _division('/'))
 register_binary_ufunc_kernel(numpy.power, _function_with_cast('**', _float_binary_sig))
+
+
+_externs_2 = [
+    (numpy.arctan2, "atan2"),
+    (numpy.logaddexp, "logaddexp"),
+    (numpy.logaddexp2, "logaddexp2"),
+]
 
 for sym, name in _externs_2:
     npy_math_extern(name, _float_binary_function_type)
