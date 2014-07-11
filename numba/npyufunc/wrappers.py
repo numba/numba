e073d282a33f5fb890574622fa2bad15e5ad06c1
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-11 12:19:58 -0500

    Fix invalid decref

--- numba/npyufunc/wrappers.py
+++ numba/npyufunc/wrappers.py
@@ -367,7 +367,8 @@ def _prepare_call_to_object_mode(context, builder, func, signature, args):
     pyapi = context.get_python_api(builder)
 
     # Release returned object
-    pyapi.decref(retval)
+    with cgutils.if_likely(builder, builder.not_(status.err)):
+        pyapi.decref(retval)
 
     builder.branch(bb_core_return)
     # At return block
