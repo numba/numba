867524c25ed1bb57eea8bf10e2b5986e51392dd9
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-11 15:52:03 -0500

    Complete radixselect

--- radixsort/radixselect.cu
+++ radixsort/radixselect.cu
@@ -10,11 +10,110 @@ For each block:
 #include <iostream>
 #include "radixutils.h"
 
+void
+radix_select(
+    uint8_t  *dev_data,
+    uint8_t  *dev_sorted,
+    unsigned *dev_hist,
+    unsigned *dev_bucket_total,
+    unsigned *dev_indices,
+    unsigned ct_block,
+    unsigned ct_data,
+    unsigned sz_data,
+    unsigned sz_bucket_total,
+    unsigned ct_bucket_total,
+    unsigned stride,
+    unsigned select_count,
+    unsigned offset)
+{
+    using namespace std;
+    unsigned bucket_total[ct_bucket_total];
+
+    cu_build_histogram<<<ct_block, BUCKET_SIZE>>>(
+        dev_data,
+        dev_hist,
+        stride,
+        offset,
+        ct_data
+    );
+    ASSERT_CUDA_LAST_ERROR();
+
+    cu_scan_histogram<<<BUCKET_SIZE, SCAN_HISTOGRAM_BLOCK_SIZE>>>(
+        dev_hist,
+        dev_bucket_total,
+        ct_block
+    );
+    ASSERT_CUDA_LAST_ERROR();
+
+    cudaMemcpy(bucket_total, dev_bucket_total, sz_bucket_total,
+               cudaMemcpyDeviceToHost);
+    ASSERT_CUDA_LAST_ERROR();
+
+    cu_scan_bucket_index<<<1, BUCKET_SIZE>>>(dev_bucket_total);
+    ASSERT_CUDA_LAST_ERROR();
+
+
+    compute_indices(
+        dev_data,
+        dev_indices,
+        dev_hist,
+        dev_bucket_total,
+        ct_data,
+        stride,
+        offset,
+        ct_block
+    );
+
+    cu_scatter<<<ct_block, BUCKET_SIZE>>>(
+        dev_data,
+        dev_sorted,
+        dev_indices,
+        ct_data,
+        stride
+    );
+
+    ASSERT_CUDA_LAST_ERROR();
+
+    cudaMemcpy(dev_data, dev_sorted, sz_data, cudaMemcpyDeviceToDevice);
+    ASSERT_CUDA_LAST_ERROR();
+
+    unsigned total_data = 0;
+
+    if (offset == 0) return;
+
+    for(unsigned i=0; i<ct_bucket_total && total_data < select_count; ++i) {
+
+        unsigned sub_ct_data = bucket_total[i];
+        unsigned sub_ct_block = (sub_ct_data + (BUCKET_SIZE-1)) / BUCKET_SIZE;
+        unsigned sub_sz_data = sub_ct_data * stride;
+        unsigned sub_data_offset = total_data * stride;
+
+        radix_select(sub_data_offset + dev_data,
+                     sub_data_offset + dev_sorted,
+                     dev_hist,
+                     dev_bucket_total,
+                     dev_indices,
+                     sub_ct_block,
+                     sub_ct_data,
+                     sub_sz_data,
+                     sz_bucket_total,
+                     ct_bucket_total,
+                     stride,
+                     select_count,
+                     offset - 1);
+
+        total_data += sub_ct_data;
+    }
+}
+
+
 int
 main()
 {
 	using std::cout;
 	using std::endl;
+
+    const int select_count = 100;
 
     typedef uint32_t data_type;
 
@@ -23,7 +122,7 @@ main()
 	unsigned ct_data = 1000;
 	unsigned sz_data = sizeof(data_type) * ct_data;
 
-    const unsigned ct_block = (ct_data + (BUCKET_SIZE-1)) / BUCKET_SIZE;
+    unsigned ct_block = (ct_data + (BUCKET_SIZE-1)) / BUCKET_SIZE;
     cout << "ct_block = " << ct_block << '\n';
 
 	unsigned ct_hist = ct_block * BUCKET_SIZE;
@@ -36,7 +135,6 @@ main()
 
 	data_type *data = new data_type[ct_data];
 	unsigned *hist = new unsigned[ct_hist];
-    unsigned *bucket_total = new unsigned[ct_bucket_total];
 
 	for (unsigned i=0; i<ct_data; ++i) {
 		data[i] = ct_data - i - 1;
@@ -59,68 +157,34 @@ main()
 	cudaMemcpy(dev_data, data, sz_data, cudaMemcpyHostToDevice);
 	ASSERT_CUDA_LAST_ERROR();
 
-
     // compute
 
-    for (unsigned r=0; r < 4; ++r) {
-        unsigned offset = 1;
+    radix_select(dev_data,
+                 dev_sorted,
+                 dev_hist,
+                 dev_bucket_total,
+                 dev_indices,
+                 ct_block,
+                 ct_data,
+                 sz_data,
+                 sz_bucket_total,
+                 ct_bucket_total,
+                 stride,
+                 select_count,
+                 3);
 
-    	cu_build_histogram<<<ct_block, BUCKET_SIZE>>>(
-    		dev_data,
-    		dev_hist,
-    		stride,
-    		offset,
-    		ct_data
-    	);
-    	ASSERT_CUDA_LAST_ERROR();
-
-        cu_scan_histogram<<<BUCKET_SIZE, SCAN_HISTOGRAM_BLOCK_SIZE>>>(
-            dev_hist,
-            dev_bucket_total,
-            ct_block
-        );
-        ASSERT_CUDA_LAST_ERROR();
-
-
-        cu_scan_bucket_index<<<1, BUCKET_SIZE>>>(dev_bucket_total);
-        ASSERT_CUDA_LAST_ERROR();
-
-        compute_indices(
-            dev_data,
-            dev_indices,
-            dev_hist,
-            dev_bucket_total,
-            ct_data,
-            stride,
-            offset,
-            ct_block
-        );
-
-        cu_scatter<<<ct_block, BUCKET_SIZE>>>(
-            dev_data,
-            dev_sorted,
-            dev_indices,
-            ct_data,
-            stride
-        );
-
-        ASSERT_CUDA_LAST_ERROR();
-
-        cudaMemcpy(dev_data, dev_sorted, sz_data, cudaMemcpyDeviceToDevice);
-        ASSERT_CUDA_LAST_ERROR();
-    }
     // write back
 
     cudaMemcpy(data, dev_sorted, sz_data, cudaMemcpyDeviceToHost);
     ASSERT_CUDA_LAST_ERROR();
 
     cout << "sorted\n";
-    for (unsigned i = 0; i < ct_data; ++i) {
+    for (unsigned i = 0; i < select_count; ++i) {
         cout << i << ' ' << data[i] << '\n';
-        // if(data[i] != i) {
-        //     cout << "error at i = " << i << " = " << data[i] << endl;
-        //     exit(1);
-        // }
+        if(data[i] != i) {
+            cout << "error at i = " << i << " = " << data[i] << endl;
+            exit(1);
+        }
     }
 
 
