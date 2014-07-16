d244809373ae2484944ecdc0344dd7d356b228ed
Scott Chadde <scott.chadde@continuum.io>
2014-07-16 16:32:23 -0500

    added debug stuff -- to be taken out in the final version

--- numba/cgutils.py
+++ numba/cgutils.py
@@ -209,10 +209,10 @@ def if_likely(builder, pred):
 
 
 @contextmanager
-def ifthen(builder, pred):
+def ifthen(builder, pred, debug=''):
     bb = builder.basic_block
-    bbif = append_basic_block(builder, bb.name + '.if')
-    bbend = append_basic_block(builder, bb.name + '.endif')
+    bbif = append_basic_block(builder, bb.name + '.if' + debug)
+    bbend = append_basic_block(builder, bb.name + '.endif' + debug)
     builder.cbranch(pred, bbif, bbend)
 
     with goto_block(builder, bbif):
@@ -291,6 +291,66 @@ def for_range(builder, count, intp):
     index.add_incoming(incr, bbbody)
 
     builder.position_at_end(bbend)
+
+
+@contextmanager
+def for_range_slice(builder, start, stop, step, intp):
+    """start """
+    bbcond = append_basic_block(builder, "for.cond")
+    bbbody = append_basic_block(builder, "for.body")
+    bbend = append_basic_block(builder, "for.end")
+
+    bbstart = builder.basic_block
+    builder.branch(bbcond)
+
+    #STEP = Constant.int(intp, 2)
+
+    with goto_block(builder, bbcond):
+        index = builder.phi(intp, name="loop.index")
+        pred = builder.icmp(lc.ICMP_SLT, index, stop)
+        builder.cbranch(pred, bbbody, bbend)
+
+    with goto_block(builder, bbbody):
+        yield index
+        bbbody = builder.basic_block
+        incr = builder.add(index, step)
+        terminate(builder, bbcond)
+
+    index.add_incoming(start, bbstart)
+    index.add_incoming(incr, bbbody)
+
+    builder.position_at_end(bbend)
+
+#decrement version of for loop, start > stop, and step < 0
+# as step < 0 we add rather than sub
+@contextmanager
+def for_range_slice_dec(builder, start, stop, step, intp):
+    """start """
+    bbcond = append_basic_block(builder, "for.cond")
+    bbbody = append_basic_block(builder, "for.body")
+    bbend = append_basic_block(builder, "for.end")
+
+    bbstart = builder.basic_block
+    builder.branch(bbcond)
+
+    #STEP = Constant.int(intp, 2)
+
+    with goto_block(builder, bbcond):
+        index = builder.phi(intp, name="loop.index")
+        pred = builder.icmp(lc.ICMP_SGT, index, stop)
+        builder.cbranch(pred, bbbody, bbend)
+
+    with goto_block(builder, bbbody):
+        yield index
+        bbbody = builder.basic_block
+        incr = builder.add(index, step)
+        terminate(builder, bbcond)
+
+    index.add_incoming(start, bbstart)
+    index.add_incoming(incr, bbbody)
+
+    builder.position_at_end(bbend)
+
 
 
 @contextmanager
