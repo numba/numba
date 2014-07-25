3d380dcaa1087f5400b9b8a91936a3deab47ac7e
Scott Chadde <scott.chadde@continuum.io>
2014-07-25 11:43:26 -0500

    refactored test to work with Python 2.6, 2.7, 3.3, and 3.4

--- numba/tests/test_llvm_version_check.py
+++ numba/tests/test_llvm_version_check.py
@@ -3,31 +3,34 @@ from numba import unittest_support as unittest
 import sys
 
 class TestLlvmVersion(unittest.TestCase):
-
     def test_llvm_version(self):
+        # test the system its running on
         import llvm
         import numba
         self.assertTrue(numba.__version__)
-        
-        if sys.version_info >= (3, 0):
-            #git version check
-            llvm.__version = '0.12.6-10-g92584ed'
-            imp.reload(numba)
-            self.assertTrue(numba.__version__)
-        
-            with self.assertRaises(SystemExit):
-                llvm.__version__ = '0.12.5' # llvmpy has to be >= 0.12.6 
-                imp.reload(numba)
-        else: 
-            #git version check
-            llvm.__version = '0.12.6-10-g92584ed'
+     
+        if sys.version_info >= (3, 4):
+            from importlib import reload
+        elif (sys.version_info[0], sys.version_info[1]) == (3, 3):
+            from imp import reload
+        else:
+            from __builtin__ import reload 
+
+        # explicitly test all 4 cases of version string
+        version_pass = '0.12.6'
+        git_version_pass = '0.12.6-10-g92584ed'
+        version_fail = '0.12.5'
+        git_version_fail = '0.12.5-10-g92584ed'
+        ver_pass = (version_pass, git_version_pass)
+        ver_fail = (version_fail, git_version_fail)
+        for v in ver_pass:
+            llvm.__version = v
             reload(numba)
             self.assertTrue(numba.__version__)
-        
+        for v in ver_fail: 
             with self.assertRaises(SystemExit):
-                llvm.__version__ = '0.12.5' # llvmpy has to be >= 0.12.6 
+                llvm.__version__ = v
                 reload(numba)
-        
 
 if __name__ == '__main__':
     unittest.main()
