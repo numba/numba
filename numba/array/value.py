14304e5cb9b27379ce30c2ac334477d5d5d9116a
Jay Bourque <jay.bourque@continuum.io>
2014-07-29 16:28:37 -0500

    Change division ufunc to divide; remove duplicate ufunc names from ufunc list

--- numba/array/value.py
+++ numba/array/value.py
@@ -5,51 +5,6 @@ import operator
 import numpy
 
 
-op_implementation = {
-    'sin':'numpy.sin',
-    'cos':'numpy.cos',
-    'tan':'numpy.tan',
-    'arcsin':'numpy.asin',
-    'arccos':'numpy.acos',
-    'arctan':'numpy.atan',
-    'degrees':'numpy.degrees',
-    'deg2rad':'numpy.radians',
-    'rad2deg':'numpy.degrees',
-    'radians':'numpy.radians',
-    'sinh':'numpy.sinh',
-    'cosh':'numpy.cosh',
-    'tanh':'numpy.tanh',
-    'arcsinh':'numpy.asinh',
-    'arccosh':'numpy.acosh',
-    'arctanh':'numpy.atanh',
-    'floor':'numpy.floor',
-    'ceil':'numpy.ceil',
-    'trunc':'numpy.trunc',
-    'rint':'numpy.rint',
-    'exp':'numpy.exp',
-    'exp2':'numpy.exp2',
-    'expm1':'numpy.expm1',
-    'log':'numpy.log',
-    'log10':'numpy.log10',
-    'log1p':'numpy.log1p',
-    'log2':'numpy.log2',
-    'sqrt':'numpy.sqrt',
-    'fabs':'numpy.fabs',
-    'abs':'numpy.abs',
-    'negative':'operator.neg',
-    'hypot':'numpy.hypot',
-    'arctan2':'numpy.atan2',
-    'logaddexp':'numpy.logaddexp',
-    'add':'operator.add',
-    'subtract':'operator.sub',
-    'multiply':'operator.mul',
-    'power':'operator.pow',
-    'division':'operator.div',
-    'floor_division':'operator.floordiv',
-    'greater':'operator.gt',
-}
-
-
 class Value(Case):
 
     @of('ArrayNode(data, owners)')
@@ -72,11 +27,13 @@ class Value(Case):
 
     @of('UnaryOperation(operand, op_str)')
     def unary_operation(self, operand, op_str):
-        return eval(op_implementation[op_str])(Value(operand))
+        op = getattr(numpy, op_str)
+        return op(Value(operand))
 
     @of('BinaryOperation(lhs, rhs, op_str)')
     def binary_operation(self, lhs, rhs, op_str):
-        return eval(op_implementation[op_str])(Value(lhs), Value(rhs))
+        op = getattr(numpy, op_str)
+        return op(Value(lhs), Value(rhs))
 
     @of('ArrayAssignOperation(operand, key, value)')
     def array_assign_operation(self, operand, key, value):
