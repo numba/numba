2e1e3889df253c9ccc378f7031bf63ee348614d6
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-30 13:46:12 -0500

    Make llvmpy version checking be more tolerance to the version formating

--- numba/__init__.py
+++ numba/__init__.py
@@ -2,9 +2,11 @@
 Expose top-level symbols that are safe for import *
 """
 from __future__ import print_function, division, absolute_import
+import re
 from . import testing, decorators
 from ._version import get_versions
 from . import special, types, config
+
 # Re-export typeof
 from .special import *
 from .pycc.decorators import export, exportmany
@@ -40,15 +42,28 @@ exportmany
 cuda
 """.split() + types.__all__ + special.__all__
 
-import llvm
-# git/travis builds append a dash and some other stuff after the version
-# For example, '0.12.6-10-g77645ed' 
-llvm_ver = llvm.__version__.split('-') 
-llvm_ver = llvm_ver[0].split('.')
-if (int(llvm_ver[0]), int(llvm_ver[1]), int(llvm_ver[2])) >= (0, 12, 6):
-    pass
-else:
-    import sys
-    print("Numba requires at least version 0.12.6 of llvmpy.\nPlease update your version of llvmpy.")
-    sys.exit()
 
+def _sentry_llvm_version():
+    """
+    Make sure we meet min llvmpy version
+    """
+    import sys
+    import warnings
+    import llvm
+    min_version = (0, 12, 6)
+
+    # Only look at the the major, minor and bugfix version numbers.
+    # Ignore other stuffs
+    regex = re.compile(r'(\d+)\.(\d+).(\d+)')
+    m = regex.match(llvm.__version__)
+    if m:
+        ver = tuple(map(int, m.groups()))
+        if ver < min_version:
+            print("Numba requires at least version %d.%d.%d of llvmpy"
+                  ".\nPlease update your version of llvmpy." % min_version)
+            sys.exit()
+    else:
+        # Not matching?
+        warnings.warns("llvmpy version format not recognized!")
+
+_sentry_llvm_version()
