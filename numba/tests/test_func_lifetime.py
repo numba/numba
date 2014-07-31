eb564a5652209e70599c68c45e0e5356e4eab8ad
Antoine Pitrou <antoine.pitrou@continuum.io>
2014-07-31 11:02:19 -0400

    Add failing test (because of separate lifetime issues?)

--- numba/tests/test_func_lifetime.py
+++ numba/tests/test_func_lifetime.py
@@ -10,13 +10,22 @@ from numba import jit, types
 from .support import TestCase
 
 
-def global_func(x):
+class Dummy(object):
+
+    def __add__(self, other):
+        return other + 5
+
+
+def global_usecase1(x):
     return x + 1
+
+def global_usecase2():
+    return global_obj + 1
 
 
 class TestFuncLifetime(TestCase):
     """
-    Test the lifetime of compiled function objects.
+    Test the lifetime of compiled function objects and their dependencies.
     """
     # NOTE: there's a test for closure lifetime in test_closure
 
@@ -39,7 +48,7 @@ class TestFuncLifetime(TestCase):
         self.check_local_func_lifetime(nopython=True)
 
     def check_global_func_lifetime(self, **jitargs):
-        c_f = jit(**jitargs)(global_func)
+        c_f = jit(**jitargs)(global_usecase1)
         self.assertPreciseEqual(c_f(1), 2)
 
         wr = weakref.ref(c_f)
@@ -52,6 +61,28 @@ class TestFuncLifetime(TestCase):
 
     def test_global_func_lifetime_npm(self):
         self.check_global_func_lifetime(nopython=True)
+
+    def check_global_obj_lifetime(self, **jitargs):
+        # Since global objects can be recorded for typing purposes,
+        # check that they are not kept around after they are removed
+        # from the globals.
+        global global_obj
+        global_obj = Dummy()
+
+        c_f = jit(**jitargs)(global_usecase2)
+        self.assertPreciseEqual(c_f(), 6)
+
+        # XXX global_obj will survive, perhaps because of buggy code
+        # generation.
+        refs = [weakref.ref(obj) for obj in (c_f, global_obj)]
+        obj = c_f = global_obj = None
+        gc.collect()
+        self.assertEqual([wr() for wr in refs], [None] * len(refs))
+
+    @unittest.expectedFailure
+    def test_global_obj_lifetime(self):
+        self.check_global_obj_lifetime(forceobj=True)
+
 
 
 if __name__ == '__main__':
