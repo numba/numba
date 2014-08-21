66564b26bcd608ee87a1ad3df84e92d1f8119162
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-08-21 12:43:09 -0500

    add build script

---
+++ build.py
@@ -0,0 +1,33 @@
+# A script to build external dependencies
+
+import os
+import subprocess
+
+
+NVCC = 'nvcc'
+CUB_INCLUDE = '-Ithirdparty/cub'
+
+GENCODE_SMXX = "-gencode arch=compute_{CC},code=sm_{CC}"
+GENCODE_SM20 = GENCODE_SMXX.format(CC=20)
+GENCODE_SM30 = GENCODE_SMXX.format(CC=30)
+GENCODE_SM35 = GENCODE_SMXX.format(CC=35)
+GENCODE_FLAGS = ' '.join([GENCODE_SM20, GENCODE_SM30, GENCODE_SM35])
+
+
+def run_shell(cmd):
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
+
+def build_radixsort():
+    basepath = 'numbapro/cudalib/sorting/details/'
+    output = os.path.join(basepath, 'radixsort.so')
+    inputs = os.path.join(basepath, 'cubradixsort.cu')
+    argtemp = '-m64 --compiler-options "-fPIC" {} -O3 {} --shared -o {} {}'
+    args = argtemp.format(CUB_INCLUDE, GENCODE_FLAGS, output, inputs)
+    cmd = ' '.join([NVCC, args])
+    run_shell(cmd)
+
+
+if __name__ == '__main__':
+    build_radixsort()
