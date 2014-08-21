983373ed7117327be4da402c7fc770e0ece1cd57
Antoine Pitrou <antoine.pitrou@continuum.io>
2014-08-21 13:12:23 -0400

    Remove redundant import

--- numba/numpy_support.py
+++ numba/numpy_support.py
@@ -166,8 +166,6 @@ def supported_ufunc_loop(ufunc, loop):
     legacy and when implementing new ufuncs the ufunc_db should be preferred,
     as it allows for a more fine-grained incremental support.
     """
-    from .targets import ufunc_db
-
     loop_sig = loop.ufunc_sig
     try:
         # check if the loop has a codegen description in the
