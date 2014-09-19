19ffc4ced7d7ce49918dc56fe79a8f4ed71ad886
Jay Bourque <jay.bourque@continuum.io>
2014-09-18 19:53:23 -0500

    Add tests for old numba regressions

---
+++ numba/tests/test_vectorization_type_inference.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+from numba import vectorize, jit, bool_, double, int_, float_, typeof, int8
+import numba.unittest_support as unittest
+import numpy as np
+
+
+def add(a, b):
+    return a + b
+
+
+def func(dtypeA, dtypeB):
+    A = np.arange(10, dtype=dtypeA)
+    B = np.arange(10, dtype=dtypeB)
+    return typeof(vector_add(A, B))
+
+
+class TestVectTypeInfer(unittest.TestCase):
+    def test_type_inference(self):
+        global vector_add
+        vector_add = vectorize([
+            bool_(double, int_),
+            double(double, double),
+            float_(double, float_),
+        ])(add)
+
+        cfunc = jit(func)
+
+        self.assertEqual(cfunc(np.dtype(np.float64), np.dtype('i')), int8[:])
+        self.assertEqual(cfunc(np.dtype(np.float64), np.dtype(np.float64)),
+                         double[:])
+        self.assertEqual(cfunc(np.dtype(np.float64), np.dtype(np.float32)),
+                         float_[:])
+
+
+if __name__ == '__main__':
+    unittest.main()
