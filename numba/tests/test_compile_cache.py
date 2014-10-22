6b13cf444a56ae1c4d82f9ddaba93e61bd9d68a5
Graham Markall <graham.markall@continuum.io>
2014-10-22 16:57:46 +0100

    Add additional check with different type
    Add an additional check for the same function with a different
    signature, in order to ensure that the cached version isn't
    incorrectly picked up.
    
    Also simplify len(list(...)) to len(...)

--- numba/tests/test_compile_cache.py
+++ numba/tests/test_compile_cache.py
@@ -16,7 +16,7 @@ class TestCompileCache(unittest.TestCase):
         i32 = lc.Type.int(32)
         llvm_fnty = lc.Type.function(i32, [i32])
         module = lc.Module.new("test_module")
-        function = module.get_or_insert_function(llvm_fnty, name='test_function')
+        function = module.get_or_insert_function(llvm_fnty, name='test_fn')
         assert function.is_declaration
         entry_block = function.append_basic_block('entry')
         builder = lc.Builder.new(entry_block)
@@ -26,21 +26,35 @@ class TestCompileCache(unittest.TestCase):
         context = cpu.CPUContext(typing_context).localized()
 
         # Ensure the cache is empty to begin with
-        self.assertEqual(0, len(list(context.cached_internal_func.keys())))
+        self.assertEqual(0, len(context.cached_internal_func.keys()))
         
         # After one compile, it should contain one entry
         context.compile_internal(builder, times2, sig, function.args)
-        self.assertEqual(1, len(list(context.cached_internal_func.keys())))
+        self.assertEqual(1, len(context.cached_internal_func.keys()))
 
         # After a second compilation of the same thing, it should still contain
         # one entry
         context.compile_internal(builder, times2, sig, function.args)
-        self.assertEqual(1, len(list(context.cached_internal_func.keys())))
+        self.assertEqual(1, len(context.cached_internal_func.keys()))
 
         # After compilation of another function, the cache should have grown by
         # one more.
         context.compile_internal(builder, times3, sig, function.args)
-        self.assertEqual(2, len(list(context.cached_internal_func.keys())))
+        self.assertEqual(2, len(context.cached_internal_func.keys()))
+
+        sig2 = typing.signature(types.float64, types.float64)
+        f64 = lc.Type.double()
+        llvm_fnty2 = lc.Type.function(f64, [f64])
+        function2 = module.get_or_insert_function(llvm_fnty2, name='test_fn_2')
+        assert function2.is_declaration
+        entry_block2 = function2.append_basic_block('entry')
+        builder2 = lc.Builder.new(entry_block2)
+        
+        # Ensure that the same function with a different signature does not
+        # reuse an entry from the cache in error
+        context.compile_internal(builder2, times3, sig2, function2.args)
+        self.assertEqual(3, len(context.cached_internal_func.keys()))
+
 
 if __name__ == '__main__':
     unittest.main()
