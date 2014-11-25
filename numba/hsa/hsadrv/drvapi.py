e51b37a78fe1b9e05962dd16e36227a25bfd9149
Oscar Villellas <oscar.villellas@continuum.io>
2014-11-25 17:59:24 +0100

    exposed callback type for hsa_iter_agents

--- numba/hsa/hsadrv/drvapi.py
+++ numba/hsa/hsadrv/drvapi.py
@@ -11,6 +11,11 @@ hsa_agent_feature_t = ctypes.c_int # enum
 hsa_device_type_t = ctypes.c_int # enum
 hsa_agent_info_t = ctypes.c_int # enum
 
+
+HSA_ITER_AGENT_CALLBACK_FUNC = ctypes.CFUNCTYPE(hsa_status_t, hsa_agent_t,
+                                                ctypes.py_object), 
+
+
 API_PROTOTYPES = {
     # hsa_status_t hsa_init(void)
     'hsa_init': (hsa_status_t, ),
@@ -19,9 +24,7 @@ API_PROTOTYPES = {
     'hsa_shut_down': (hsa_status_t, ),
 
     # hsa_status_t hsa_iterate_agents(hsa_status_t(*)(hsa_agent_t, void*), void*)
-    'hsa_iterate_agents': (hsa_status_t,
-                           ctypes.CFUNCTYPE(hsa_status_t, hsa_agent_t, 
-                                            ctypes.py_object), 
+    'hsa_iterate_agents': (hsa_status_t, HSA_ITER_AGENT_CALLBACK_FUNC,
                            ctypes.py_object),
     
 }
