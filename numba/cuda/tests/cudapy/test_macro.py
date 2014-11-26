488d603b829cd62dbab9c456deff5976fad6d8a1
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-11-26 11:16:27 -0600

    Fix test

--- numba/cuda/tests/cudapy/test_macro.py
+++ numba/cuda/tests/cudapy/test_macro.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, division, absolute_import
 import numpy as np
-from timeit import default_timer as time
 from numba import cuda, float32
 from numba.cuda.testing import unittest
+from numba.macro import MacroError
 
 GLOBAL_CONSTANT = 5
 GLOBAL_CONSTANT_2 = 6
@@ -60,8 +60,11 @@ class TestMacro(unittest.TestCase):
         udt(self.getarg())
 
     def test_global_build_list(self):
-        udt = cuda.jit((float32[:, :],))(udt_global_build_tuple)
-        udt(self.getarg())
+        with self.assertRaises(MacroError) as raises:
+            cuda.jit((float32[:, :],))(udt_global_build_list)
+
+        self.assertIn("invalid type for shape; got <class 'list'>",
+                      str(raises.exception))
 
     def test_global_constant_tuple(self):
         udt = cuda.jit((float32[:, :],))(udt_global_constant_tuple)
@@ -69,16 +72,14 @@ class TestMacro(unittest.TestCase):
 
     def test_invalid_1(self):
         with self.assertRaises(ValueError) as raises:
-            udt = cuda.jit((float32[:],))(udt_invalid_1)
-            udt(self.getarg())
+            cuda.jit((float32[:],))(udt_invalid_1)
 
         self.assertIn("Argument 'shape' must be a constant at",
                       str(raises.exception))
 
     def test_invalid_2(self):
         with self.assertRaises(ValueError) as raises:
-            udt = cuda.jit((float32[:, :],))(udt_invalid_2)
-            udt(self.getarg())
+            cuda.jit((float32[:, :],))(udt_invalid_2)
 
         self.assertIn("Argument 'shape' must be a constant at",
                       str(raises.exception))
