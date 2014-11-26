78394d1627b53a1557bd808ba977d6d912c7d189
Oscar Villellas <oscar.villellas@continuum.io>
2014-11-26 20:26:31 +0100

    corrected typos

--- numba/hsa/hsadrv/driver.py
+++ numba/hsa/hsadrv/driver.py
@@ -150,11 +150,11 @@ class Driver(object):
     _agent_map = None
 
     _hsa_properties = {
-        'version_major': (enum.HSA_SYSTEM_INFO_VERSION_MAJOR, ctypes.c_uint16),
-        'version_minor': (enum.HSA_SYSTEM_INFO_VERSIOM_MINOR, ctypes.c_uint16),
-        'timestamp': (enum.HSA_SYSTEM_INFO_TIMESTAMP, ctypes.c_uint64),
-        'timestamp_frequency': (enum.HSA_SYSTEM_INFOTIME_STAMP_FREQUENCY, ctypes.c_uint16),
-        'signal_max_wait': (enum.HSA_SYSTEM_INFO_SIGNAL_MAX_WAIT, ctypes.c_uint64),
+        'version_major': (enums.HSA_SYSTEM_INFO_VERSION_MAJOR, ctypes.c_uint16),
+        'version_minor': (enums.HSA_SYSTEM_INFO_VERSIOM_MINOR, ctypes.c_uint16),
+        'timestamp': (enums.HSA_SYSTEM_INFO_TIMESTAMP, ctypes.c_uint64),
+        'timestamp_frequency': (enums.HSA_SYSTEM_INFOTIME_STAMP_FREQUENCY, ctypes.c_uint16),
+        'signal_max_wait': (enums.HSA_SYSTEM_INFO_SIGNAL_MAX_WAIT, ctypes.c_uint64),
     }
 
     def __new__(cls):
