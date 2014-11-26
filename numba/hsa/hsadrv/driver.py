a7a69f6d7168b4629eb2d5cc3d2bee053abb82e4
Oscar Villellas <oscar.villellas@continuum.io>
2014-11-26 20:31:32 +0100

    typos…

--- numba/hsa/hsadrv/driver.py
+++ numba/hsa/hsadrv/driver.py
@@ -153,7 +153,7 @@ class Driver(object):
         'version_major': (enums.HSA_SYSTEM_INFO_VERSION_MAJOR, ctypes.c_uint16),
         'version_minor': (enums.HSA_SYSTEM_INFO_VERSION_MINOR, ctypes.c_uint16),
         'timestamp': (enums.HSA_SYSTEM_INFO_TIMESTAMP, ctypes.c_uint64),
-        'timestamp_frequency': (enums.HSA_SYSTEM_INFOTIME_STAMP_FREQUENCY, ctypes.c_uint16),
+        'timestamp_frequency': (enums.HSA_SYSTEM_INFO_TIMESTAMP_FREQUENCY, ctypes.c_uint16),
         'signal_max_wait': (enums.HSA_SYSTEM_INFO_SIGNAL_MAX_WAIT, ctypes.c_uint64),
     }
 
