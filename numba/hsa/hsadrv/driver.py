97eccb2c5d862cfce58405fec42eb91e0c0f36ed
Oscar Villellas <oscar.villellas@continuum.io>
2014-11-27 19:39:34 +0100

    need to instance pointer :)

--- numba/hsa/hsadrv/driver.py
+++ numba/hsa/hsadrv/driver.py
@@ -410,7 +410,7 @@ class Agent(HsaWrapper):
     def create_queue_single(self, size, callback=None, service_queue=None):
         cb = 0 if callback is None else drvapi.HSA_QUEUE_CALLBACK_FUNC(callback)
         sq = 0 if service_queue is None else service_queue._id
-        result = ctypes.POINTER(drvapi.hsa_queue_t)
+        result = ctypes.POINTER(drvapi.hsa_queue_t)()
         hsa.hsa_queue_create(self._id, size, enums.HSA_QUEUE_TYPE_SINGLE,
                              cb, sq, ctypes.byref(result))
         return Queue(result)
