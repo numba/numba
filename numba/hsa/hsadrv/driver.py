e4b86be3041e98e386bc6f0a8af37c36faf2f952
Oscar Villellas <oscar.villellas@continuum.io>
2014-11-27 19:37:04 +0100

    added missing parameter to create queue

--- numba/hsa/hsadrv/driver.py
+++ numba/hsa/hsadrv/driver.py
@@ -411,7 +411,8 @@ class Agent(HsaWrapper):
         cb = 0 if callback is None else drvapi.HSA_QUEUE_CALLBACK_FUNC(callback)
         sq = 0 if service_queue is None else service_queue._id
         result = ctypes.POINTER(drvapi.hsa_queue_t)
-        hsa.hsa_queue_create(self._id, size, cb, sq, ctypes.byref(result))
+        hsa.hsa_queue_create(self._id, size, enums.HSA_QUEUE_TYPE_SINGLE,
+                             cb, sq, ctypes.byref(result))
         return Queue(result)
 
     def __repr__(self):
