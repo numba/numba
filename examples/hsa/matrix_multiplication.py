bfb423087e01882514b5a278d1245c5c97db26d2
Oscar Villellas <oscar.villellas@continuum.io>
2014-11-28 12:33:48 +0100

    a bit of progress on the matrix_multiplication hsa script

--- examples/hsa/matrix_multiplication.py
+++ examples/hsa/matrix_multiplication.py
@@ -4,13 +4,31 @@ Sample low-level HSA runtime example.
 from __future__ import print_function, division
 
 import numpy as np
-from numba import hsa
-
+from numba.hsa.hsadrv.driver import hsa
 
 
 if __name__=='__main__':
-    hsa.init()
+    WIDTH = 4
+    HEIGHT = 4
+    # note that hsa library is automaticaaly initialized on first use
+    components = [a for a in hsa.agents if a.is_component]
 
-    hsa.shut_down()
+    # select the first one
+    if len(components) < 1:
+        sys.exit("No HSA component found!")
+
+    gpu = components[0]
+
+    print("Using agent: {0} with queue size: {1}".format(gpu.name, gpu.max_queue_size))
+    q = gpu.create_queue_multi(gpy.max_queue_size)
+
+    a = np.random(WIDTH*HEIGHT).reshape((HEIGHT, WIDTH))
+    b = np.random(WIDTH*HEIGHT).reshape((WIDTH, HEIGHT))
+    print("input matrix A:\n", a)
+    print("input matrix B:\n", b)
+
+    s = hsa.create_signal(1)
+
+    print(s)
 
     return 0
