733e90ca27250ee2c0c7d51077687801a1ea8b3c
Oscar Villellas <oscar.villellas@continuum.io>
2014-12-09 17:54:00 +0100

    fix elf_utils

--- numba/hsa/hsadrv/elf_utils.c
+++ numba/hsa/hsadrv/elf_utils.c
@@ -223,6 +223,61 @@ void destroy_brig_module(hsa_ext_brig_module_t* brig_module) {
 
 
 /*
+ * Define required BRIG data structures.
+ */
+
+typedef uint32_t BrigCodeOffset32_t;
+
+typedef uint32_t BrigDataOffset32_t;
+
+typedef uint16_t BrigKinds16_t;
+
+typedef uint8_t BrigLinkage8_t;
+
+typedef uint8_t BrigExecutableModifier8_t;
+
+typedef BrigDataOffset32_t BrigDataOffsetString32_t;
+
+enum BrigKinds {
+    BRIG_KIND_NONE = 0x0000,
+    BRIG_KIND_DIRECTIVE_BEGIN = 0x1000,
+    BRIG_KIND_DIRECTIVE_KERNEL = 0x1008,
+};
+
+typedef struct BrigBase BrigBase;
+struct BrigBase {
+    uint16_t byteCount;
+    BrigKinds16_t kind;
+};
+
+typedef struct BrigExecutableModifier BrigExecutableModifier;
+struct BrigExecutableModifier {
+    BrigExecutableModifier8_t allBits;
+};
+
+typedef struct BrigDirectiveExecutable BrigDirectiveExecutable;
+struct BrigDirectiveExecutable {
+    uint16_t byteCount;
+    BrigKinds16_t kind;
+    BrigDataOffsetString32_t name;
+    uint16_t outArgCount;
+    uint16_t inArgCount;
+    BrigCodeOffset32_t firstInArg;
+    BrigCodeOffset32_t firstCodeBlockEntry;
+    BrigCodeOffset32_t nextModuleEntry;
+    uint32_t codeBlockEntryCount;
+    BrigExecutableModifier modifier;
+    BrigLinkage8_t linkage;
+    uint16_t reserved;
+};
+
+typedef struct BrigData BrigData;
+struct BrigData {
+    uint32_t byteCount;
+    uint8_t bytes[1];
+};
+
+/*
  * Finds the specified symbols offset in the specified brig_module.
  * If the symbol is found the function returns HSA_STATUS_SUCCESS, 
  * otherwise it returns HSA_STATUS_ERROR.
