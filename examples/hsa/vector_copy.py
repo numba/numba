1c29df0be3b0ee4d5f1b1014433a34f8e84de301
Oscar Villellas <oscar.villellas@continuum.io>
2014-12-15 06:10:22 -0600

    simplified the vector copy script using the region support in the driver

--- examples/hsa/vector_copy.py
+++ examples/hsa/vector_copy.py
@@ -14,36 +14,12 @@ import numpy as np
 from numba.hsa.hsadrv.driver import hsa, BrigModule
 from numba.hsa.hsadrv import drvapi, enums
 
-libc = ctypes.CDLL(find_library('c'))
-libc.posix_memalign.restype = ctypes.c_int
-libc.posix_memalign.argtypes = [
-    ctypes.POINTER(ctypes.c_void_p),
-    ctypes.c_size_t,
-    ctypes.c_size_t
-]
-
-libc.free.restype = None
-libc.free.argtypes = [ctypes.c_void_p]
-
 
 def dump_aql_packet(aql):
     for field_desc in drvapi.hsa_dispatch_packet_t._fields_:
         fname = field_desc[0]
         print (fname, getattr(aql, fname))
 
-def alloc_aligned(typ, alignment):
-    sz = ctypes.sizeof(typ)
-    result = ctypes.c_void_p()
-    err = libc.posix_memalign(ctypes.byref(result),
-                              alignment, sz)
-    if err != 0:
-        raise Exception('ENOMEM')
-
-    ctypes.memset(result, 0, sz)
-    return ctypes.cast(result, ctypes.POINTER(typ)).contents
-
-def free(obj):
-    libc.free(ctypes.byref(obj))
 
 def create_program(device, brig_file, symbol):
     brig_module = BrigModule.from_file(brig_file)
@@ -55,16 +31,6 @@ def create_program(device, brig_file, symbol):
     code_descriptor = program.finalize(device, module, symbol_offset)
 
     return program, code_descriptor
-
-
-def get_kernarg(region, result):
-    flags = drvapi.hsa_region_flag_t()
-    hsa.hsa_region_get_info(region, enums.HSA_REGION_INFO_FLAGS, ctypes.byref(flags))
-    print("region {0} with flags {1}".format(hex(region), hex(flags.value)))
-    if flags.value & enums.HSA_REGION_FLAG_KERNARG:
-        result.value = region
-
-    return enums.HSA_STATUS_SUCCESS
 
 
 def main(src, dst):
@@ -96,9 +62,7 @@ def main(src, dst):
     s = hsa.create_signal(1)
 
     # manually build an aql packet
-    aql = alloc_aligned(drvapi.hsa_dispatch_packet_t, 64)
-    print (aql)
-
+    aql = drvapi.hsa_dispatch_packet_t()
     aql.completion_signal = s._id
     aql.dimensions = 1
     aql.workgroup_size_x = 256
@@ -118,28 +82,20 @@ def main(src, dst):
     hsa.hsa_memory_register(src.ctypes.data, src.nbytes)
     hsa.hsa_memory_register(dst.ctypes.data, dst.nbytes)
 
-    kernarg_region = drvapi.hsa_region_t(0)
-    callback = drvapi.HSA_AGENT_ITERATE_REGIONS_CALLBACK_FUNC(get_kernarg)
-    hsa.hsa_agent_iterate_regions(gpu._id, callback, kernarg_region)
-    assert kernarg_region.value != 0
-
-    kernel_arg_buffer_size = code_descriptor._id.kernarg_segment_byte_size
-    print ('Kernel has kernarg_segment_byte_size {0}'.format(kernel_arg_buffer_size))
-
-    kernel_arg_buffer = ctypes.c_void_p()
-    hsa.hsa_memory_allocate(kernarg_region, kernel_arg_buffer_size,
-                            ctypes.byref(kernel_arg_buffer))
-    kernargs = ctypes.cast(kernel_arg_buffer, ctypes.POINTER(ctypes.c_void_p * 2)).contents
+    kernarg_regions = [r for r in gpu.regions if r.supports_kernargs]
+    assert kernarg_regions
+    kernarg_region = kernarg_regions[0]
+    # Specify the argument types required by the kernel and allocate them
+    # note: in an ideal world this should come from kernel metadata
+    kernarg_types = ctypes.c_void_p * 2
+    kernargs = kernarg_region.allocate(kernarg_types)
     kernargs[0] = src.ctypes.data
     kernargs[1] = dst.ctypes.data
-
-    print ('kernel_arg_buffer is {0}; kernargs is {1}'.format(hex(kernel_arg_buffer.value),
-                                                              ctypes.byref(kernargs)))
 
     hsa.hsa_memory_register(ctypes.byref(kernargs), ctypes.sizeof(kernargs))
 
     aql.kernel_object_address = code_descriptor._id.code.handle
-    aql.kernarg_address = kernel_arg_buffer.value
+    aql.kernarg_address = ctypes.cast(ctypes.byref(kernargs), ctypes.c_void_p).value
 
     dump_aql_packet(aql)
 
@@ -159,8 +115,9 @@ def main(src, dst):
     # wait for results
     hsa.hsa_signal_wait_acquire(s._id, enums.HSA_LT, 1, -1, enums.HSA_WAIT_EXPECTANCY_UNKNOWN)
 
-    hsa.hsa_memory_free(kernel_arg_buffer)
-    free(aql)
+    # this is placed in the kernarg_region for simetry, but shouldn't be required.
+    kernarg_region.free(kernargs)
+    #free(aql)
 
 
 if __name__=='__main__':
