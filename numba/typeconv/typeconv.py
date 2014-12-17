93eb65e5c8364ee4b1e3f94f15020b42e3ff7f57
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-12-17 16:55:18 -0600

    Refactor and add docs

--- numba/typeconv/typeconv.py
+++ numba/typeconv/typeconv.py
@@ -37,32 +37,56 @@ class TypeManager(object):
 
 
 class TypeCastingRules(object):
+    """
+    A helper for establishing type casting rules.
+    """
     def __init__(self, tm):
         self._tm = tm
         self._tg = castgraph.TypeGraph(self._cb_update)
 
     def promote(self, a, b):
+        """
+        Set `a` can promote to `b`
+        """
         self._tg.promote(a, b)
 
-    def promote_unsafe(self, a, b):
-        self.promote(a, b)
-        self.unsafe(b, a)
-
     def unsafe(self, a, b):
+        """
+        Set `a` can unsafe convert to `b`
+        """
         self._tg.unsafe(a, b)
 
     def safe(self, a, b):
+        """
+        Set `a` can safe convert to `b`
+        """
         self._tg.safe(a, b)
 
+    def promote_unsafe(self, a, b):
+        """
+        Set `a` can promote to `b` and `b` can unsafe convert to `a`
+        """
+        self.promote(a, b)
+        self.unsafe(b, a)
+
     def safe_unsafe(self, a, b):
+        """
+        Set `a` can safe convert to `b` and `b` can unsafe convert to `a`
+        """
         self._tg.safe(a, b)
         self._tg.unsafe(b, a)
 
     def unsafe_unsafe(self, a, b):
+        """
+        Set `a` can unsafe convert to `b` and `b` can unsafe convert to `a`
+        """
         self._tg.unsafe(a, b)
         self._tg.unsafe(b, a)
 
     def _cb_update(self, a, b, rel):
+        """
+        Callback for updating.
+        """
         if rel == castgraph.Promote:
             self._tm.set_promote(a, b)
         elif rel == castgraph.Safe:
