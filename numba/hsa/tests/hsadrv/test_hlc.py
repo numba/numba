e4bbc64cc7baff4d58928fbc71f9f14a6e6bf61c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-12-31 12:04:32 -0600

    Fix test for older HSAIL LLVM

--- numba/hsa/tests/hsadrv/test_hlc.py
+++ numba/hsa/tests/hsadrv/test_hlc.py
@@ -5,16 +5,15 @@ from numba.hsa.hlc import hlc
 
 SPIR_SAMPLE = """
 ; ModuleID = 'kernel.out.bc'
-target datalayout = "e-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-n32"
-target triple = "hsail64-pc-unknown-amdopencl"
+target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64-v96:128:128-v128:128:128-v192:256:256-v256:256:256-v512:512:512-v1024:1024:1024-n32"
+target triple = "spir64-unknown-unknown"
 
-; Function Attrs: nounwind
-define spir_kernel void @square(float addrspace(1)* nocapture %input, float addrspace(1)* nocapture %output) #0 {
+define spir_kernel void @square(float addrspace(1)* nocapture %input, float addrspace(1)* nocapture %output) {
   %1 = load float addrspace(1)* %input, align 4, !tbaa !8
   store float %1, float addrspace(1)* %output, align 4, !tbaa !8
   ret void
 }
-attributes #0 = { nounwind }
+
 !opencl.kernels = !{!0}
 !opencl.enable.FP_CONTRACT = !{}
 !opencl.spir.version = !{!6}
