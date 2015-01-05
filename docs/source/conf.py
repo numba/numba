904218ad9f159804294f4c6f1b3a7e4c32c6a156
Antoine Pitrou <antoine@python.org>
2015-01-05 20:05:37 +0100

    Try to fix import error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,13 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
-import numba
+try:
+    # Numba is installed
+    import numba
+except ImportError:
+    # Numba is run from its source checkout
+    sys.path.insert(0, os.path.abspath('../..'))
+    import numba
 
 # -- General configuration ------------------------------------------------
 
