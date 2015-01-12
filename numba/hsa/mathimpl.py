71b99d35adaf99af05213767b8eb3802a957b477
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-01-12 15:44:49 -0600

    Implement cos(f32), cos(f64)

--- numba/hsa/mathimpl.py
+++ numba/hsa/mathimpl.py
@@ -26,3 +26,19 @@ def math_sin(context, builder, sig, args):
     fn = _declare_function(context, builder, 'sin', sig, ['double'])
     return builder.call(fn, [val])
 
+
+@register
+@implement(math.cos, types.float32)
+def math_cos(context, builder, sig, args):
+    [val] = args
+    fn = _declare_function(context, builder, 'cos', sig, ['float'])
+    return builder.call(fn, [val])
+
+
+@register
+@implement(math.cos, types.float64)
+def math_cos(context, builder, sig, args):
+    [val] = args
+    fn = _declare_function(context, builder, 'cos', sig, ['double'])
+    return builder.call(fn, [val])
+
