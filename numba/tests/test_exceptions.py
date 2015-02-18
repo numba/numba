8700798665212c5b9c79c0648947697e9b8c0f9d
Antoine Pitrou <antoine@python.org>
2015-02-18 19:10:01 +0100

    Mark two-argument assert unsupported on 2.6 (because of old bytecode)

--- numba/tests/test_exceptions.py
+++ numba/tests/test_exceptions.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import sys
 
 from numba.compiler import compile_isolated, Flags
 from numba import jit, types
@@ -166,6 +167,8 @@ class TestRaising(TestCase):
         with self.assertTypingError():
             self.check_raise_invalid_class(1, flags=no_pyobj_flags)
 
+    @unittest.skipIf(sys.version_info < (2, 7),
+                     "assert statement uses unsupported 2-argument raise bytecode on 2.6")
     def check_assert_statement(self, flags):
         pyfunc = assert_usecase
         cres = compile_isolated(pyfunc, (types.int32,), flags=flags)
