3fda7e7f4a5e1f7d402da1b103aba5a3f352e388
Jon Riehl <jriehl@spaceship.com>
2015-03-26 14:21:37 -0500

    Added import of identity macros to npyufunc.__init__.

--- numba/npyufunc/__init__.py
+++ numba/npyufunc/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import
 from .decorators import Vectorize, GUVectorize, vectorize, guvectorize
+from ._internal import PyUFunc_None, PyUFunc_Zero, PyUFunc_One
+from . import _internal
+if hasattr(_internal, 'PyUFunc_ReorderableNone'):
+    PyUFunc_ReorderableNone = _internal.PyUFunc_ReorderableNone
+del _internal
