dfdbbb553a416542f5547bb9a76bcbba98277462
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-04-02 17:42:04 -0500

    More MemInfo work - enable defer dtor

--- numba/runtime/nrt.h
+++ numba/runtime/nrt.h
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -14,19 +13,29 @@ typedef union MemInfo MemInfo;
 typedef struct MemSys MemSys;
 
 /* Memory System API */
+void NRT_MemSys_init();
 void NRT_MemSys_insert_meminfo(MemInfo *newnode);
 MemInfo* NRT_MemSys_pop_meminfo();
 void NRT_MemSys_set_atomic_inc_dec(atomic_inc_dec_func inc,
                                    atomic_inc_dec_func dec);
 void NRT_MemSys_set_atomic_inc_dec_stub();
+void NRT_MemSys_process_defer_dtor();
 
 /* Memory Info API */
+
+/* Create a new MemInfo
+ *
+ * data: data pointer being tracked
+ * dtor: destructor to execute
+ * dtor_info: additional information to pass to the destructor
+ */
 MemInfo* NRT_MemInfo_new(void *data, dtor_function dtor, void *dtor_info);
 MemInfo* NRT_MemInfo_alloc(size_t size);
 void NRT_MemInfo_destroy(MemInfo *mi);
 void NRT_MemInfo_acquire(MemInfo* mi);
-void NRT_MemInfo_release(MemInfo* mi);
+void NRT_MemInfo_release(MemInfo* mi, int defer);
 void* NRT_MemInfo_data(MemInfo* mi);
+void NRT_MemInfo_defer_dtor(MemInfo* mi);
 
 /* General allocator */
 void* NRT_Allocate(size_t size);
--- numba/tests/test_nrt.py
+++ numba/tests/test_nrt.py
@@ -1,30 +1,71 @@
 from __future__ import absolute_import, division, print_function
 
 from numba import unittest_support as unittest
+from numba.runtime import rtsys
 
 
-class TestNRT(unittest.TestCase):
-    def test_meminfo_op(self):
-        from numba.runtime import _nrt_python as _nrt
+class Dummy(object):
+    alive = 0
 
-        class Dummy(object):
-            alive = 0
+    def __init__(self):
+        type(self).alive += 1
 
-            def __init__(self):
-                type(self).alive += 1
+    def __del__(self):
+        type(self).alive -= 1
 
-            def __del__(self):
-                type(self).alive -= 1
 
+class TestNrtMemInfo(unittest.TestCase):
+    """
+    Unitest for core MemInfo functionality
+    """
+
+    def test_meminfo_refct_1(self):
         d = Dummy()
         self.assertEqual(Dummy.alive, 1)
-        addr = 0xdeadcafe
-        mi = _nrt.meminfo_new(addr, d)
+        addr = 0xdeadcafe  # some made up location
+
+        mi = rtsys.meminfo_new(addr, d)
         del d
         self.assertEqual(Dummy.alive, 1)
-        _nrt.meminfo_acquire(mi)
+        mi.acquire()
         self.assertEqual(Dummy.alive, 1)
-        _nrt.meminfo_release(mi)
+        mi.release()
+        del mi
+        self.assertEqual(Dummy.alive, 0)
+
+    def test_meminfo_refct_2(self):
+        d = Dummy()
+        self.assertEqual(Dummy.alive, 1)
+        addr = 0xdeadcafe  # some made up location
+
+        mi = rtsys.meminfo_new(addr, d)
+        del d
+        self.assertEqual(Dummy.alive, 1)
+        for _ in range(100):
+            mi.acquire()
+        self.assertEqual(Dummy.alive, 1)
+        for _ in range(100):
+            mi.release()
+        del mi
+        self.assertEqual(Dummy.alive, 0)
+
+    def test_defer_dtor(self):
+        d = Dummy()
+        self.assertEqual(Dummy.alive, 1)
+        addr = 0xdeadcafe  # some made up location
+
+        mi = rtsys.meminfo_new(addr, d)
+        # Set defer flag
+        mi.defer = True
+        del d
+        self.assertEqual(Dummy.alive, 1)
+        mi.acquire()
+        self.assertEqual(Dummy.alive, 1)
+        mi.release()
+        del mi
+        # mi refct is zero but not yet removed due to deferring
+        self.assertEqual(Dummy.alive, 1)
+        rtsys.process_defer_dtor()
         self.assertEqual(Dummy.alive, 0)
 
 
