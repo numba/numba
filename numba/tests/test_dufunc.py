8d1d68cff59303e23e7a9532922156348a484a0f
Jon Riehl <jriehl@spaceship.com>
2015-04-03 17:46:40 -0500

    Starting work on supporting NPM calls to DUFunc's.

--- numba/tests/test_dufunc.py
+++ numba/tests/test_dufunc.py
@@ -3,6 +3,7 @@ from numba import unittest_support as unittest
 
 import numpy as np
 
+from numba import njit
 from numba.npyufunc import dufunc
 
 def pyuadd(a0, a1):
@@ -23,6 +24,27 @@ class TestDUFunc(unittest.TestCase):
     def test_scalar(self):
         duadd = dufunc.DUFunc(pyuadd, nopython=True)
         self.assertEqual(pyuadd(1,2), duadd(1,2))
+
+    def test_npm_call(self):
+        duadd = dufunc.DUFunc(pyuadd, nopython=True)
+        @njit
+        def npmadd(a0, a1, o0):
+            duadd(a0, a1, o0)
+        X = np.linspace(0,1.9,20)
+        X0 = X[:10]
+        X1 = X[10:]
+        out0 = np.zeros(10)
+        npmadd(X0, X1, out0)
+        self.assertTrue(np.all(X0 + X1 == out0))
+        Y0 = X0.reshape((2,5))
+        Y1 = X1.reshape((2,5))
+        out1 = np.zeros((2,5))
+        npmadd(Y0, Y1, out1)
+        self.assertTrue(np.all(Y0 + Y1 == out1))
+        Y2 = X1[:5]
+        out2 = np.zeros((2,5))
+        npmadd(Y0, Y2, out2)
+        self.assertTrue(np.all(Y0 + Y2 == out2))
 
 if __name__ == "__main__":
     unittest.main()
