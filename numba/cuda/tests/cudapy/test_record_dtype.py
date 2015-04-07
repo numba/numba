45411f8bb61a3f14b7a50df156a76001b880c5bb
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-04-07 13:15:17 -0500

    Reverse accidental test modification

--- numba/cuda/tests/cudapy/test_record_dtype.py
+++ numba/cuda/tests/cudapy/test_record_dtype.py
@@ -57,12 +57,12 @@ def record_write_array(r):
 
 def record_write_2d_array(r):
     r.i = 3
-    # r.j[0, 0] = 5.0
-    # r.j[0, 1] = 6.0
-    # r.j[1, 0] = 7.0
-    # r.j[1, 1] = 8.0
-    # r.j[2, 0] = 9.0
-    # r.j[2, 1] = 10.0
+    r.j[0, 0] = 5.0
+    r.j[0, 1] = 6.0
+    r.j[1, 0] = 7.0
+    r.j[1, 1] = 8.0
+    r.j[2, 0] = 9.0
+    r.j[2, 1] = 10.0
 
 
 def record_read_array(r, a):
