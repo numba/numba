3e609a2ac90f684520893ea5f5ccc24ec38c3b77
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-04-08 12:10:41 -0500

    Set safe allocation as default for now - safe allocation zero the memory at allocation and deallocation time

--- numba/targets/base.py
+++ numba/targets/base.py
@@ -968,7 +968,7 @@ class BaseContext(object):
         mod = cgutils.get_module(builder)
         fnty = llvmir.FunctionType(llvmir.IntType(8).as_pointer(),
             [self.get_value_type(types.intp)])
-        fn = mod.get_or_insert_function(fnty, name="NRT_MemInfo_alloc")
+        fn = mod.get_or_insert_function(fnty, name="NRT_MemInfo_alloc_safe")
         return builder.call(fn, [size])
 
     def nrt_meminfo_data(self, builder, meminfo):
