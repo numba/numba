f1d4babb770b5dee41fbc10110214b28246c161a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-04-08 15:00:17 -0500

    Add more test cases for dynanmic array

--- numba/tests/test_dyn_array.py
+++ numba/tests/test_dyn_array.py
@@ -49,6 +49,29 @@ class TestDynArray(unittest.TestCase):
 
         del got_arr
 
+    def test_empty_2d_sliced(self):
+        def pyfunc(m, n, p):
+            arr = np.empty((m, n), np.int32)
+            for i in range(m):
+                for j in range(n):
+                    arr[i, j] = i + j
+
+            return arr[p]
+
+        cfunc = njit(pyfunc)
+        m = 4
+        n = 3
+        p = 2
+        expected_arr = pyfunc(m, n, p)
+        got_arr = cfunc(m, n, p)
+        np.testing.assert_equal(expected_arr, got_arr)
+
+        self.assertEqual(expected_arr.size, got_arr.size)
+        self.assertEqual(expected_arr.shape, got_arr.shape)
+        self.assertEqual(expected_arr.strides, got_arr.strides)
+
+        del got_arr
+
     def test_return_global_array(self):
         y = np.ones(4, dtype=np.float32)
 
@@ -61,6 +84,48 @@ class TestDynArray(unittest.TestCase):
         np.testing.assert_equal(y, out)
         np.testing.assert_equal(y, np.ones(4, dtype=np.float32))
         np.testing.assert_equal(out, np.ones(4, dtype=np.float32))
+
+    def test_return_global_array_sliced(self):
+        y = np.ones(4, dtype=np.float32)
+
+        def return_external_array():
+            return y[2:]
+
+        cfunc = njit(return_external_array)
+        out = cfunc()
+
+        yy = y[2:]
+        np.testing.assert_equal(yy, out)
+        np.testing.assert_equal(yy, np.ones(2, dtype=np.float32))
+        np.testing.assert_equal(out, np.ones(2, dtype=np.float32))
+
+    def test_array_pass_through(self):
+        def pyfunc(y):
+            return y
+
+        arr = np.ones(4, dtype=np.float32)
+
+        cfunc = njit(pyfunc)
+        expected = cfunc(arr)
+        got = pyfunc(arr)
+
+        np.testing.assert_equal(expected, arr)
+        np.testing.assert_equal(expected, got)
+        self.assertIs(expected, arr)
+        self.assertIs(expected, got)
+
+    def test_array_pass_through_sliced(self):
+        def pyfunc(y):
+            return y[y.size // 2:]
+
+        arr = np.ones(4, dtype=np.float32)
+
+        cfunc = njit(pyfunc)
+        expected = cfunc(arr)
+        got = pyfunc(arr)
+
+        np.testing.assert_equal(expected, arr[arr.size // 2])
+        np.testing.assert_equal(expected, got)
 
 
 if __name__ == "__main__":
