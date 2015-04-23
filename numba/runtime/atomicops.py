927bd17e8b5c95278884ac3089e1a8b599ab4ed7
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-04-23 17:28:17 -0500

    Add docs about atomic cmpxchg

--- numba/runtime/atomicops.py
+++ numba/runtime/atomicops.py
@@ -94,6 +94,16 @@ def _define_atomic_inc_dec(module, op, ordering):
 
 
 def _define_atomic_cmpxchg(module, ordering):
+    """Define a llvm function for atomic compare-and-swap.
+    The generated function is a direct wrapper of the LLVM cmpxchg with the
+    difference that the a int indicate success (1) or failure (0) is returned
+    and the last argument is a output pointer for storing the old value.
+
+    Note
+    ----
+    On failure, the generated function behaves like an atomic load.  The loaded
+    value is stored to the last argument.
+    """
     ftype = ir.FunctionType(ir.IntType(32), [_word_type.as_pointer(),
                                              _word_type, _word_type,
                                              _word_type.as_pointer()])
