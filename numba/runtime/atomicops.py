ede6942b28f5471a64025d70ba4522ff71689f36
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-04-23 17:28:04 -0500

    Avoid loading after atomic inc/dec to retrieve the new value. Instead, perform the operation on the old value to create the "new" value

--- numba/runtime/atomicops.py
+++ numba/runtime/atomicops.py
@@ -75,7 +75,7 @@ def install_atomic_refct(module):
 def _define_atomic_inc_dec(module, op, ordering):
     """Define a llvm function for atomic increment/decrement to the given module
     Argument ``op`` is the operation "add"/"sub".  Argument ``ordering`` is
-    the memory ordering.
+    the memory ordering.  The generated function returns the new value.
     """
     ftype = ir.FunctionType(_word_type, [_word_type.as_pointer()])
     fn_atomic = ir.Function(module, ftype, name="nrt_atomic_{0}".format(op))
@@ -84,8 +84,10 @@ def _define_atomic_inc_dec(module, op, ordering):
     bb = fn_atomic.append_basic_block()
     builder = ir.IRBuilder(bb)
     ONE = ir.Constant(_word_type, 1)
-    builder.atomic_rmw(op, ptr, ONE, ordering=ordering)
-    res = builder.load(ptr)
+    oldval = builder.atomic_rmw(op, ptr, ONE, ordering=ordering)
+    # Perform the operation on the old value so that we can pretend returning
+    # the "new" value.
+    res = getattr(builder, op)(oldval, ONE)
     builder.ret(res)
 
     return fn_atomic
