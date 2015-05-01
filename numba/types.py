99322ae0760cc84b5630f48bea8fa55c66241eeb
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-05-01 10:24:41 -0500

    Add docs, comments

--- numba/types.py
+++ numba/types.py
@@ -823,7 +823,12 @@ class Array(Buffer):
 
 
 class ArrayCTypes(Type):
+    """
+    This is the type for `numpy.ndarray.ctypes`.
+    """
     def __init__(self, arytype):
+        # This depends on the ndim for the shape and strides attributes,
+        # even though they are not implemented, yet.
         self.ndim = arytype.ndim
         name = "ArrayCType(ndim={0})".format(self.ndim)
         super(ArrayCTypes, self).__init__(name, param=True)
