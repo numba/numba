e6a6d86c297795cc9f56c7d909b70966f8f67a36
Antoine Pitrou <antoine@python.org>
2015-05-11 15:25:53 +0200

    Fix failures in ufuncs

--- numba/targets/npyimpl.py
+++ numba/targets/npyimpl.py
@@ -280,7 +280,7 @@ def _build_array(context, builder, array_ty, arg_arrays):
                            for dest_shape_addr in dest_shape_addrs)
     array_val = arrayobj._empty_nd_impl(context, builder, array_ty,
                                         dest_shape_tup)
-    return _prepare_argument(context, builder, array_val, array_ty,
+    return _prepare_argument(context, builder, array_val._getvalue(), array_ty,
                              where='implicit output argument')
 
 
