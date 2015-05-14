be837c2b71e1df2c42e85383b9e1a949f9bc5abb
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-05-14 14:05:41 -0500

    Implement string_as_string_and_size

--- numba/pythonapi.py
+++ numba/pythonapi.py
@@ -691,6 +691,42 @@ class PythonAPI(object):
         fn = self._get_function(fnty, name=fname)
         return self.builder.call(fn, [strobj])
 
+    def string_as_string_and_size(self, strobj):
+        """
+        Returns a tuple of ``(ok, buffer, length)``.
+        The ``ok`` is i1 value that is set if ok.
+        The ``buffer`` is a i8* of the output buffer.
+        The ``length`` is a i32/i64 (py_ssize_t) of the length of the buffer.
+        """
+
+        p_length = cgutils.alloca_once(self.builder, self.py_ssize_t)
+        if PYVERSION >= (3, 0):
+            fnty = Type.function(self.cstring, [self.pyobj,
+                                                self.py_ssize_t.as_pointer()])
+            fname = "PyUnicode_AsUTF8AndSize"
+            fn = self._get_function(fnty, name=fname)
+
+            buffer = self.builder.call(fn, [strobj, p_length])
+            ok = self.builder.icmp_unsigned('!=',
+                                            ir.Constant(buffer.type, None),
+                                            buffer)
+        else:
+            fnty = Type.function(lc.Type.int(), [self.pyobj,
+                                                 self.cstring.as_pointer(),
+                                                 self.py_ssize_t.as_pointer()])
+            fname = "PyString_AsStringAndSize"
+            fn = self._get_function(fnty, name=fname)
+            # Allocate space for the output parameters
+            p_buffer = cgutils.alloca_once(self.builder, self.cstring)
+
+            status = self.builder.call(fn, [strobj, p_buffer, p_length])
+
+            negone = ir.Constant(status.type, -1)
+            ok = builder.icmp_signed("!=", status, negone)
+            buffer = self.builder.load(p_buffer)
+
+        return (ok, buffer, self.builder.load(p_length))
+
     def string_from_string_and_size(self, string, size):
         fnty = Type.function(self.pyobj, [self.cstring, self.py_ssize_t])
         if PYVERSION >= (3, 0):
