bc308fa882905875c458efb471ddc7d469558819
Jon Riehl <jriehl@spaceship.com>
2015-05-13 20:02:29 -0500

    Added ufunc properties to DUFunc.

--- numba/tests/test_dufunc.py
+++ numba/tests/test_dufunc.py
@@ -66,5 +66,17 @@ class TestDUFunc(unittest.TestCase):
         out3 = npmadd(1.,2.)
         self.assertEqual(out3, 3.)
 
+    def test_ufunc_props(self):
+        duadd = dufunc.DUFunc(pyuadd, nopython=True)
+        self.assertEqual(duadd.nin, 2)
+        self.assertEqual(duadd.nout, 1)
+        self.assertEqual(duadd.nargs, duadd.nin + duadd.nout)
+        self.assertEqual(duadd.ntypes, 0)
+        self.assertEqual(duadd.types, [])
+        self.assertEqual(duadd.identity, None)
+        duadd(1, 2)
+        self.assertEqual(duadd.ntypes, 1)
+        self.assertEqual(duadd.ntypes, len(duadd.types))
+
 if __name__ == "__main__":
     unittest.main()
