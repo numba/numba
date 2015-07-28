0d2d16efa6cd40c7af51c5cc200fac5866fdde75
Antoine Pitrou <antoine@python.org>
2015-07-28 18:08:58 +0200

    Add test based on tweaking a function's bytecode

--- numba/tests/support.py
+++ numba/tests/support.py
@@ -312,6 +312,31 @@ def compile_function(name, code, globs):
     eval(co, globs, ns)
     return ns[name]
 
+def tweak_code(func, codestring=None, consts=None):
+    """
+    Tweak the code object of the given function by replacing its
+    *codestring* (a bytes object) and *consts* tuple, optionally.
+    """
+    co = func.__code__
+    tp = type(co)
+    if codestring is None:
+        codestring = co.co_code
+    if consts is None:
+        consts = co.co_consts
+    if sys.version_info >= (3,):
+        new_code = tp(co.co_argcount, co.co_kwonlyargcount, co.co_nlocals,
+                      co.co_stacksize, co.co_flags, codestring,
+                      consts, co.co_names, co.co_varnames,
+                      co.co_filename, co.co_name, co.co_firstlineno,
+                      co.co_lnotab)
+    else:
+        new_code = tp(co.co_argcount, co.co_nlocals,
+                      co.co_stacksize, co.co_flags, codestring,
+                      consts, co.co_names, co.co_varnames,
+                      co.co_filename, co.co_name, co.co_firstlineno,
+                      co.co_lnotab)
+    func.__code__ = new_code
+
 
 # From CPython
 
