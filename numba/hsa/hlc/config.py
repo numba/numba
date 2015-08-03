d1017915eab16ebc3d1ab3d14b588d7517b9f1e1
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-08-03 17:43:04 -0500

    Uses the builtins-hsail.opt.bc from libHLC by default

--- numba/hsa/hlc/config.py
+++ numba/hsa/hlc/config.py
@@ -1,10 +1,10 @@
 from __future__ import print_function, absolute_import
 
+import sys
 import os
 
 os.environ['HSAILBIN'] = os.environ.get('HSAILBIN', '/opt/amd/bin')
 
-DEFAULT_BUILTIN_PATH = "{0}/builtins-hsail.opt.bc".format(
-    os.environ['HSAILBIN'])
+DEFAULT_BUILTIN_PATH = os.path.join(sys.prefix, 'lib', 'builtins-hsail.opt.bc')
 
 BUILTIN_PATH = os.environ.get("NUMBA_HSAIL_BUILTINS_BC", DEFAULT_BUILTIN_PATH)
