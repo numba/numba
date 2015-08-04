f3f029c738ea4f6d6ed0cbdb2038e4aab1fad1c1
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-08-04 18:15:30 -0500

    Remove dead code

--- numba/hsa/codegen.py
+++ numba/hsa/codegen.py
@@ -2,50 +2,9 @@ from __future__ import print_function, absolute_import
 
 from llvmlite import binding as ll
 from llvmlite.llvmpy import core as lc
-from llvmlite import ir as llvmir
 from numba import utils
 from numba.targets.codegen import BaseCPUCodegen, CodeLibrary
 from .hlc import DATALAYOUT, TRIPLE, hlc
-
-#
-# class ModuleCollection(object):
-#     def __init__(self):
-#         self._modules = []
-#         self._gvars = {}
-#
-#     def add(self, ir_module):
-#         # if not ir_module.global_variables:
-#         # # Ignore empty module
-#         #     return
-#         assert isinstance(ir_module, llvmir.Module)
-#         self._modules.append(ir_module)
-#
-#     def _load_globals(self, ir_module):
-#         for gv in ir_module.global_values:
-#             glbl = self._gvars.get(gv.name)
-#             if glbl is None:
-#                 self._gvars[gv.name] = gv
-#             else:
-#                 if not glbl.is_declaration:
-#                     self._gvars[gv.name] = glbl
-#                 elif not gv.is_declaration:
-#                     self._gvars[gv.name] = gv
-#
-#     @property
-#     def global_variables(self):
-#         return self._gvars.values()
-#
-#     def verify(self):
-#         return True
-#
-#     def link_in(self, module):
-#         assert isinstance(module, ModuleCollection)
-#         self._modules += module._modules
-#         for m in module._modules:
-#             self._load_globals(m)
-#
-#     def get_function(self, name):
-#         return self._gvars[name]
 
 
 class HSACodeLibrary(CodeLibrary):
@@ -57,16 +16,6 @@ class HSACodeLibrary(CodeLibrary):
 
     def _finalize_specific(self):
         pass
-
-    # def _link_in(self, module):
-    #     self._final_module.link_in(module._final_module)
-    #
-    # def add_llvm_module(self, ll_module):
-    #     """
-    #     Override base class
-    #     """
-    #     self._optimize_functions(ll_module)
-    #     self._final_module.link_in(ll_module)
 
     def get_asm_str(self):
         """
@@ -85,11 +34,6 @@ class JITHSACodegen(BaseCPUCodegen):
         assert list(llvm_module.global_variables) == [], "Module isn't empty"
         self._data_layout = DATALAYOUT[utils.MACHINE_BITS]
         self._target_data = ll.create_target_data(self._data_layout)
-    #
-    # def _materialize_module(self, ir_module):
-    #     modcol = ModuleCollection()
-    #     modcol.add(ir_module)
-    #     return modcol
 
     def _create_empty_module(self, name):
         ir_module = lc.Module.new(name)
