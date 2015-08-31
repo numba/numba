d87a15841ca38c5a248bf62c8ae9b6b8e5f4a0cb
Antoine Pitrou <antoine@python.org>
2015-08-31 18:28:49 +0200

    Make cuda print() tests programmatic

--- numba/cuda/tests/cudapy/test_print.py
+++ numba/cuda/tests/cudapy/test_print.py
@@ -1,12 +1,35 @@
 from __future__ import print_function
+
+import contextlib
+import io
+import os
+
 import numpy
+
 from numba import cuda
 from numba import unittest_support as unittest
 
 
+@contextlib.contextmanager
+def redirect_fd(fd):
+    """
+    Temporarily redirect *fd* to a pipe's write end and return a file object
+    wrapping the pipe's read end.
+    """
+    save = os.dup(fd)
+    r, w = os.pipe()
+    try:
+        os.dup2(w, fd)
+        yield io.open(r, "r")
+    finally:
+        os.close(w)
+        os.dup2(save, fd)
+        os.close(save)
+
+
 def cuhello():
     i = cuda.grid(1)
-    print(i, 1.234)
+    print(i, 999)
 
 
 def printfloat():
@@ -20,21 +43,23 @@ def cuprintary(A):
 
 
 class TestPrint(unittest.TestCase):
+
     def test_cuhello(self):
-        """
-        Eyeballing required
-        """
         jcuhello = cuda.jit('void()', debug=False)(cuhello)
-        jcuhello[2, 3]()
-        cuda.synchronize()
+        with redirect_fd(1) as stdout:
+            jcuhello[2, 3]()
+            cuda.synchronize()
+        # The output of GPU threads is intermingled, just sanity check it
+        out = stdout.read()
+        expected = ''.join('%d 999\n' % i for i in range(6))
+        self.assertEqual(sorted(out), sorted(expected))
 
     def test_printfloat(self):
-        """
-        Eyeballing required
-        """
         jprintfloat = cuda.jit('void()', debug=False)(printfloat)
-        jprintfloat()
-        cuda.synchronize()
+        with redirect_fd(1) as stdout:
+            jprintfloat()
+            cuda.synchronize()
+        self.assertEqual(stdout.read(), "0 23 34.300000 321\n")
 
     @unittest.skipIf(True, "Print string not implemented yet")
     def test_print_array(self):
