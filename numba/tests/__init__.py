30d1dbaf605624ccd31923fcd7e534f2a47d8419
Antoine Pitrou <antoine@python.org>
2015-08-31 18:31:49 +0200

    Just a check: change timeout value

--- numba/tests/__init__.py
+++ numba/tests/__init__.py
@@ -389,7 +389,7 @@ class ParallelTestRunner(runner.TextTestRunner):
 
     resultclass = ParallelTestResult
     # A test can't run longer than 2 minutes
-    timeout = 120
+    timeout = 150
 
     def __init__(self, runner_cls, **kwargs):
         runner.TextTestRunner.__init__(self, **kwargs)
