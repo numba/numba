97933e02edf7ff8161dade448b08655b61bef05b
Antoine Pitrou <antoine@python.org>
2015-08-31 20:30:40 +0200

    Fix tests to also run with the simulator enabled o

--- numba/cuda/tests/cudapy/test_print.py
+++ numba/cuda/tests/cudapy/test_print.py
@@ -1,30 +1,10 @@
 from __future__ import print_function
 
-import contextlib
-import io
-import os
-
 import numpy
 
 from numba import cuda
 from numba import unittest_support as unittest
-
-
-@contextlib.contextmanager
-def redirect_fd(fd):
-    """
-    Temporarily redirect *fd* to a pipe's write end and return a file object
-    wrapping the pipe's read end.
-    """
-    save = os.dup(fd)
-    r, w = os.pipe()
-    try:
-        os.dup2(w, fd)
-        yield io.open(r, "r")
-    finally:
-        os.close(w)
-        os.dup2(save, fd)
-        os.close(save)
+from numba.cuda.testing import captured_cuda_stdout
 
 
 def cuhello():
@@ -34,7 +14,7 @@ def cuhello():
 
 def printfloat():
     i = cuda.grid(1)
-    print(i, 23, 34.3, 321)
+    print(i, 23, 34.75, 321)
 
 
 def cuprintary(A):
@@ -46,20 +26,21 @@ class TestPrint(unittest.TestCase):
 
     def test_cuhello(self):
         jcuhello = cuda.jit('void()', debug=False)(cuhello)
-        with redirect_fd(1) as stdout:
+        with captured_cuda_stdout() as stdout:
             jcuhello[2, 3]()
-            cuda.synchronize()
+            stdout.sync()
         # The output of GPU threads is intermingled, just sanity check it
-        out = stdout.read()
+        out = stdout.getvalue()
         expected = ''.join('%d 999\n' % i for i in range(6))
         self.assertEqual(sorted(out), sorted(expected))
 
     def test_printfloat(self):
         jprintfloat = cuda.jit('void()', debug=False)(printfloat)
-        with redirect_fd(1) as stdout:
+        with captured_cuda_stdout() as stdout:
             jprintfloat()
-            cuda.synchronize()
-        self.assertEqual(stdout.read(), "0 23 34.300000 321\n")
+            stdout.sync()
+        self.assertIn(stdout.getvalue(), ["0 23 34.750000 321\n",
+                                          "0 23 34.75 321\n"])
 
     @unittest.skipIf(True, "Print string not implemented yet")
     def test_print_array(self):
