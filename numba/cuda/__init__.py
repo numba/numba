f0610d5eff330b340ca0b767f78a9aebb7f11f72
Graham Markall <graham.markall@continuum.io>
2015-08-31 19:29:02 +0100

    Add support for reduce in the simulator

--- numba/cuda/__init__.py
+++ numba/cuda/__init__.py
@@ -7,9 +7,6 @@ else:
     from .device_init import *
     from .device_init import _auto_device
 
-from . import reduction
-reduce = Reduce = reduction.Reduce
-
 def test():
     if not is_available():
         raise cuda_error()
