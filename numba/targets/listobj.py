2e81a1b89ade3ce5cc3d9fcaaceb3e063d7d329f
Antoine Pitrou <antoine@python.org>
2015-09-16 13:10:49 +0200

    Comment about data vs value

--- numba/targets/listobj.py
+++ numba/targets/listobj.py
@@ -29,6 +29,8 @@ def make_payload_cls(list_type):
     Return the Structure representation of the given *list_type*'s payload
     (an instance of types.List).
     """
+    # Note the payload is stored durably in memory, so we consider it
+    # data and not value.
     return cgutils.create_struct_proxy(types.ListPayload(list_type),
                                        kind='data')
 
