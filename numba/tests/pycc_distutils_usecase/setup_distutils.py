eac28b1247aae7dadb7e6e318826bb9265e71365
Antoine Pitrou <antoine@python.org>
2015-10-14 21:04:36 +0200

    I mismatched filenames and their contents

--- numba/tests/pycc_distutils_usecase/setup_distutils.py
+++ numba/tests/pycc_distutils_usecase/setup_distutils.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from distutils.core import setup
 
 from source_module import cc
 
--- numba/tests/pycc_distutils_usecase/setup_setuptools.py
+++ numba/tests/pycc_distutils_usecase/setup_setuptools.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 from source_module import cc
 
