dd9bd7c7ecf76a2296a1ccb3de6b1bdff4943a3a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-10-27 11:35:39 -0500

    Add test for issue #1498

--- numba/tests/test_indexing.py
+++ numba/tests/test_indexing.py
@@ -630,6 +630,36 @@ class TestGetItem(TestCase):
     def test_ellipsis3_npm(self):
         self.test_ellipsis3(flags=Noflags)
 
+    def test_ellipsis_issue1498(self):
+
+        @njit
+        def udt(arr):
+            out = np.zeros_like(arr)
+            i = 0
+            for index, val in np.ndenumerate(arr[..., i]):
+                out[index][i] = val
+
+            return out
+
+        py_func = udt.py_func
+
+        outersize = 4
+        innersize = 4
+        arr = np.arange(outersize * innersize).reshape(outersize, innersize)
+        print('---arr---')
+        print(arr)
+        got = udt(arr)
+        expected = py_func(arr)
+
+        print('---got---')
+        print(got)
+
+        print('---expected---')
+        print(expected)
+
+        np.testing.assert_equal(got, expected)
+
+
     def test_none_index(self, flags=enable_pyobj_flags):
         pyfunc = none_index_usecase
         arraytype = types.Array(types.int32, 2, 'C')
