0b049e41dafb7b193c87e801845f877b1ef3942f
Stuart Archibald <stuart.archibald@googlemail.com>
2015-12-09 11:40:11 -0600

    HSA: Patch to make tests run again from @sklam
    Re-enables tests discovery and testing for the HSA project.
    (thanks @sklam)

--- numba/hsa/tests/__init__.py
+++ numba/hsa/tests/__init__.py
@@ -1,5 +1,17 @@
-from numba.tests import SerialSuite
+from numba.testing import SerialSuite
+from numba.testing import load_testsuite
+from numba import hsa
+from os.path import dirname, join
 
 def load_tests(loader, tests, pattern):
-    suite = loader.discover("numba.hsa.tests")
-    return SerialSuite(suite)
+
+    suite = SerialSuite()
+    this_dir = dirname(__file__)
+
+    if hsa.is_available():
+        suite.addTests(load_testsuite(loader, join(this_dir, 'hsadrv')))
+        suite.addTests(load_testsuite(loader, join(this_dir, 'hsapy')))
+
+    else:
+        print("skipped HSA tests")
+    return suite
