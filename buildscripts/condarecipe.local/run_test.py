706e899fcfe0ac16709ea4b69ddd5c7239e70245
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-12-10 18:29:06 -0600

    Adapt buildscripts for changes in our testing framework

--- buildscripts/condarecipe.local/run_test.py
+++ buildscripts/condarecipe.local/run_test.py
@@ -1,12 +1,13 @@
 import sys
 import numba
 
+args = []
 if sys.platform.startswith('win32'):
-    sys.argv += ['-b']
+    args += ['-b']
 else:
-    sys.argv += ['-m', '-b']
+    args += ['-m', '-b']
 
-if not numba.test():
+if not numba.test(*args):
     print("Test failed")
     sys.exit(1)
 print('numba.__version__: %s' % numba.__version__)
