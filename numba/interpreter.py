28b741f85c55d7de985007ef8b7ac18aeee78bda
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-01-13 15:31:00 -0600

    Add description of the new _insert_var_dels algorithm

--- numba/interpreter.py
+++ numba/interpreter.py
@@ -244,6 +244,17 @@ class Interpreter(object):
         Insert del statements for each variable.
         Returns a 2-tuple of (variable definition map, variable deletion map)
         which indicates variables defined and deleted in each block.
+
+        The algorithm avoids relying on explicit knowledge on loops and
+        distinguish between variables that are defined locally vs variables that
+        come from incoming blocks.
+        We start with simple usage (variable reference) and definition (variable
+        creation) maps on each block. Propagate the liveness info to predecessor
+        blocks until it stabilize, at which point we know which variables must
+        exist before entering each block. Then, we compute the end of variable
+        lives and insert del statements accordingly. Variables are deleted after
+        the last use. Variable referenced by terminators (e.g. conditional
+        branch and return) are deleted by the successors or the caller.
         """
         var_use_map, var_def_map = self._compute_use_defs()
         live_map = self._compute_live_map(var_use_map, var_def_map)
