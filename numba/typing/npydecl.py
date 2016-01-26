2aae766e42021a999485bb4803d439cff34f49a8
Stuart Archibald <stuart.archibald@googlemail.com>
2016-01-26 21:14:04 +0000

    [#1642] implement np.diag()
    This patch implements `np.diag()` functionality. Tests aim to check
    vector and matrix context with and without a kwarg to specified an
    offset.
    
    NOTE: the implementation returns new data whereas numpy returns
    a RO view. This detail is purely for implementation simplicity on
    the numba side.

--- numba/typing/npydecl.py
+++ numba/typing/npydecl.py
@@ -904,3 +904,20 @@ class Angle(CallableTemplate):
             else:
                 return types.float64
         return typer
+
+@infer_global(numpy.diag)
+class DiagCtor(CallableTemplate):
+    """
+    Typing template for np.diag()
+    """
+    def generic(self):
+        def typer(ref, k = 0):
+            if(isinstance(ref, types.Array)):
+                if ref.ndim == 1:
+                    rdim = 2
+                elif ref.ndim == 2:
+                    rdim = 1
+                else:
+                    return None
+                return types.Array(ndim=rdim, dtype=ref.dtype, layout='C')
+        return typer
