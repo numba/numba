b60b631181766287e138740c247ca284da1242da
Antoine Pitrou <antoine@python.org>
2016-03-21 18:09:44 +0100

    Avoid spurious output in cuda.tests.cudapy.test_debug
    This was caused by early-binding to sys.stdout, which ignored the redirection done by the test.

--- numba/cuda/tests/cudapy/test_debug.py
+++ numba/cuda/tests/cudapy/test_debug.py
@@ -3,7 +3,7 @@ from __future__ import print_function, absolute_import
 import numpy as np
 
 from numba.cuda.testing import skip_on_cudasim
-from numba.tests.support import override_config, captured_stdout
+from numba.tests.support import override_config, captured_stderr, captured_stdout
 from numba import unittest_support as unittest
 from numba import cuda, float64
 
@@ -17,14 +17,17 @@ def simple_cuda(A, B):
 class TestDebugOutput(unittest.TestCase):
 
     def compile_simple_cuda(self):
-        with captured_stdout() as out:
-            cfunc = cuda.jit((float64[:], float64[:]))(simple_cuda)
-            # Call compiled function (to ensure PTX is generated)
-            # and sanity-check results.
-            A = np.linspace(0, 1, 10).astype(np.float64)
-            B = np.zeros_like(A)
-            cfunc[1, 10](A, B)
-            self.assertTrue(np.allclose(A + 1.5, B))
+        with captured_stderr() as err:
+            with captured_stdout() as out:
+                cfunc = cuda.jit((float64[:], float64[:]))(simple_cuda)
+                # Call compiled function (to ensure PTX is generated)
+                # and sanity-check results.
+                A = np.linspace(0, 1, 10).astype(np.float64)
+                B = np.zeros_like(A)
+                cfunc[1, 10](A, B)
+                self.assertTrue(np.allclose(A + 1.5, B))
+        # stderr shouldn't be affected by debug output
+        self.assertFalse(err.getvalue())
         return out.getvalue()
 
     def assert_fails(self, *args, **kwargs):
@@ -52,6 +55,7 @@ class TestDebugOutput(unittest.TestCase):
 
     def _check_dump_ir(self, out):
         self.assertIn('--IR DUMP: simple_cuda--', out)
+        self.assertIn('const(float, 1.5)', out)
 
     def _check_dump_llvm(self, out):
         self.assertIn('--LLVM DUMP', out)
