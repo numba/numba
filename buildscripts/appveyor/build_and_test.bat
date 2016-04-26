e61c86fec04a00108c3549248023961abd7afaac
Antoine Pitrou <antoine@python.org>
2016-04-26 20:31:46 +0200

    Try to fix use of environment variables

--- buildscripts/appveyor/build_and_test.bat
+++ buildscripts/appveyor/build_and_test.bat
@@ -1,15 +1,12 @@
 @rem This script will build and test numba.  llvmlite is installed from
 @rem the repos, so as not to have to build it separately.
 
-conda config --add channels numba
-conda create -q -n testenv python=%CONDA_PY% numpy=%CONDA_NPY% llvmlite cffi scipy
-call activate testenv
 @echo on
 python setup.py build_ext -q --inplace
 
 @rem Run a subset of the test suite, as AppVeyor is quite slow
 @rem (Python 3.4+ only)
 @rem Also, note %CMD_IN_ENV% is needed for distutils/setuptools-based tests
-@rem on Python 2.
+@rem on certain build configurations.
 
 %CMD_IN_ENV% python runtests.py -b --tags important
