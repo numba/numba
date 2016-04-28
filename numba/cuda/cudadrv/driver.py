3719eea1ab6910df42f5da5c9a7e505163259b0a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-04-28 13:33:39 -0500

    Fix #1858

--- numba/cuda/cudadrv/driver.py
+++ numba/cuda/cudadrv/driver.py
@@ -775,8 +775,7 @@ def _module_finalizer(context, handle):
 
     def core():
         def cleanup():
-            if modules:
-                del modules[handle.value]
+            assert handle.value not in modules
             driver.cuModuleUnload(handle)
 
         trashing.add_trash(cleanup)
@@ -1038,14 +1037,8 @@ class Module(object):
         self.handle = handle
         self.info_log = info_log
         self.finalizer = finalizer
-        self.is_managed = self.finalizer is not None
-
-    def __del__(self):
-        try:
-            if self.is_managed:
-                self.finalizer()
-        except:
-            traceback.print_exc()
+        if self.finalizer is not None:
+            self._finalizer = utils.finalize(self, finalizer)
 
     def unload(self):
         self.context.unload_module(self)
