c5ac9b10664dbcd433479f9e3cf3e7adae92833b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-05-02 11:45:34 -0500

    Add comments as per pitrou suggestion

--- numba/cuda/cudadrv/driver.py
+++ numba/cuda/cudadrv/driver.py
@@ -775,6 +775,15 @@ def _module_finalizer(context, handle):
 
     def core():
         def cleanup():
+            # All modules are owned by their parent Context.
+            # A Module is either released by a call to
+            # Context.unload_module, which clear the handle (pointer) mapping
+            # (checked by the following assertion), or, by Context.reset().
+            # Both releases the sole reference to the Module and trigger the
+            # finalizer for the Module instance.  The actual call to
+            # cuModuleUnload is deferred to the trashing service to avoid
+            # further corruption of the CUDA context if a fatal error has
+            # occurred in the CUDA driver.
             assert handle.value not in modules
             driver.cuModuleUnload(handle)
 
