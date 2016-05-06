4b3a4785f81a250ef042a90748f8f2545931f6b6
Stefan Seefeld <stefan@seefeld.name>
2016-05-06 11:29:40 -0400

    Add Windows support to incremental build infrastructure.

--- buildscripts/incremental/create_conda_environment.sh
+++ buildscripts/incremental/create_conda_environment.sh
@@ -2,5 +2,7 @@
 
 # Setup environment
 conda update --yes conda
+# Clean up any left-over from a previous build
+conda env remove -q -y -n $CONDA_ENV
 # Scipy, CFFI and jinja2 are optional dependencies, but exercised in the test suite
 conda create -n $CONDA_ENV --yes python=$PYTHON numpy=$NUMPY cffi pip scipy jinja2
