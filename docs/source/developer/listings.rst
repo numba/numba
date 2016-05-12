8ebbb319ad63b8c40c81403b298ef610fe1bc317
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-05-12 15:09:21 -0500

    Improve formatting and add descriptions

--- docs/source/developer/listings.rst
+++ docs/source/developer/listings.rst
@@ -1,6 +1,10 @@
 Listings
 ========
 
+This shows listings from compiler internal registries  (e.g. lowering
+definitions).  The information is provided as developer reference.
+When possible, links to source code are provided via github links.
+
 .. toctree::
    :maxdepth: 2
 
--- numba/scripts/generate_lower_listing.py
+++ numba/scripts/generate_lower_listing.py
@@ -5,16 +5,21 @@ using reStructured text.
 
 from __future__ import print_function
 
+from subprocess import check_output
+
 import os.path
 from io import StringIO
 from collections import defaultdict
 import inspect
 from functools import partial
-import textwrap
-
 
 import numba
 from numba.targets.registry import cpu_target
+
+
+def git_hash():
+    out = check_output(['git', 'log', "--pretty=format:'%H'", '-n', '1'])
+    return out.decode('ascii').strip("'\"")
 
 
 def gather_function_info(backend):
@@ -32,7 +37,6 @@ def gather_function_info(backend):
                 'name': impl.__qualname__,
                 'filename': os.path.relpath(path, start=basepath),
                 'lines': (firstlineno, firstlineno + len(code) - 1),
-                'source': textwrap.dedent(''.join(code)),
                 'docstring': impl.__doc__
             }
 
@@ -53,6 +57,17 @@ def format_signature(sig):
     return '`({0})`'.format(', '.join(out))
 
 
+github_url = 'https://github.com/numba/numba/blob/{commit}/{path}#L{firstline}-L{lastline}'
+
+description = """
+This lists all lowering definition registered to the CPU target.
+Each subsection corresponds to a Python function that is supported by numba
+nopython mode. These functions have one or more lower implementation with
+different signatures. The compiler chooses the most specific implementation
+from all overloads.
+"""
+
+
 def format_function_infos(fninfos):
     with StringIO() as buf:
         print = bind_file_to_print(buf)
@@ -60,6 +75,10 @@ def format_function_infos(fninfos):
         title_line = "Lowering Listing"
         print(title_line)
         print('=' * len(title_line))
+
+        print(description)
+
+        commit = git_hash()
 
         def format_fname(fn):
             try:
@@ -70,37 +89,49 @@ def format_function_infos(fninfos):
 
         for fn, fname in sorted(map(format_fname, fninfos), key=lambda x: x[1]):
             impinfos = fninfos[fn]
-            header_line = "Lower Impl ``{0}``".format(fname)
+            header_line = "``{0}``".format(fname)
             print(header_line)
             print('-' * len(header_line))
             print()
 
-            formatted_sigs = map(lambda x: format_signature(x['sig']), impinfos)
+            formatted_sigs = map(
+                lambda x: format_signature(x['sig']), impinfos)
             sorted_impinfos = sorted(zip(formatted_sigs, impinfos),
                                      key=lambda x: x[0])
 
-            # generate summary
-            print('Signatures\n')
+            col_signatures = ['Signature']
+            col_urls = ['Definition']
 
             for fmtsig, info in sorted_impinfos:
-                print('* `{0}`'.format(fmtsig))
-            print()
-
-            for fmtsig, info in sorted_impinfos:
-                sig_line = 'signature {0}'.format(fmtsig)
-                print(sig_line)
-                print('~' * len(sig_line))
-                print()
-
                 impl = info['impl']
-                print('by', '``{0}``'.format(impl['name']),
-                      '`{0}`'.format(impl['filename']),
-                      'lines {0}-{1}'.format(*impl['lines']))
-                print()
-                print(".. code-block:: python")
-                print()
-                print(textwrap.indent(impl['source'], prefix=' ' * 4))
-                print()
+
+                filename = impl['filename']
+                lines = impl['lines']
+                fname = impl['name']
+
+                source = '{0} lines {1}-{2}'.format(filename, *lines)
+                link = github_url.format(commit=commit, path=filename,
+                                         firstline=lines[0], lastline=lines[1])
+                url = '``{0}`` `{1} <{2}>`_'.format(fname, source, link)
+
+                col_signatures.append(fmtsig)
+                col_urls.append(url)
+
+            # table formatting
+            max_width_col_sig = max(map(len, col_signatures))
+            max_width_col_url = max(map(len, col_urls))
+            padding = 2
+            width_col_sig = padding * 2 + max_width_col_sig
+            width_col_url = padding * 2 + max_width_col_url
+            line_format = "{{0:^{0}}}  {{1:^{1}}}".format(width_col_sig,
+                                                          width_col_url)
+            print(line_format.format('=' * width_col_sig, '=' * width_col_url))
+            print(line_format.format(col_signatures[0], col_urls[0]))
+            print(line_format.format('=' * width_col_sig, '=' * width_col_url))
+            for sig, url in zip(col_signatures[1:], col_urls[1:]):
+                print(line_format.format(sig, url))
+            print(line_format.format('=' * width_col_sig, '=' * width_col_url))
+            print()
 
         return buf.getvalue()
 
