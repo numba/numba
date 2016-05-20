9e7abdc825e72fd32d431a5ca5c65c1ddaca45da
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-05-20 17:27:36 -0500

    Revert "Workaround missing dependency for funcsigs"
    This reverts commit 52ddf17bf806d0f9bc5fdc5de749bc7a920b5661.
    and set build branch to release0.26.0

--- buildscripts/condarecipe.hsa/bld.bat
+++ buildscripts/condarecipe.hsa/bld.bat
@@ -1,5 +1,3 @@
-@rem Hack to workaround https://github.com/testing-cabal/funcsigs/issues/18
-%PYTHON% -m pip install ordereddict
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
 
--- buildscripts/condarecipe.local/bld.bat
+++ buildscripts/condarecipe.local/bld.bat
@@ -1,4 +1,2 @@
-@rem Hack to workaround https://github.com/testing-cabal/funcsigs/issues/18
-%PYTHON% -m pip install ordereddict
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
