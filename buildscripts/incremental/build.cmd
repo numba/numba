b98458cfd275e85c5ad57dce05a1cbd43121fd06
Antoine Pitrou <antoine@python.org>
2016-07-25 21:25:12 +0200

    Try building without installing

--- buildscripts/incremental/build.cmd
+++ buildscripts/incremental/build.cmd
@@ -3,4 +3,3 @@ call activate %CONDA_ENV%
 
 @rem Build numba extensions without silencing compile errors
 python setup.py build_ext -q --inplace
-python setup.py install -q > NUL
--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -7,4 +7,5 @@ set -v -e
 
 # Build numba extensions without silencing compile errors
 python setup.py build_ext -q --inplace
-python setup.py install -q >/dev/null
+# (note we don't install to avoid problems with extra long Windows paths
+#  during distutils-dependent tests -- e.g. test_pycc)
