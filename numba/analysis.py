b80ed3e695583811f84f2b45b6d89384fd6edc73
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-07-27 14:06:31 -0500

    Move live variable compute

--- numba/analysis.py
+++ numba/analysis.py
@@ -141,3 +141,43 @@ def compute_dead_maps(cfg, blocks, live_map, var_def_map):
     return _dead_maps_result(internal=internal_dead_map,
                              escaping=escaping_dead_map)
 
+
+def compute_live_variables(cfg, blocks, var_def_map, var_dead_map):
+    """
+    Compute the live variables at the beginning of each block
+    and at each yield point.
+    The ``var_def_map`` and ``var_dead_map`` indicates the variable defined
+    and deleted at each block, respectively.
+    """
+    # live var at the entry per block
+    block_entry_vars = defaultdict(set)
+
+    def fix_point_progress():
+        return tuple(map(len, block_entry_vars.values()))
+
+    old_point = None
+    new_point = fix_point_progress()
+
+    # Propagate defined variables and still live the successors.
+    # (note the entry block automatically gets an empty set)
+
+    # Note: This is finding the actual available variables at the entry
+    #       of each block. The algorithm in compute_live_map() is finding
+    #       the variable that must be available at the entry of each block.
+    #       This is top-down in the dataflow.  The other one is bottom-up.
+    while old_point != new_point:
+        # We iterate until the result stabilizes.  This is necessary
+        # because of loops in the graphself.
+        for offset in blocks:
+            # vars available + variable defined
+            avail = block_entry_vars[offset] | var_def_map[offset]
+            # substract variables deleted
+            avail -= var_dead_map[offset]
+            # add ``avail`` to each successors
+            for succ, _data in cfg.successors(offset):
+                block_entry_vars[succ] |= avail
+
+        old_point = new_point
+        new_point = fix_point_progress()
+
+    return block_entry_vars
--- numba/interpreter.py
+++ numba/interpreter.py
@@ -187,40 +187,10 @@ class Interpreter(object):
         The ``var_def_map`` and ``var_dead_map`` indicates the variable defined
         and deleted at each block, respectively.
         """
-        # live var at the entry per block
-        block_entry_vars = collections.defaultdict(set)
-
-        def fix_point_progress():
-            return tuple(map(len, block_entry_vars.values()))
-
-        cfg = self.cfa.graph
-        old_point = None
-        new_point = fix_point_progress()
-
-        # Propagate defined variables and still live the successors.
-        # (note the entry block automatically gets an empty set)
-
-        # Note: This is finding the actual available variables at the entry
-        #       of each block. The algorithm in compute_live_map() is finding
-        #       the variable that must be available at the entry of each block.
-        #       This is top-down in the dataflow.  The other one is bottom-up.
-        while old_point != new_point:
-            # We iterate until the result stabilizes.  This is necessary
-            # because of loops in the graphself.
-            for offset in self.blocks:
-                # vars available + variable defined
-                avail = block_entry_vars[offset] | var_def_map[offset]
-                # substract variables deleted
-                avail -= var_dead_map[offset]
-                # add ``avail`` to each successors
-                for succ, _data in cfg.successors(offset):
-                    block_entry_vars[succ] |= avail
-
-            old_point = new_point
-            new_point = fix_point_progress()
-
+        bev = analysis.compute_live_variables(self.cfa.graph, self.blocks,
+                                              var_def_map, var_dead_map)
         for offset, ir_block in self.blocks.items():
-            self.block_entry_vars[ir_block] = block_entry_vars[offset]
+            self.block_entry_vars[ir_block] = bev[offset]
 
     def _populate_generator_info(self):
         dct = self.generator_info.yield_points
