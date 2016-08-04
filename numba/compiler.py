4bebf7a743a3c53eef16956a822977d61f8cd4b4
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-08-04 17:37:56 -0500

    Remove code duplication

--- numba/compiler.py
+++ numba/compiler.py
@@ -640,12 +640,16 @@ class Pipeline(object):
         if self.interp is not None:
             self.interp.reset()
 
-    def _compile_bytecode(self):
+    def _compile_core(self):
+        """
+        Populate and run compiler pipeline
+        """
         pm = _PipelineManager()
 
         if not self.flags.force_pyobject:
             pm.create_pipeline("nopython")
-            pm.add_stage(self.stage_analyze_bytecode, "analyzing bytecode")
+            if self.interp is None:
+                pm.add_stage(self.stage_analyze_bytecode, "analyzing bytecode")
             if not self.flags.no_rewrites:
                 pm.add_stage(self.stage_generic_rewrites, "nopython rewrites")
             pm.add_stage(self.stage_nopython_frontend, "nopython frontend")
@@ -657,7 +661,8 @@ class Pipeline(object):
 
         if self.status.can_fallback or self.flags.force_pyobject:
             pm.create_pipeline("object")
-            pm.add_stage(self.stage_analyze_bytecode, "analyzing bytecode")
+            if self.interp is None:
+                pm.add_stage(self.stage_analyze_bytecode, "analyzing bytecode")
             pm.add_stage(self.stage_objectmode_frontend, "object mode frontend")
             pm.add_stage(self.stage_annotate_type, "annotate type")
             pm.add_stage(self.stage_objectmode_backend, "object mode backend")
@@ -676,41 +681,20 @@ class Pipeline(object):
         else:
             assert self.cr is not None
             return self.cr
+
+    def _compile_bytecode(self):
+        """
+        Populate and run pipeline for bytecode input
+        """
+        assert self.interp is None
+        return self._compile_core()
 
     def _compile_ir(self):
-        pm = _PipelineManager()
-
-        if not self.flags.force_pyobject:
-            pm.create_pipeline("nopython")
-            if not self.flags.no_rewrites:
-                pm.add_stage(self.stage_generic_rewrites, "nopython rewrites")
-            pm.add_stage(self.stage_nopython_frontend, "nopython frontend")
-            pm.add_stage(self.stage_annotate_type, "annotate type")
-            if not self.flags.no_rewrites:
-                pm.add_stage(self.stage_nopython_rewrites, "nopython rewrites")
-            pm.add_stage(self.stage_nopython_backend, "nopython mode backend")
-            pm.add_stage(self.stage_cleanup, "cleanup intermediate results")
-
-        if self.status.can_fallback or self.flags.force_pyobject:
-            pm.create_pipeline("object")
-            pm.add_stage(self.stage_objectmode_frontend, "object mode frontend")
-            pm.add_stage(self.stage_annotate_type, "annotate type")
-            pm.add_stage(self.stage_objectmode_backend, "object mode backend")
-            pm.add_stage(self.stage_cleanup, "cleanup intermediate results")
-
-        if self.status.can_giveup:
-            pm.create_pipeline("interp")
-            pm.add_stage(self.stage_compile_interp_mode, "compiling with interpreter mode")
-            pm.add_stage(self.stage_cleanup, "cleanup intermediate results")
-
-        pm.finalize()
-        res = pm.run(self.status)
-        if res is not None:
-            # Early pipeline completion
-            return res
-        else:
-            assert self.cr is not None
-            return self.cr
+        """
+        Populate and run pipeline for IR input
+        """
+        assert self.interp is not None
+        return self._compile_core()
 
 
 def _make_subtarget(targetctx, flags):
