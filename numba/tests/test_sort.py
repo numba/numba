7ec4f1921f2abb0db57dbd93a0cdbde59cc059d4
Antoine Pitrou <antoine@python.org>
2016-08-15 11:32:08 +0200

    Implement argsort()

--- numba/tests/test_sort.py
+++ numba/tests/test_sort.py
@@ -41,6 +41,9 @@ jit_quicksort = make_jit_quicksort()
 def sort_usecase(val):
     val.sort()
 
+def argsort_usecase(val):
+    return val.argsort()
+
 def sorted_usecase(val):
     return sorted(val)
 
@@ -49,6 +52,9 @@ def sorted_reverse_usecase(val, b):
 
 def np_sort_usecase(val):
     return np.sort(val)
+
+def np_argsort_usecase(val):
+    return np.argsort(val)
 
 def list_sort_usecase(n):
     np.random.seed(42)
@@ -704,6 +710,29 @@ class TestNumpySort(TestCase):
     def setUp(self):
         np.random.seed(42)
 
+    def int_arrays(self):
+        for size in (5, 20, 50, 500):
+            yield np.random.randint(99, size=size)
+
+    def float_arrays(self):
+        for size in (5, 20, 50, 500):
+            yield np.random.random(size=size) * 100
+        # Now with NaNs.  Numpy sorts them at the end.
+        for size in (5, 20, 50, 500):
+            orig = np.random.random(size=size) * 100
+            orig[np.random.random(size=size) < 0.1] = float('nan')
+            yield orig
+
+    def has_duplicates(self, arr):
+        """
+        Whether the array has duplicates.  Takes NaNs into account.
+        """
+        if np.isnan(arr).size > 1:
+            return True
+        if np.unique(arr).size < arr.size:
+            return True
+        return False
+
     def check_sort_inplace(self, pyfunc, cfunc, val):
         expected = copy.copy(val)
         got = copy.copy(val)
@@ -719,12 +748,24 @@ class TestNumpySort(TestCase):
         # The original wasn't mutated
         self.assertPreciseEqual(val, orig)
 
+    def check_argsort(self, pyfunc, cfunc, val):
+        orig = copy.copy(val)
+        expected = pyfunc(val)
+        got = cfunc(val)
+        self.assertPreciseEqual(orig[got], np.sort(orig),
+                                msg="the array wasn't argsorted")
+        # Numba and Numpy results may differ if there are duplicates
+        # in the array
+        if not self.has_duplicates(orig):
+            self.assertPreciseEqual(got, expected)
+        # The original wasn't mutated
+        self.assertPreciseEqual(val, orig)
+
     def test_array_sort_int(self):
         pyfunc = sort_usecase
         cfunc = jit(nopython=True)(pyfunc)
 
-        for size in (5, 20, 50, 500):
-            orig = np.random.randint(99, size=size)
+        for orig in self.int_arrays():
             self.check_sort_inplace(pyfunc, cfunc, orig)
 
     @tag('important')
@@ -732,22 +773,14 @@ class TestNumpySort(TestCase):
         pyfunc = sort_usecase
         cfunc = jit(nopython=True)(pyfunc)
 
-        for size in (5, 20, 50, 500):
-            orig = np.random.random(size=size) * 100
-            self.check_sort_inplace(pyfunc, cfunc, orig)
-
-        # Now with NaNs.  Numpy sorts them at the end.
-        for size in (5, 20, 50, 500):
-            orig = np.random.random(size=size) * 100
-            orig[np.random.random(size=size) < 0.1] = float('nan')
+        for orig in self.float_arrays():
             self.check_sort_inplace(pyfunc, cfunc, orig)
 
     def test_np_sort_int(self):
         pyfunc = np_sort_usecase
         cfunc = jit(nopython=True)(pyfunc)
 
-        for size in (5, 20, 50, 500):
-            orig = np.random.randint(99, size=size)
+        for orig in self.int_arrays():
             self.check_sort_copy(pyfunc, cfunc, orig)
 
     def test_np_sort_float(self):
@@ -758,6 +791,25 @@ class TestNumpySort(TestCase):
             orig = np.random.random(size=size) * 100
             orig[np.random.random(size=size) < 0.1] = float('nan')
             self.check_sort_copy(pyfunc, cfunc, orig)
+
+    def test_argsort_int(self):
+        def check(pyfunc):
+            cfunc = jit(nopython=True)(pyfunc)
+            for orig in self.int_arrays():
+                self.check_argsort(pyfunc, cfunc, orig)
+
+        check(argsort_usecase)
+        check(np_argsort_usecase)
+
+    @tag('important')
+    def test_argsort_float(self):
+        def check(pyfunc):
+            cfunc = jit(nopython=True)(pyfunc)
+            for orig in self.float_arrays():
+                self.check_argsort(pyfunc, cfunc, orig)
+
+        check(argsort_usecase)
+        check(np_argsort_usecase)
 
 
 class TestPythonSort(TestCase):
