5b69afac9fb2c80c6187fac2ebaa57223362f17b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-08-22 12:55:07 -0500

    Add test for #2051

--- numba/tests/test_tuples.py
+++ numba/tests/test_tuples.py
@@ -420,6 +420,20 @@ class TestNamedTuple(TestCase, MemoryLeakMixin):
             self.assertPreciseEqual(got, expected)
 
 
+class TestTupleNRT(TestCase, MemoryLeakMixin):
+    def test_tuple_add(self):
+        def pyfunc(x):
+            a = np.arange(3)
+            return (a,) + (x,)
+
+        cfunc = jit(nopython=True)(pyfunc)
+        x = 123
+        expect_a, expect_x = pyfunc(x)
+        got_a, got_x = cfunc(x)
+        np.testing.assert_equal(got_a, expect_a)
+        self.assertEqual(got_x, expect_x)
+
+
 class TestNamedTupleNRT(TestCase, MemoryLeakMixin):
 
     def test_return(self):
