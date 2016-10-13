9c1597a0a2273820b10b70bfb4c321694fa1f943
Siu Kwan Lam <michael.lam.sk@gmail.com>
2016-10-13 09:37:50 -0500

    Explain call_unresolved

--- numba/targets/base.py
+++ numba/targets/base.py
@@ -797,6 +797,31 @@ class BaseContext(object):
         Insert a function call to an unresolved symbol with the given *name*.
 
         Note: this is used for recursive call.
+
+        In the mutual recursion case::
+
+            @njit
+            def foo():
+                ...  # calls bar()
+
+            @njit
+            def bar():
+                ... # calls foo()
+
+            foo()
+
+        When foo() is called, the compilation of bar() is fully completed
+        (codegen'ed and loaded) before foo() is. Since MCJIT's eager compilation
+        doesn't allow loading modules with declare-only functions (which is
+        needed for foo() in bar()), the call_unresolved injects a global
+        variable that the "linker" can update even after the module is loaded by
+        MCJIT. The linker would allocate space for the global variable before
+        the bar() module is loaded. When later foo() module is defined, it will
+        update bar()'s reference to foo().
+
+        The legacy lazy JIT and the new ORC JIT would allow a declare-only
+        function be used in a module as long as it is defined by the time of its
+        first use.
         """
         # Insert an unresolved reference to the function being called.
         codegen = self.codegen()
