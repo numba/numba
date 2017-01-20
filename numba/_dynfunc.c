7d635701515280cefcca67e42da127057536bb7c
Graham Markall <graham.markall@embecosm.com>
2017-01-20 21:34:49 +0000

    Use PyObject_GC_UnTrack to untrack objects
    _PyObject_GC_UNTRACK is not supported by PyPy's cpyext.
    PyObject_GC_UnTrack performs the same function, but with a check
    that allows it to be called twice on an object. This commit simply
    changes _PyObject_GC_UNTRACK to PyObject_GC_UnTrack, as it is not
    expected to have an impact on performance.

--- numba/_dynfunc.c
+++ numba/_dynfunc.c
@@ -51,7 +51,7 @@ env_clear(EnvironmentObject *env)
 static void
 env_dealloc(EnvironmentObject *env)
 {
-    _PyObject_GC_UNTRACK((PyObject *) env);
+    PyObject_GC_UnTrack((PyObject *) env);
     env_clear(env);
     Py_TYPE(env)->tp_free((PyObject *) env);
 }
@@ -174,7 +174,7 @@ closure_traverse(ClosureObject *clo, visitproc visit, void *arg)
 static void
 closure_dealloc(ClosureObject *clo)
 {
-    _PyObject_GC_UNTRACK((PyObject *) clo);
+    PyObject_GC_UnTrack((PyObject *) clo);
     if (clo->weakreflist != NULL)
         PyObject_ClearWeakRefs((PyObject *) clo);
     PyObject_Free((void *) clo->def.ml_name);
@@ -343,7 +343,7 @@ generator_clear(GeneratorObject *gen)
 static void
 generator_dealloc(GeneratorObject *gen)
 {
-    _PyObject_GC_UNTRACK((PyObject *) gen);
+    PyObject_GC_UnTrack((PyObject *) gen);
     if (gen->weakreflist != NULL)
         PyObject_ClearWeakRefs((PyObject *) gen);
     /* XXX The finalizer may be called after the LLVM module has been
