62d5bf17789beb326f4512612a00c660bc4af8ed
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-01-26 13:43:28 -0600

    Update test that depends on the mangler

--- numba/tests/test_inlining.py
+++ numba/tests/test_inlining.py
@@ -1,9 +1,10 @@
 from __future__ import print_function, absolute_import
 
+import re
+
 from .support import TestCase, override_config, captured_stdout
 from numba import unittest_support as unittest
 from numba import jit, types
-from numba.compiler import compile_isolated
 
 
 @jit((types.int32,), nopython=True)
@@ -29,6 +30,21 @@ class TestInlining(TestCase):
     We just trust LLVM's inlining heuristics.
     """
 
+    def make_pattern(self, fullname):
+        """
+        Make regexpr to match mangled name
+        """
+        parts = fullname.split('.')
+        return r'_ZN?' + r''.join([r'\d+{}'.format(p) for p in parts])
+
+    def assert_has_pattern(self, fullname, text):
+        pat = self.make_pattern(fullname)
+        self.assertIsNotNone(re.search(pat, text), msg=text)
+
+    def assert_not_has_pattern(self, fullname, text):
+        pat = self.make_pattern(fullname)
+        self.assertIsNone(re.search(pat, text), msg=text)
+
     def test_inner_function(self):
         with override_config('DUMP_ASSEMBLY', True):
             with captured_stdout() as out:
@@ -38,8 +54,8 @@ class TestInlining(TestCase):
         # guarantees it was inlined into the outer function).
         asm = out.getvalue()
         prefix = __name__
-        self.assertIn('%s.outer_simple' % prefix, asm)
-        self.assertNotIn('%s.inner' % prefix, asm)
+        self.assert_has_pattern('%s.outer_simple' % prefix, asm)
+        self.assert_not_has_pattern('%s.inner' % prefix, asm)
 
     def test_multiple_inner_functions(self):
         # Same with multiple inner functions, and multiple calls to
@@ -51,9 +67,9 @@ class TestInlining(TestCase):
         self.assertPreciseEqual(cfunc(1), 6)
         asm = out.getvalue()
         prefix = __name__
-        self.assertIn('%s.outer_multiple' % prefix, asm)
-        self.assertNotIn('%s.more' % prefix, asm)
-        self.assertNotIn('%s.inner' % prefix, asm)
+        self.assert_has_pattern('%s.outer_multiple' % prefix, asm)
+        self.assert_not_has_pattern('%s.more' % prefix, asm)
+        self.assert_not_has_pattern('%s.inner' % prefix, asm)
 
 
 if __name__ == '__main__':
--- numba/tests/test_mangling.py
+++ numba/tests/test_mangling.py
@@ -4,6 +4,8 @@ Test function name mangling.
 The mangling affects the ABI of numba compiled binaries.
 """
 
+import re
+
 from numba import types
 from numba.funcdesc import default_mangler
 from .support import unittest, TestCase
@@ -14,22 +16,27 @@ class TestMangling(TestCase):
         fname = 'foo'
         argtypes = types.int32,
         name = default_mangler(fname, argtypes)
-        self.assertEqual(name, 'foo.int32')
+        self.assertEqual(name, '_Z3fooi')
 
     def test_two_args(self):
         fname = 'foo'
         argtypes = types.int32, types.float32
         name = default_mangler(fname, argtypes)
-        self.assertEqual(name, 'foo.int32.float32')
+        self.assertEqual(name, '_Z3fooif')
 
     def test_unicode_fname(self):
         fname = u'foಠ'
         argtypes = types.int32, types.float32
         name = default_mangler(fname, argtypes)
         self.assertIsInstance(name, str)
-        expect = 'fo\\u{0:04x}.int32.float32'.format(ord(u'ಠ'))
+        # manually encode it
+        unichar = fname[2]
+        enc = ''.join('${:02x}'.format(c) for c in unichar.encode('utf8'))
+        text = 'fo' + enc
+        expect = '_Z{}{}if'.format(len(text), text)
         self.assertEqual(name, expect)
-
+        # ensure result chars are in the right charset
+        self.assertIsNotNone(re.match(r'^_Z[a-zA-Z0-9_\$]+$', name))
 
 if __name__ == '__main__':
     unittest.main()
