4f739c08e86e0d088be57d88f348a72022f2cac8
Paul H. Liu <hai.liu@intel.com>
2017-02-14 01:54:13 -0800

    In addition to string, allow GUFunc signature to be passed as data structure for internal use

--- numba/npyufunc/ufuncbuilder.py
+++ numba/npyufunc/ufuncbuilder.py
@@ -264,7 +264,11 @@ class GUFuncBuilder(_BaseUFuncBuilder):
         self.identity = parse_identity(identity)
         self.nb_func = jit(target='npyufunc', cache=cache)(py_func)
         self.signature = signature
-        self.sin, self.sout = parse_signature(signature)
+        # allow internal use to pass in data structure that represents signature
+        if isinstance(signature, tuple):
+          self.sin, self.sout = signature
+        else:
+          self.sin, self.sout = parse_signature(signature)
         self.targetoptions = targetoptions
         self.cache = cache
         self._sigs = []
