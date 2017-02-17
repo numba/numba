b7f1ce41d0e8a42ff169694c0f02edbfb9bb1571
Paul H. Liu <hai.liu@intel.com>
2017-02-17 08:35:02 -0800

    Remove redudant function

--- numba/npyufunc/parfor.py
+++ numba/npyufunc/parfor.py
@@ -369,29 +369,6 @@ class LegalizeNames(ast.NodeTransformer):
         ast.fix_missing_locations(new_node)
         return ast.Name(new_name, node.ctx)
 
-@contextlib.contextmanager
-def _legalize_parameter_names(var_list):
-    """
-    Legalize names in the variable list for use as a Python function's
-    parameter names.
-    """
-    var_map = OrderedDict()
-    for var in var_list:
-        old_name = var.name
-        new_name = old_name.replace("$", "_").replace(".", "_")
-        # Caller should ensure the names are unique
-        assert new_name not in var_map
-        var_map[new_name] = var, old_name
-        var.name = new_name
-    param_names = list(var_map)
-    try:
-        yield param_names
-    finally:
-        # Make sure the old names are restored, to avoid confusing
-        # other parts of Numba (see issue #1466)
-        for var, old_name in var_map.values():
-            var.name = old_name
-
 
 def _lower_parfor(lowerer, expr):
     '''Lower an array expression built by RewriteParfor.
