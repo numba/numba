066865dd3b9e7408936b17d7b38e1c1c45e53e90
Ehsan Totoni <ehsan.totoni@intel.com>
2017-03-16 10:32:44 -0700

    clean up examples (time, etc)

--- examples/blackscholes/blackscholes_vec.py
+++ examples/blackscholes/blackscholes_vec.py
@@ -1,53 +1,40 @@
-import numba 
+import numba
 import numpy as np
 # from scipy.special import erf
 import math
 import argparse
 import time
 
-parallel = numba.config.NUMBA_NUM_THREADS > 1
-
 @numba.vectorize(nopython=True)
 def cndf2(inp):
     out = 0.5 + 0.5 * math.erf(0.707106781 * inp)
     return out
 
-#@numba.njit(parallel=parallel)
 @numba.vectorize('f8(f8,f8,f8,f8,f8)', target="parallel")
-def blackscholes(sptprice, strike, rate, volatility, time):
+def blackscholes(sptprice, strike, rate, volatility, timev):
     logterm = np.log10(sptprice / strike)
     powterm = 0.5 * volatility * volatility
-    den = volatility * np.sqrt(time)
-    d1 = (((rate + powterm) * time) + logterm) / den
+    den = volatility * np.sqrt(timev)
+    d1 = (((rate + powterm) * timev) + logterm) / den
     d2 = d1 - den
     NofXd1 = cndf2(d1)
     NofXd2 = cndf2(d2)
-    futureValue = strike * np.exp(- rate * time)
+    futureValue = strike * np.exp(- rate * timev)
     c1 = futureValue * NofXd2
     call = sptprice * NofXd1 - c1
     put  = call - futureValue + sptprice
     return put
-
-all_tics = []
-def tic():
-    global all_tics
-    all_tics.append(time.time())
-
-def toq():
-    global all_tics
-    return (time.time() - all_tics.pop())
-
 
 def run(iterations):
     sptprice   = np.full((iterations,), 42.0)
     initStrike = 40 + (np.arange(iterations) + 1.0) / iterations
     rate       = np.full((iterations,), 0.5)
     volatility = np.full((iterations,), 0.2)
-    time       = np.full((iterations,), 0.5)
+    timev       = np.full((iterations,), 0.5)
 
-    tic()
-    put = blackscholes(sptprice, initStrike, rate, volatility, time)
-    t = toq()
+    t1 = time.time()
+    put = blackscholes(sptprice, initStrike, rate, volatility, timev)
+    t = time.time()-t1
     print("checksum: ", sum(put))
     print("SELFTIMED ", t)
 
--- examples/logistic-regression/logistic_regression.py
+++ examples/logistic-regression/logistic_regression.py
@@ -3,22 +3,13 @@ import numpy as np
 import argparse
 import time
 
-parallel = numba.config.NUMBA_NUM_THREADS > 1
+run_parallel = numba.config.NUMBA_NUM_THREADS > 1
 
-@numba.njit(parallel=parallel)
+@numba.njit(parallel=run_parallel)
 def logistic_regression(Y,X,w,iterations):
     for i in range(iterations):
         w -= np.dot(((1.0 / (1.0 + np.exp(-Y * np.dot(X,w))) - 1.0) * Y),X)
     return w
-
-all_tics = []
-def tic():
-    global all_tics
-    all_tics.append(time.time())
-
-def toq():
-    global all_tics
-    return (time.time() - all_tics.pop())
 
 def main():
     parser = argparse.ArgumentParser(description='Logistic Regression.')
@@ -34,9 +25,9 @@ def main():
     points = np.ones((N, D))
     labels = np.ones(N)
     w = 2.0*np.ones(D)-1.3
-    tic()
+    t1 = time.time()
     w = logistic_regression(labels, points, w, iterations)
-    compiletime = toq()
+    compiletime = time.time()-t1
     print("SELFPRIMED ", compiletime)
     print("checksum ", w)
 
@@ -47,9 +38,9 @@ def main():
     points = np.random.random((N, D))
     labels = np.random.random(N)
     w = 2.0*np.ones(D)-1.3
-    tic()
+    t2 = time.time()
     w = logistic_regression(labels, points, w, iterations)
-    selftimed = toq()
+    selftimed = time.time()-t2
     print("SELFTIMED ", selftimed)
     print("checksum: ", np.sum(w))
 
