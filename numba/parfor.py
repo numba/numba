a4d808493ac1ac3964e8fd460818813bc1256b3e
Ehsan Totoni <ehsan.totoni@intel.com>
2017-05-23 10:23:53 -0700

    parfor dead code add back edge for proper liveness

--- numba/parfor.py
+++ numba/parfor.py
@@ -1227,24 +1227,30 @@ def remove_dead_parfor_recursive(parfor, lives, args):
     first_body_block = min(blocks.keys())
     assert first_body_block > 0 # we are using 0 for init block here
     last_label = max(blocks.keys())
+    return_label = last_label+1
     if len(blocks[last_label].body) == 0:
         return
     loc = blocks[last_label].body[-1].loc
     scope = blocks[last_label].scope
+    blocks[return_label] = ir.Block(scope, loc)
 
     # add dummy jump in init_block for CFG to work
     blocks[0] = parfor.init_block
     blocks[0].body.append(ir.Jump(first_body_block, loc))
+
     # add lives in a dummpy return to last block to avoid their removal
     tuple_var = ir.Var(scope, mk_unique_var("$tuple_var"), loc)
     live_vars = [ ir.Var(scope,v,loc) for v in lives ]
     tuple_call = ir.Expr.build_tuple(live_vars, loc)
-    blocks[last_label].body.append(ir.Assign(tuple_call, tuple_var, loc))
-    blocks[last_label].body.append(ir.Return(tuple_var,loc))
+    blocks[return_label].body.append(ir.Assign(tuple_call, tuple_var, loc))
+    blocks[return_label].body.append(ir.Return(tuple_var,loc))
+
+    branch = ir.Branch(0, first_body_block, return_label, loc)
+    blocks[last_label].body.append(branch)
+
     remove_dead(blocks, args)
     blocks[0].body.pop() # remove dummy jump
-    blocks[last_label].body.pop() # remove dummy return
-    blocks[last_label].body.pop() # remove dummy tupple
+    blocks[last_label].body.pop() # remove branch
     return
 
 def remove_dead_class_sizes(blocks, array_analysis):
