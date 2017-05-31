cd9b38213648772298e658094d28b9cf216b7034
Ehsan Totoni <ehsan.totoni@intel.com>
2017-05-31 14:13:04 -0700

    remove dead no side effect add dot_3_mv_check_args

--- numba/ir_utils.py
+++ numba/ir_utils.py
@@ -456,6 +456,12 @@ def has_no_side_effect(rhs, lives, call_table):
         call_list = call_table[func_name]
         if call_list==['empty', numpy] or call_list==[slice]:
             return True
+        from numba.targets.registry import CPUDispatcher
+        from numba.targets.linalg import dot_3_mv_check_args
+        if isinstance(call_list[0], CPUDispatcher):
+            py_func = call_list[0].py_func
+            if py_func==dot_3_mv_check_args:
+                return True
         return False
     if isinstance(rhs, ir.Expr) and rhs.op=='inplace_binop':
         return rhs.lhs.name not in lives
