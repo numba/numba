4e235d0a5916eb7599af098de86c0bb6e07a45a9
Ehsan Totoni <ehsan.totoni@intel.com>
2017-06-25 11:02:48 -0700

    minor call_table fix

--- numba/ir_utils.py
+++ numba/ir_utils.py
@@ -491,7 +491,7 @@ def has_no_side_effect(rhs, lives, call_table):
     # TODO: find side-effect free calls like Numpy calls
     if isinstance(rhs, ir.Expr) and rhs.op == 'call':
         func_name = rhs.func.name
-        if func_name not in call_table:
+        if func_name not in call_table or call_table[func_name]==[]:
             return False
         call_list = call_table[func_name]
         if call_list == ['empty', numpy] or call_list == [slice]:
