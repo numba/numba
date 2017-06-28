fb6970fdafede36e540bb11cca24cb2166e94a0c
Ehsan Totoni <ehsan.totoni@intel.com>
2017-06-27 19:43:07 -0700

    remove leftover merge code

--- numba/special.py
+++ numba/special.py
@@ -3,8 +3,4 @@ from __future__ import print_function, division, absolute_import
 from .typing.typeof import typeof
 from .parfor import prange
 
-def set_user_pipeline_func(func):
-    from .compiler import set_user_pipeline_func
-    set_user_pipeline_func(func)
-
-__all__ = ['typeof','prange','set_user_pipeline_func']
+__all__ = ['typeof','prange']
