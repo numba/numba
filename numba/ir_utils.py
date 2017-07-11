5d0096018edb201c3669a6ab86f4e9d259740d9c
Todd A. Anderson <todd.a.anderson@intel.com>
2017-07-11 11:10:16 -0700

    Add simplify routine.

--- numba/ir_utils.py
+++ numba/ir_utils.py
@@ -1097,3 +1097,22 @@ def merge_adjacent_blocks(func_ir):
         for stmts in next_block.body:
             block.body.append(stmts)
         del func_ir.blocks[next_label]
+
+def simplify(func_ir, typemap, array_analysis, calltypes, update_analysis):
+    remove_dels(func_ir.blocks)
+    # get copies in to blocks and out from blocks
+    in_cps, out_cps = copy_propagate(func_ir.blocks, typemap)
+    # table mapping variable names to ir.Var objects to help replacement
+    name_var_table = get_name_var_table(func_ir.blocks)
+    apply_copy_propagate(
+        func_ir.blocks,
+        in_cps,
+        name_var_table,
+        update_analysis,
+        array_analysis,
+        typemap,
+        calltypes)
+    # remove dead code to enable fusion
+    remove_dead(func_ir.blocks, func_ir.arg_names, typemap)
+    if config.DEBUG_ARRAY_OPT == 1:
+        dprint_func_ir(func_ir, "after simplify")
