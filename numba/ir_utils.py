13a4a2d50ee79e865b22b1218e1d8579f0267cb4
Ehsan Totoni <ehsan.totoni@intel.com>
2017-07-12 18:02:26 -0700

    get_array_access support multiple accesses to same array (for stencil)

--- numba/ir_utils.py
+++ numba/ir_utils.py
@@ -1075,24 +1075,24 @@ array_accesses_extensions = {}
 
 
 def get_array_accesses(blocks, accesses=None):
-    """returns a dictionary of arrays accessed and their indices.
+    """returns a set of arrays accessed and their indices.
     """
     if accesses is None:
-        accesses = {}
+        accesses = set()
 
     for block in blocks.values():
         for inst in block.body:
             if isinstance(inst, ir.SetItem):
-                accesses[inst.target.name] = inst.index.name
+                accesses.add((inst.target.name, inst.index.name))
             if isinstance(inst, ir.StaticSetItem):
-                accesses[inst.target.name] = inst.index_var.name
+                accesses.add((inst.target.name, inst.index_var.name))
             if isinstance(inst, ir.Assign):
                 lhs = inst.target.name
                 rhs = inst.value
                 if isinstance(rhs, ir.Expr) and rhs.op == 'getitem':
-                    accesses[rhs.value.name] = rhs.index.name
+                    accesses.add((rhs.value.name, rhs.index.name))
                 if isinstance(rhs, ir.Expr) and rhs.op == 'static_getitem':
-                    accesses[rhs.value.name] = rhs.index_var.name
+                    accesses.add((rhs.value.name, rhs.index_var.name))
             for T, f in array_accesses_extensions.items():
                 if isinstance(inst, T):
                     f(inst, accesses)
