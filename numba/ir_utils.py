f0180fa633fbc0930d4670f479475133022d4696
Ehsan Totoni <ehsan.totoni@intel.com>
2017-07-24 11:25:31 -0700

    get static array access fix

--- numba/ir_utils.py
+++ numba/ir_utils.py
@@ -1092,7 +1092,7 @@ def get_array_accesses(blocks, accesses=None):
                 if isinstance(rhs, ir.Expr) and rhs.op == 'getitem':
                     accesses.add((rhs.value.name, rhs.index.name))
                 if isinstance(rhs, ir.Expr) and rhs.op == 'static_getitem':
-                    accesses.add((rhs.value.name, rhs.index_var.name))
+                    accesses.add((rhs.value.name, rhs.index))
             for T, f in array_accesses_extensions.items():
                 if isinstance(inst, T):
                     f(inst, accesses)
