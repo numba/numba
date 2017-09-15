5eb197b1539703863189df126555dd12c61dce62
Paul H. Liu <hai.liu@intel.com>
2017-09-15 10:41:21 -0700

    Implementation of SymbolicEquivSet:
    * enable equivalence of ranges/slices
    * reason about equivalence of simple arithmetic like + and -
    * support extra shape for SetItem nodes, which is not a variable assignment.

--- numba/array_analysis.py
+++ numba/array_analysis.py
@@ -552,6 +552,219 @@ class ShapeEquivSet(EquivSet):
             if v > 0:
                 self.define(k)
 
+class SymbolicEquivSet(ShapeEquivSet):
+
+    """Just like ShapeEquivSet, except that it also reasons about variable
+    equivalence symbolically by using their arithmetic definitions.
+    The goal is to automatically derive the equivalence of array ranges
+    (slicing). For instance, a[1:m] and a[0:m-1] shall be considered
+    size-equivalence.
+    """
+
+    def __init__(self, typemap, def_by=None, ref_by=None, ext_shapes=None,
+                 defs=None, ind_to_var=None, obj_to_ind=None,
+                 ind_to_obj=None, next_id=0):
+        """Create a new SymbolicEquivSet object, where typemap is a dictionary
+        that maps variable names to their types, and it will not be modified.
+        Optional keyword arguments are for internal use only.
+        """
+        # A "defined-by" table that maps A to a tuple of (B, i), which
+        # means A is defined as: A = B + i, where A,B are variable names,
+        # and i is an integer constants.
+        self.def_by = def_by if def_by else {}
+        # A "refered-by" table that maps A to a list of [(B, i), (C, j) ...],
+        # which implies a sequence of definitions: B = A - i, C = A - j, and
+        # so on, where A,B,C,... are variable names, and i,j,... are
+        # integer constants.
+        self.ref_by = ref_by if ref_by else {}
+        # A extended shape table that can map an arbitrary object to a shape,
+        # currently only used to remember shapes for SetItem IR node.
+        self.ext_shapes = ext_shapes if ext_shapes else {}
+        super(SymbolicEquivSet, self).__init__(
+            typemap, defs, ind_to_var, obj_to_ind, ind_to_obj, next_id)
+
+    def empty(self):
+        """Return an empty SymbolicEquivSet.
+        """
+        return SymbolicEquivSet(self.typemap)
+
+    def __repr__(self):
+        return ("SymbolicEquivSet({}, ind_to_var={}, def_by={}, "
+                "ref_by={}, ext_shapes={})".format(self.ind_to_obj,
+                self.ind_to_var, self.def_by, self.ref_by, self.ext_shapes))
+
+    def clone(self):
+        """Return a new copy.
+        """
+        return SymbolicEquivSet(
+            self.typemap,
+            def_by=copy.copy(self.def_by),
+            ref_by=copy.copy(self.ref_by),
+            ext_shapes=copy.copy(self.ext_shapes),
+            defs=copy.copy(self.defs),
+            ind_to_var=copy.copy(self.ind_to_var),
+            obj_to_ind=copy.deepcopy(self.obj_to_ind),
+            ind_to_obj=copy.deepcopy(self.ind_to_obj),
+            next_id=self.next_ind)
+
+    def get_rel(self, name):
+        """Retrieve a definition pair for the given variable,
+        or return None if it is not available.
+        """
+        return guard(self._get_or_set_rel, name)
+
+    def _get_or_set_rel(self, name, func_ir=None):
+        """Retrieve a definition pair for the given variable,
+        and if it is not already available, try to look it up
+        in the given func_ir, and remember it for future use.
+        """
+        if isinstance(name, ir.Var):
+            name = name.name
+        require(self.defs.get(name, 0) == 1)
+        if name in self.def_by:
+            return self.def_by[name]
+        else:
+            require(func_ir != None)
+            def plus(x, y):
+                x_is_const = isinstance(x, int)
+                y_is_const = isinstance(y, int)
+                if x_is_const:
+                    if y_is_const:
+                        return x + y
+                    else:
+                        (var, offset) = y
+                        return (var, x + offset)
+                else:
+                    (var, offset) = x
+                    if y_is_const:
+                        return (var, y + offset)
+                    else:
+                        return None
+            def minus(x, y):
+                if isinstance(y, int):
+                    return plus(x, -y)
+                else:
+                    return None
+            expr = get_definition(func_ir, name)
+            value = (name, 0) # default to its own name
+            if isinstance(expr, ir.Expr):
+                if expr.op == 'call':
+                    fname, mod_name = find_callname(
+                            func_ir, expr, typemap=self.typemap)
+                    if fname == 'slice' and mod_name == 'type':
+                        # NOTE: steps is not supported now
+                        require(len(expr.args) == 2)
+                        fr = self._get_or_set_rel(expr.args[0], func_ir)
+                        to = self._get_or_set_rel(expr.args[1], func_ir)
+                        value = minus(to, fr)
+                elif expr.op == 'binop':
+                    lhs = self._get_or_set_rel(expr.lhs, func_ir)
+                    rhs = self._get_or_set_rel(expr.rhs, func_ir)
+                    if expr.fn == '+':
+                        value = plus(lhs, rhs)
+                    elif expr.fn == '-':
+                        value = minus(lhs, rhs)
+            elif isinstance(expr, ir.Const) and isinstance(expr.value, int):
+                value = expr.value
+            require(value != None)
+            # update def_by table
+            self.def_by[name] = value
+            if isinstance(value, int) or (isinstance(value, tuple) and
+                (value[0] != name or value[1] != 0)):
+                # update ref_by table too
+                if isinstance(value, tuple):
+                    (var, offset) = value
+                    if not (var in self.ref_by):
+                        self.ref_by[var] = []
+                    self.ref_by[var].append((name, -offset))
+                    # insert new equivalence if found
+                    ind = self._get_ind(var)
+                    if ind >= 0:
+                        objs = self.ind_to_obj[ind]
+                        names = []
+                        for obj in objs:
+                            if obj in self.ref_by:
+                                names += [ x for (x, i) in self.ref_by[obj]
+                                           if i == -offset ]
+                        if len(names) > 1:
+                            super(SymbolicEquivSet, self)._insert(names)
+            return value
+
+    def define(self, name, func_ir=None):
+        """Besides incrementing the definition count of the given variable
+        name, it will also retrieve and simplify its definition from func_ir,
+        and remeber the result for later equivalence comparison. Supported
+        operations are:
+          1. arithmetic plus and minus with constants
+          2. slice
+        """
+        if isinstance(name, ir.Var):
+            name = name.name
+        super(SymbolicEquivSet, self).define(name)
+        if func_ir and self.defs.get(name, 0) == 1:
+            value = guard(self._get_or_set_rel, name, func_ir)
+
+    def _insert(self, objs):
+        """Overload _insert method to handle ind changes between relative
+        objects.
+        """
+        indset = set()
+        uniqs = set()
+        for obj in objs:
+            ind = self._get_ind(obj)
+            if ind == -1:
+                uniqs.add(obj)
+            elif not (ind in indset):
+                uniqs.add(obj)
+                indset.add(ind)
+        if len(uniqs) <= 1:
+            return
+        uniqs = list(uniqs)
+        super(SymbolicEquivSet, self)._insert(uniqs)
+        objs = self.ind_to_obj[self._get_ind(uniqs[0])]
+
+        # New equivalence guided by def_by and ref_by
+        offset_dict = {}
+        def get_or_set(d, k):
+            if k in d:
+                v = d[k]
+            else:
+                v = []
+                d[k] = v
+            return v
+        for obj in objs:
+            if obj in self.def_by:
+                value = self.def_by[obj]
+                if isinstance(value, tuple):
+                    (name, offset) = value
+                    get_or_set(offset_dict, -offset).append(name)
+                    if name in self.ref_by: # relative to name
+                        for (v, i) in self.ref_by[name]:
+                            get_or_set(offset_dict, -(offset+i)).append(v)
+            if obj in self.ref_by:
+                for (name, offset) in self.ref_by[obj]:
+                    get_or_set(offset_dict, offset).append(name)
+        for names in offset_dict.values():
+            self._insert(names)
+
+    def set_shape(self, obj, shape):
+        """Overload set_shape to remember shapes of SetItem IR nodes
+        in addition to just variables.
+        """
+        if isinstance(obj, ir.SetItem):
+            self.ext_shapes[obj] = shape
+        else:
+            super(SymbolicEquivSet, self).set_shape(obj, shape)
+
+    def _get_shape(self, obj):
+        """Overload _get_shape to retrieve the shape of SetItem IR nodes
+        in addition to just variables.
+        """
+        if isinstance(obj, ir.SetItem):
+            require(obj in self.ext_shapes)
+            return self.ext_shapes[obj]
+        else:
+            return super(SymbolicEquivSet, self)._get_shape(obj)
 
 class ArrayAnalysis(object):
 
@@ -627,7 +840,7 @@ class ArrayAnalysis(object):
 
             # Start with a new equiv_set if none is computed
             if equiv_set == None:
-                equiv_set = ShapeEquivSet(self.typemap)
+                equiv_set = SymbolicEquivSet(self.typemap)
             self.equiv_sets[label] = equiv_set
             # Go through instructions in a block, and insert pre/post
             # instructions as we analyze them.
@@ -693,7 +906,7 @@ class ArrayAnalysis(object):
 
             if shape != None:
                 equiv_set.insert_equiv(lhs, shape)
-            equiv_set.define(lhs)
+            equiv_set.define(lhs, self.func_ir)
         elif isinstance(inst, ir.Branch):
             cond_var = inst.cond
             cond_def = guard(get_definition, self.func_ir, cond_var)
