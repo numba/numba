60ab188221f929d3a7ea78e31b54c3c937d05e51
Todd A. Anderson <drtodd13@comcast.net>
2017-10-26 09:09:09 -0700

    Extend test_stencil_standard_indexing_2d to feed the output of the first stencil into a second stencil call so as to test multiple stencil calls in one function.

--- numba/tests/test_stencils.py
+++ numba/tests/test_stencils.py
@@ -265,24 +265,30 @@ class TestStencils(unittest.TestCase):
     @skip_unsupported
     @tag('important')
     def test_stencil_standard_indexing_2d(self):
-        """Tests standard indexing with a 2d array.
+        """Tests standard indexing with a 2d array and multiple stencil calls.
         """
         def test_seq(n):
             A = np.arange(n**2).reshape((n, n))
             B = np.ones((3,3))
             C = stencil_with_standard_indexing_2d(A, B)
-            return C
+            D = stencil_with_standard_indexing_2d(C, B)
+            return D
 
         def test_impl_seq(n):
             A = np.arange(n**2).reshape((n, n))
             B = np.ones((3,3))
             C = np.zeros(n**2).reshape((n, n))
+            D = np.zeros(n**2).reshape((n, n))
 
             for i in range(1, n-1):
                 for j in range(1, n-1):
                     C[i,j] = (A[i,j+1] * B[0,1]  + A[i+1,j] * B[1,0] +
                               A[i,j-1] * B[0,-1] + A[i-1,j] * B[-1,0])
-            return C
+            for i in range(1, n-1):
+                for j in range(1, n-1):
+                    D[i,j] = (C[i,j+1] * B[0,1]  + C[i+1,j] * B[1,0] +
+                              C[i,j-1] * B[0,-1] + C[i-1,j] * B[-1,0])
+            return D
 
         n = 5
         self.check(test_impl_seq, test_seq, n)
