1375087865ed88106d5819871479d70c843276ee
Todd A. Anderson <drtodd13@comcast.net>
2017-11-02 20:04:35 -0700

    Support color jpg.

--- examples/gaussian-blur/gaussian-blur.py
+++ examples/gaussian-blur/gaussian-blur.py
@@ -38,8 +38,27 @@ def gaussian_blur_std(a, res):
                         a[i-2,j+2] * 0.003  + a[i-1,j+2] * 0.0133 + a[i,j+2] * 0.0219 + a[i+1,j+2] * 0.0133 + a[i+2,j+2] * 0.0030)
     return res
 
+@stencil()
+def gaussian_blur_3d(a):
+    return (a[-2,-2,0] * 0.003  + a[-1,-2,0] * 0.0133 + a[0,-2,0] * 0.0219 + a[1,-2,0] * 0.0133 + a[2,-2,0] * 0.0030 +
+            a[-2,-1,0] * 0.0133 + a[-1,-1,0] * 0.0596 + a[0,-1,0] * 0.0983 + a[1,-1,0] * 0.0596 + a[2,-1,0] * 0.0133 +
+            a[-2, 0,0] * 0.0219 + a[-1, 0,0] * 0.0983 + a[0, 0,0] * 0.1621 + a[1, 0,0] * 0.0983 + a[2, 0,0] * 0.0219 +
+            a[-2, 1,0] * 0.0133 + a[-1, 1,0] * 0.0596 + a[0, 1,0] * 0.0983 + a[1, 1,0] * 0.0596 + a[2, 1,0] * 0.0133 +
+            a[-2, 2,0] * 0.003  + a[-1, 2,0] * 0.0133 + a[0, 2,0] * 0.0219 + a[1, 2,0] * 0.0133 + a[2, 2,0] * 0.0030)
+
+def gaussian_blur_std_3d(a, res):
+    ashape = a.shape
+    for i in range(2,ashape[0]-2):
+        for j in range(2,ashape[1]-2):
+            for k in range(ashape[2]):
+                res[i,j,k] = (a[i-2,j-2,k] * 0.003  + a[i-1,j-2,k] * 0.0133 + a[i,j-2,k] * 0.0219 + a[i+1,j-2,k] * 0.0133 + a[i+2,j-2,k] * 0.0030 +
+                              a[i-2,j-1,k] * 0.0133 + a[i-1,j-1,k] * 0.0596 + a[i,j-1,k] * 0.0983 + a[i+1,j-1,k] * 0.0596 + a[i+2,j-1,k] * 0.0133 +
+                              a[i-2,j+0,k] * 0.0219 + a[i-1,j+0,k] * 0.0983 + a[i,j+0,k] * 0.1621 + a[i+1,j+0,k] * 0.0983 + a[i+2,j+0,k] * 0.0219 +
+                              a[i-2,j+1,k] * 0.0133 + a[i-1,j+1,k] * 0.0596 + a[i,j+1,k] * 0.0983 + a[i+1,j+1,k] * 0.0596 + a[i+2,j+1,k] * 0.0133 +
+                              a[i-2,j+2,k] * 0.003  + a[i-1,j+2,k] * 0.0133 + a[i,j+2,k] * 0.0219 + a[i+1,j+2,k] * 0.0133 + a[i+2,j+2,k] * 0.0030)
+    return res
 def main (*args):
-    iterations = 60
+    iterations = 10
     
     if len(args) > 0:
         input_file = args[0]
@@ -55,14 +74,23 @@ def main (*args):
     input_img = Image.open(input_file)
     input_arr = np.array(input_img)
 
+    assert(input_arr.ndim == 2 or input_arr.ndim == 3)
+
     tstart = time.time()
-    output_arr = gaussian_blur(input_arr)
+    if input_arr.ndim == 2:
+        output_arr = gaussian_blur(input_arr)
+    else:
+        output_arr = gaussian_blur_3d(input_arr)
+
     htime = time.time() - tstart
     print("SELFPRIMED ", htime)
 
     tstart = time.time()
     for i in range(iterations):
-        output_arr = gaussian_blur(input_arr)
+        if input_arr.ndim == 2:
+            output_arr = gaussian_blur(input_arr)
+        else:
+            output_arr = gaussian_blur_3d(input_arr)
         input_arr, output_arr = output_arr, input_arr
     htime = time.time() - tstart
     print("SELFTIMED ", htime)
@@ -80,7 +108,10 @@ def main (*args):
     tstart = time.time()
     output_arr = input_arr.copy()
     for i in range(iterations):
-        gaussian_blur_std(input_arr, output_arr)
+        if input_arr.ndim == 2:
+            gaussian_blur_std(input_arr, output_arr)
+        else:
+            gaussian_blur_std_3d(input_arr, output_arr)
         input_arr, output_arr = output_arr, input_arr
     htime = time.time() - tstart
     print("Standard Python time", htime)
