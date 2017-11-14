9f3150db045d54394569016d0549135cf1a743f6
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-11-14 15:21:42 -0600

    Fix #2606.  Add setattr redirection for optional type.

--- numba/targets/optional.py
+++ numba/targets/optional.py
@@ -1,9 +1,9 @@
 from __future__ import print_function, absolute_import, division
 
-from numba import types, cgutils
+from numba import types, cgutils, typing
 
 from .imputils import (lower_cast, lower_builtin, lower_getattr_generic,
-                       impl_ret_untracked)
+                       impl_ret_untracked, lower_setattr_generic)
 
 
 def always_return_true_impl(context, builder, sig, args):
@@ -51,6 +51,21 @@ def optional_getattr(context, builder, typ, value, attr):
     val = context.cast(builder, value, typ, inner_type)
     imp = context.get_getattr(inner_type, attr)
     return imp(context, builder, inner_type, val, attr)
+
+
+@lower_setattr_generic(types.Optional)
+def optional_setattr(context, builder, sig, args, attr):
+    """
+    Optional.__setattr__ => redirect to the wrapped type.
+    """
+    basety, valty = sig.args
+    target, val = args
+    target_type = basety.type
+    target = context.cast(builder, target, basety, target_type)
+
+    newsig = typing.signature(sig.return_type, target_type, valty)
+    imp = context.get_setattr(attr, newsig)
+    return imp(builder, (target, val))
 
 
 @lower_cast(types.Optional, types.Optional)
