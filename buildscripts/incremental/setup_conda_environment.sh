ce1887396d969568557d03e229bbb14f8b1da09d
Stuart Archibald <stuart.archibald@googlemail.com>
2017-11-20 16:49:52 +0000

    fix osx, patch svd parfors

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -24,7 +24,7 @@ set -v
 if [[ $(uname) == Linux ]]; then
 $CONDA_INSTALL gcc_linux-64 gxx_linux-64
 elif  [[ $(uname) == Darwin ]]; then
-$CONDA_INSTALL clang_osx-64 clang_osx-64
+$CONDA_INSTALL clang_osx-64 clang++_osx-64
 fi
 
 # Install latest llvmlite build
