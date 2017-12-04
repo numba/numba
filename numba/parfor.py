ebace5f098ddc7be7972ed9b676eeb7961ca0ca5
Ehsan Totoni <ehsan.totoni@intel.com>
2017-12-04 12:40:56 -0800

    fix parfor dump() to use file for all prints

--- numba/parfor.py
+++ numba/parfor.py
@@ -239,11 +239,11 @@ class Parfor(ir.Expr, ir.Stmt):
     def dump(self, file=None):
         file = file or sys.stdout
         print(("begin parfor {}".format(self.id)).center(20, '-'), file=file)
-        print("index_var = ", self.index_var)
+        print("index_var = ", self.index_var, file=file)
         for loopnest in self.loop_nests:
             print(loopnest, file=file)
         print("init block:", file=file)
-        self.init_block.dump()
+        self.init_block.dump(file)
         for offset, block in sorted(self.loop_body.items()):
             print('label %s:' % (offset,), file=file)
             block.dump(file)
