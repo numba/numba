b4e4f71c531208e1026fdc38a671056ecdb9d837
Stuart Archibald <stuart.archibald@googlemail.com>
2018-01-19 18:34:00 +0000

    Update llvmlite version dependency.
    Move to llvmlite>=0.22

--- numba/__init__.py
+++ numba/__init__.py
@@ -51,7 +51,7 @@ __all__ = """
     """.split() + types.__all__ + errors.__all__
 
 
-_min_llvmlite_version = (0, 21, 0)
+_min_llvmlite_version = (0, 22, 0)
 _min_llvm_version = (5, 0, 0)
 
 def _ensure_llvm():
