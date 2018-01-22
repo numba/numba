d228e4a2100f61ab9a77bada5cdebfc7f3a40cd8
Ehsan Totoni <ehsan.totoni@intel.com>
2018-01-21 20:23:25 -0500

    docstring for pndindex and prange

--- numba/special.py
+++ numba/special.py
@@ -4,9 +4,15 @@ from .typing.typeof import typeof
 import numpy as np
 
 def pndindex(*args):
+    """ Provides an n-dimensional parallel iterator that generates index tuples
+    for each iteration point. Sequentially, pndindex is identical to np.ndindex.
+    """
     return np.ndindex(*args)
 
 class prange(object):
+    """ Provides a 1D parallel iterator that generates a sequence of integers.
+    Sequentially, prange is identical to range.
+    """
     def __new__(cls, *args):
         return range(*args)
 
