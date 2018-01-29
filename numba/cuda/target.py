bf3df11ace35820e93dd9e065c0aebfc3611322d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-01-29 12:01:34 -0600

    Remove unnecessary changes

--- numba/cuda/target.py
+++ numba/cuda/target.py
@@ -252,24 +252,4 @@ class CUDATargetContext(BaseContext):
 
 
 class CUDACallConv(MinimalCallConv):
-
-    def call_function(self, builder, callee, resty, argtys, args, env=None):
-        """
-        Call the Numba-compiled *callee*.
-        """
-        assert env is None
-        retty = callee.args[0].type.pointee
-        retvaltmp = cgutils.alloca_once(builder, retty)
-        # initialize return value
-        builder.store(cgutils.get_null_value(retty), retvaltmp)
-
-        arginfo = self._get_arg_packer(argtys)
-        args = arginfo.as_arguments(builder, args)
-        realargs = [retvaltmp] + list(args)
-        code = builder.call(callee, realargs)
-        status = self._get_return_status(builder, code)
-        retval = builder.load(retvaltmp)
-        out = self.context.get_returned_value(builder, resty, retval)
-        return status, out
-
-
+    pass
