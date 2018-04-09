caeccddc9071cd2164e8947202db65231188671f
Nick White <n.j.white@gmail.com>
2018-04-09 21:07:14 +0100

    typo

--- numba/cuda/tests/cudapy/test_intrinsics.py
+++ numba/cuda/tests/cudapy/test_intrinsics.py
@@ -279,7 +279,7 @@ class TestCudaIntrinsic(SerialMixin, unittest.TestCase):
         Although the CUDA Math API (http://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH__INTRINSIC__INT.html)
         only says int32 & int64 arguments are supported in C code, the LLVM
         IR input supports i8, i16, i32 & i64 (LLVM doesn't have a concept of
-        unsigned integers, just unsigned operations on integers.
+        unsigned integers, just unsigned operations on integers).
         http://docs.nvidia.com/cuda/nvvm-ir-spec/index.html#bit-manipulations-intrinics
         """
         compiled = cuda.jit("void(int32[:], uint32)")(simple_clz)
