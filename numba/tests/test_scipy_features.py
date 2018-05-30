af22e3c15ec37681d1d003bd2ac75af76aa7f5fb
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-05-30 10:54:02 -0500

    Add cffi test and fixes the ctypes test expected value.

--- numba/tests/test_scipy_features.py
+++ numba/tests/test_scipy_features.py
@@ -6,13 +6,17 @@ from __future__ import print_function, absolute_import
 
 import ctypes
 
-import numpy as np
-
 try:
     import scipy
     has_scipy = True
 except ImportError:
     has_scipy = False
+
+try:
+    import cffi
+    has_cffi = True
+except ImportError:
+    has_cffi = False
 
 import numba.unittest_support as unittest
 from .support import TestCase
@@ -20,6 +24,7 @@ from numba import njit, types, typeof
 
 
 skip_if_no_scipy = unittest.skipIf(not has_scipy, "Skip, no scipy")
+skip_if_no_cffi = unittest.skipIf(not has_cffi, "Skip, no cffi")
 
 
 @skip_if_no_scipy
@@ -35,8 +40,24 @@ class TestLowLevelCallable(TestCase):
         llc = scipy.LowLevelCallable(twice)
         return llc
 
+    def make_cffi_llc(self):
+        ffi = cffi.FFI()
+        self.ffi = ffi
+
+        @ffi.callback('int(int, int)')
+        def twice(x):
+            return x + x
+
+        llc = scipy.LowLevelCallable(twice)
+        return llc
+
     def test_typeof_ctypes(self):
         llc = self.make_ctypes_llc()
+        llc_type = typeof(llc)
+        self.assertIsInstance(llc_type, types.LowLevelCallable)
+
+    def test_typeof_cffi(self):
+        llc = self.make_cffi_llc()
         llc_type = typeof(llc)
         self.assertIsInstance(llc_type, types.LowLevelCallable)
 
@@ -47,7 +68,17 @@ class TestLowLevelCallable(TestCase):
 
         llc = self.make_ctypes_llc()
         got = caller(llc, 123)
-        expect = caller(llc, 123)
+        expect = llc.function(123)
+        self.assertEqual(got, expect)
+
+    def test_call_cffi(self):
+        @njit
+        def caller(fn, x):
+            return fn.function(x)
+
+        llc = self.make_ctypes_llc()
+        got = caller(llc, 123)
+        expect = llc.function(123)
         self.assertEqual(got, expect)
 
 
