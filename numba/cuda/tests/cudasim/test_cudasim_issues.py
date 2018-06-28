25eefbaf3d4fdc2e9c6e59cbdef76e31cb691c8f
Alex Ford <fordas@uw.edu>
2018-06-28 15:15:13 -0600

    Add repro test case for #3063 to cudasim test suite.

--- numba/cuda/tests/cudasim/test_cudasim_issues.py
+++ numba/cuda/tests/cudasim/test_cudasim_issues.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import, print_function, division
 
+import threading
+
 import numpy as np
 
 from numba import unittest_support as unittest
 from numba import cuda
 from numba.cuda.testing import SerialMixin
+import numba.cuda.simulator as simulator
 
 
 class TestCudaSimIssues(SerialMixin, unittest.TestCase):
@@ -27,6 +30,33 @@ class TestCudaSimIssues(SerialMixin, unittest.TestCase):
         outer[1, 11](arr)
         expected = np.arange(arr.size, dtype=np.int32)
         np.testing.assert_equal(expected, arr)
+
+    def test_deadlock_on_exception(self):
+        def assert_no_blockthreads():
+            blockthreads = [
+                t for t in threading.enumerate()
+                if isinstance(t, simulator.kernel.BlockThread)
+            ]
+
+            self.assertListEqual(blockthreads, [])
+
+        @simulator.jit
+        def assign_with_sync(x, y):
+            i = cuda.grid(1)
+            y[i] = x[i]
+
+            cuda.syncthreads()
+
+        x = np.arange(20)
+        y = np.empty(20)
+        assign_with_sync[1, 20](x, y)
+        np.testing.assert_array_equal(x, y)
+        assert_no_blockthreads()
+
+
+        with self.assertRaises(IndexError):
+            assign_with_sync[1, 30](x, y)
+        assert_no_blockthreads()
 
 
 if __name__ == '__main__':
