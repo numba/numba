18b5d4437c9d58a4f4da704a2a09de277c2c4a5d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-08-03 11:12:51 -0500

    Test and fix datetime in cuda ufunc

--- numba/cuda/tests/cudapy/test_datetime.py
+++ numba/cuda/tests/cudapy/test_datetime.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 
 import numpy as np
 
-from numba import cuda
+from numba import cuda, vectorize, typeof
 from numba import unittest_support as unittest
+from numba.numpy_support import from_dtype
 from numba.tests.support import TestCase
 from numba.cuda.testing import SerialMixin
 
 
-class TestCudaAutojit(SerialMixin, TestCase):
+class TestCudaDateTime(SerialMixin, TestCase):
     def test_basic_datetime_kernel(self):
         @cuda.jit
         def foo(start, end, delta):
@@ -20,6 +21,20 @@ class TestCudaAutojit(SerialMixin, TestCase):
         delta = np.zeros_like(arr1, dtype='timedelta64[D]')
 
         foo[1, 32](arr1, arr2, delta)
+
+        self.assertPreciseEqual(delta, arr2 - arr1)
+
+    def test_ufunc(self):
+        datetime_t = from_dtype(np.dtype('datetime64[D]'))
+
+        @vectorize([(datetime_t, datetime_t)], target='cuda')
+        def timediff(start, end):
+            return end - start
+
+        arr1 = np.arange('2005-02', '2006-02', dtype='datetime64[D]')
+        arr2 = arr1 + np.random.randint(0, 10000, arr1.size)
+
+        delta = timediff(arr1, arr2)
 
         self.assertPreciseEqual(delta, arr2 - arr1)
 
