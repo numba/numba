b2601510b9c2bf8134e5adea0194730ab3bc7e61
Ehsan Totoni <ehsan.totoni@intel.com>
2018-08-14 13:18:59 -0400

    add test for recursive stencil const inference

--- numba/tests/test_stencils.py
+++ numba/tests/test_stencils.py
@@ -450,6 +450,14 @@ class TestStencil(TestStencilBase):
                                                                    A, c, out=B)
             return B
 
+        def test_impl3(n):
+            A = np.arange(n)
+            B = np.zeros(n)
+            c = 2
+            numba.stencil(lambda a,c : 0.3 * (a[-c+1] + a[0] + a[c-1]))(
+                                                                   A, c, out=B)
+            return B
+
         def test_impl_seq(n):
             A = np.arange(n)
             B = np.zeros(n)
@@ -462,12 +470,15 @@ class TestStencil(TestStencilBase):
         # constant inference is only possible in parallel path
         cpfunc1 = self.compile_parallel(test_impl1, (types.intp,))
         cpfunc2 = self.compile_parallel(test_impl2, (types.intp,))
+        cpfunc3 = self.compile_parallel(test_impl3, (types.intp,))
         expected = test_impl_seq(n)
         # parfor result
         parfor_output1 = cpfunc1.entry_point(n)
         parfor_output2 = cpfunc2.entry_point(n)
+        parfor_output3 = cpfunc3.entry_point(n)
         np.testing.assert_almost_equal(parfor_output1, expected, decimal=3)
         np.testing.assert_almost_equal(parfor_output2, expected, decimal=3)
+        np.testing.assert_almost_equal(parfor_output3, expected, decimal=3)
 
     @skip_unsupported
     @tag('important')
