c1f4eb795e22f9b5dc1c0e4a644dc9f26dc52f88
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-08-29 10:12:44 -0500

    Add compiler locks

--- numba/npyufunc/ufuncbuilder.py
+++ numba/npyufunc/ufuncbuilder.py
@@ -17,9 +17,8 @@ from . import _internal
 from .sigparse import parse_signature
 from .wrappers import build_ufunc_wrapper, build_gufunc_wrapper
 from numba.caching import FunctionCache, NullCache
+from numba.compiler import lock_compiler
 
-
-import llvmlite.llvmpy.core as lc
 
 class UFuncTargetOptions(TargetOptions):
     OPTIONS = {
@@ -221,40 +220,41 @@ class UFuncBuilder(_BaseUFuncBuilder):
         return _finalize_ufunc_signature(cres, args, return_type)
 
     def build_ufunc(self):
-        dtypelist = []
-        ptrlist = []
-        if not self.nb_func:
-            raise TypeError("No definition")
+        with lock_compiler:
+            dtypelist = []
+            ptrlist = []
+            if not self.nb_func:
+                raise TypeError("No definition")
 
-        # Get signature in the order they are added
-        keepalive = []
-        cres = None
-        for sig in self._sigs:
-            cres = self._cres[sig]
-            dtypenums, ptr, env = self.build(cres, sig)
-            dtypelist.append(dtypenums)
-            ptrlist.append(utils.longint(ptr))
-            keepalive.append((cres.library, env))
+            # Get signature in the order they are added
+            keepalive = []
+            cres = None
+            for sig in self._sigs:
+                cres = self._cres[sig]
+                dtypenums, ptr, env = self.build(cres, sig)
+                dtypelist.append(dtypenums)
+                ptrlist.append(utils.longint(ptr))
+                keepalive.append((cres.library, env))
 
-        datlist = [None] * len(ptrlist)
+            datlist = [None] * len(ptrlist)
 
-        if cres is None:
-            argspec = inspect.getargspec(self.py_func)
-            inct = len(argspec.args)
-        else:
-            inct = len(cres.signature.args)
-        outct = 1
+            if cres is None:
+                argspec = inspect.getargspec(self.py_func)
+                inct = len(argspec.args)
+            else:
+                inct = len(cres.signature.args)
+            outct = 1
 
-        # Becareful that fromfunc does not provide full error checking yet.
-        # If typenum is out-of-bound, we have nasty memory corruptions.
-        # For instance, -1 for typenum will cause segfault.
-        # If elements of type-list (2nd arg) is tuple instead,
-        # there will also memory corruption. (Seems like code rewrite.)
-        ufunc = _internal.fromfunc(self.py_func.__name__, self.py_func.__doc__,
-                                   ptrlist, dtypelist, inct, outct, datlist,
-                                   keepalive, self.identity)
+            # Becareful that fromfunc does not provide full error checking yet.
+            # If typenum is out-of-bound, we have nasty memory corruptions.
+            # For instance, -1 for typenum will cause segfault.
+            # If elements of type-list (2nd arg) is tuple instead,
+            # there will also memory corruption. (Seems like code rewrite.)
+            ufunc = _internal.fromfunc(self.py_func.__name__, self.py_func.__doc__,
+                                    ptrlist, dtypelist, inct, outct, datlist,
+                                    keepalive, self.identity)
 
-        return ufunc
+            return ufunc
 
     def build(self, cres, signature):
         '''Slated for deprecation, use
@@ -288,30 +288,31 @@ class GUFuncBuilder(_BaseUFuncBuilder):
         return return_type(*args)
 
     def build_ufunc(self):
-        dtypelist = []
-        ptrlist = []
-        if not self.nb_func:
-            raise TypeError("No definition")
+        with lock_compiler:
+            dtypelist = []
+            ptrlist = []
+            if not self.nb_func:
+                raise TypeError("No definition")
 
-        # Get signature in the order they are added
-        keepalive = []
-        for sig in self._sigs:
-            cres = self._cres[sig]
-            dtypenums, ptr, env = self.build(cres)
-            dtypelist.append(dtypenums)
-            ptrlist.append(utils.longint(ptr))
-            keepalive.append((cres.library, env))
+            # Get signature in the order they are added
+            keepalive = []
+            for sig in self._sigs:
+                cres = self._cres[sig]
+                dtypenums, ptr, env = self.build(cres)
+                dtypelist.append(dtypenums)
+                ptrlist.append(utils.longint(ptr))
+                keepalive.append((cres.library, env))
 
-        datlist = [None] * len(ptrlist)
+            datlist = [None] * len(ptrlist)
 
-        inct = len(self.sin)
-        outct = len(self.sout)
+            inct = len(self.sin)
+            outct = len(self.sout)
 
-        # Pass envs to fromfuncsig to bind to the lifetime of the ufunc object
-        ufunc = _internal.fromfunc(self.py_func.__name__, self.py_func.__doc__,
-                                   ptrlist, dtypelist, inct, outct, datlist,
-                                   keepalive, self.identity, self.signature)
-        return ufunc
+            # Pass envs to fromfuncsig to bind to the lifetime of the ufunc object
+            ufunc = _internal.fromfunc(self.py_func.__name__, self.py_func.__doc__,
+                                    ptrlist, dtypelist, inct, outct, datlist,
+                                    keepalive, self.identity, self.signature)
+            return ufunc
 
     def build(self, cres):
         """
--- numba/tests/test_codegen.py
+++ numba/tests/test_codegen.py
@@ -17,6 +17,7 @@ import llvmlite.binding as ll
 import numba.unittest_support as unittest
 from numba import utils
 from numba.targets.codegen import JITCPUCodegen
+from numba.compiler import lock_compiler
 from .support import TestCase
 
 
@@ -55,10 +56,12 @@ class JITCPUCodegenTestCase(TestCase):
     """
 
     def setUp(self):
+        lock_compiler.acquire()
         self.codegen = JITCPUCodegen('test_codegen')
 
     def tearDown(self):
         del self.codegen
+        lock_compiler.release()
 
     def compile_module(self, asm, linking_asm=None):
         library = self.codegen.create_library('compiled_module')
