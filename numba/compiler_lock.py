b7419fd27ceaa47493f5963bd3c97aa03e53b96f
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-08-29 13:57:23 -0500

    Revert.  Rely on _is_owned() and borrow is_owned definition from threading.Condition if it's not defined in the lock.

--- numba/compiler_lock.py
+++ numba/compiler_lock.py
@@ -6,14 +6,11 @@ import functools
 class _CompilerLock(object):
     def __init__(self):
         self._lock = threading.RLock()
-        self._locked = 0
 
     def acquire(self):
         self._lock.acquire()
-        self._locked += 1
 
     def release(self):
-        self._locked -= 1
         self._lock.release()
 
     def __enter__(self):
@@ -23,14 +20,28 @@ class _CompilerLock(object):
         self.release()
 
     def is_locked(self):
-        return self._locked > 0
+        is_owned = getattr(self._lock, '_is_owned')
+        if not callable(is_owned):
+            is_owned = self._is_owned
+        return is_owned()
 
     def __call__(self, func):
         @functools.wraps(func)
-        def wrapped(*args, **kwargs):
+        def _acquire_compile_lock(*args, **kwargs):
             with self:
+                require_global_compiler_lock()
                 return func(*args, **kwargs)
-        return wrapped
+        return _acquire_compile_lock
+
+    def _is_owned(self):
+        # This method is borrowed from threading.Condition.
+        # Return True if lock is owned by current_thread.
+        # This method is called only if _lock doesn't have _is_owned().
+        if self._lock.acquire(0):
+            self._lock.release()
+            return False
+        else:
+            return True
 
 
 global_compiler_lock = _CompilerLock()
