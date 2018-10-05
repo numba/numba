680f8fcb4efa2b6a4f68fa0381b8f138a2f5cab9
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-10-05 11:50:06 -0500

    Fix for #3355 Add func.__globals__ reference  to FunctionDescriptor if function is created dynamically. Such jitted function is however un-picklable.

--- numba/lowering.py
+++ numba/lowering.py
@@ -28,12 +28,11 @@ class Environment(_dynfunc.Environment):
 
     @classmethod
     def from_fndesc(cls, fndesc):
-        mod = fndesc.lookup_module()
         try:
             # Avoid creating new Env
             return cls._memo[fndesc.env_name]
         except KeyError:
-            inst = cls(mod.__dict__)
+            inst = cls(fndesc.lookup_globals())
             inst.env_name = fndesc.env_name
             cls._memo[fndesc.env_name] = inst
             return inst
