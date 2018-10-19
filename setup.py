45fa1d8fddd1fc843448ea45c4329b2908824f97
Stuart Archibald <stuart.archibald@googlemail.com>
2018-10-19 14:35:20 +0100

    Updates for llvmlite 0.26
    As title.

--- setup.py
+++ setup.py
@@ -304,7 +304,7 @@ packages = find_packages("numba", "numba")
 
 build_requires = ['numpy']
 
-install_requires = ['llvmlite>=0.25.0dev0', 'numpy']
+install_requires = ['llvmlite>=0.26.0dev0', 'numpy']
 if sys.version_info < (3, 4):
     install_requires.extend(['enum34', 'singledispatch'])
 if sys.version_info < (3, 3):
