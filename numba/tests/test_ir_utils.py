3dbf91d1b63de0d49327f7e20a80777ac0963f7b
Ehsan Totoni <ehsan.totoni@intel.com>
2018-11-18 13:55:10 -0500

    flake8 fixes

--- numba/tests/test_ir_utils.py
+++ numba/tests/test_ir_utils.py
@@ -1,11 +1,11 @@
 import numba
 from numba import unittest_support as unittest
 from .support import TestCase
-from numba import compiler, typing, jitclass
+from numba import compiler, jitclass
 from numba.targets.registry import cpu_target
 
 
-@jitclass([('val', numba.types.List(numba.intp)),])
+@jitclass([('val', numba.types.List(numba.intp))])
 class Dummy(object):
     def __init__(self, val):
         self.val = val
@@ -23,7 +23,7 @@ class TestIrUtils(TestCase):
         def test_func():
             d = Dummy([1])
             d.val.append(2)
-        
+
         test_ir = compiler.run_frontend(test_func)
         typingctx = cpu_target.typing_context
         typemap, _, _ = compiler.type_inference_stage(
@@ -31,7 +31,8 @@ class TestIrUtils(TestCase):
         matched_call = numba.ir_utils.find_callname(
             test_ir, test_ir.blocks[0].body[14].value, typemap)
         self.assertTrue(isinstance(matched_call, tuple)
-            and len(matched_call) == 2 and matched_call[0] == 'append')
+                        and len(matched_call) == 2
+                        and matched_call[0] == 'append')
 
 
 if __name__ == "__main__":
