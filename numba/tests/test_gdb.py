3cc18180d2014185cad1fe64b3c701fe43cca0a5
Stuart Archibald <stuart.archibald@googlemail.com>
2018-11-27 18:05:19 +0000

    Fix up tests.
    As title.

--- numba/tests/test_gdb.py
+++ numba/tests/test_gdb.py
@@ -1,5 +1,5 @@
 """
-Test gdb
+Tests gdb bindings
 """
 from __future__ import print_function
 import ctypes
@@ -8,12 +8,26 @@ import subprocess
 import sys
 import threading
 
-from numba import njit, gdb, gdb_init, gdb_breakpoint, prange, testing
+from numba import njit, gdb, gdb_init, gdb_breakpoint, prange, testing, config
+from numba import unittest_support as unittest
 
 from .support import (TestCase, override_env_config, captured_stdout, tag)
+from .test_parfors import skip_unsupported as parfors_skip_unsupported
+from .test_parfors import linux_only
 
+_linux = sys.platform.startswith('linux')
+
+_gdb_cond = os.environ.get('GDB_TEST', None) == '1'
+needs_gdb_harness = unittest.skipUnless(_gdb_cond, "needs gdb harness")
+_gdbloc = config.GDB_BINARY
+_has_gdb = (os.path.exists(_gdbloc) and os.path.isfile(_gdbloc))
+needs_gdb = unittest.skipUnless(_has_gdb, "gdb binary is required")
+
+
+@linux_only
 class TestGdbBindImpls(TestCase):
 
+    @needs_gdb_harness
     def test_gdb_cmd_lang_impl(self):
         @njit(debug=True)
         def impl(a):
@@ -24,6 +38,7 @@ class TestGdbBindImpls(TestCase):
             print(a, b, c, d)
         impl(10)
 
+    @needs_gdb_harness
     def test_gdb_split_init_and_break_impl(self):
         @njit(debug=True)
         def impl(a):
@@ -35,6 +50,8 @@ class TestGdbBindImpls(TestCase):
             print(a, b, c, d)
         impl(10)
 
+    @parfors_skip_unsupported
+    @needs_gdb_harness
     def test_gdb_split_init_and_break_impl_w_parallel(self):
         @njit(debug=True, parallel=True)
         def impl(a):
@@ -48,6 +65,8 @@ class TestGdbBindImpls(TestCase):
                 print(a, b, c, d)
         impl(10)
 
+@linux_only
+@needs_gdb
 class TestGdbBinding(TestCase):
 
     # test mutates env
@@ -83,6 +102,7 @@ class TestGdbBinding(TestCase):
     def run_test_in_separate_process(self, test, **kwargs):
         env_copy = os.environ.copy()
         env_copy['NUMBA_OPT'] = '1'
+        env_copy['GDB_TEST'] = '1'
         cmdline = [sys.executable, "-m", "numba.runtests", test]
         return self.run_cmd(' '.join(cmdline), env_copy, **kwargs)
 
