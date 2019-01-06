f855ceb286c4be2ebfe2c95c8a271b4aa8227f77
rjenc29 <rjenc29@gmail.com>
2019-01-06 18:08:32 +0000

    min max test

--- numba/tests/test_array_reductions.py
+++ numba/tests/test_array_reductions.py
@@ -757,6 +757,28 @@ class TestArrayReductions(MemoryLeakMixin, TestCase):
         msg = "zero-size array reduction not possible"
         self.assertIn(msg, str(e.exception))
 
+    def test_min_max_complex_basic(self):
+        pyfuncs = array_min_global, array_max_global
+
+        for pyfunc in pyfuncs:
+            cfunc = jit(nopython=True)(pyfunc)
+
+            def check(a):
+                expected = pyfunc(a)
+                got = cfunc(a)
+                self.assertPreciseEqual(expected, got)
+
+            real = np.linspace(-10, 10, 40)
+            real[:4] = real[-1]
+            imag = real * 2
+
+            for _ in range(10):
+                self.random.shuffle(real)
+                self.random.shuffle(imag)
+                a = real - imag * 1j
+                a[:4] = a[-1]
+                check(a)
+
     @classmethod
     def install_generated_tests(cls):
         # These form a testing product where each of the combinations are tested
