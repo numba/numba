c4ac3c545d1b2e86a8fe1274a7debb801180cd56
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-01-24 13:53:50 -0600

    Cleanup and PEP8

--- numba/cgutils.py
+++ numba/cgutils.py
@@ -52,6 +52,7 @@ def make_bytearray(buf):
 
 _struct_proxy_cache = {}
 
+
 def create_struct_proxy(fe_type, kind='value'):
     """
     Returns a specialized StructProxy subclass for the given fe_type.
@@ -177,7 +178,7 @@ class _StructProxy(object):
         value = self._cast_member_from_value(index, value)
         if value.type != ptr.type.pointee:
             if (is_pointer(value.type) and is_pointer(ptr.type.pointee)
-                and value.type.pointee == ptr.type.pointee.pointee):
+                    and value.type.pointee == ptr.type.pointee.pointee):
                 # Differ by address-space only
                 # Auto coerce it
                 value = self._context.addrspacecast(self._builder,
@@ -440,6 +441,7 @@ def increment_index(builder, val):
 
 
 Loop = collections.namedtuple('Loop', ('index', 'do_break'))
+
 
 @contextmanager
 def for_range(builder, count, start=None, intp=None):
@@ -744,6 +746,7 @@ def is_scalar_zero_or_nan(builder, value):
     return _scalar_pred_against_zero(
         builder, value, functools.partial(builder.fcmp_unordered, '=='), '==')
 
+
 is_true = is_not_scalar_zero
 is_false = is_scalar_zero
 
@@ -766,6 +769,7 @@ def guard_null(context, builder, value, exc_tuple):
         exc_args = exc_tuple[1:] or None
         context.call_conv.return_user_exc(builder, exc, exc_args)
 
+
 def guard_memory_error(context, builder, pointer, msg=None):
     """
     Guard against *pointer* being NULL (and raise a MemoryError).
@@ -774,6 +778,7 @@ def guard_memory_error(context, builder, pointer, msg=None):
     exc_args = (msg,) if msg else ()
     with builder.if_then(is_null(builder, pointer), likely=False):
         context.call_conv.return_user_exc(builder, MemoryError, exc_args)
+
 
 @contextmanager
 def if_zero(builder, value, likely=False):
@@ -849,8 +854,6 @@ def memset(builder, ptr, size, value):
     """
     Fill *size* bytes starting from *ptr* with *value*.
     """
-    sizety = size.type
-    memset = "llvm.memset.p0i8.i%d" % (sizety.width)
     fn = builder.module.declare_intrinsic('llvm.memset', (voidptr_t, size.type))
     ptr = builder.bitcast(ptr, voidptr_t)
     if isinstance(value, int):
@@ -961,6 +964,7 @@ def raw_memcpy(builder, dst, src, count, itemsize, align=1):
     """
     return _raw_memcpy(builder, 'llvm.memcpy', dst, src, count, itemsize, align)
 
+
 def raw_memmove(builder, dst, src, count, itemsize, align=1):
     """
     Emit a raw memmove() call for `count` items of size `itemsize`
@@ -1030,8 +1034,9 @@ def snprintf(builder, buffer, bufsz, format, *args):
     ptr_fmt = builder.bitcast(global_fmt, cstring)
     return builder.call(fn, [buffer, bufsz, ptr_fmt] + list(args))
 
+
 def snprintf_stackbuffer(builder, bufsz, format, *args):
-    """Similar to `sprint()` but the buffer is stack allocated to size *bufsz*.
+    """Similar to `snprintf()` but the buffer is stack allocated to size *bufsz*.
 
     Returns the buffer pointer as i8*.
     """
