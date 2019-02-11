8701642104c7cf348da22979ac930eda7f9e7a99
Dimitri Vorona <vorona@in.tum.de>
2019-02-11 12:24:36 +0100

    Rename cffi typing file

--- numba/core/typing/context.py
+++ numba/core/typing/context.py
@@ -684,9 +684,10 @@ class BaseContext(object):
 class Context(BaseContext):
 
     def load_additional_registries(self):
-        from . import (cffi_utils, cmathdecl, enumdecl, listdecl, mathdecl,
+
+        from . import (cffidecl, cmathdecl, enumdecl, listdecl, mathdecl,
                        npydecl, randomdecl, setdecl, dictdecl)
-        self.install_registry(cffi_utils.registry)
+        self.install_registry(cffidecl.registry)
         self.install_registry(cmathdecl.registry)
         self.install_registry(enumdecl.registry)
         self.install_registry(listdecl.registry)
