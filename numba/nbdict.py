fa7e54ddeb1e981c27a556a5341e3888787bbfee
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-02-21 11:45:57 -0600

    Group dict pointer and type into a 2-tuple

--- numba/nbdict.py
+++ numba/nbdict.py
@@ -13,50 +13,50 @@ def _make_dict(keyty, valty):
 
 
 @njit
-def _length(box, dct_type):
-    d = dictobject._unbox(box, dct_type)
+def _length(opaque):
+    d = dictobject._unbox(*opaque)
     return len(d)
 
 
 @njit
-def _setitem(box, dct_type, key, value):
-    d = dictobject._unbox(box, dct_type)
+def _setitem(opaque, key, value):
+    d = dictobject._unbox(*opaque)
     d[key] = value
 
 
 @njit
-def _getitem(box, dct_type, key):
-    d = dictobject._unbox(box, dct_type)
+def _getitem(opaque, key):
+    d = dictobject._unbox(*opaque)
     return d[key]
 
 
 @njit
-def _delitem(box, dct_type, key):
-    d = dictobject._unbox(box, dct_type)
+def _delitem(opaque, key):
+    d = dictobject._unbox(*opaque)
     del d[key]
 
 
 @njit
-def _contains(box, dct_type, key):
-    d = dictobject._unbox(box, dct_type)
+def _contains(opaque, key):
+    d = dictobject._unbox(*opaque)
     return key in d
 
 
 @njit
-def _get(box, dct_type, key, default):
-    d = dictobject._unbox(box, dct_type)
+def _get(opaque, key, default):
+    d = dictobject._unbox(*opaque)
     return d.get(key, default)
 
 
 @njit
-def _setdefault(box, dct_type, key, default):
-    d = dictobject._unbox(box, dct_type)
+def _setdefault(opaque, key, default):
+    d = dictobject._unbox(*opaque)
     return d.setdefault(key, default)
 
 
 @njit
-def _iter(box, dct_type):
-    d = dictobject._unbox(box, dct_type)
+def _iter(opaque):
+    d = dictobject._unbox(*opaque)
     return list(d.keys())
 
 
@@ -81,31 +81,32 @@ class NBDict(MutableMapping):
         if not isinstance(dcttype, DictType):
             raise TypeError('*dcttype* must be a DictType')
         self._dict_type = dcttype
-        self._opaque = _make_dict(
+        ptr = _make_dict(
             self._dict_type.key_type,
             self._dict_type.value_type,
         )
+        self._opaque = (ptr, self._dict_type)
 
     def __getitem__(self, key):
-        return _getitem(self._opaque, self._dict_type, key)
+        return _getitem(self._opaque, key)
 
     def __setitem__(self, key, value):
-        return _setitem(self._opaque, self._dict_type, key, value)
+        return _setitem(self._opaque, key, value)
 
     def __delitem__(self, key):
-        _delitem(self._opaque, self._dict_type, key)
+        _delitem(self._opaque, key)
 
     def __iter__(self):
-        return iter(_iter(self._opaque, self._dict_type))
+        return iter(_iter(self._opaque))
 
     def __len__(self):
-        return _length(self._opaque, self._dict_type)
+        return _length((self._opaque))
 
     def __contains__(self, key):
-        return _contains(self._opaque, self._dict_type, key)
+        return _contains(self._opaque, key)
 
     def get(self, key, default=None):
-        return _get(self._opaque, self._dict_type, key, default)
+        return _get(self._opaque, key, default)
 
     def setdefault(self, key, default=None):
-        return _setdefault(self._opaque, self._dict_type, key, default)
+        return _setdefault(self._opaque, key, default)
