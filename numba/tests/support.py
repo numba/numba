483442de8b49688ec0f9402e0356d881352f029c
Stuart Archibald <stuart.archibald@googlemail.com>
2019-03-04 19:05:01 +0000

    Add in np.nat guard

--- numba/tests/support.py
+++ numba/tests/support.py
@@ -400,7 +400,8 @@ class TestCase(unittest.TestCase):
             _assertNumberEqual(first.real, second.real, delta)
             _assertNumberEqual(first.imag, second.imag, delta)
         elif isinstance(first, (np.timedelta64, np.datetime64)):
-            if np.isnat(first):
+            # Since Np 1.16 NaT == NaT is False, so special comparison needed
+            if numpy_support.version >= (1, 16) and np.isnat(first):
                 self.assertEqual(np.isnat(first), np.isnat(second))
             else:
                 _assertNumberEqual(first, second, delta)
