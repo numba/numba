62d1716e05779c97d5b9f622cad112c551db8786
Todd A. Anderson <drtodd13@comcast.net>
2019-03-29 08:58:13 -0700

    Change DEBUG_ARRAY_OPT debugging to >= so that we can use higher debug levels for more detail in parfor.py.

--- numba/ir_utils.py
+++ numba/ir_utils.py
@@ -487,7 +487,7 @@ def remove_dead(blocks, args, func_ir, typemap=None, alias_map=None, arg_aliases
     if alias_map is None or arg_aliases is None:
         alias_map, arg_aliases = find_potential_aliases(blocks, args, typemap,
                                                         func_ir)
-    if config.DEBUG_ARRAY_OPT == 1:
+    if config.DEBUG_ARRAY_OPT >= 2:
         print("alias map:", alias_map)
     # keep set for easier search
     alias_set = set(alias_map.keys())
@@ -769,7 +769,7 @@ def copy_propagate(blocks, typemap):
                                  | (in_copies[label] - kill_copies[label]))
         old_point = new_point
         new_point = copy.deepcopy(out_copies)
-    if config.DEBUG_ARRAY_OPT == 1:
+    if config.DEBUG_ARRAY_OPT >= 2:
         print("copy propagate out_copies:", out_copies)
     return in_copies, out_copies
 
@@ -969,7 +969,7 @@ def dprint_func_ir(func_ir, title, blocks=None):
     """Debug print function IR, with an optional blocks argument
     that may differ from the IR's original blocks.
     """
-    if config.DEBUG_ARRAY_OPT == 1:
+    if config.DEBUG_ARRAY_OPT >= 1:
         ir_blocks = func_ir.blocks
         func_ir.blocks = ir_blocks if blocks == None else blocks
         name = func_ir.func_id.func_qualname
@@ -1327,7 +1327,7 @@ def simplify(func_ir, typemap, calltypes):
     # remove dead code to enable fusion
     remove_dead(func_ir.blocks, func_ir.arg_names, func_ir, typemap)
     func_ir.blocks = simplify_CFG(func_ir.blocks)
-    if config.DEBUG_ARRAY_OPT == 1:
+    if config.DEBUG_ARRAY_OPT >= 1:
         dprint_func_ir(func_ir, "after simplify")
 
 class GuardException(Exception):
