713f59a85e8796b4c3fea7c0a3292b75d42dd99c
Valentin Haenel <vhaenel@anaconda.com>
2019-04-02 15:32:51 +0200

    adding tests for bitcasting

---
+++ numba/tests/test_numbers.py
@@ -0,0 +1,52 @@
+# (Some) Tests for numbers.py
+from __future__ import print_function, absolute_import, division
+
+from numba import njit
+from .support import TestCase
+
+import numpy as np
+
+
+@njit
+def view(scalar_val, astype):
+    return scalar_val.view(astype)
+
+
+class TestViewIntFloat(TestCase):
+
+    def test_32_bits(self):
+        dtypes = (np.uint32, np.int32, np.float32)
+        inputs = ((np.uint32(1),    (1, 1, 1.401298464324817e-45)),
+                  (np.int32(-1),    (4294967295, -1, np.nan)),
+                  (np.float32(1.0), (1065353216, 1065353216, 1.0)))
+        for input_, expected in inputs:
+            for type_, result in zip(dtypes, expected):
+                if not np.isnan(result):
+                    self.assertEqual(view(input_, type_), type_(result))
+                else:
+                    self.assertTrue(np.isnan(view(input_, type_)))
+
+    def test_64_bits(self):
+        dtypes = (np.uint64, np.int64, np.float64)
+        inputs = ((np.uint64(1),    (1, 1, 5e-324)),
+                  (np.int64(-1),    (18446744073709551615, -1, np.nan)),
+                  (np.float64(1.0), (4607182418800017408,
+                                     4607182418800017408,
+                                     1.0))
+                  )
+        for input_, expected in inputs:
+            for type_, result in zip(dtypes, expected):
+                if not np.isnan(result):
+                    self.assertEqual(view(input_, type_), type_(result))
+                else:
+                    self.assertTrue(np.isnan(view(input_, type_)))
+
+
+#            try:
+#                foo(10, np.int32)
+#            except:
+#                print("Exception")
+#            try:
+#                foo(10.0, np.float32)
+#            except:
+#            print("Exception")
