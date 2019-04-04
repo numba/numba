830259945f47e9137e521f20a4d5e32e995384c2
Valentin Haenel <vhaenel@anaconda.com>
2019-04-04 17:32:07 +0200

    adapt the tests so that they pass

--- numba/tests/test_numbers.py
+++ numba/tests/test_numbers.py
@@ -9,47 +9,67 @@ import numpy as np
 
 
 @njit
-def view(scalar_val, astype):
-    return scalar_val.view(astype)
+def view32(scalar_val, initial_type, astype):
+    if initial_type == "uint32":
+        return np.uint32(scalar_val).view(astype)
+    elif initial_type == "int32":
+        return np.int32(scalar_val).view(astype)
+    elif initial_type == "float32":
+        return np.float32(scalar_val).view(astype)
+
+
+@njit
+def view64(scalar_val, initial_type, astype):
+    if initial_type == "uint64":
+        return np.uint64(scalar_val).view(astype)
+    elif initial_type == "int64":
+        return np.int64(scalar_val).view(astype)
+    elif initial_type == "float64":
+        return np.float64(scalar_val).view(astype)
 
 
 class TestViewIntFloat(TestCase):
-
     def test_32_bits(self):
         dtypes = (np.uint32, np.int32, np.float32)
-        inputs = ((np.uint32(1),    (1, 1, 1.401298464324817e-45)),
-                  (np.int32(-1),    (4294967295, -1, np.nan)),
-                  (np.float32(1.0), (1065353216, 1065353216, 1.0)))
-        for input_, expected in inputs:
-            for type_, result in zip(dtypes, expected):
+        inputs = ((1,   "uint32",    (1, 1, 1.401298464324817e-45)),
+                  (-1,  "int32",     (4294967295, -1, np.nan)),
+                  (1.0, "float32",   (1065353216, 1065353216, 1.0)))
+        for value, initial_type, expected in inputs:
+            for target_type, result in zip(dtypes, expected):
                 if not np.isnan(result):
-                    self.assertEqual(view(input_, type_), type_(result))
+                    self.assertEqual(view32(value, initial_type, target_type),
+                                     target_type(result))
                 else:
-                    self.assertTrue(np.isnan(view(input_, type_)))
+                    self.assertTrue(np.isnan(view32(value,
+                                                    initial_type,
+                                                    target_type)))
 
     def test_64_bits(self):
         dtypes = (np.uint64, np.int64, np.float64)
-        inputs = ((np.uint64(1),    (1, 1, 5e-324)),
-                  (np.int64(-1),    (18446744073709551615, -1, np.nan)),
-                  (np.float64(1.0), (4607182418800017408,
-                                     4607182418800017408,
-                                     1.0))
+        inputs = ((1,   "uint64",    (1, 1, 5e-324)),
+                  (-1,  "int64",     (18446744073709551615, -1, np.nan)),
+                  (1.0, "float64",   (4607182418800017408,
+                                      4607182418800017408,
+                                      1.0))
                   )
-        for input_, expected in inputs:
-            for type_, result in zip(dtypes, expected):
+        for value, initial_type, expected in inputs:
+            for target_type, result in zip(dtypes, expected):
                 if not np.isnan(result):
-                    self.assertEqual(view(input_, type_), type_(result))
+                    self.assertEqual(view64(value, initial_type, target_type),
+                                     target_type(result))
                 else:
-                    self.assertTrue(np.isnan(view(input_, type_)))
+                    self.assertTrue(np.isnan(view64(value,
+                                                    initial_type,
+                                                    target_type)))
 
     def test_exceptions(self):
         with self.assertRaises(TypingError) as e:
-            view(np.int32(1), np.int64)
+            view32(1, "int32", np.int64)
         self.assertIn("Changing the dtype of a 0d array is only supported "
                       "if the itemsize is unchanged",
                       str(e.exception))
         with self.assertRaises(TypingError) as e:
-            view(np.int64(1), np.int32)
+            view64(1, "int64", np.int32)
         self.assertIn("Changing the dtype of a 0d array is only supported "
                       "if the itemsize is unchanged",
                       str(e.exception))
