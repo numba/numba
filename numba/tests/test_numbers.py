b18f73df80435b2848d7d1a32d011555d0dff36a
Valentin Haenel <vhaenel@anaconda.com>
2019-04-05 11:19:30 +0200

    simplify tests and make them python2.7 compat

--- numba/tests/test_numbers.py
+++ numba/tests/test_numbers.py
@@ -8,68 +8,52 @@ from .support import TestCase
 import numpy as np
 
 
-@njit
-def view32(scalar_val, initial_type, astype):
-    if initial_type == "uint32":
-        return np.uint32(scalar_val).view(astype)
-    elif initial_type == "int32":
-        return np.int32(scalar_val).view(astype)
-    elif initial_type == "float32":
-        return np.float32(scalar_val).view(astype)
-
-
-@njit
-def view64(scalar_val, initial_type, astype):
-    if initial_type == "uint64":
-        return np.uint64(scalar_val).view(astype)
-    elif initial_type == "int64":
-        return np.int64(scalar_val).view(astype)
-    elif initial_type == "float64":
-        return np.float64(scalar_val).view(astype)
+def gen_view(a,b):
+    def impl(x):
+        return a(x).view(b)
+    return impl
 
 
 class TestViewIntFloat(TestCase):
-    def test_32_bits(self):
-        dtypes = (np.uint32, np.int32, np.float32)
-        inputs = ((1,   "uint32",    (1, 1, 1.401298464324817e-45)),
-                  (-1,  "int32",     (4294967295, -1, np.nan)),
-                  (1.0, "float32",   (1065353216, 1065353216, 1.0)))
+
+    def do_testing(self, inputs, dtypes):
         for value, initial_type, expected in inputs:
             for target_type, result in zip(dtypes, expected):
+                view = njit(gen_view(initial_type, target_type))
                 if not np.isnan(result):
-                    self.assertEqual(view32(value, initial_type, target_type),
-                                     target_type(result))
+                    self.assertEqual(view(value), target_type(result))
                 else:
-                    self.assertTrue(np.isnan(view32(value,
-                                                    initial_type,
-                                                    target_type)))
+                    self.assertTrue(np.isnan(view(value)))
+
+    def test_32_bits(self):
+        dtypes = (np.uint32, np.int32, np.float32)
+        #        Value  Initial Type   Expected answers using dtypes
+        inputs = ((1,   np.uint32,    (1, 1, 1.401298464324817e-45)),
+                  (-1,  np.int32,     (4294967295, -1, np.nan)),
+                  (1.0, np.float32,   (1065353216, 1065353216, 1.0)))
+        self.do_testing(inputs, dtypes)
 
     def test_64_bits(self):
         dtypes = (np.uint64, np.int64, np.float64)
-        inputs = ((1,   "uint64",    (1, 1, 5e-324)),
-                  (-1,  "int64",     (18446744073709551615, -1, np.nan)),
-                  (1.0, "float64",   (4607182418800017408,
-                                      4607182418800017408,
-                                      1.0))
+        #        Value  Initial Type   Expected answers using dtypes
+        inputs = ((1,   np.uint64,    (1, 1, 5e-324)),
+                  (-1,  np.int64,     (18446744073709551615, -1, np.nan)),
+                  (1.0, np.float64,   (4607182418800017408,
+                                       4607182418800017408,
+                                       1.0))
                   )
-        for value, initial_type, expected in inputs:
-            for target_type, result in zip(dtypes, expected):
-                if not np.isnan(result):
-                    self.assertEqual(view64(value, initial_type, target_type),
-                                     target_type(result))
-                else:
-                    self.assertTrue(np.isnan(view64(value,
-                                                    initial_type,
-                                                    target_type)))
+        self.do_testing(inputs, dtypes)
 
     def test_exceptions(self):
         with self.assertRaises(TypingError) as e:
-            view32(1, "int32", np.int64)
+            view = njit(gen_view(np.int32, np.int64))
+            view(1)
         self.assertIn("Changing the dtype of a 0d array is only supported "
                       "if the itemsize is unchanged",
                       str(e.exception))
         with self.assertRaises(TypingError) as e:
-            view64(1, "int64", np.int32)
+            view = njit(gen_view(np.int64, np.int32))
+            view(1)
         self.assertIn("Changing the dtype of a 0d array is only supported "
                       "if the itemsize is unchanged",
                       str(e.exception))
