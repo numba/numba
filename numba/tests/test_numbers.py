92cc0dbe3300b6a813258f8a8e7735385c319efd
Valentin Haenel <vhaenel@anaconda.com>
2019-04-06 19:04:30 +0200

    skip 64 bit tests on 32 bits

--- numba/tests/test_numbers.py
+++ numba/tests/test_numbers.py
@@ -1,11 +1,15 @@
 # (Some) Tests for numbers.py
 from __future__ import print_function, absolute_import, division
 
-from numba import njit
-from numba.errors import TypingError
-from .support import TestCase
+import sys
 
 import numpy as np
+
+from numba import njit
+from numba.errors import TypingError
+from .support import TestCase, unittest
+
+_32bit_only = not (sys.maxsize > 2 ** 32)
 
 
 def gen_view(a,b):
@@ -40,6 +44,7 @@ class TestViewIntFloat(TestCase):
                   (1.0, np.float32,   (1065353216, 1065353216, 1.0)))
         self.do_testing(inputs, dtypes)
 
+    @unittest.skipIf(_32bit_only, "64 bit test only")
     def test_64_bits(self):
         dtypes = (np.uint64, np.int64, np.float64)
         #        Value  Initial Type   Expected answers using dtypes
@@ -53,25 +58,39 @@ class TestViewIntFloat(TestCase):
 
     def test_python_scalar_exception(self):
         @njit
-        def call_view_on_scalar():
+        def view32():
+            a = 1
+            a.view(np.float32)
+
+        @njit
+        def view64():
             a = 1
             a.view(np.float64)
+
+        def call_view_on_scalar():
+            if _32bit_only:
+                view32()
+            else:
+                view64()
         with self.assertRaises(TypingError) as e:
             call_view_on_scalar()
         self.assertIn("'view' can only be called on NumPy dtypes, "
                       "try wrapping the variable with 'np.<dtype>()'",
                       str(e.exception))
 
-    def test_exceptions(self):
+    def do_testing_exceptions(self, pair):
         with self.assertRaises(TypingError) as e:
-            view = njit(gen_view(np.int32, np.int64))
+            view = njit(gen_view(pair[0], pair[1]))
             view(1)
         self.assertIn("Changing the dtype of a 0d array is only supported "
                       "if the itemsize is unchanged",
                       str(e.exception))
-        with self.assertRaises(TypingError) as e:
-            view = njit(gen_view(np.int64, np.int32))
-            view(1)
-        self.assertIn("Changing the dtype of a 0d array is only supported "
-                      "if the itemsize is unchanged",
-                      str(e.exception))
+
+    def test_exceptions32(self):
+        for pair in ((np.int32, np.int8), (np.int8, np.int32)):
+            self.do_testing_exceptions(pair)
+
+    @unittest.skipIf(_32bit_only, "64 bit test only")
+    def test_exceptions64(self):
+        for pair in ((np.int32, np.int64), (np.int64, np.int32)):
+            self.do_testing_exceptions(pair)
