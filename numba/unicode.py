4151156ae6fec829de1ca0c018461d4caf51d968
Sergey Shalnov <Sergey.Shalnov@intel.com>
2019-04-11 13:34:01 +0200

    Unicode_split_algo_versioning

--- numba/unicode.py
+++ numba/unicode.py
@@ -517,17 +517,24 @@ def unicode_split(a, sep=None, maxsplit=-1):
             parts = []
             last = 0
             idx = 0
-            split_count = 0
 
-            while idx < a_len and (maxsplit == -1 or split_count < maxsplit):
-                if _cmp_region(a, idx, sep, 0, sep_len) == 0:
-                    parts.append(a[last:idx])
-                    idx += sep_len
-                    last = idx
-                    split_count += 1
-                else:
-                    idx += 1
-
+            if sep_len == 1 and maxsplit == -1:
+                for idx in range(a_len):
+                    if _get_code_point(a, idx) == _get_code_point(sep, 0):
+                        parts.append(a[last:idx])
+                        last = idx + 1
+            else:
+                split_count = 0
+    
+                while idx < a_len and (maxsplit == -1 or split_count < maxsplit):
+                    if _cmp_region(a, idx, sep, 0, sep_len) == 0:
+                        parts.append(a[last:idx])
+                        idx += sep_len
+                        last = idx
+                        split_count += 1
+                    else:
+                        idx += 1
+    
             if last <= a_len:
                 parts.append(a[last:])
 
