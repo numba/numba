727a3f5962e3fcb01a2ed567407b1d83aa093a66
Stuart Archibald <stuart.archibald@googlemail.com>
2019-04-24 17:46:47 +0100

    Add NumPy version guard.
    As title.

--- numba/tests/test_linalg.py
+++ numba/tests/test_linalg.py
@@ -2053,11 +2053,14 @@ class TestLinalgCond(TestLinalgBase):
             self.assert_raise_on_empty(cfunc, (np.empty(sz),))
 
         # singular systems to trip divide-by-zero
-        x = np.array([[1, 0], [0, 0]], dtype=np.float64)
-        check(x)
-        check(x, p=2)
-        x = np.array([[0, 0], [0, 0]], dtype=np.float64)
-        check(x, p=-2)
+        # only for np > 1.14, before this norm was computed via inversion which
+        # will fail with numpy.linalg.linalg.LinAlgError: Singular matrix
+        if numpy_version > (1, 14):
+            x = np.array([[1, 0], [0, 0]], dtype=np.float64)
+            check(x)
+            check(x, p=2)
+            x = np.array([[0, 0], [0, 0]], dtype=np.float64)
+            check(x, p=-2)
 
         # try an ill-conditioned system with 2-norm, make sure np raises an
         # overflow warning as the result is `+inf` and that the result from
