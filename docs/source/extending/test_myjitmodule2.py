152c21c8ab0ea2135e57b5fe4b940f363bd4e29d
Valentin Haenel <vhaenel@anaconda.com>
2019-05-15 10:09:29 +0200

    fix tests after changes imn exception wording
    After changing the wordning of some of the exceptions, some of the tests
    failed. This commit fixes that.

--- docs/source/extending/test_myjitmodule2.py
+++ docs/source/extending/test_myjitmodule2.py
@@ -30,28 +30,28 @@ class TestSetToX(support.TestCase):
         a[0] = np.nan
         with self.assertRaises(ValueError) as e:
             wrap_set_to_x(a, 1.0)
-        self.assertIn("no element of arr must be nan",
+        self.assertIn("no element of arr may be NaN",
                       str(e.exception))
 
     def test_type_mismatch(self):
         a = np.arange(10)
         with self.assertRaises(TypingError) as e:
             wrap_set_to_x(a, 1.0)
-        self.assertIn("the types of the input do not match",
+        self.assertIn("the types of the inputs do not match",
                       str(e.exception))
 
     def test_exception_on_unsupported_dtype(self):
         a = np.arange(10, dtype=np.complex128)
         with self.assertRaises(TypingError) as e:
             wrap_set_to_x(a, np.complex128(1.0))
-        self.assertIn("only integer and floating-point types allowed",
+        self.assertIn("only integer and floating-point types are allowed",
                       str(e.exception))
 
     def test_exception_on_tuple(self):
         a = (1, 2, 3)
         with self.assertRaises(TypingError) as e:
             wrap_set_to_x(a, 1)
-        self.assertIn("tuple isn't allowed as input, use numpy arrays",
+        self.assertIn("tuple isn't allowed as input, use NumPy ndarray",
                       str(e.exception))
 
 
