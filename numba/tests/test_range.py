258408e61a71ea74aaa2bd4fb4e8d58834c3e437
Joshua L. Adelman <joshua.adelman@gmail.com>
2019-06-03 21:02:29 -0400

    Fixes to tests for range contains

--- numba/tests/test_range.py
+++ numba/tests/test_range.py
@@ -67,10 +67,7 @@ def range_contains(val, start, stop, step):
     r1 = range(start)
     r2 = range(start, stop)
     r3 = range(start, stop, step)
-    tmp = []
-    for r in (r1, r2, r3):
-        tmp.append(val in r)
-    return tmp
+    return [val in r for r in (r1, r2, r3)]
 
 
 class TestRange(unittest.TestCase):
@@ -191,24 +188,34 @@ class TestRange(unittest.TestCase):
     def test_range_contains(self):
         pyfunc = range_contains
         arglist = [(0, 0, 1),
+                   (-1, 0, 1),
+                   (1, 0, -1),
                    (0, -1, 1),
+                   (0, 1, -1),
                    (-1, 1, 1),
                    (-1, 4, 1),
                    (-1, 4, 10),
                    (5, -5, -2),]
 
-        vallist = [-10, -6, -5, -4, -2, -1, 0,
-                1, 2, 4, 5, 6, 10, -1.1, False, True,
-                1 + 0j, 1 + 1j, 1.1 + 0j, 1.0 + 1.1j]
+        bool_vals = [True, False]
+        int_vals = [-10, -6, -5, -4, -2, -1, 0,
+                     1, 2, 4, 5, 6, 10]
+        float_vals = [-1.1, -1.0, 0.0, 1.0, 1.1]
+        complex_vals = [1 + 0j, 1 + 1j, 1.1 + 0j, 1.0 + 1.1j]
+
+        vallist = (bool_vals + int_vals + float_vals
+                + complex_vals)
 
         cfunc = njit(pyfunc)
         for arg in arglist:
             for val in vallist:
                 self.assertEqual(cfunc(val, *arg), pyfunc(val, *arg))
 
+        non_numeric_vals = [{'a': 1}, [1, ], 'abc', (1,)]
+
         cfunc_obj = jit(pyfunc, forceobj=True)
         for arg in arglist:
-            for val in [{'a': 1}, [1,], 'abc']:
+            for val in non_numeric_vals:
                 self.assertEqual(cfunc_obj(val, *arg), pyfunc(val, *arg))
 
 
