7cacc03015f171d5924aadc71409612282912dd3
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-06-06 02:36:53 -0300

    Add helper functions and tests
    helper functions for getting the immediate dominator (dominator tree) and dominance frontier

--- numba/controlflow.py
+++ numba/controlflow.py
@@ -144,6 +144,26 @@ class CFGraph(object):
         """
         return self._post_doms
 
+    def immediate_dominators(self):
+        """
+        Return a dictionary of {node -> node} mapping each node to its
+        immediate dominator (idom).
+
+        The idom(B) is the closest strict dominator of V
+        """
+        return self._idom
+
+    def dominance_frontier(self):
+        """
+        Return a dictionary of {node -> set(nodes)} mapping each node to
+        the nodes in its dominance frontier.
+
+        The dominance frontier _df(N) is the set of all nodes that are
+        immediate successors to blocks dominanted by N but which aren't
+        stricly dominanted by N
+        """
+        return self._df
+
     def descendents(self, node):
         """
         Return the set of descendents of the given *node*, in topological
@@ -360,7 +380,7 @@ class CFGraph(object):
                     df[v].add(u)
                     v = idom[v]
 
-        self._frontier = df
+        self._df = df
 
     def _find_dominators_internal(self, post=False):
         # See theoretical description in
--- numba/tests/test_flow_control.py
+++ numba/tests/test_flow_control.py
@@ -818,6 +818,28 @@ class TestCFGraph(TestCase):
                                16: [16],
                                })
 
+    def test_immediate_dominators(self):
+        def check(graph, expected):
+            idoms = graph.immediate_dominators()
+            self.assertEqual(idoms, expected)
+
+        check(self.loopless1(),
+              {0: 0, 12: 0, 18: 0, 21: 0})
+        check(self.loopless2(),
+              {18: 99, 12: 99, 21: 99, 42: 21, 34: 21, 99: 99})
+        check(self.loopless1_dead_nodes(),
+              {0: 0, 12: 0, 18: 0, 21: 0})
+        check(self.multiple_loops(),
+              {0: 0, 7: 0, 10: 7, 13: 10, 20: 13, 23: 20,
+               32: 23, 44: 23, 56: 20, 57: 56, 60: 7, 61: 60,
+               68: 61, 71: 68, 80: 71, 87: 68, 88: 87})
+        check(self.multiple_exits(),
+              {0:0, 7: 0, 10: 7, 19: 10, 23: 10, 29: 23, 36: 7, 37: 7})
+        check(self.infinite_loop1(),
+              {0: 0, 6: 0, 10: 0, 13: 10, 19: 13, 26: 13})
+        check(self.infinite_loop2(),
+              {0: 0, 3: 0, 9: 3, 16: 3})
+
     def test_backbone_loopless(self):
         for g in [self.loopless1(), self.loopless1_dead_nodes()]:
             self.assertEqual(sorted(g.backbone()), [0, 21])
