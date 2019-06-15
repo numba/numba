72d033e92c5af924c6e24aa13deb34aa81b56b05
oxyflour <oxyflour@hotmail.com>
2019-06-15 11:08:35 +0800

    fix: lint

--- numba/cuda/graph.py
+++ numba/cuda/graph.py
@@ -2,6 +2,7 @@ from ctypes import c_void_p, addressof, c_uint, POINTER, Structure
 from numba.cuda.cudadrv.driver import driver, is_device_memory, device_ctypes_pointer, byref
 from numba.cuda.compiler import AutoJitCUDAKernel, CUDAKernel
 
+
 class KernelParams(Structure):
     _fields_ = [
         ('func', c_void_p),
@@ -16,18 +17,20 @@ class KernelParams(Structure):
         ('extra', POINTER(c_void_p)),
     ]
 
+
 class Node:
     pass
 
+
 class KernelNode(Node):
-    def __init__(self, kernel, args = None, deps = None, params = None):
+    def __init__(self, kernel, args=None, deps=None, params=None):
         self.kernel = kernel
         self.args = args or []
         self.deps = deps or []
         self.params = params or { }
         self.builded = { }
         super().__init__()
-    
+
     def _get_params(self):
         params = KernelParams()
         params.blockDimX = self.params.get('blockDimX', 1)
@@ -49,7 +52,7 @@ class KernelNode(Node):
         retr, kernel_args = [], []
         for t, v in zip(kernel.argument_types, self.args):
             kernel._prepare_args(t, v, 0, retr, kernel_args)
-        
+
         # TODO: take care of retr after graph launched
         if len(retr):
             raise Exception('host array as kernel node args not supported yet')
@@ -64,8 +67,8 @@ class KernelNode(Node):
         params.kernelParams = (c_void_p * len(param_vals))(*param_vals) if len(param_vals) else None
         params.extra = self.params.get('extra', None)
         return params
-    
-    def build(self, graph = None):
+
+    def build(self, graph=None):
         if not graph:
             graph = Graph()
 
@@ -82,6 +85,7 @@ class KernelNode(Node):
         driver.cuGraphAddKernelNode(byref(builded.handle), graph.handle, dep_arr, dep_num, byref(params))
         return builded
 
+
 class BuildedNode:
     def __init__(self, handle, graph):
         self.handle = handle
@@ -90,21 +94,23 @@ class BuildedNode:
     def instantiate(self):
         return GraphExec(self.graph)
 
-    def launch(self, stream = None):
+    def launch(self, stream=None):
         return GraphExec(self.graph).launch(stream)
 
+
 class Graph:
-    def __init__(self, flags = 0):
+    def __init__(self, flags=0):
         self.handle = c_void_p()
         driver.cuGraphCreate(byref(self.handle), flags)
 
     def instantiate(self):
         return GraphExec(self)
 
+
 class GraphExec:
     def __init__(self, graph):
         self.handle = c_void_p()
         driver.cuGraphInstantiate(byref(self.handle), graph.handle, c_void_p(), c_void_p(), 0)
-    
-    def launch(self, stream = None):
+
+    def launch(self, stream=None):
         driver.cuGraphLaunch(self.handle, stream.handle if stream else 0)
