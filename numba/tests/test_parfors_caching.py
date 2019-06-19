0d31b345e280cd52802e44ba0d4aaf23d84e0f7a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-06-19 10:06:37 -0500

    Better tests

--- numba/tests/test_parfors_caching.py
+++ numba/tests/test_parfors_caching.py
@@ -14,17 +14,32 @@ class TestParForsCache(BaseCacheUsecasesTest):
     usecases_file = os.path.join(here, "parfors_cache_usecases.py")
     modname = "parfors_caching_test_fodder"
 
-    def test_arrayexprs(self):
+    def run_test(self, fname, num_funcs=1):
         mod = self.import_module()
         self.check_pycache(0)
-        f = mod.arrayexprs
+        f = getattr(mod, fname)
         ary = np.ones(10)
-        self.assertPreciseEqual(f(ary), ary / ary.sum())
+        self.assertPreciseEqual(f(ary), f.py_func(ary))
 
         dynamic_globals = [cres.library.has_dynamic_globals
                            for cres in f.overloads.values()]
         [cres] = f.overloads.values()
         self.assertEqual(dynamic_globals, [False])
-        self.check_pycache(2)  # 1 index, 1 data
+        # For each cached func, there're 2 entries (index + data)
+        self.check_pycache(num_funcs * 2)
 
         self.run_in_separate_process()
+
+    def test_arrayexprs(self):
+        f = 'arrayexprs_case'
+        self.run_test(f)
+
+    def test_prange(self):
+        f = 'prange_case'
+        self.run_test(f)
+
+    def test_caller(self):
+        f = 'caller_case'
+        # num_funcs=3 because, there's the `caller_case()` which calls
+        # the `prange_case()` and `arrayexprs_case()`
+        self.run_test(f, num_funcs=3)
