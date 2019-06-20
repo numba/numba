16dff4ea0f28ec39086508fe8d152b0a21bdb581
Stuart Archibald <stuart.archibald@googlemail.com>
2019-06-20 21:03:19 +0100

    Try this.
    As title.

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -2,6 +2,15 @@
 
 set -v -e
 
+# first configure conda to have more tolerance of network problems, these
+# numbers are not scientifically chosen, just merely larger than defaults
+conda config
+conda config --set remote_connect_timeout_secs 30.15
+conda config --set remote_max_retries 10
+conda config --set remote_read_timeout_secs 120.2
+conda info
+conda config --show
+
 CONDA_INSTALL="conda install -q -y"
 PIP_INSTALL="pip install -q"
 
