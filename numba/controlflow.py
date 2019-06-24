40d785706e80cc3a94748bffe32445d6824a5bbe
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-06-23 23:26:38 -0300

    Add dominator tree

--- numba/controlflow.py
+++ numba/controlflow.py
@@ -124,6 +124,7 @@ class CFGraph(object):
         self._find_post_dominators()
         self._find_immediate_dominators()
         self._find_dominance_frontier()
+        self._find_dominator_tree()
 
     def dominators(self):
         """
@@ -163,6 +164,15 @@ class CFGraph(object):
         stricly dominanted by N
         """
         return self._df
+
+    def dominator_tree(self):
+        """
+        return a dictionary of {node -> set(nodes)} mapping each node to
+        the set of nodes it dominates
+
+        The domtree(B) is the closest strict set of nodes that B dominates
+        """
+        return self._domtree
 
     def descendents(self, node):
         """
@@ -367,6 +377,20 @@ class CFGraph(object):
                     changed = True
 
         self._idom = idom
+
+    def _find_dominator_tree(self):
+        idom = self._idom
+        domtree = {}
+
+        for u, v in idom.items():
+            # v dominates u
+            if u not in domtree:
+                domtree[u] = set()
+
+            if u != v:
+                domtree[v].add(u)
+
+        self._domtree = domtree
 
     def _find_dominance_frontier(self):
         idom = self._idom
