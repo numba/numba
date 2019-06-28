2016afc028313d66e3746e4ae3e8092000981a67
James Bourbeau <jrbourbeau@gmail.com>
2019-06-28 11:38:07 -0500

    Updates Annotate docstring

--- numba/pretty_annotate.py
+++ numba/pretty_annotate.py
@@ -228,23 +228,39 @@ class Annotate:
     Example:
 
     >>> import numba
-    ... from numba.pretty_annotate import Annotate
-    ...
-    ... @numba.jit
+    >>> from numba.pretty_annotate import Annotate
+    >>> @numba.jit
     ... def test(q):
     ...     res = 0
     ...     for i in range(q):
     ...         res += i
     ...     return res
     ...
-    ... test(10)
-    ... Annotate(test)
+    >>> test(10)
+    45
+    >>> Annotate(test)
 
     The last line will return an HTML and/or ANSI representation that will be
     displayed accordingly in Jupyter/IPython.
 
     Function annotations persist across compilation for newly encountered
-    type signatures and as a result annotations are shown for all signatures.
+    type signatures and as a result annotations are shown for all signatures
+    by default.
+
+    Annotations for a specific signature can be shown by using the
+    ``signature`` parameter.
+
+    >>> @numba.jit
+    ... def add(x, y):
+    ...     return x + y
+    ...
+    >>> add(1, 2)
+    3
+    >>> add(1.3, 5.7)
+    7.0
+    >>> add.signatures
+    [(int64, int64), (float64, float64)]
+    >>> Annotate(add, signature=add.signatures[1])  # annotation for (float64, float64)
     """
     def __init__(self, function, signature=None, **kwargs):
 
