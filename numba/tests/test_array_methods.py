e0370f7852a3f0a294f522a386e1f83c77513d01
luk-f-a <lucioarjona@hotmail.com>
2019-09-17 19:00:37 +0200

    split out tests for int32 and uint32 due to special numpy behaviour

--- numba/tests/test_array_methods.py
+++ numba/tests/test_array_methods.py
@@ -815,8 +815,8 @@ class TestArrayMethods(MemoryLeakMixin, TestCase):
     def test_sum_axis_kws2(self):
         pyfunc = array_sum_axis_kws
         cfunc = jit(nopython=True)(pyfunc)
-        all_dtypes = [np.float64, np.float32, np.int64, np.int32, np.uint32,
-                      np.uint64, np.complex64]
+        all_dtypes = [np.float64, np.float32, np.int64, np.uint64, np.complex64]
+        # timedelta test cannot be enabled until issue #4540 is fixed
         # all_dtypes += [np.timedelta64]
         all_test_arrays = [
             [np.ones((7, 6, 5, 4, 3), arr_dtype),
@@ -834,11 +834,46 @@ class TestArrayMethods(MemoryLeakMixin, TestCase):
                         self.assertPreciseEqual(pyfunc(arr, axis=axis),
                                                 cfunc(arr, axis=axis))
 
+    def test_sum_axis_kws3(self):
+        """ uint32 and int32 must be tested separately because Numpy's current
+        behaviour is different in 64bits Windows (accumulates as int32)
+        and 64bits Linux (accumulates as int64), while Numba has decided to always
+        accumulate as int64, when the OS is 64bits. No testing has been done
+        for behaviours in 32 bits platforms.
+
+        :return:
+        """
+        pyfunc = array_sum_axis_kws
+        cfunc = jit(nopython=True)(pyfunc)
+        all_dtypes = [np.int32, np.uint32]
+        # expected return dtypes in Numba
+        out_dtypes = {np.dtype('int32'): np.int64, np.dtype('uint32'): np.uint64,
+                      np.dtype('int64'): np.int64}
+        # timedelta test cannot be enabled until issue #4540 is fixed
+        # all_dtypes += [np.timedelta64]
+        all_test_arrays = [
+            [np.ones((7, 6, 5, 4, 3), arr_dtype),
+             np.ones(1, arr_dtype),
+             np.ones((7, 3), arr_dtype) * -5]
+            for arr_dtype in all_dtypes]
+
+        for arr_list in all_test_arrays:
+            for arr in arr_list:
+                for axis in (0, 1, 2):
+                    if axis > len(arr.shape)-1:
+                        continue
+                    with self.subTest("Testing np.sum(axis) with {} "
+                                      "input ".format(arr.dtype)):
+                        self.assertPreciseEqual(
+                            pyfunc(arr, axis=axis).astype(out_dtypes[arr.dtype]),
+                            cfunc(arr, axis=axis))
+
     def test_sum_dtype_kws(self):
         pyfunc = array_sum_dtype_kws
         cfunc = jit(nopython=True)(pyfunc)
         all_dtypes = [np.float64, np.float32, np.int64, np.int32, np.uint32,
                       np.uint64, np.complex64]
+        # timedelta test cannot be enabled until issue #4540 is fixed
         # all_dtypes += [np.timedelta64]
         all_test_arrays = [
             [np.ones((7, 6, 5, 4, 3), arr_dtype),
