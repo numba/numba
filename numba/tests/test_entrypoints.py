6e88946a448dcfea3e3090920ec9607195c4bc9d
Stan Seibert <stan.seibert@continuum.io>
2019-09-18 10:34:53 -0500

    Remove fake module from sys.modules after the test

--- numba/tests/test_entrypoints.py
+++ numba/tests/test_entrypoints.py
@@ -17,35 +17,41 @@ class TestEntrypoints(TestCase):
 
         # FIXME: Python 2 workaround because nonlocal doesn't exist
         counters = {'init': 0}
-
         def init_function():
             counters['init'] += 1
 
         mod = types.ModuleType("_test_numba_extension")
         mod.init_func = init_function
-        sys.modules[mod.__name__] = mod
 
-        # We are registering an entry point using the "numba" package
-        # ("distribution" in pkg_resources-speak) itself, though these are
-        # normally registered by other packages.
-        dist = "numba"
-        entrypoints = pkg_resources.get_entry_map(dist)
-        my_entrypoint = pkg_resources.EntryPoint(
-            "init", # name of entry point
-            mod.__name__, # module with entry point object
-            attrs=['init_func'], # name of entry point object
-            dist=pkg_resources.get_distribution(dist)
-        )
-        entrypoints.setdefault('numba_extensions', {})['init'] = my_entrypoint
+        try:
+            # will remove this module at the end of the test
+            sys.modules[mod.__name__] = mod
 
-        import numba.entrypoints
-        numba.entrypoints._already_initialized = False  # Allow reinitialization
+            # We are registering an entry point using the "numba" package
+            # ("distribution" in pkg_resources-speak) itself, though these are
+            # normally registered by other packages.
+            dist = "numba"
+            entrypoints = pkg_resources.get_entry_map(dist)
+            my_entrypoint = pkg_resources.EntryPoint(
+                "init", # name of entry point
+                mod.__name__, # module with entry point object
+                attrs=['init_func'], # name of entry point object
+                dist=pkg_resources.get_distribution(dist)
+            )
+            entrypoints.setdefault('numba_extensions', {})['init'] = my_entrypoint
 
-        numba.entrypoints.init_all()
+            import numba.entrypoints
+            numba.entrypoints._already_initialized = False  # Allow reinitialization
 
-        # was our init function called?
-        self.assertEqual(counters['init'], 1)
+            numba.entrypoints.init_all()
 
-        # ensure we do not initialize twice
-        numba.entrypoints.init_all()
-        self.assertEqual(counters['init'], 1)
+            # was our init function called?
+            self.assertEqual(counters['init'], 1)
+
+            # ensure we do not initialize twice
+            numba.entrypoints.init_all()
+            self.assertEqual(counters['init'], 1)
+        finally:
+            # remove fake module
+            if mod.__name__ in sys.modules:
+                del sys.modules[mod.__name__]
