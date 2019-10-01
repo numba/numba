337bf5fcd5ebc896ddd353753f9f13c40a6b35e9
Todd A. Anderson <drtodd13@comcast.net>
2019-10-01 10:35:06 -0700

    Handle 0 correctly as slice parameter.

--- numba/array_analysis.py
+++ numba/array_analysis.py
@@ -79,7 +79,7 @@ def wrap_index(typingctx, idx, size):
         zero = llvmlite.ir.Constant(idx.type, 0)
         is_negative = builder.icmp_signed('<', rem, zero)
         wrapped_rem = builder.add(rem, size)
-        is_oversize = builder.icmp_signed('>', wrapped_rem, size)
+        is_oversize = builder.icmp_signed('>=', wrapped_rem, size)
         mod = builder.select(is_negative, wrapped_rem,
                 builder.select(is_oversize, rem, wrapped_rem))
         return mod
