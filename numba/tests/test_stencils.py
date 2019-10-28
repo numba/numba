86a0e664068ace01f98b093501f630ac42a72dca
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-10-28 12:38:24 -0500

    Fix stencil tests

--- numba/tests/test_stencils.py
+++ numba/tests/test_stencils.py
@@ -1268,7 +1268,8 @@ class pyStencilGenerator:
                 vararg=None,
                 kwarg=None,
                 kwonlyargs=[],
-                kw_defaults=[])
+                kw_defaults=[],
+                posonlyargs=[])
             new = ast.FunctionDef(
                 name='__%s' %
                 node.name,
