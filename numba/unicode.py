40069b5d97a98b8c94431dfac54dd8b5d6b51d6e
Denis <denis.smirnov@intel.com>
2019-11-13 15:52:22 +0300

    Improve str.casefold()
    Added case folding of ascii, merged _do_casefold to the main code and
    added processing of empty input string.

--- numba/unicode.py
+++ numba/unicode.py
@@ -1361,36 +1361,42 @@ def unicode_upper(a):
     return impl
 
 
-# https://github.com/python/cpython/blob/1d4b6ba19466aba0eb91c4ba01ba509acf18c723/Objects/unicodeobject.c#L9819-L9834    # noqa: E501
+# https://github.com/python/cpython/blob/1d4b6ba19466aba0eb91c4ba01ba509acf18c723/Objects/unicodeobject.c#L10782-L10791    # noqa: E501
 @overload_method(types.UnicodeType, 'casefold')
 def unicode_casefold(data):
     """Implements str.casefold()"""
     def impl(data):
-        # https://github.com/python/cpython/blob/1d4b6ba19466aba0eb91c4ba01ba509acf18c723/Objects/unicodeobject.c#L10782-L10791    # noqa: E501
-        def _do_casefold(data, length, res, maxchars):
-            """Translation of the function to case fold a unicode string."""
-            k = 0
-            mapped = np.zeros(3, dtype=_Py_UCS4)
-            for idx in range(length):
-                mapped.fill(0)
-                code_point = _get_code_point(data, idx)
-                n_res = _PyUnicode_ToFoldedFull(code_point, mapped)
-                for m in mapped[:n_res]:
-                    maxchar = maxchars[0]
-                    maxchars[0] = max(maxchar, m)
-                    _set_code_point(res, k, m)
-                    k += 1
-            return k
-
         length = len(data)
-        tmp = _empty_string(PY_UNICODE_4BYTE_KIND, 3 * length, data._is_ascii)
-        # maxchar should be inside of a list to be pass as argument by reference
-        maxchars = [0]
-        newlength = _do_casefold(data, length, tmp, maxchars)
-        maxchar = maxchars[0]
+        if length == 0:
+            return _empty_string(data._kind, length, data._is_ascii)
+
+        if data._is_ascii:
+            # https://github.com/python/cpython/blob/1d4b6ba19466aba0eb91c4ba01ba509acf18c723/Objects/unicodeobject.c#L9678-L9694    # noqa: E501
+            res = _empty_string(data._kind, length, 1)
+            for idx in range(length):
+                code_point = _get_code_point(data, idx)
+                _set_code_point(res, idx, _Py_TOLOWER(code_point))
+
+            return res
+
+        # https://github.com/python/cpython/blob/1d4b6ba19466aba0eb91c4ba01ba509acf18c723/Objects/unicodeobject.c#L9863-L9908    # noqa: E501
+        # mixed with:
+        # https://github.com/python/cpython/blob/1d4b6ba19466aba0eb91c4ba01ba509acf18c723/Objects/unicodeobject.c#L9819-L9834    # noqa: E501
+        k = 0
+        maxchar = 0
+        mapped = np.zeros(3, dtype=_Py_UCS4)
+        tmp = _empty_string(PY_UNICODE_4BYTE_KIND, 3 * length)
+        for idx in range(length):
+            mapped.fill(0)
+            code_point = _get_code_point(data, idx)
+            n_res = _PyUnicode_ToFoldedFull(code_point, mapped)
+            for m in mapped[:n_res]:
+                maxchar = max(maxchar, m)
+                _set_code_point(tmp, k, m)
+                k += 1
         newkind = _codepoint_to_kind(maxchar)
-        res = _empty_string(newkind, newlength, _codepoint_is_ascii(maxchar))
-        for i in range(newlength):
+        res = _empty_string(newkind, k)
+        for i in range(k):
             _set_code_point(res, i, _get_code_point(tmp, i))
 
         return res
