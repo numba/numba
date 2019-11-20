b15e371f2203a1814eb12b5c2a218d38604bf0a8
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-11-20 15:18:37 -0600

    Add tests

--- numba/tests/test_try_except.py
+++ numba/tests/test_try_except.py
@@ -4,12 +4,16 @@ from numba import njit
 from .support import TestCase, unittest
 
 
+class MyError(Exception):
+    pass
+
+
 class TestUsecases(TestCase):
     def test_try_inner_raise(self):
         @njit
         def inner(x):
             if x:
-                raise ZeroDivisionError
+                raise MyError
 
         @njit
         def udt(x):
@@ -21,6 +25,31 @@ class TestUsecases(TestCase):
 
         self.assertEqual(udt(False), "not raised")
         self.assertEqual(udt(True), "caught")
+
+    def test_try_state_reset(self):
+        @njit
+        def inner(x):
+            if x == 1:
+                raise MyError("one")
+            elif x == 2:
+                raise MyError("two")
+
+        @njit
+        def udt(x):
+            try:
+                inner(x)
+                res = "not raised"
+            except:             # noqa: E722
+                res = "caught"
+            if x == 0:
+                inner(2)
+            return res
+
+        with self.assertRaises(MyError) as raises:
+            udt(0)
+        self.assertEqual(str(raises.exception), "two")
+        self.assertEqual(udt(1), "caught")
+        self.assertEqual(udt(-1), "not raised")
 
 
 if __name__ == '__main__':
