a873bc96020fc06cdb43f39d735dae4eb3454b5d
Stan Seibert <stan.seibert@continuum.io>
2019-11-21 14:34:09 -0600

    return type of arange is max of bounds types

--- numba/targets/arrayobj.py
+++ numba/targets/arrayobj.py
@@ -3765,7 +3765,7 @@ def _arange_dtype(*args):
     elif any(isinstance(a, types.Float) for a in bounds):
         dtype = types.float64
     else:
-        dtype = types.intc
+        dtype = max(bounds)
 
     return dtype
 
