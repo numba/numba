9b63fb6c3e7422274f26884b7c1af7e041a681fa
Stuart Archibald <stuart.archibald@googlemail.com>
2019-11-26 15:22:31 +0000

    Try this.
    As title.

--- numba/targets/arrayobj.py
+++ numba/targets/arrayobj.py
@@ -3765,30 +3765,14 @@ def _arange_dtype(*args):
     elif any(isinstance(a, types.Float) for a in bounds):
         dtype = types.float64
     else:
-        # It's not possible for the dtype to replicate NumPy as integer literals
-        # are intp size in Numba but np.dtype(int) (i.e. intc) in NumPy. The
-        # NumPy logic is to basically do max(np.long, *[type(x) for x in args]).
-        # On windows 64, in NumPy, that would mean:
-        # * np.arange(1, 10) -> max(np.long, np.intc, np.intc) -> int32
-        # but in Numba:
-        # * np.arange(1, 10) -> max(np.long, np.intp, np.intp) -> int64
-        #
-        # It's therefore not possible to replicate the case where integer
-        # literals are supplied, however best effort is made to correctly handle
-        # cases like:
-        # * np.arange(np.int8(10)) -> max(np.long, np.int8) -> np.long
+        # NPY_LONG required as the return type, this is a np.dtype('l') which is
+        # not the same as a np.long and a np.int or a Python long or int.
         #
         # Alg ref:
         # https://github.com/numpy/numpy/blob/maintenance/1.17.x/numpy/core/src/multiarray/ctors.c#L3376-L3377    # noqa: E501
         #
-        # Also note Py2.7 on 32 bit linux as a 32bit np.long where as Py3 has a
-        # 64bit!
-        if utils.IS_PY3 and utils.MACHINE_BITS == 32:
-            _long = types.int32
-        else:
-            _long = types.long_
-
-        dtype = max(bounds + [_long,])
+        NPY_LONG = getattr(types, "int%s" % (8 * np.dtype('l').itemsize))
+        dtype = max(bounds + [NPY_LONG,])
 
     return dtype
 
