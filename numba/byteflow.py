9b2b035bb198c2dac8c9c631749019099aa3b4c0
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-11-27 16:13:39 -0600

    Fix loop break

--- numba/byteflow.py
+++ numba/byteflow.py
@@ -594,7 +594,7 @@ class TraceRunner(object):
 
     def op_BREAK_LOOP(self, state, inst):
         # NOTE: bytecode removed since py3.8
-        end = state.get_top_block('loop')['end']
+        end = state.get_top_block('LOOP')['end']
         state.append(inst, end=end)
         state.pop_block()
         state.fork(pc=end)
