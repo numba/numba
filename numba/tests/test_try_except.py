f2e4a729e212d1288776eba3cf920bc5bb417905
Siu Kwan Lam <michael.lam.sk@gmail.com>
2019-12-02 11:50:52 -0600

    Add test for nested try-except-finally

--- numba/tests/test_try_except.py
+++ numba/tests/test_try_except.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+from itertools import product
+
 from numba import njit
 from numba.errors import UnsupportedError
 from .support import (
@@ -304,6 +306,100 @@ class TestTryExceptCaught(TestCase):
             ["A", "B", "D"],
         )
         self.assertEqual(res, 123)
+
+
+@skip_tryexcept_unsupported
+class TestTryExceptNested(TestCase):
+    "Tests for complicated nesting"
+
+    def check_compare(self, cfunc, pyfunc, *args, **kwargs):
+        with captured_stdout() as stdout:
+            pyfunc(*args, **kwargs)
+        expect = stdout.getvalue()
+
+        with captured_stdout() as stdout:
+            cfunc(*args, **kwargs)
+        got = stdout.getvalue()
+        self.assertEqual(
+            expect, got,
+            msg="args={} kwargs={}".format(args, kwargs)
+        )
+
+    def test_try_except_else(self):
+        @njit
+        def udt(x, y, z, p):
+            print('A')
+            if x:
+                print('B')
+                try:
+                    print('C')
+                    if y:
+                        print('D')
+                        raise MyError("y")
+                    print('E')
+                except Exception as e: # noqa: F841
+                    print('F')
+                    try:
+                        print('H')
+                        try:
+                            print('I')
+                            if z:
+                                print('J')
+                                raise MyError('z')
+                            print('K')
+                        except Exception:
+                            print('L')
+                        else:
+                            print('M')
+                    except Exception:
+                        print('N')
+                    else:
+                        print('O')
+                    print('P')
+                else:
+                    print('G')
+                print('Q')
+            print('R')
+
+        cases = list(product([True, False], repeat=4))
+        self.assertTrue(cases)
+        for x, y, z, p in cases:
+            self.check_compare(
+                udt, udt.py_func,
+                x=x, y=y, z=z, p=p,
+            )
+
+    def test_try_except_finally(self):
+        @njit
+        def udt(p, q):
+            try:
+                print('A')
+                if p:
+                    print('B')
+                    raise MyError
+                print('C')
+            except:             # noqa: E722
+                print('D')
+            finally:
+                try:
+                    print('E')
+                    if q:
+                        print('F')
+                        raise MyError
+                except Exception:
+                    print('G')
+                else:
+                    print('H')
+                finally:
+                    print('I')
+
+        cases = list(product([True, False], repeat=2))
+        self.assertTrue(cases)
+        for p, q in cases:
+            self.check_compare(
+                udt, udt.py_func,
+                p=p, q=q,
+            )
 
 
 @skip_tryexcept_supported
