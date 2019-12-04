661c6918ca3d2c58bfa691a5f06f3ae84339bfb8
Todd A. Anderson <todd.a.anderson@intel.com>
2019-12-04 01:07:03 -0600

    Add the build of numbaoneapiglu.so in the main build script.

--- build_for_conda.sh
+++ build_for_conda.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# Have to be on the pvc branch of Numba.
-
 find . -name "*.so" -exec rm {} \;
+
+cd numba/oneapi/oneapidriver
+gcc -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -shared -fPIC -o libnumbaoneapiglue.so numba_oneapi_glue.c
+cd ../../..
+
 python setup.py clean --all
 python setup.py build_ext --inplace
 python setup.py develop
