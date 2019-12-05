c954c8788902788aaa666d7c5f995b5f459f185f
Todd A. Anderson <todd.a.anderson@intel.com>
2019-12-05 12:04:57 -0600

    Check glue lib compile status and exit early if that fails before the Numba build.
    Change-Id: I7d280c64d8e0aa62f1631b977547df9ceb05d68c

--- build_for_conda.sh
+++ build_for_conda.sh
@@ -4,6 +4,12 @@ find . -name "*.so" -exec rm {} \;
 
 cd numba/oneapi/oneapidriver
 gcc -g -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC -c numba_oneapi_glue.c -o numba_oneapi_glue.o
+gcc_ret=$?
+if [ $gcc_ret -ne 0 ]; then
+    echo "numba_oneapi_glue failed to build...exiting"
+    cd ../../..
+    exit $gcc_ret
+fi
 ar rcs libnumbaoneapiglue.a numba_oneapi_glue.o
 #gcc -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -shared -fPIC -o libnumbaoneapiglue.so numba_oneapi_glue.c
 cd ../../..
