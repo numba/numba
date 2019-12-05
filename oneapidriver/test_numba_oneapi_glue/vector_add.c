e2c21aeea87bf84b9b6ad43f4f02736525b03555
Diptorup Deb <diptorup.deb@intel.com>
2019-12-05 10:44:28 -0600

    (numba/oneapi) Update the vector_add test case.
    Change-Id: I40c5a164f5677608dd386831f143d0e363647b64

--- oneapidriver/test_numba_oneapi_glue/vector_add.c
+++ oneapidriver/test_numba_oneapi_glue/vector_add.c
@@ -8,6 +8,9 @@ typedef enum
     ON_GPU
 } execution_ty;
 
+// Forward declaration
+void buildAndExecuteKernel (runtime_t rt, execution_ty ex);
+
 // Array sizes
 static const size_t N = 2048;
 
@@ -19,13 +22,11 @@ const char* programSource =
     "   int idx = get_global_id(0);                                       \n"
     "   C[idx] = A[idx] + B[idx];                                         \n"
     "}";
-#if 0
-void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
+
+void buildAndExecuteKernel (runtime_t rt, execution_ty ex)
 {
     int err;
-    device_t device;
-    void *context;
-    void *queue;
+    env_t env_t_ptr;
     program_t program_ptr;
     kernel_t kernel_ptr;
     float *A, *B, *C;
@@ -35,27 +36,10 @@ void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
     size_t indexSpaceSize[1], workGroupSize[1];
     buffer_t buffers[3];
 
-    mem_flags_t memflags[3] = {
-            NUMBA_ONEAPI_READ_ONLY,
-            NUMBA_ONEAPI_READ_ONLY,
-            NUMBA_ONEAPI_WRITE_ONLY
-    };
-    size_t datasizes[3] = {
-            sizeof(float)*N,
-            sizeof(float)*N,
-            sizeof(float)*N
-    };
-
-    if(ex == ON_CPU) {
-        device = rt->first_cpu_device;
-        context = rt->first_cpu_device.context;
-        queue = rt->first_cpu_device.queue;
-    }
-    else if(ex == ON_GPU) {
-        device = rt->first_gpu_device;
-        context = rt->first_gpu_device.context;
-        queue = rt->first_gpu_device.queue;
-    }
+    if(ex == ON_CPU)
+        env_t_ptr = rt->first_cpu_env;
+    else if(ex == ON_GPU)
+        env_t_ptr = rt->first_gpu_env;
 
     // Memory requirement
     datasize = sizeof(float)*N;
@@ -70,13 +54,9 @@ void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
         B[i] = 2*(i+1);
     }
 
-    // Allocate two input buffers and one output buffers for the vectors
-    //err = create_numba_oneapi_mem_buffers(context, buffers, num_buffers,
-    //        memflags, datasizes);
-
-    err =  create_numba_oneapi_rw_mem_buffer(context, &buffers[0], datasize);
-    err |= create_numba_oneapi_rw_mem_buffer(context, &buffers[1], datasize);
-    err |= create_numba_oneapi_rw_mem_buffer(context, &buffers[2], datasize);
+    err =  create_numba_oneapi_rw_mem_buffer(env_t_ptr, datasize, &buffers[0]);
+    err |= create_numba_oneapi_rw_mem_buffer(env_t_ptr, datasize, &buffers[1]);
+    err |= create_numba_oneapi_rw_mem_buffer(env_t_ptr, datasize, &buffers[2]);
 
     if(err) {
         fprintf(stderr, "Buffer creation failed. Abort!\n");
@@ -84,22 +64,23 @@ void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
     }
 
     // Write data from the input arrays to the buffers
-    err = write_numba_oneapi_mem_buffer_to_device(queue, buffers[0], true,
+    err = write_numba_oneapi_mem_buffer_to_device(env_t_ptr, buffers[0], true,
             0, datasize, A);
-    err |= write_numba_oneapi_mem_buffer_to_device(queue, buffers[1], true,
+    err |= write_numba_oneapi_mem_buffer_to_device(env_t_ptr, buffers[1], true,
             0, datasize, B);
     if(err) {
         fprintf(stderr, "Could not write to buffer. Abort!\n");
         exit(1);
     }
 
-    err = create_and_build_numba_oneapi_program_from_source(&device, 1,
+    err = create_numba_oneapi_program_from_source(env_t_ptr, 1,
             (const char **)&programSource, NULL, &program_ptr);
+    err |= build_numba_oneapi_program (env_t_ptr, program_ptr);
     if(err) {
         fprintf(stderr, "Could not create the program. Abort!\n");
         exit(1);
     }
-    err = create_numba_oneapi_kernel(context, program_ptr, "vecadd",
+    err = create_numba_oneapi_kernel(env_t_ptr, program_ptr, "vecadd",
             &kernel_ptr);
     if(err) {
         fprintf(stderr, "Could not create the kernel. Abort!\n");
@@ -130,8 +111,8 @@ void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
     }
 #endif
     // Copy the device output buffer to the host output array
-    err = read_numba_oneapi_mem_buffer_from_device(queue, buffers[0], true, 0,
-            datasize, C);
+    err = read_numba_oneapi_mem_buffer_from_device(env_t_ptr, buffers[0], true,
+            0, datasize, C);
 
 #if 1
     // Validate the output
@@ -152,7 +133,6 @@ void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
     // free the program
     destroy_numba_oneapi_program(&program_ptr);
     // free the buffers
-    //destroy_numba_oneapi_mem_buffers(buffers, num_buffers);
     destroy_numba_oneapi_rw_mem_buffer(&buffers[0]);
     destroy_numba_oneapi_rw_mem_buffer(&buffers[1]);
     destroy_numba_oneapi_rw_mem_buffer(&buffers[2]);
@@ -161,7 +141,7 @@ void buildAndExecuteKernel (const runtime_t rt, execution_ty ex)
     free(B);
     free(C);
 }
-#endif
+
 
 int main (int argc, char** argv)
 {
@@ -176,7 +156,7 @@ int main (int argc, char** argv)
     //--- Execute on CPU
     printf("Executing on the first CPU device info: \n");
     rt->first_cpu_env->dump_fn(rt->first_cpu_env);
-    //buildAndExecuteKernel(rt, ON_CPU);
+    buildAndExecuteKernel(rt, ON_CPU);
 
     printf("\n===================================\n\n");
 
