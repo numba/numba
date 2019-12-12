0a528288c526aae4ef1e19ca942719e248ee0409
Todd A. Anderson <todd.a.anderson@intel.com>
2019-12-12 10:51:07 -0600

    Build static and shared library versions of numba oneapi glue.
    Change-Id: Ifbbc66f4093443b00ff5d33cb02f5a4997cc7341

--- build_for_conda.sh
+++ build_for_conda.sh
@@ -3,6 +3,7 @@
 find . -name "*.so" -exec rm {} \;
 
 cd numba/oneapi/oneapidriver
+
 gcc -g -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC -c numba_oneapi_glue.c -o numba_oneapi_glue.o
 gcc_ret=$?
 if [ $gcc_ret -ne 0 ]; then
@@ -11,7 +12,17 @@ if [ $gcc_ret -ne 0 ]; then
     exit $gcc_ret
 fi
 ar rcs libnumbaoneapiglue.a numba_oneapi_glue.o
-#gcc -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -shared -fPIC -o libnumbaoneapiglue.so numba_oneapi_glue.c
+
+#gcc -L. -shared -o libnumbaoneapiglue.so -Wl,--whole-archive -lnumbaoneapiglue
+
+gcc -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -shared -fPIC -o libnumbaoneapiglue_so.so numba_oneapi_glue.c
+gcc_ret=$?
+if [ $gcc_ret -ne 0 ]; then
+    echo "numba_oneapi_glue_so failed to build...exiting"
+    cd ../../..
+    exit $gcc_ret
+fi
+
 cd ../../..
 
 python setup.py clean --all
