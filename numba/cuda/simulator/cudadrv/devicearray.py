1a70b256b19788f2623c4b90f3761818b462fee7
Graham Markall <gmarkall@nvidia.com>
2019-12-16 09:56:56 -0800

    Make device_array_like create contiguous arrays
    As reported in Issue #4832, device_array_like maintains the
    strides of source arrays, rather than following the behaviour of
    Numpy and always creating a contiguous array. This commit changes
    device_array_like so that it always creates a contiguous array.
    
    Strides for the new array are computing using the same logic as
    Numpy uses when keeping the order of the source array.

--- numba/cuda/simulator/cudadrv/devicearray.py
+++ numba/cuda/simulator/cudadrv/devicearray.py
@@ -218,7 +218,30 @@ def device_array(*args, **kwargs):
 
 
 def device_array_like(ary, stream=0):
-    return FakeCUDAArray(np.empty_like(ary))
+    # Avoid attempting to recompute strides if the default strides will be
+    # sufficient to create a contiguous array.
+    if ary.is_c_contiguous() or ary.ndim <= 1:
+        return FakeCUDAArray(np.ndarray(shape=ary.shape, dtype=ary.dtype))
+
+    # Otherwise, we need to compute new strides using an algorithm adapted from
+    # Numpy's PyArray_NewLikeArrayWithShape in core/src/multiarray/ctors.c. We
+    # permute the strides in ascending order then compute the stride for the
+    # dimensions with the same permutation.
+
+    # Stride permuation. E.g. a stride array (4, -2, 12) becomes
+    # [(1, -2), (0, 4), (2, 12)]
+    strideperm = [ x for x in enumerate(ary.strides) ]
+    strideperm.sort(key = lambda x: x[1])
+
+    # Compute new strides using permutation
+    strides = [0] * len(ary.strides)
+    stride = ary.dtype.itemsize
+    for i_perm, _ in strideperm:
+        strides[i_perm] = stride
+        stride *= ary.shape[i_perm]
+    strides = tuple(strides)
+
+    return FakeCUDAArray(np.ndarray(shape=ary.shape, dtype=ary.dtype, strides=strides))
 
 
 def auto_device(ary, stream=0, copy=True):
