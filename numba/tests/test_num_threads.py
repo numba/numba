8fba01b1d29aed680b4e52055f6e5bb59fb41afc
Aaron Meurer <asmeurer@gmail.com>
2019-12-16 17:24:18 -0700

    Parameterize the num_threads tests over NUMBA_NUM_THREADS

--- numba/tests/test_num_threads.py
+++ numba/tests/test_num_threads.py
@@ -2,6 +2,7 @@
 from __future__ import print_function, absolute_import, division
 
 import sys
+import os
 
 import numpy as np
 
@@ -487,20 +488,31 @@ class TestNumThreadsBackends(TestInSubprocess, TestCase):
     _class = TestNumThreads
     _DEBUG = False
 
+    # 1 is mainly here to ensure tests skip correctly
+    num_threads = [i for i in [1, 2, 4, 8, 16] if i <= config.NUMBA_NUM_THREADS]
+
+    def run_test_in_separate_process(self, test, threading_layer, num_threads):
+        env_copy = os.environ.copy()
+        env_copy['NUMBA_THREADING_LAYER'] = str(threading_layer)
+        env_copy['NUMBA_NUM_THREADS'] = str(num_threads)
+        cmdline = [sys.executable, "-m", "numba.runtests", test]
+        return self.run_cmd(cmdline, env_copy)
+
     @classmethod
-    def _inject(cls, name, backend, backend_guard):
+    def _inject(cls, name, backend, backend_guard, num_threads):
         themod = cls.__module__
         thecls = cls._class.__name__
         injected_method = '%s.%s.%s' % (themod, thecls, name)
 
         def test_template(self):
-            o, e = self.run_test_in_separate_process(injected_method, backend)
+            o, e = self.run_test_in_separate_process(injected_method, backend,
+                                                     num_threads)
             if self._DEBUG:
                 print('stdout:\n "%s"\n stderr:\n "%s"' % (o, e))
             self.assertIn('OK', e)
             self.assertTrue('FAIL' not in e)
             self.assertTrue('ERROR' not in e)
-        injected_test = "%s_%s" % (name[1:], backend)
+        injected_test = "%s_%s_%s_threads" % (name[1:], backend, num_threads)
         setattr(cls, injected_test,
                 backend_guard(test_template))
 
@@ -508,9 +520,10 @@ class TestNumThreadsBackends(TestInSubprocess, TestCase):
     def generate(cls):
         for name in cls._class.__dict__.copy():
             for backend, backend_guard in cls.backends.items():
-                if not name.startswith('_test_'):
-                    continue
-                cls._inject(name, backend, backend_guard)
+                for num_threads in cls.num_threads:
+                    if not name.startswith('_test_'):
+                        continue
+                    cls._inject(name, backend, backend_guard, num_threads)
 
 TestNumThreadsBackends.generate()
 
