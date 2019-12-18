b84857c9f3ee2254b11eaa25aa4c135b7ee32586
Valentin Haenel <vhaenel@anaconda.com>
2019-12-18 19:44:14 +0100

    increase test coverage for `disable_reflected_list`
    Using a technique similar to mocking, we ensure that the tests yield
    different results if the `disable_reflected_list` isn't present.

--- numba/tests/test_disable_reflected_list.py
+++ numba/tests/test_disable_reflected_list.py
@@ -11,7 +11,7 @@ disable_reflected_list()
 
 class TestTypedList(MemoryLeakMixin, TestCase):
 
-    def test(self):
+    def test_with_global_active(self):
         @njit
         def foo():
             l = [0]
@@ -29,3 +29,23 @@ class TestTypedList(MemoryLeakMixin, TestCase):
         pyfunc_received = foo.py_func()
         self.assertEqual(pyfunc_expected, pyfunc_received)
         self.assertTrue(isinstance(pyfunc_received, list))
+
+    def test_with_global_removed(self):
+        # remove the global for this test
+        dlr = globals().pop('disable_reflected_list')
+
+        @njit
+        def foo():
+            l = [0]
+            return l
+
+        # Both JITed and non-JITed version will return a Python list
+        pyfunc_expected = cfunc_expected = [0]
+        pyfunc_received, cfunc_received = foo.py_func(), foo()
+        self.assertEqual(pyfunc_expected, pyfunc_received)
+        self.assertEqual(cfunc_expected, cfunc_received)
+        self.assertTrue(isinstance(pyfunc_received, list))
+        self.assertTrue(isinstance(cfunc_received, list))
+
+        # add the global for future tests
+        globals()['disable_reflected_list'] = dlr
