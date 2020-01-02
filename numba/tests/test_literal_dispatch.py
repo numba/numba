96371bdd54fc17c0be795a3b2e6b959be1d61b4c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2020-01-02 11:06:28 -0600

    Fix #5011. Fix a problem with folding arguments on methods.

--- numba/tests/test_literal_dispatch.py
+++ numba/tests/test_literal_dispatch.py
@@ -5,8 +5,12 @@ import numpy as np
 import numba
 import numba.unittest_support as unittest
 from numba.tests.support import TestCase
-from numba import njit, types, errors
-from numba.extending import overload, SentryLiteralArgs
+from numba import njit, types, errors, cgutils
+from numba.typing import signature
+from numba.datamodel import models
+from numba.extending import (
+    overload, SentryLiteralArgs, overload_method, register_model, intrinsic,
+)
 from numba.special import literally
 
 
@@ -324,6 +328,73 @@ class TestLiteralDispatch(TestCase):
             foo(a=123, b=321)
         self.assertIn("Repeated literal typing request",
                       str(raises.exception))
+
+
+class TestLiteralDispatchWithCustomType(TestCase):
+    def make_dummy_type(self):
+        class Dummy:
+            def lit(self, a):
+                return a
+
+        class DummyType(numba.types.Type):
+            def __init__(self):
+                super().__init__(name="dummy")
+
+        @register_model(DummyType)
+        class DummyTypeModel(models.StructModel):
+            def __init__(self, dmm, fe_type):
+                members = []
+                super().__init__(dmm, fe_type, members)
+
+        @intrinsic
+        def init_dummy(typingctx):
+            def codegen(context, builder, signature, args):
+                dummy = cgutils.create_struct_proxy(
+                    signature.return_type)(context, builder)
+
+                return dummy._getvalue()
+
+            sig = signature(DummyType())
+            return sig, codegen
+
+        @overload(Dummy)
+        def dummy_overload():
+            def ctor():
+                return init_dummy()
+
+            return ctor
+
+        return (DummyType, Dummy)
+
+    def test_overload_method(self):
+        # from issue #5011
+        DummyType, Dummy = self.make_dummy_type()
+
+        @overload_method(DummyType, 'lit')
+        def lit_overload(self, a):
+            def impl(self, a):
+                return literally(a)  # <-- using literally here
+
+            return impl
+
+        @njit
+        def test_impl(a):
+            d = Dummy()
+
+            return d.lit(a)
+
+        # Successful case
+        self.assertEqual(test_impl(5), 5)
+
+        # Failing case
+        @njit
+        def inside(a):
+            return test_impl(a + 1)
+
+        with self.assertRaises(errors.TypingError) as raises:
+            inside(4)
+
+        self.assertIn("Cannot request literal type.", str(raises.exception))
 
 
 if __name__ == '__main__':
