2bcf552685ed9edcbf571097b0004e64bfedcadb
Stuart Archibald <stuart.archibald@googlemail.com>
2020-01-09 15:35:41 +0000

    Update signal for windows

--- numba/tests/test_parallel_backend.py
+++ numba/tests/test_parallel_backend.py
@@ -615,7 +615,13 @@ class TestMiscBackendIssues(ThreadLayerTestHelper):
                 print(out, err)
             e_msg = str(e)
             self.assertIn("failed with code", e_msg)
-            self.assertIn(str(signal.SIGTERM.value), e_msg)
+            # raised a SIGTERM, on windows in practise this seems to 
+            # materialise as a SIGQUIT
+            # https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/signal?view=vs-2019
+            if _windows:
+                self.assertIn(str(signal.SIGQUIT.value), e_msg)
+            else:
+                self.assertIn(str(signal.SIGTERM.value), e_msg)
             self.assertIn("Terminating: Nested parallel kernel launch detected",
                           e_msg)
 
