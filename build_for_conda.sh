44b2a1b3917bfdb230e29a4a24ff8c50611ca33a
reazul.hoque <reazul.hoque@intel.com>
2020-01-14 14:24:26 -0600

    Added command to build dpglue without DEBUG flag

--- build_for_conda.sh
+++ build_for_conda.sh
@@ -6,6 +6,7 @@ find . -name "*.so" -exec rm {} \;
 cd numba/dppy/dppy_driver
 
 gcc -g -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC -c opencllite.c -o opencllite.o
+#gcc -g -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC -c opencllite.c -o opencllite.o
 gcc_ret=$?
 if [ $gcc_ret -ne 0 ]; then
     echo "opencllite failed to build...exiting"
@@ -17,6 +18,7 @@ ar rcs libdpglue.a opencllite.o
 #gcc -L. -shared -o libdpglue.so -Wl,--whole-archive -lopencllite
 
 gcc -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -shared -fPIC -o libdpglue_so.so opencllite.c
+#gcc -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -shared -fPIC -o libdpglue_so.so opencllite.c
 gcc_ret=$?
 if [ $gcc_ret -ne 0 ]; then
     echo "opencllite failed to build...exiting"
