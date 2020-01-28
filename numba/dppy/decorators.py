ba30dc344a7d7bc60113ed550f5fdb40f76edbfb
reazul.hoque <reazul.hoque@intel.com>
2020-01-28 16:58:46 -0600

    Added example using access_type for input parameter

--- numba/dppy/decorators.py
+++ numba/dppy/decorators.py
@@ -4,30 +4,31 @@ from .compiler import (compile_kernel, JitDPPyKernel)
 from inspect import signature
 
 
-def kernel(signature=None, debug=False):
+def kernel(signature=None, access_types=None, debug=False):
     """JIT compile a python function conforming using the DPPy backend.
 
     A kernel is equvalent to an OpenCL kernel function, and has the
     same restrictions as definined by SPIR_KERNEL calling convention.
     """
     if signature is None:
-        return autojit(debug=False)
+        return autojit(debug=False, access_types=access_types)
     elif not sigutils.is_signature(signature):
         func = signature
-        return autojit(debug=False)(func)
+        return autojit(debug=False, access_types=access_types)(func)
     else:
         return _kernel_jit(signature, debug)
 
 
-def autojit(debug=False):
+def autojit(debug=False, access_types=None):
     def _kernel_autojit(pyfunc):
         # Construct a list of access type of each arg according to their position
         ordered_arg_access_types = []
         sig = signature(pyfunc, follow_wrapped=False)
         for idx, arg_name in enumerate(sig.parameters):
-            for key in access_types:
-                if arg_name in access_types[key]:
-                    ordered_arg_access_types.append(key)
+            if access_types:
+                for key in access_types:
+                    if arg_name in access_types[key]:
+                        ordered_arg_access_types.append(key)
             if len(ordered_arg_access_types) <= idx:
                 ordered_arg_access_types.append(None)
 
