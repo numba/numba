757d3662c56353739d41b77ec9225dcf5689b4c4
Diptorup Deb <diptorup.deb@intel.com>
2020-02-04 14:48:21 -0600

    Generate executables with .exe suffix. Add *.exe to gitignore.

--- numba/dppy/dppy_driver/test_dp_glue/build_examples.sh
+++ numba/dppy/dppy_driver/test_dp_glue/build_examples.sh
@@ -2,4 +2,4 @@
 set -xv
 rm -rf *.o
 base="${1%.*}"
-gcc -g -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC  -I.. ../opencllite.c ${1} -o $base -lOpenCL
+gcc -g -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC  -I.. ../opencllite.c ${1} -o $base.exe -lOpenCL
