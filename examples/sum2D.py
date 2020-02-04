ad0ef7da3b58d69738a0f53be1d48e527f1a5e51
Diptorup Deb <diptorup.deb@intel.com>
2020-02-04 14:53:55 -0600

    (numba/dppy) Update examples...

--- examples/sum2D.py
+++ examples/sum2D.py
@@ -8,22 +8,24 @@ from numba import dppy
 from numba.dppy.dppy_driver import driver as ocldrv
 
 
-@dppy.jit
+@dppy.kernel
 def data_parallel_sum(a, b, c):
     i = dppy.get_global_id(0)
     j = dppy.get_global_id(1)
     c[i,j] = a[i,j] + b[i,j]
 
-global_size = 48, 1
-local_size = 3, 1, 1
-
 # Array dimesnions
-X = 1024
-Y = 1024
+X = 8
+Y = 8
+global_size = X,Y
 
-a = np.array(np.random.random(X*Y), dtype=np.float32).reshape(X,Y)
+#a = np.array(np.random.random(X*Y), dtype=np.float32).reshape(X,Y)
+#a.fill(1)
+a = np.arange(X*Y, dtype=np.float32).reshape(X,Y)
 b = np.array(np.random.random(X*Y), dtype=np.float32).reshape(X,Y)
+#b.fill(1)
 c = np.ones_like(a).reshape(X,Y)
+
 
 # Select a device for executing the kernel
 device_env = None
@@ -45,8 +47,10 @@ dA = device_env.copy_array_to_device(a)
 dB = device_env.copy_array_to_device(b)
 dC = ocldrv.DeviceArray(device_env.get_env_ptr(), c)
 
+print("before : ", dB._ndarray)
+print("before : ", dA._ndarray)
 print("before : ", dC._ndarray)
-#data_parallel_sum[device_env,global_size,local_size](dA, dB, dC)
+data_parallel_sum[device_env,global_size](dA, dB, dC)
 device_env.copy_array_from_device(dC)
 print("after : ", dC._ndarray)
 
