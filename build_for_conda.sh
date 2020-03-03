812b374649139cd7e360dfc1870bf1e62bd3e551
reazul.hoque <reazul.hoque@intel.com>
2020-03-03 16:55:54 -0600

    Fixed getting lib so intall and develop both works

--- build_for_conda.sh
+++ build_for_conda.sh
@@ -31,8 +31,6 @@ set -x
 
 cd numba/dppy/dppy_driver
 
-export CC=clang
-
 $CC ${DEBUG_FLAGS} -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -fPIC ${OPENCL_INCLUDE} -c opencllite.c -o opencllite.o
 #gcc -g -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC -c opencllite.c -o opencllite.o
 
@@ -57,19 +55,6 @@ if [ $gcc_ret -ne 0 ]; then
 fi
 
 cd ../../..
-
-LLVM_SPIRV_ERROR_MSG="We need llvm-spirv (https://github.com/KhronosGroup/SPIRV-LLVM-Translator) installed for NUMBA-PVC"
-SPIRV_TOOLS_ERROR_MSG="We need SPIRV-Tools (https://github.com/KhronosGroup/SPIRV-Tools/tree/stable) installed for NUMBA-PVC"
-
-if ! { command -v opt && command -v llvm-spirv; }; then
-    echo $LLVM_SPIRV_ERRO_MSG
-    exit 1
-fi
-
-if ! { command -v spirv-dis && command -v spirv-val && command -v spirv-opt; }; then
-    echo $SPIRV_TOOLS_ERROR_MSG
-    exit 1
-fi
 
 python setup.py clean --all
 python setup.py build_ext --inplace
