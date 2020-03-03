ede408bc730e758521e731368afd0616babb7923
reazul.hoque <reazul.hoque@intel.com>
2020-03-03 16:29:12 -0600

    (numba/dppy) Added changes to build conda package for linux

--- dppy_driver/driverapi.py
+++ dppy_driver/driverapi.py
@@ -7,9 +7,19 @@ from distutils import sysconfig
 ffibuilder = FFI()
 
 libDPGlueHome = os.path.dirname(os.path.realpath(__file__))
-dpgluelib_dir = os.getcwd() + "/builds/numba/dppy/dppy_driver"
-dpgluelib = "dpglue" +  sysconfig.get_config_var('EXT_SUFFIX')[:-3]
-print("====== ", dpgluelib_dir, "   === ", dpgluelib)
+opencl_root = os.getenv('OPENCL_HOME')
+opencllib_dir = ""
+if not opencl_root:
+    opencllib_dir = "/usr/lib/x86_64-linux-gnu"
+else:
+    opencllib_dir = opencl_root  + "/lib"
+
+# couldn't find a better way to extract the path of --inplace build for setuptools
+#build_dir = "build/lib.{}-{}.{}".format(sysconfig.get_platform(), *sys.version_info[:2])
+#print(build_dir)
+#dpgluelib_dir = os.getcwd() + "/" + build_dir  +"/numba/dppy/dppy_driver"
+#print(dpgluelib_dir)
+#dpgluelib = "dpglue" +  sysconfig.get_config_var('EXT_SUFFIX')[:-3]
 
 BAD_ENV_PATH_ERRMSG = """
 DP_GLUE_HOME is set to '{0}' which is not a valid path to a
@@ -53,10 +63,14 @@ ffibuilder.set_source(
     """
          #include "dp_glue.h"   // the C header of the library
     """,
-    libraries=[dpgluelib, "OpenCL"],
+    #libraries=[dpgluelib, "OpenCL"],
+    #libraries=[dpgluelib],
     include_dirs=[libDPGlueHome],
-    library_dirs=[dpgluelib_dir],
-    extra_link_args=["-Wl,-rpath=" + dpgluelib_dir]
+    #library_dirs=[dpgluelib_dir],
+    library_dirs=[libDPGlueHome, opencllib_dir],
+    libraries=["dpglue", "OpenCL"],
+    #extra_link_args=["-Wl,-rpath=" + dpgluelib_dir + ":${ORIGIN}"],
+    #runtime_library_dirs=["./"],
 )   # library name, for the linker
 
 
