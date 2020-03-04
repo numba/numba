5cdfc60c544aca308ab4b823a9007b809ace9569
Stuart Archibald <stuart.archibald@googlemail.com>
2020-03-04 22:19:32 +0000

    Add shim to accommodate refactoring.
    As title.

---
+++ numba/targets/slicing.py
@@ -0,0 +1,11 @@
+import numba.core.errors as _errors
+from numba.core.utils import PYVERSION as _PYVERSION
+
+_moved_mod = "numba.cpython.slicing"
+
+if _PYVERSION >= (3, 7):
+    __getattr__ = _errors.deprecate_moved_module_getattr(__name__, _moved_mod)
+else:
+    from numba.cpython.slicing import *  # noqa: F403, F401
+
+_errors.deprecate_moved_module(__name__, _moved_mod)
