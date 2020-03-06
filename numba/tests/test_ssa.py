9db91ede5b22c682bc7572726ceb2f33f90acf3f
Siu Kwan Lam <michael.lam.sk@gmail.com>
2020-03-06 13:35:03 -0600

    Add SSA tests

---
+++ numba/tests/test_ssa.py
@@ -0,0 +1,102 @@
+"""
+Tests for SSA reconstruction
+"""
+import sys
+import logging
+
+from numba import njit
+from numba.core import errors
+from numba.tests.support import TestCase
+
+
+_DEBUG = False
+
+if _DEBUG:
+    # Enable debug logger on SSA reconstruction
+    ssa_logger = logging.getLogger("numba.core.ssa")
+    ssa_logger.setLevel(level=logging.DEBUG)
+    ssa_logger.addHandler(logging.StreamHandler(sys.stderr))
+
+
+class TestSSA(TestCase):
+    """
+    Contains tests to help isolate problems in SSA
+    """
+    def check_func(self, func, *args):
+        got = func(*args)
+        exp = func.py_func(*args)
+        self.assertEqual(got, exp)
+
+    def test_argument_name_reused(self):
+        @njit
+        def foo(x):
+            x += 1
+            return x
+
+        self.check_func(foo, 123)
+
+    def test_if_else_redefine(self):
+        @njit
+        def foo(x, y):
+            z = x * y
+            if x < y:
+                z = x
+            else:
+                z = y
+            return z
+
+        self.check_func(foo, 3, 2)
+        self.check_func(foo, 2, 3)
+
+    def test_sum_loop(self):
+        @njit
+        def foo(n):
+            c = 0
+            for i in range(n):
+                c += i
+            return c
+
+        self.check_func(foo, 0)
+        self.check_func(foo, 10)
+
+    def test_sum_loop_2vars(self):
+        @njit
+        def foo(n):
+            c = 0
+            d = n
+            for i in range(n):
+                c += i
+                d += n
+            return c, d
+
+        self.check_func(foo, 0)
+        self.check_func(foo, 10)
+
+    def test_sum_2d_loop(self):
+        @njit
+        def foo(n):
+            c = 0
+            for i in range(n):
+                for j in range(n):
+                    c += j
+                c += i
+            return c
+
+        self.check_func(foo, 0)
+        self.check_func(foo, 10)
+
+    def test_undefined_var(self):
+        @njit
+        def foo(n):
+            if n:
+                if n > 0:
+                    c = 0
+                return c
+            else:
+                # variable c is not defined in this branch
+                c += 1
+                return c
+
+        with self.assertRaises(errors.NotDefinedError) as raises:
+            self.check_func(foo, 1)
+        self.assertEqual(raises.exception.name, "c")
