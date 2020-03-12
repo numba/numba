0951310e891d0d094531cbfb00d773bb063dba26
Valentin Haenel <vhaenel@anaconda.com>
2020-03-12 21:15:26 +0100

    extend tests
    Test all the various Numbers, integers, floats and complex. Reject
    everything else for now.

--- numba/np/arraymath.py
+++ numba/np/arraymath.py
@@ -3878,6 +3878,10 @@ def np_asarray(a, dtype=None):
         def impl(a, dtype=None):
             return np.array(a, ty)
     elif isinstance(a, types.containers.ListType):
+        if not isinstance(a.dtype, types.Number):
+            raise TypingError(
+                "asarray support for List is limited to None and Number types")
+
         target_dtype = a.dtype if is_nonelike(dtype) else dtype
 
         def impl(a, dtype=None):
