ea4522480023e7a7ab54e1083d4aa014a7a125e8
reazul.hoque <reazul.hoque@intel.com>
2020-03-16 10:01:51 -0500

    Update examples

--- numba/dppy/examples/pa_examples/test1.py
+++ numba/dppy/examples/pa_examples/test1.py
@@ -6,23 +6,25 @@ def f1(a, b):
     c = a + b
     return c
 
-global_size = 50, 1
-local_size = 32, 1, 1
-N = global_size[0] * local_size[0]
-print("N", N)
 
-a = np.ones(N, dtype=np.float32)
-b = np.ones(N, dtype=np.float32)
-#a = np.array(np.random.random(N), dtype=np.float32)
-#b = np.array(np.random.random(N), dtype=np.float32)
-#c = np.ones_like(a)
+def main():
+    global_size = 64
+    local_size = 32
+    N = global_size * local_size
+    print("N", N)
 
-print("a:", a, hex(a.ctypes.data))
-print("b:", b, hex(b.ctypes.data))
-#print("c:", c, hex(c.ctypes.data))
-c = f1(a,b)
-print("BIG RESULT c:", c, hex(c.ctypes.data))
-for i in range(N):
-    if c[i] != 2.0:
-        print("First index not equal to 2.0 was", i)
-        break
+    a = np.ones(N, dtype=np.float32)
+    b = np.ones(N, dtype=np.float32)
+
+    print("a:", a, hex(a.ctypes.data))
+    print("b:", b, hex(b.ctypes.data))
+    c = f1(a,b)
+    print("RESULT c:", c, hex(c.ctypes.data))
+    for i in range(N):
+        if c[i] != 2.0:
+            print("First index not equal to 2.0 was", i)
+            break
+
+
+if __name__ == '__main__':
+    main()
