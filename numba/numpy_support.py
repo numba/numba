13677c8343d89a3c072574645ce91ab370abdcef
Eric Wieser <wieser.eric@gmail.com>
2020-04-03 11:03:39 +0100

    Made `numba.numpy_support` available without an import

--- numba/numpy_support.py
+++ numba/numpy_support.py
@@ -1,11 +1,9 @@
 import numba.core.errors as _errors
-from numba.core.utils import PYVERSION as _PYVERSION
+from types import ModuleType
 
 _moved_mod = "numba.np.numpy_support"
 
-if _PYVERSION >= (3, 7):
+class module(ModuleType):
     __getattr__ = _errors.deprecate_moved_module_getattr(__name__, _moved_mod)
-else:
-    from numba.np.numpy_support import *  # noqa: F403, F401
 
-_errors.deprecate_moved_module(__name__, _moved_mod)
+sys.modules[__name__] = module(__name__)
