3802995efec33632b16ac5af97c3334655cde90d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2020-04-08 16:30:02 -0500

    Merge pull request #5498 from eric-wieser/better-shims
     Make the shim deprecation warnings work on python 3.6 too

--- numba/targets/imputils.py
+++ numba/targets/imputils.py
@@ -1,11 +1,3 @@
-import numba.core.errors as _errors
-from numba.core.utils import PYVERSION as _PYVERSION
-
-_moved_mod = "numba.core.imputils"
-
-if _PYVERSION >= (3, 7):
-    __getattr__ = _errors.deprecate_moved_module_getattr(__name__, _moved_mod)
-else:
-    from numba.core.imputils import *  # noqa: F403, F401
-
-_errors.deprecate_moved_module(__name__, _moved_mod)
+import sys
+from numba.core.errors import _MovedModule
+sys.modules[__name__] = _MovedModule(locals(), "numba.core.imputils")
