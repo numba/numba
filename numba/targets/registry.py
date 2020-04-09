f6ee24b07b08e5e317e09785dfddceca836a0519
reazul.hoque <reazul.hoque@intel.com>
2020-04-09 14:18:07 -0500

    njit with parallel options will go through dppy pipeline

--- numba/targets/registry.py
+++ numba/targets/registry.py
@@ -76,6 +76,9 @@ cpu_target = CPUTarget()
 class CPUDispatcher(dispatcher.Dispatcher):
     targetdescr = cpu_target
 
+    def __init__(self, py_func, locals={}, targetoptions={}, impl_kind='direct'):
+        dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
+                targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=DPPyCompiler)
 
 class DPPyDispatcher(dispatcher.Dispatcher):
     targetdescr = cpu_target
