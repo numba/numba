203e2c8aceca81cf9aedfe764249734133ccc290
reazul.hoque <reazul.hoque@intel.com>
2020-04-13 09:38:37 -0500

    Correct njit flag behavior

--- numba/targets/registry.py
+++ numba/targets/registry.py
@@ -77,11 +77,13 @@ class CPUDispatcher(dispatcher.Dispatcher):
     targetdescr = cpu_target
 
     def __init__(self, py_func, locals={}, targetoptions={}, impl_kind='direct'):
-        dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
-                targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=DPPyCompiler)
-
-        #dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
-        #        targetoptions=targetoptions, impl_kind=impl_kind)
+        if ('parallel' in targetoptions and isinstance(targetoptions['parallel'], dict) and
+                'spirv' in targetoptions['parallel'] and  targetoptions['parallel']['spirv'] == True):
+            dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
+                    targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=DPPyCompiler)
+        else:
+            dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
+                targetoptions=targetoptions, impl_kind=impl_kind)
 
 class DPPyDispatcher(dispatcher.Dispatcher):
     targetdescr = cpu_target
