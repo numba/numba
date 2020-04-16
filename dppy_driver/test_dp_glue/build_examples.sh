2fe8f092f45253b7ba72987381b9eb76e8e13f97
Hoque, Reazul <reazul.hoque@intel.com>
2020-04-16 16:04:15 -0400

    (numba/dppy) Remove dppy backend (#44)
    * Building NUMBA-DPPY successfully
    
    * Removed some missed dppy_driver artifacts
    
    * Removed new lines where it makes sense and replaced numba.dppy with .

--- dppy_driver/test_dp_glue/build_examples.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xv
-rm -rf *.o
-base="${1%.*}"
-gcc -g -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -std=c99 -fdiagnostics-color=auto -pedantic-errors -fPIC  -I.. ../opencllite.c ${1} -o $base.exe -lOpenCL
