dc22351612e22d2e8a8e1528ad88f78c29929386
Shan Sikdar <ssikdar1@bu.edu>
2020-04-23 10:35:54 -0400

    fix test_funcdesc unittest

--- numba/tests/test_funcdesc.py
+++ numba/tests/test_funcdesc.py
@@ -21,8 +21,10 @@ def f(x):
         exec(compiled, objs)
 
         compiled_f = njit(objs['f'])
-        with self.assertRaises(ModuleNotFoundError):
+        with self.assertRaises(ModuleNotFoundError) as raises:
             compiled_f(0)
+        msg = "can't compile f: import of module mypackage failed"
+        self.assertIn(msg, str(raises.exception))
 
 
 if __name__ == '__main__':
