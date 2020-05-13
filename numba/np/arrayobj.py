6f7ca5a0d23a2edefe1cf62963d6191211243436
Isaac Virshup <ivirshup@gmail.com>
2020-05-13 17:26:34 +1000

    Minor optimization to np.split when integers are passed

--- numba/np/arrayobj.py
+++ numba/np/arrayobj.py
@@ -5007,7 +5007,7 @@ def np_split(a, indices, axis=0):
             l, rem = divmod(a.shape[axis], indices)
             if rem != 0:
                 raise ValueError("array split does not result in an equal division")
-            return np.split(a, list(range(l, len(a), l)), axis=axis)
+            return np.split(a, np.arange(l, len(a), l), axis=axis)
 
     else:
 
