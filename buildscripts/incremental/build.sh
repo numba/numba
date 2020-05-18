2767d662f89c6ea348d1ec536b3312055a52cdba
Siu Kwan Lam <michael.lam.sk@gmail.com>
2020-05-18 13:42:03 -0500

    Temp turn off llvmlite requirement

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -12,4 +12,5 @@ python setup.py build_ext -q --inplace
 
 # Install numba locally for use in `numba -s` sys info tool at test time
 # `-iNOWHERE` make a fake index to avoid auto downloading dependency
-python -m pip install -iNOWHERE -e .
+# python -m pip install -iNOWHERE -e .
+python setup.py develop --no-deps  # XXX: revert me
