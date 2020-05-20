9217c3b4c889e467e35b8775a82cb182e4a15907
sungrae <sungrae@umich.edu>
2020-05-20 14:37:25 -0400

    no longer checks if second argument is types.stringLiteral

--- numba/np/arraymath.py
+++ numba/np/arraymath.py
@@ -4259,7 +4259,7 @@ def np_trim_zeros(a, trim='fb'):
         raise TypeError('The first argument must be an array-like')
     if hasattr(a, 'ndim') and a.ndim > 1:
         raise TypingError('array must be 1D')
-    if not isinstance(trim, (str, types.UnicodeType, types.StringLiteral)):
+    if not isinstance(trim, (str, types.UnicodeType)):
         raise TypeError('The second argument must be a string')
 
     def impl(a, trim='fb'):
