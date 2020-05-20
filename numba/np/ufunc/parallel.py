947af3d2c7bb2f8210f5cd548fc6b12f4a8a303a
Daichi SUZUO <belltailjp@gmail.com>
2020-05-20 14:21:50 +0900

    Flake8

--- numba/np/ufunc/parallel.py
+++ numba/np/ufunc/parallel.py
@@ -301,19 +301,22 @@ def _set_init_process_lock():
             _backend_init_process_lock = multiprocessing.get_context().RLock()
         else:
             _backend_init_process_lock = _nop()
-    
+
     except OSError as e:
-    
+
         # probably lack of /dev/shm for semaphore writes, warn the user
-        msg = ("Could not obtain multiprocessing lock due to OS level error: %s\n"
-               "A likely cause of this problem is '/dev/shm' is missing or"
-               "read-only such that necessary semaphores cannot be written.\n"
-               "*** The responsibility of ensuring multiprocessing safe access to "
-               "this initialization sequence/module import is deferred to the "
-               "user! ***\n")
+        msg = (
+            "Could not obtain multiprocessing lock due to OS level error: %s\n"
+            "A likely cause of this problem is '/dev/shm' is missing or"
+            "read-only such that necessary semaphores cannot be written.\n"
+            "*** The responsibility of ensuring multiprocessing safe access to "
+            "this initialization sequence/module import is deferred to the "
+            "user! ***\n"
+        )
         warnings.warn(msg % str(e))
-    
+
         _backend_init_process_lock = _nop()
+
 
 _is_initialized = False
 
