e074a4a50c9e6385edaaf836d267c6f60fa6b229
reazul.hoque <reazul.hoque@intel.com>
2020-05-26 11:12:35 -0500

    Added missing kwargs to cpu_dispatcher

--- numba/core/cpu_dispatcher.py
+++ numba/core/cpu_dispatcher.py
@@ -1,4 +1,4 @@
-from numba.core import dispatcher
+from numba.core import dispatcher, compiler
 from numba.core.registry import cpu_target, dispatcher_registry
 from numba.dppy.compiler import DPPyCompiler
 
@@ -6,14 +6,14 @@ from numba.dppy.compiler import DPPyCompiler
 class CPUDispatcher(dispatcher.Dispatcher):
     targetdescr = cpu_target
 
-    def __init__(self, py_func, locals={}, targetoptions={}, impl_kind='direct'):
+    def __init__(self, py_func, locals={}, targetoptions={}, impl_kind='direct', pipeline_class=compiler.Compiler):
         if ('parallel' in targetoptions and isinstance(targetoptions['parallel'], dict) and
                 'spirv' in targetoptions['parallel'] and  targetoptions['parallel']['spirv'] == True):
             dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
                     targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=DPPyCompiler)
         else:
             dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
-                targetoptions=targetoptions, impl_kind=impl_kind)
+                targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=pipeline_class)
 
 
 dispatcher_registry['cpu'] = CPUDispatcher
