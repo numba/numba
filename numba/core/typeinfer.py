aebfdc74de541942740cedab93ac1edebfcedf02
Graham Markall <gmarkall@nvidia.com>
2020-06-04 10:32:48 +0100

    Attempt to remove ir.Intrinsic

--- numba/core/typeinfer.py
+++ numba/core/typeinfer.py
@@ -1514,12 +1514,7 @@ http://numba.pydata.org/numba-doc/latest/user/troubleshoot.html#my-code-has-an-u
 
     def typeof_expr(self, inst, target, expr):
         if expr.op == 'call':
-            if isinstance(expr.func, ir.Intrinsic):
-                sig = expr.func.type
-                self.add_type(target.name, sig.return_type, loc=inst.loc)
-                self.add_calltype(expr, sig)
-            else:
-                self.typeof_call(inst, target, expr)
+            self.typeof_call(inst, target, expr)
         elif expr.op in ('getiter', 'iternext'):
             self.typeof_intrinsic_call(inst, target, expr.op, expr.value)
         elif expr.op == 'exhaust_iter':
