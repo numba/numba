07ca8eb5ea0daf2a154ff067ccfa7dae91dfa4b2
luk-f-a <lucioarjona@hotmail.com>
2020-06-12 19:16:06 +0200

    merged branch and moved tests

--- numba/tests/test_array_exprs.py
+++ numba/tests/test_array_exprs.py
@@ -10,8 +10,6 @@ from numba.core.compiler import Compiler, Flags
 from numba.tests.support import MemoryLeakMixin, TestCase
 import unittest
 
-TIMEDELTA_M = np.dtype('timedelta64[M]')
-TIMEDELTA_Y = np.dtype('timedelta64[Y]')
 
 class Namespace(dict):
     def __getattr__(s, k):
@@ -627,29 +625,6 @@ class TestOptionalsExceptions(MemoryLeakMixin, unittest.TestCase):
         self.assertTrue(isinstance(oty, types.Optional))
         self.assertTrue(isinstance(oty.type, types.Array))
         self.assertTrue(isinstance(oty.type.dtype, types.Float))
-
-
-class TestDatetimeDeltaOps(TestCase):
-    def test_div(self):
-        """
-        Test the division of a timedelta by numeric types
-        """
-        def arr_div(a, b):
-            return a / b
-
-        py_func = arr_div
-        cfunc = njit(arr_div)
-        test_cases = [
-            (np.ones(3, TIMEDELTA_M), np.ones(3, TIMEDELTA_M)),
-            (np.ones(3, TIMEDELTA_M), np.ones(3, TIMEDELTA_Y)),
-            (np.ones(3, TIMEDELTA_Y), np.ones(3, TIMEDELTA_M)),
-            (np.ones(3, TIMEDELTA_Y), np.ones(3, TIMEDELTA_Y)),
-            (np.ones(3, TIMEDELTA_M), 1),
-            (np.ones(3, TIMEDELTA_M), np.ones(3, np.int64)),
-            (np.ones(3, TIMEDELTA_M), np.ones(3, np.float64)),
-        ]
-        for a, b in test_cases:
-            self.assertTrue(np.array_equal(py_func(a, b), cfunc(a, b)))
 
 
 if __name__ == "__main__":
