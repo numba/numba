46bc1c60702871b5ec4b7c4d9bd1fdf9c2ea7a27
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-06-16 17:04:28 -0300

    try enable test again

--- numba/tests/test_utils.py
+++ numba/tests/test_utils.py
@@ -5,86 +5,88 @@ from numba.misc import utils
 from numba.tests.support import TestCase
 from numba import njit
 
+
 # Solution for capturing stdout
 # https://stackoverflow.com/questions/24277488/in-python-how-to-capture-the-stdout-from-a-c-shared-library-to-a-variable
-# stdout_fileno = sys.stdout.fileno()
-# stdout_save = os.dup(stdout_fileno)
-# stdout_pipe = os.pipe()
-# os.dup2(stdout_pipe[1], stdout_fileno)
-# os.close(stdout_pipe[1])
-
-# captured_stdout = b''
-# def drain_pipe():
-# 	global captured_stdout
-# 	while True:
-# 		data = os.read(stdout_pipe[0], 1024)
-# 		if not data:
-# 			break
-# 		captured_stdout += data
+captured_stdout = b''
+def drain_pipe():
+	global captured_stdout
+	while True:
+		data = os.read(stdout_pipe[0], 1024)
+		if not data:
+			break
+		captured_stdout += data
 
 
-# def printf():
-# 	d = i = 392
-# 	utils.printf("d=%d, i=%i\n", d, i)
+def printf():
+	d = i = 392
+	utils.printf("d=%d, i=%i\n", d, i)
 
-# 	u = 7325
-# 	utils.printf("u=%u\n", u)
+	u = 7325
+	utils.printf("u=%u\n", u)
 
-# 	o = 610
-# 	utils.printf("o=%o\n", o)
+	o = 610
+	utils.printf("o=%o\n", o)
 
-# 	x = 2042
-# 	utils.printf("x=%x\n", x)
-# 	X = 2042
-# 	utils.printf("X=%X\n", X)
+	x = 2042
+	utils.printf("x=%x\n", x)
+	X = 2042
+	utils.printf("X=%X\n", X)
 
-# 	f = 392.651231
-# 	utils.printf("f=%.3f\n", f)
-# 	F = 392.651231
-# 	utils.printf("F=%.4F\n", F)
+	f = 392.651231
+	utils.printf("f=%.3f\n", f)
+	F = 392.651231
+	utils.printf("F=%.4F\n", F)
 
-# 	e = 3.9264e+2
-# 	utils.printf("e=%e\n", e)
-# 	E = 3.9264e+2
-# 	utils.printf("E=%E\n", E)
+	e = 3.9264e+2
+	utils.printf("e=%e\n", e)
+	E = 3.9264e+2
+	utils.printf("E=%E\n", E)
 
-# 	g = 392.65
-# 	utils.printf("g=%g\n", g)
-# 	G = 392.65
-# 	utils.printf("G=%G\n", G)
+	g = 392.65
+	utils.printf("g=%g\n", g)
+	G = 392.65
+	utils.printf("G=%G\n", G)
 
-# 	c = 'a'
-# 	s = 'sample'
-# 	utils.printf("c=%s\n", c)
-# 	utils.printf("s=%s\n", s)
+	c = 'a'
+	s = 'sample'
+	utils.printf("c=%s\n", c)
+	utils.printf("s=%s\n", s)
 
-# 	p = 10
-# 	utils.printf("p=%p\n", p)
+	p = 10
+	utils.printf("p=%p\n", p)
 
-# 	utils.fflush()
+	utils.fflush()
 
 
-# class TestUtils(TestCase):
+class TestUtils(TestCase):
 
-# 	def test_printf(self):
-# 		d = 3
-# 		cfunc = njit(printf)
-# 		t = threading.Thread(target=drain_pipe)
-# 		t.start()
-# 		cfunc()
-# 		os.close(stdout_fileno)
-# 		t.join()	
-# 		os.close(stdout_pipe[0])
-# 		os.dup2(stdout_save, stdout_fileno)
-# 		os.close(stdout_save)
+	@unittest.skipIf(sys.platform.startswith('linux'), 'MacOs and Windows only')
+	def test_printf(self):
+		stdout_fileno = sys.stdout.fileno()
+		stdout_save = os.dup(stdout_fileno)
+		stdout_pipe = os.pipe()
+		os.dup2(stdout_pipe[1], stdout_fileno)
+		os.close(stdout_pipe[1])
 
-# 		expected = 'd=392, i=392\nu=7325\no=1142\nx=7fa\nX=7FA\n' + \
-# 				'f=392.651\nF=392.6512\ne=3.926400e+02\n' + \
-# 				'E=3.926400E+02\ng=392.65\nG=392.65\n' + \
-# 				'c=a\ns=sample\np=0xa\n'
+		d = 3
+		cfunc = njit(printf)
+		t = threading.Thread(target=drain_pipe)
+		t.start()
+		cfunc()
+		os.close(stdout_fileno)
+		t.join()	
+		os.close(stdout_pipe[0])
+		os.dup2(stdout_save, stdout_fileno)
+		os.close(stdout_save)
 
-# 		output = captured_stdout.decode('utf-8')
-# 		self.assertEqual(expected, output)
+		expected = 'd=392, i=392\nu=7325\no=1142\nx=7fa\nX=7FA\n' + \
+				'f=392.651\nF=392.6512\ne=3.926400e+02\n' + \
+				'E=3.926400E+02\ng=392.65\nG=392.65\n' + \
+				'c=a\ns=sample\np=0xa\n'
+
+		output = captured_stdout.decode('utf-8')
+		self.assertEqual(expected, output)
 
 if __name__ == '__main__':
 	unittest.main()
