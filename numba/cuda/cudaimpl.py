3399d618b334c75a7c5c1ab85ec02769fb3b4c3b
Eric Wieser <wieser.eric@gmail.com>
2020-06-25 10:54:42 +0100

    cuda: Remove special-casing of 0d arrays
    This undoes some of the workaround that appeared in gh-5343.

--- numba/cuda/cudaimpl.py
+++ numba/cuda/cudaimpl.py
@@ -607,8 +607,9 @@ def _generic_array(context, builder, shape, dtype, symbol_name, addrspace,
                    can_dynsized=False):
     elemcount = reduce(operator.mul, shape, 1)
 
-    # Check for valid shape for this type of allocation
-    dynamic_smem = elemcount <= 0 and can_dynsized
+    # Check for valid shape for this type of allocation.
+    # Only 1d arrays can be dynamic.
+    dynamic_smem = elemcount <= 0 and can_dynsized and len(shape) == 1
     if elemcount <= 0 and not dynamic_smem:
         raise ValueError("array length <= 0")
 
@@ -657,9 +658,11 @@ def _generic_array(context, builder, shape, dtype, symbol_name, addrspace,
     itemsize = lldtype.get_abi_size(targetdata)
 
     # Compute strides
-    rstrides = [itemsize]
-    for i, lastsize in enumerate(reversed(shape[1:])):
-        rstrides.append(lastsize * rstrides[-1])
+    laststride = itemsize
+    rstrides = []
+    for i, lastsize in enumerate(reversed(shape)):
+        rstrides.append(laststride)
+        laststride *= lastsize
     strides = [s for s in reversed(rstrides)]
     kstrides = [context.get_constant(types.intp, s) for s in strides]
 
