d6e6981cb233fe0f69f47e371bdb0bcce5223987
reazul.hoque <reazul.hoque@intel.com>
2020-07-08 20:17:37 -0500

    (numba/dppy) Added check to not error out if OpenCL is absent

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,16 @@
 from numba.testing import SerialSuite
 from numba.testing import load_testsuite
 from os.path import dirname, join
-import dppy.ocldrv as ocldrv
+
+
+import numba.dppy_config as dppy_config
 
 def load_tests(loader, tests, pattern):
 
     suite = SerialSuite()
     this_dir = dirname(__file__)
 
-    if ocldrv.is_available():
+    if dppy_config.dppy_present and dppy_config.is_available():
         suite.addTests(load_testsuite(loader, join(this_dir, 'dppy')))
     else:
         print("skipped DPPY tests")
