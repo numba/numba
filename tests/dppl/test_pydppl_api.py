c5f2b1c56a041e4da614c5253647d4b303a2f5f7
reazul.hoque <reazul.hoque@intel.com>
2020-07-16 16:01:12 -0500

    (numba/dppl) Added unit test to check dppl apis

---
+++ tests/dppl/test_pydppl_api.py
@@ -0,0 +1,32 @@
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+
+from numba.dppl.testing import unittest
+from numba.dppl.testing import DPPLTestCase
+import dppl.ocldrv as ocldrv
+
+
+@unittest.skipUnless(ocldrv.has_gpu_device, 'test only on GPU system')
+class TestPYDPPLAPI(DPPLTestCase):
+    def test_pydppl_api(self):
+        A = np.array(np.random.random(10), dtype=np.float32)
+        with ocldrv.igpu_context(0) as device_env:
+            # DeviceEnv APIs
+            dA = device_env.copy_array_to_device(A)
+
+            device_env.copy_array_from_device(dA)
+
+            dB = device_env.create_device_array(A) # A is given here as meta-data
+
+            device_env.dump()
+
+            max_work_group_size = device_env.get_max_work_group_size()
+            max_work_item_dims  = device_env.get_max_work_item_dims()
+
+
+            # DeviceArray APIs
+            self.assertTrue(hex(id(A)) == hex(id(dA.get_ndarray())))
+
+if __name__ == '__main__':
+    unittest.main()
