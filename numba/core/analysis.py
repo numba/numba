4a68ec299cd547c11d30bebc8280082b9ee1e2e9
Stuart Archibald <stuart.archibald@googlemail.com>
2020-07-17 16:34:41 +0100

    Fix problem in branch pruning with LiteralStrKeyDict
    Branch pruning tries to evaluate literal types via (eventually)
    `typeof(literal_value)`, LiteralStrKeyDict's `literal_value`
    is a python dict and has no associated `typeof` translation.
    This patch mitigates such a query breaking the branch pruning
    pass.

--- numba/core/analysis.py
+++ numba/core/analysis.py
@@ -396,7 +396,12 @@ def dead_branch_prune(func_ir, called_args):
         # literal type, return the type itself so comparisons like `x == None`
         # still work as e.g. x = types.int64 will never be None/NoneType so
         # the branch can still be pruned
-        return getattr(input_arg_ty, 'literal_type', Unknown())
+        try:
+            return getattr(input_arg_ty, 'literal_type', Unknown())
+        except ValueError:
+            # some things have a literal_value but their literal_type is
+            # meaningless, for example LiteralStrKeyDict
+            return Unknown()
 
     if DEBUG > 1:
         print("before".center(80, '-'))
