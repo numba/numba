0d4a0e94085ccdcf54830bc3a9384fa097d07bf4
reazul.hoque <reazul.hoque@intel.com>
2020-08-05 14:25:18 -0500

    Added atomic OpenCL kernel that will provide the atomic SPIR-V module

---
+++ numba/dppl/ocl/atomics/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+atomic_spirv_path = os.path.join(os.path.dirname(__file__), 'atomic_ops.spir')
+
+def read_atomic_spirv_file():
+    with open(atomic_spirv_path, 'rb') as fin:
+        spirv = fin.read()
+    return spirv
