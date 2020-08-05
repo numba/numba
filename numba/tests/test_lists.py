bd3008a47eda788bef55798838161a863aabbe12
Stuart Archibald <stuart.archibald@googlemail.com>
2020-08-05 17:18:37 +0100

    Fix bug in initial value unify.
    As title, the ctor initial value was already an initial value, it
    didn't need the getattr on it.
    
    Fixes #6082

--- numba/tests/test_lists.py
+++ numba/tests/test_lists.py
@@ -1516,6 +1516,22 @@ class TestListInitialValues(MemoryLeakMixin, TestCase):
             larg = baz.signatures[0][0]
             self.assertEqual(larg.initial_value, iv)
 
+    def test_list_of_list_ctor(self):
+        # see issue 6082
+        @njit
+        def bar(x):
+            pass
+
+        @njit
+        def foo():
+            x = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 6]]
+            bar(x)
+
+        foo()
+        larg = bar.signatures[0][0]
+        self.assertEqual(larg.initial_value, None)
+        self.assertEqual(larg.dtype.initial_value, None)
+
 
 class TestLiteralLists(MemoryLeakMixin, TestCase):
 
