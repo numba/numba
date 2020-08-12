4f17d852a03c08097c8def67cc377b29d27e455a
Graham Markall <graham@big-grey.co.uk>
2020-08-12 11:51:19 +0100

    CUDA: Add Dispatcher specialization caching tests
    This test takes array ordering into account, and uses multiple
    arguments.
    
    Also, the argument types in other tests should have been passed as
    arguments to specialize(), not as tuples of argument types.

--- numba/cuda/tests/cudapy/test_dispatcher.py
+++ numba/cuda/tests/cudapy/test_dispatcher.py
@@ -7,7 +7,7 @@ class TestDispatcher(CUDATestCase):
     def _test_no_double_specialize(self, dispatcher, ty):
 
         with self.assertRaises(RuntimeError) as e:
-            dispatcher.specialize((ty,))
+            dispatcher.specialize(ty)
 
         self.assertIn('Dispatcher already specialized', str(e.exception))
 
@@ -27,7 +27,7 @@ class TestDispatcher(CUDATestCase):
         def f(x):
             pass
 
-        f_specialized = f.specialize((float32[::1],))
+        f_specialized = f.specialize(float32[::1])
         self._test_no_double_specialize(f_specialized, float32[::1])
 
     def test_no_double_specialize_sig_diff_types(self):
@@ -44,7 +44,7 @@ class TestDispatcher(CUDATestCase):
         def f(x):
             pass
 
-        f_specialized = f.specialize((int32[::1],))
+        f_specialized = f.specialize(int32[::1])
         self._test_no_double_specialize(f_specialized, float32[::1])
 
     def test_specialize_cache_same(self):
@@ -57,16 +57,41 @@ class TestDispatcher(CUDATestCase):
 
         self.assertEqual(len(f.specializations), 0)
 
-        f_float32 = f.specialize((float32[::1],))
+        f_float32 = f.specialize(float32[::1])
         self.assertEqual(len(f.specializations), 1)
 
-        f_float32_2 = f.specialize((float32[::1],))
+        f_float32_2 = f.specialize(float32[::1])
         self.assertEqual(len(f.specializations), 1)
         self.assertIs(f_float32, f_float32_2)
 
-        f_int32 = f.specialize((int32[::1],))
+        f_int32 = f.specialize(int32[::1])
         self.assertEqual(len(f.specializations), 2)
         self.assertIsNot(f_int32, f_float32)
+
+    def test_specialize_cache_same_with_ordering(self):
+        # Ensure that the same dispatcher is returned for the same argument
+        # types, and that different dispatchers are returned for different
+        # argument types, taking into account array ordering and multiple
+        # arguments.
+        @cuda.jit
+        def f(x, y):
+            pass
+
+        self.assertEqual(len(f.specializations), 0)
+
+        # 'A' order specialization
+        f_f32a_f32a = f.specialize(float32[:], float32[:])
+        self.assertEqual(len(f.specializations), 1)
+
+        # 'C' order specialization
+        f_f32c_f32c = f.specialize(float32[::1], float32[::1])
+        self.assertEqual(len(f.specializations), 2)
+        self.assertIsNot(f_f32a_f32a, f_f32c_f32c)
+
+        # Reuse 'C' order specialization
+        f_f32c_f32c_2 = f.specialize(float32[::1], float32[::1])
+        self.assertEqual(len(f.specializations), 2)
+        self.assertIs(f_f32c_f32c, f_f32c_f32c_2)
 
 
 if __name__ == '__main__':
