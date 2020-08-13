4e6d619fb7a0673676a5217ef1068f551b9bece0
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-08-13 19:26:16 +0300

    (numba/dppl) Use ctypes.util.find_library for finding DPPLOpenCLInterface and OpenCL (#7)

--- initialize.py
+++ initialize.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function
 import llvmlite.binding as ll
 import os
+from ctypes.util import find_library
 
 
 def init_jit():
@@ -11,5 +12,5 @@ def initialize_all():
     from numba.core.registry import dispatcher_registry
     dispatcher_registry.ondemand['dppl'] = init_jit
 
-    ll.load_library_permanently('libDPPLOpenCLInterface.so')
-    ll.load_library_permanently('libOpenCL.so')
+    ll.load_library_permanently(find_library('DPPLOpenCLInterface'))
+    ll.load_library_permanently(find_library('OpenCL'))
