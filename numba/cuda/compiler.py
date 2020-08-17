8670b7f41d07b6c45229b4f3544f6e17cac6d6d2
Eric Wieser <wieser.eric@gmail.com>
2020-08-17 12:43:11 +0100

    Replace calls to utils.iteritems(d) with d.items()
    This looks like a holdover from Python 2 support.

--- numba/cuda/compiler.py
+++ numba/cuda/compiler.py
@@ -997,7 +997,7 @@ class Dispatcher(serialize.ReduceMixin):
         if self.specialized:
             self.definition.inspect_types(file=file)
         else:
-            for _, defn in utils.iteritems(self.definitions):
+            for _, defn in self.definitions.items():
                 defn.inspect_types(file=file)
 
     @property
