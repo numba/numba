367100c78c201ddac3ecb9e06e9382755989fb9d
Zhihao Yuan <zy@simplerose.com>
2020-08-25 15:49:43 -0500

    Ensure CUDA lowering of math.frexp behave the same
    For non-finite values, exp = 0.  Note that this is different
    from np.frexp, which gives exp = -1.

--- numba/cuda/tests/cudapy/test_frexp_ldexp.py
+++ numba/cuda/tests/cudapy/test_frexp_ldexp.py
@@ -1,56 +1,65 @@
 import numpy as np
 import math
 from numba import cuda
-from numba.types import float32, float64, int32
+from numba.types import float32, float64, int32, void
 from numba.cuda.testing import unittest, CUDATestCase
 
 
-def simple_frexp(x, exp, arg):
-    x[0], exp[0] = math.frexp(arg)
+def simple_frexp(aryx, aryexp, arg):
+    aryx[0], aryexp[0] = math.frexp(arg)
 
 
-def simple_ldexp(val, arg, exp):
-    val[0] = math.ldexp(arg, exp)
+def simple_ldexp(aryx, arg, exp):
+    aryx[0] = math.ldexp(arg, exp)
 
 
 class TestCudaFrexpLdexp(CUDATestCase):
-    def template_test_frexp_ldexp(self, value, npyty, npmty):
-        frexp_kernel = cuda.jit((npmty[:], int32[:], npmty))(simple_frexp)
+    def template_test_frexp(self, nptype, nbtype):
+        compiled = cuda.jit(void(nbtype[:], int32[:], nbtype))(simple_frexp)
+        arg = 3.1415
+        aryx = np.zeros(1, dtype=nptype)
+        aryexp = np.zeros(1, dtype=np.int32)
+        compiled[1, 1](aryx, aryexp, arg)
+        np.testing.assert_array_equal(aryx, nptype(0.785375))
+        self.assertEquals(aryexp, 2)
 
-        arg = npyty(value)
-        x = np.empty(1, dtype=npyty)
-        xneg = np.empty_like(x)
-        exp = np.empty_like(x, dtype=np.int32)
-        npx, npexp = np.frexp(arg)
+        arg = np.inf
+        compiled[1, 1](aryx, aryexp, arg)
+        np.testing.assert_array_equal(aryx, nptype(np.inf))
+        self.assertEquals(aryexp, 0)  # np.frexp gives -1
 
-        frexp_kernel[1, 1](x, exp, arg)
+        arg = np.nan
+        compiled[1, 1](aryx, aryexp, arg)
+        np.testing.assert_array_equal(aryx, nptype(np.nan))
+        self.assertEquals(aryexp, 0)  # np.frexp gives -1
 
-        np.testing.assert_array_equal(x, npx)
-        self.assertEquals(exp, npexp)
+    def template_test_ldexp(self, nptype, nbtype):
+        compiled = cuda.jit(void(nbtype[:], nbtype, int32))(simple_ldexp)
+        arg = 0.785375
+        exp = 2
+        aryx = np.zeros(1, dtype=nptype)
+        compiled[1, 1](aryx, arg, exp)
+        np.testing.assert_array_equal(aryx, nptype(3.1415))
 
-        frexp_kernel[1, 1](xneg, exp, -arg)
+        arg = np.inf
+        compiled[1, 1](aryx, arg, exp)
+        np.testing.assert_array_equal(aryx, nptype(np.inf))
 
-        np.testing.assert_array_equal(xneg, -npx)
-        self.assertEquals(exp, npexp)
+        arg = np.nan
+        compiled[1, 1](aryx, arg, exp)
+        np.testing.assert_array_equal(aryx, nptype(np.nan))
 
-        ldexp_kernel = cuda.jit((npmty[:], npmty, int32))(simple_ldexp)
+    def test_frexp_f4(self):
+        self.template_test_frexp(np.float32, float32)
 
-        val = np.empty(1, dtype=npyty)
+    def test_ldexp_f4(self):
+        self.template_test_ldexp(np.float32, float32)
 
-        ldexp_kernel[1, 1](val, x.item(), exp.item())
+    def test_frexp_f8(self):
+        self.template_test_frexp(np.float64, float64)
 
-        np.testing.assert_array_equal(val, np.ldexp(x, exp))
-        np.testing.assert_array_equal(val, arg)  # roundtrip
-
-        ldexp_kernel[1, 1](val, xneg.item(), exp.item())
-
-        np.testing.assert_array_equal(val, np.ldexp(xneg, exp))
-        np.testing.assert_array_equal(val, -arg)  # roundtrip
-
-    def test_ldexp_frexp(self):
-        for value in [0., 3.1e-10, 0.785375, 2., 1.3e10]:
-            self.template_test_frexp_ldexp(value, np.float32, float32)
-            self.template_test_frexp_ldexp(value, np.float64, float64)
+    def test_ldexp_f8(self):
+        self.template_test_ldexp(np.float64, float64)
 
 
 if __name__ == '__main__':
