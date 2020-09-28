ff7004e43e8654d05903f2945b84813a8aa3c1b7
Stuart Archibald <stuart.archibald@googlemail.com>
2020-09-28 12:23:56 +0100

    Ban refcounted types from set(), they leak

--- numba/cpython/setobj.py
+++ numba/cpython/setobj.py
@@ -201,6 +201,11 @@ class _SetPayload(object):
         tyctx = context.typing_context
         fnty = tyctx.resolve_value_type(operator.eq)
         sig = fnty.get_call_type(tyctx, (dtype, dtype), {})
+        for item in sig.args:
+            if context.data_model_manager[item].contains_nrt_meminfo():
+                msg = ("Use of reference counted items in 'set()' is "
+                       "unsupported, offending type is: '{}'.")
+                raise ValueError(msg.format(item))
         eqfn = context.get_function(fnty, sig)
 
         one = ir.Constant(intp_t, 1)
