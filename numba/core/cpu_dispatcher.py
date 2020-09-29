9666627b73e1ea3c67e364ff93940f6c88c1890b
Elena Totmenina <totmeninal@mail.ru>
2020-09-29 19:05:45 +0300

    Added separate dispatcher (#42)

--- numba/core/cpu_dispatcher.py
+++ numba/core/cpu_dispatcher.py
@@ -6,22 +6,8 @@ class CPUDispatcher(dispatcher.Dispatcher):
     targetdescr = cpu_target
 
     def __init__(self, py_func, locals={}, targetoptions={}, impl_kind='direct', pipeline_class=compiler.Compiler):
-        if ('parallel' in targetoptions and isinstance(targetoptions['parallel'], dict) and
-                'offload' in targetoptions['parallel'] and  targetoptions['parallel']['offload'] == True):
-            import numba.dppl_config as dppl_config
-            if dppl_config.dppl_present:
-                from numba.dppl.compiler import DPPLCompiler
-                dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
-                        targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=DPPLCompiler)
-            else:
-                print("---------------------------------------------------------------------------")
-                print("WARNING : offload=True option ignored. Ensure OpenCL drivers are installed.")
-                print("---------------------------------------------------------------------------")
-                dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
-                    targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=pipeline_class)
-        else:
-            dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
-                targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=pipeline_class)
+        dispatcher.Dispatcher.__init__(self, py_func, locals=locals,
+            targetoptions=targetoptions, impl_kind=impl_kind, pipeline_class=pipeline_class)
 
 
 dispatcher_registry['cpu'] = CPUDispatcher
