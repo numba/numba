99ca92c84f6281d72034d873720e43965d1e339e
Graham Markall <gmarkall@nvidia.com>
2020-10-13 14:17:33 +0100

    Guard against non-64-bit integers in lower_{u,bi}nary_int_impl

--- numba/cuda/mathimpl.py
+++ numba/cuda/mathimpl.py
@@ -83,8 +83,12 @@ def impl_unary_int(key, ty, libfunc):
     def lower_unary_int_impl(context, builder, sig, args):
         if sig.args[0] == int64:
             convert = builder.sitofp
-        else:
+        elif sig.args[0] == uint64:
             convert = builder.uitofp
+        else:
+            m = 'Only 64-bit integers are supported for generic unary int ops'
+            raise TypeError(m)
+
         arg = convert(args[0], ir.DoubleType())
         sig = typing.signature(float64, float64)
         libfunc_impl = context.get_function(libfunc, sig)
@@ -106,8 +110,12 @@ def impl_binary_int(key, ty, libfunc):
     def lower_binary_int_impl(context, builder, sig, args):
         if sig.args[0] == int64:
             convert = builder.sitofp
-        else:
+        elif sig.args[0] == uint64:
             convert = builder.uitofp
+        else:
+            m = 'Only 64-bit integers are supported for generic binary int ops'
+            raise TypeError(m)
+
         args = [convert(arg, ir.DoubleType()) for arg in args]
         sig = typing.signature(float64, float64, float64)
         libfunc_impl = context.get_function(libfunc, sig)
