a10674c8f6e359841f8edb222e8db082810515a0
Danny Weitekamp <dannyweitekamp@gmail.com>
2020-10-23 12:50:32 -0400

    missing imports

--- numba/tests/compile_with_pycc.py
+++ numba/tests/compile_with_pycc.py
@@ -3,6 +3,7 @@ import cmath
 import numpy as np
 
 from numba import float32
+from numba.types import unicode_type,i8
 from numba.pycc import CC, exportmany, export
 from numba.tests.support import has_blas
 from numba import typed
