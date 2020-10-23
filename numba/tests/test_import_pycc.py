70220bce863c66c66029df41168f3d58dc40e90d
Danny Weitekamp <dannyweitekamp@gmail.com>
2020-10-22 22:29:19 -0400

    added test case for issue #6386

---
+++ numba/tests/test_import_pycc.py
@@ -0,0 +1,49 @@
+from numba.pycc import CC
+from numba import i8
+from numba.core.types import DictType, unicode_type
+from numba.typed import List, Dict
+from numba.tests.support import TestCase
+import unittest
+import os, glob
+
+
+class TestImportStringHashing(TestCase):
+
+    """
+    Tests that numba compiled modules can be imported if they have
+     functions that internally hash strings. See issue #6386.
+    """
+    def setUp(self):
+        cc = CC('str_dict_module')
+
+        nb_nt = DictType(unicode_type,i8)
+
+        @cc.export('in_str_dict',(DictType(unicode_type,i8),unicode_type))
+        def in_str_dict(d,x):
+           if(x not in d):
+              d[x] = len(d)
+
+        @cc.export('out_str_dict',DictType(unicode_type,i8)(unicode_type,))
+        def out_str_dict(x):
+           d = Dict.empty(unicode_type,i8)
+           if(x not in d):
+              d[x] = len(d)
+           return d
+
+        cc.compile()
+
+    def test_import_str_dicts(self):
+        from str_dict_module import in_str_dict as _in_str_dict
+        from str_dict_module import out_str_dict as _out_str_dict
+        d = _out_str_dict('boop')
+        _in_str_dict(d,'beep')
+        self.assertEqual(d['beep'], 1)
+
+    def tearDown(self):
+        os.remove(glob.glob("str_dict_module.*[dll|so]")[0])
+
+if __name__ == "__main__":
+    unittest.main()
+        
+
+
