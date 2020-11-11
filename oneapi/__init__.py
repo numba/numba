1081eae9ebc18c1d77114e255a223b3402d18475
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-11-11 06:24:31 -0600

    Remove history folders: oneapi, dppy, dppl
    History folders contains history for numba/oneapi, numba/dppy and
    numba/dppl folders.

--- oneapi/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-One-API Module to interact with Intel and Other SPIR-V based devices
-"""
-from __future__ import print_function, absolute_import, division
-
-from numba import config
-
-from .device_init import *
