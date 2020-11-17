fb465a8601bc4d45fce99227353c395b47e58ca1
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2020-11-17 13:44:35 -0600

    Update setup.py
    Co-authored-by: stuartarchibald <stuartarchibald@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -42,7 +42,8 @@ def _guard_py_ver():
     cur_py = parse('.'.join(map(str, sys.version_info[:3])))
 
     if not min_py <= cur_py < max_py:
-        msg = 'cannot install on Python version {}; only supporting >={},<{}'
+        msg = ('Cannot install on Python version {}; only versions >={},<{} ',
+               'are supported.')
         raise RuntimeError(msg.format(cur_py, min_py, max_py))
 
 
