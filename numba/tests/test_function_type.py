ff23049b42b8b095640ba0b7362bf0c4ab258bb7
luk-f-a <lucioarjona@hotmail.com>
2021-01-20 11:00:19 +0100

    adding test for two function parameters

--- numba/tests/test_function_type.py
+++ numba/tests/test_function_type.py
@@ -1214,3 +1214,41 @@ class TestBasicSubtyping(TestCase):
                 x += fcs[i](a)
             return x
         self.assertEqual(bar(tup), foo1(a) + foo2(a))
+
+    def test_basic5(self):
+        a = 1
+
+        @njit
+        def foo1(x):
+            return x + 1
+
+        @njit
+        def foo2(x):
+            return x + 2
+
+        @njit
+        def bar1(x):
+            return x / 10
+
+        @njit
+        def bar2(x):
+            return x / 1000
+
+        tup = (foo1, foo2)
+        tup_bar = (bar1, bar2)
+        int_int_fc = types.FunctionType(types.int64(types.int64,))
+
+        flt_flt_fc = types.FunctionType(types.float64(types.float64,))
+
+        @njit((types.UniTuple(int_int_fc, 2), types.UniTuple(flt_flt_fc, 2)))
+        def bar(fcs, ffs):
+            x = 0
+            for i in range(2):
+                x += fcs[i](a)
+            for fn in ffs:
+                x += fn(a)
+            return x
+
+        got = bar(tup, tup_bar)
+        expected = foo1(a) + foo2(a) + bar1(a) + bar2(a)
+        self.assertEqual(got, expected)
