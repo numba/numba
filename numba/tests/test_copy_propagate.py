4aec70cc6c540cc59f456384c7aa273c9a11e958
Ehsan Totoni <ehsan.tn@gmail.com>
2021-02-02 09:36:02 -0500

    Tests for chained assignment corner cases

--- numba/tests/test_copy_propagate.py
+++ numba/tests/test_copy_propagate.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-from numba import njit
+from numba import jit, njit
 from numba.core import types, typing, ir, config, compiler, cpu
 from numba.core.registry import cpu_target
 from numba.core.annotations import type_annotations
@@ -11,6 +11,7 @@ from numba.core.ir_utils import (copy_propagate, apply_copy_propagate,
                             get_name_var_table)
 from numba.core.typed_passes import type_inference_stage
 from numba.tests.test_ir_inlining import InlineTestPipeline
+import numpy as np
 import unittest
 
 
@@ -128,6 +129,32 @@ class TestCopyPropagate(unittest.TestCase):
                     and stmt.value.op == "binop" and stmt.value.lhs.name == "a")
 
         self.assertTrue(b_found)
+
+    def test_input_ir_copy_remove_transform(self):
+        """make sure Interpreter._remove_unused_temporaries() does not generate
+        invalid code for rare chained assignment cases
+        """
+        # regular chained assignment
+        def impl1(a):
+            b = c = a + 1
+            return (b, c)
+
+        # chained assignment with setitem
+        def impl2(A, i, a):
+            b = A[i] = a + 1
+            return b, A[i] + 2
+
+        # chained assignment with setattr
+        def impl3(A, a):
+            b = A.a = a + 1
+            return b, A.a + 2
+
+        class C:
+            pass
+
+        self.assertEqual(impl1(5), njit(impl1)(5))
+        self.assertEqual(impl2(np.ones(3), 0, 5), njit(impl2)(np.ones(3), 0, 5))
+        self.assertEqual(impl3(C(), 5), jit(forceobj=True)(impl3)(C(), 5))
 
 
 if __name__ == "__main__":
