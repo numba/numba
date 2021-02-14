10d863c5da5336182e226fcf430daee43a9d270b
root <74091944+abebeos@users.noreply.github.com>
2021-02-14 17:46:53 +0000

    allow use on other architectures, re #6711

--- buildscripts/incremental/install_miniconda.sh
+++ buildscripts/incremental/install_miniconda.sh
@@ -4,11 +4,12 @@ set -v -e
 
 # Install Miniconda
 unamestr=`uname`
+arch=`uname -m`
 if [[ "$unamestr" == 'Linux' ]]; then
     if [[ "$BITS32" == "yes" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
     else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-"$arch".sh -O miniconda.sh
     fi
 elif [[ "$unamestr" == 'Darwin' ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
