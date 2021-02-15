b1d60974a8d40361c3ad024bd4171cdd3ff37629
Graham Markall <gmarkall@nvidia.com>
2021-02-15 13:03:23 +0000

    CUDA: Fix compile to PTX with debug for CUDA 11.2
    The NVVM IR version metadata needs to be present in all modules passed
    to NVVM. The IR version was only set when kernel wrapper functions were
    generated, so device functions never had the IR version added.
    
    This commit remedies this by adding the IR version to all modules in the
    CUDA target, rather than relying on the kernel wrapper generation
    function to do it.
    
    Fixes #6719.

--- numba/cuda/cudadrv/nvvm.py
+++ numba/cuda/cudadrv/nvvm.py
@@ -896,15 +896,17 @@ def _replace_llvm_memset_declaration(m):
 def set_cuda_kernel(lfunc):
     from llvmlite.llvmpy.core import MetaData, MetaDataString, Constant, Type
 
-    m = lfunc.module
+    mod = lfunc.module
 
-    ops = lfunc, MetaDataString.get(m, "kernel"), Constant.int(Type.int(), 1)
-    md = MetaData.get(m, ops)
+    ops = lfunc, MetaDataString.get(mod, "kernel"), Constant.int(Type.int(), 1)
+    md = MetaData.get(mod, ops)
 
-    nmd = m.get_or_insert_named_metadata('nvvm.annotations')
+    nmd = mod.get_or_insert_named_metadata('nvvm.annotations')
     nmd.add(md)
 
-    # Set NVVM IR version
+
+def add_ir_version(mod):
+    """Add NVVM IR version to module"""
     i32 = ir.IntType(32)
     if NVVM().is_nvvm70:
         # NVVM IR 1.6, DWARF 3.0
@@ -913,8 +915,8 @@ def set_cuda_kernel(lfunc):
         # NVVM IR 1.1, DWARF 2.0
         ir_versions = [i32(1), i32(2), i32(2), i32(0)]
 
-    md_ver = m.add_metadata(ir_versions)
-    m.add_named_metadata('nvvmir.version', md_ver)
+    md_ver = mod.add_metadata(ir_versions)
+    mod.add_named_metadata('nvvmir.version', md_ver)
 
 
 def fix_data_layout(module):
