dea0341b139764e93f0533d4d5537c3e14a9b8a1
Graham Markall <gmarkall@nvidia.com>
2021-02-16 09:53:11 +0000

    Prevent load of libnvvm.so at import time
    The previous commit caused libnvvm.so to be loaded at import time for
    various reasons, some of which could be resolved by reworking imports in
    the CUDA target, but the @cuda.jit-decoration of functions in
    numba.cuda.intrinsic_wrapper would force the load of libnvvm.so through
    an eventual call to compile_device_template. compile_device_template
    imports the CUDA target descriptor, which creates an empty module and
    needs to load NVVM to determine which version it is for the metadata
    addition.
    
    This commit resolves the issue by modifying the CUDA target descriptor
    such that it only initializes the typing and target contexts when they
    are required - compile_device_template only needs the typing context, so
    the target context initialization (and therefore the load of libnvvm.so)
    are avoided at import time. The modifications to the descriptor also
    bring it into line with the idiom used for the CPU and ufunc targets,
    which also construct a single instance of the target class rather than
    having class variables for the typing and target contexts.
    
    Unfortunately it is not yet possible to move the imports of the CUDA
    target descriptor up to the module level in numba.cuda.compiler, as this
    creates a circular import. This may be solvable with more effort :-)

--- numba/cuda/compiler.py
+++ numba/cuda/compiler.py
@@ -30,12 +30,10 @@ from .args import wrap_arg
 
 @global_compiler_lock
 def compile_cuda(pyfunc, return_type, args, debug=False, inline=False):
-    # First compilation will trigger the initialization of the CUDA backend.
-    from .descriptor import CUDATargetDesc
+    from .descriptor import cuda_target
+    typingctx = cuda_target.typingctx
+    targetctx = cuda_target.targetctx
 
-    typingctx = CUDATargetDesc.typingctx
-    targetctx = CUDATargetDesc.targetctx
-    # TODO handle debug flag
     flags = compiler.Flags()
     # Do not compile (generate native code), just lower (to LLVM)
     flags.set('no_compile')
@@ -249,7 +247,7 @@ def compile_device_template(pyfunc, debug=False, inline=False, opt=True):
     """Create a DeviceFunctionTemplate object and register the object to
     the CUDA typing context.
     """
-    from .descriptor import CUDATargetDesc
+    from .descriptor import cuda_target
 
     dft = DeviceFunctionTemplate(pyfunc, debug=debug, inline=inline, opt=opt)
 
@@ -275,7 +273,7 @@ def compile_device_template(pyfunc, debug=False, inline=False, opt=True):
             }
             return info
 
-    typingctx = CUDATargetDesc.typingctx
+    typingctx = cuda_target.typingctx
     typingctx.insert_user_function(dft, device_function_template)
     return dft
 
@@ -285,10 +283,9 @@ def compile_device(pyfunc, return_type, args, inline=True, debug=False):
 
 
 def declare_device_function(name, restype, argtypes):
-    from .descriptor import CUDATargetDesc
-
-    typingctx = CUDATargetDesc.typingctx
-    targetctx = CUDATargetDesc.targetctx
+    from .descriptor import cuda_target
+    typingctx = cuda_target.typingctx
+    targetctx = cuda_target.targetctx
     sig = typing.signature(restype, *argtypes)
     extfn = ExternFunction(name, sig)
 
@@ -869,9 +866,9 @@ class Dispatcher(_dispatcher.Dispatcher, serialize.ReduceMixin):
         self.targetoptions['extensions'] = \
             list(self.targetoptions.get('extensions', []))
 
-        from .descriptor import CUDATargetDesc
+        from .descriptor import cuda_target
 
-        self.typingctx = CUDATargetDesc.typingctx
+        self.typingctx = cuda_target.typingctx
 
         self._tm = default_type_manager
 
