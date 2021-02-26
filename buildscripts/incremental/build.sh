99389d7e5629e7f0c3a0847e5bdeeef2c0c1ced8
Stuart Archibald <stuart.archibald@googlemail.com>
2021-02-26 10:44:15 +0000

    Build script and CI cleaning
    As title.

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -15,5 +15,10 @@ python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS
 # (note we don't install to avoid problems with extra long Windows paths
 #  during distutils-dependent tests -- e.g. test_pycc)
 
+# Don't build with tbb support unless tbb testing was requested. Loading the
+# threading layers with an unsupported tbb raises a warning that conflicts with
+# tests counting warnings.
+if [ "$TEST_THREADING" != "tbb" ]; then DISABLE_TBB=1; fi
+
 # Install numba locally for use in `numba -s` sys info tool at test time
-python -m pip install --no-deps -e .
+NUMBA_DISABLE_TBB=$DISABLE_TBB python -m pip install --no-deps -e .
