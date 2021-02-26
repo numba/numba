c8bbe1f095cd658f0a82c2429adccdb433345462
Stuart Archibald <stuart.archibald@googlemail.com>
2021-02-26 11:05:29 +0000

    Move TBB logic to the right place
    As title

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -11,14 +11,15 @@ if [[ "$(uname -s)" == *"Linux"* ]] && [[ "$(uname -p)" == *"86"* ]]; then
 else
     EXTRA_BUILD_EXT_FLAGS=""
 fi
-python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS
-# (note we don't install to avoid problems with extra long Windows paths
-#  during distutils-dependent tests -- e.g. test_pycc)
 
 # Don't build with tbb support unless tbb testing was requested. Loading the
 # threading layers with an unsupported tbb raises a warning that conflicts with
 # tests counting warnings.
-if [ "$TEST_THREADING" != "tbb" ]; then DISABLE_TBB=1; fi
+if [ "$TEST_THREADING" != "tbb" ]; then export NUMBA_DISABLE_TBB=1; fi
+
+python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS
+# (note we don't install to avoid problems with extra long Windows paths
+#  during distutils-dependent tests -- e.g. test_pycc)
 
 # Install numba locally for use in `numba -s` sys info tool at test time
-NUMBA_DISABLE_TBB=$DISABLE_TBB python -m pip install --no-deps -e .
+python -m pip install --no-deps -e .
