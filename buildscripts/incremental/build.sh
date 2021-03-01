6d524fecafda4247d913acc083beb0f195e5ca53
Stuart Archibald <stuart.archibald@googlemail.com>
2021-03-01 17:53:35 +0000

    Remove redundant TBB check.
    As title.

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -12,11 +12,6 @@ else
     EXTRA_BUILD_EXT_FLAGS=""
 fi
 
-# Don't build with tbb support unless tbb testing was requested. Loading the
-# threading layers with an unsupported tbb raises a warning that conflicts with
-# tests counting warnings.
-if [ "$TEST_THREADING" != "tbb" ]; then export NUMBA_DISABLE_TBB=1; fi
-
 python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS
 # (note we don't install to avoid problems with extra long Windows paths
 #  during distutils-dependent tests -- e.g. test_pycc)
