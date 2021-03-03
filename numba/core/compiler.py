092def7655227b21029fb55d80fa3d556f452405
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2021-03-03 16:54:14 -0600

    A quick hack for a stack to keep track of target option. Make NRT flag inherit from parent.

--- numba/core/compiler.py
+++ numba/core/compiler.py
@@ -372,41 +372,44 @@ class CompilerBase(object):
         """
         Populate and run compiler pipeline
         """
-        pms = self.define_pipelines()
-        for pm in pms:
-            pipeline_name = pm.pipeline_name
-            func_name = "%s.%s" % (self.state.func_id.modname,
-                                   self.state.func_id.func_qualname)
+        from numba.core import utils
+        with utils.ConfigStack().enter(self.state.flags):
 
-            event("Pipeline: %s for %s" % (pipeline_name, func_name))
-            self.state.metadata['pipeline_times'] = {pipeline_name:
-                                                     pm.exec_times}
-            is_final_pipeline = pm == pms[-1]
-            res = None
-            try:
-                pm.run(self.state)
-                if self.state.cr is not None:
+            pms = self.define_pipelines()
+            for pm in pms:
+                pipeline_name = pm.pipeline_name
+                func_name = "%s.%s" % (self.state.func_id.modname,
+                                    self.state.func_id.func_qualname)
+
+                event("Pipeline: %s for %s" % (pipeline_name, func_name))
+                self.state.metadata['pipeline_times'] = {pipeline_name:
+                                                        pm.exec_times}
+                is_final_pipeline = pm == pms[-1]
+                res = None
+                try:
+                    pm.run(self.state)
+                    if self.state.cr is not None:
+                        break
+                except _EarlyPipelineCompletion as e:
+                    res = e.result
                     break
-            except _EarlyPipelineCompletion as e:
-                res = e.result
-                break
-            except Exception as e:
-                self.state.status.fail_reason = e
-                if is_final_pipeline:
-                    raise e
-        else:
-            raise CompilerError("All available pipelines exhausted")
+                except Exception as e:
+                    self.state.status.fail_reason = e
+                    if is_final_pipeline:
+                        raise e
+            else:
+                raise CompilerError("All available pipelines exhausted")
 
-        # Pipeline is done, remove self reference to release refs to user code
-        self.state.pipeline = None
+            # Pipeline is done, remove self reference to release refs to user code
+            self.state.pipeline = None
 
-        # organise a return
-        if res is not None:
-            # Early pipeline completion
-            return res
-        else:
-            assert self.state.cr is not None
-            return self.state.cr
+            # organise a return
+            if res is not None:
+                # Early pipeline completion
+                return res
+            else:
+                assert self.state.cr is not None
+                return self.state.cr
 
     def _compile_bytecode(self):
         """
