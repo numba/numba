26a8caf57e141adcc006ddf921cb3c3096fb639b
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2021-04-07 13:52:46 +0100

    flake8

--- numba/core/extending_hardware.py
+++ numba/core/extending_hardware.py
@@ -4,7 +4,8 @@ from numba.core.registry import TargetRegistry, CPUDispatcher
 from threading import local as tls
 
 _active_context = tls()
-_active_context.default ='cpu'
+_active_context.default = 'cpu'
+
 
 class hardware_target(object):
     def __init__(self, name):
@@ -16,12 +17,15 @@ class hardware_target(object):
         _active_context.target = self.target
 
     def __exit__(self, ty, val, tb):
-        _active_context = self._orig_target
+        _active_context.target = self._orig_target
+
 
 def current_target():
     return getattr(_active_context, 'target', _active_context.default)
 
+
 hardware_registry = TargetRegistry()
+
 
 class JitDecorator(ABC):
 
@@ -33,31 +37,38 @@ class JitDecorator(ABC):
 class Target(ABC):
     """ Implements a hardware/pseudo-hardware target """
 
+
 class Generic(Target):
     """Mark the hardware target as generic, i.e. suitable for compilation on
     any target. All hardware must inherit from this.
     """
 
+
 class CPU(Generic):
     """Mark the hardware target as CPU.
     """
+
 
 class GPU(Generic):
     """Mark the hardware target as GPU, i.e. suitable for compilation on a GPU
     target.
     """
 
+
 class CUDA(GPU):
     """Mark the hardware target as CUDA.
     """
+
 
 class ROCm(GPU):
     """Mark the hardware target as ROCm.
     """
 
+
 class NPyUfunc(Target):
     """Mark the hardware target as a ufunc
     """
+
 
 hardware_registry['generic'] = Generic
 hardware_registry['CPU'] = CPU
