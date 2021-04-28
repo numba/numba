cd36374a0be66d1768f9edae9e0ecbf4ac410221
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2021-04-28 12:51:52 -0500

    Remove _RedirectSubpackage. Use PEP-562.

--- numba/types/__init__.py
+++ numba/types/__init__.py
@@ -1,4 +1,7 @@
-import sys
-from numba.core.utils import _RedirectSubpackage
-sys.modules[__name__] = _RedirectSubpackage(locals(), "numba.core.types")
+import importlib
 
+
+def __getattr__(name):
+    # Uses PEP-562 but requires python>3.6
+    basemod = importlib.import_module(f"numba.core.types", __name__)
+    return getattr(basemod, name)
