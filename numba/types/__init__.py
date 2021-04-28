cb2c4e0f2859ece80a196a33642ab867a9ff0fe5
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2021-04-28 13:08:45 -0500

    fix

--- numba/types/__init__.py
+++ numba/types/__init__.py
@@ -3,7 +3,5 @@ import importlib
 
 def __getattr__(name):
     # Uses PEP-562 but requires python>3.6
-    try:
-        return importlib.import_module(f"numba.core.types.{name}", __name__)
-    except ModuleNotFoundError:
-        raise AttributeError
+    basemod = importlib.import_module(f"numba.core.types", __name__)
+    return getattr(basemod, name)
