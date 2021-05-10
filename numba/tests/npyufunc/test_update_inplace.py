3f160f51a25fae5db9c04b9703cb4da29a52f6cb
dmitryb <dmitry.belov@cubistsystematic.com>
2021-05-09 23:24:17 -0400

    Fixed line width and space to comply with flake8.

--- numba/tests/npyufunc/test_update_inplace.py
+++ numba/tests/npyufunc/test_update_inplace.py
@@ -12,6 +12,7 @@ def py_replace_2nd(x_t, y_1):
     for t in range(0, x_t.shape[0], 2):
         x_t[t] = y_1[0]
 
+
 class TestUpdateInplace(TestCase):
 
     def _run_test_for_gufunc(self, gufunc, assume_f4_works=True):
@@ -44,17 +45,21 @@ class TestUpdateInplace(TestCase):
     def test_update_inplace_with_cache(self):
         # test with writable_args
         gufunc = guvectorize(['void(f8[:], f8[:])'], '(t),()',
-                             nopython=True, writable_args=(0,), cache=True)(py_replace_2nd)
+                             nopython=True, writable_args=(0,),
+                             cache=True)(py_replace_2nd)
         # 2nd time it is loaded from cache
         gufunc = guvectorize(['void(f8[:], f8[:])'], '(t),()',
-                             nopython=True, writable_args=(0,), cache=True)(py_replace_2nd)
+                             nopython=True, writable_args=(0,),
+                             cache=True)(py_replace_2nd)
         self._run_test_for_gufunc(gufunc)
 
     def test_update_inplace_parallel(self):
         # test with writable_args
         gufunc = guvectorize(['void(f8[:], f8[:])'], '(t),()',
-                             nopython=True, writable_args=(0,), target='parallel')(py_replace_2nd)
+                             nopython=True, writable_args=(0,),
+                             target='parallel')(py_replace_2nd)
         self._run_test_for_gufunc(gufunc)
+
 
 if __name__ == '__main__':
     unittest.main()
