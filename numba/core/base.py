f138ff585430d7a0c62fdf94dcaca1f273040eac
Kalyan reddy kasireddy <kalyan.ben10@live.com>
2021-05-21 22:08:37 +0530

    create_module to raise NotImplementedError

--- numba/core/base.py
+++ numba/core/base.py
@@ -1128,8 +1128,12 @@ class BaseContext(object):
 
     def create_module(self, name):
         """Create a LLVM module
+        
+        The default implementation in BaseContext always raises a
+        ``NotImplementedError`` exception. SubClass should implement 
+        this method.
         """
-        return llvmir.Module(name)
+        raise NotImplementedError
 
     @property
     def active_code_library(self):
