c05bf7e058337482944034253e5f0849611ea9cf
Guilherme Leobas <guilhermeleobas@gmail.com>
2021-05-28 14:54:56 -0300

    remove np.isclose implementation

--- numba/np/arraymath.py
+++ numba/np/arraymath.py
@@ -866,48 +866,6 @@ def np_isscalar(num):
     return impl
 
 
-@register_jitable
-def _within_tol(a, b, rtol, atol):
-    return np.less_equal(np.abs(a - b), atol + rtol * np.abs(b))
-
-
-@overload(np.isclose)
-def np_isclose(a, b, rtol=1e-05, atol=1e-08, equal_nan=False):
-    # Based on NumPy impl.
-    # https://github.com/numpy/numpy/blob/d9b1e32cb8ef90d6b4a47853241db2a28146a57d/numpy/core/numeric.py#L2180-L2292
-
-    if not(type_can_asarray(a) and type_can_asarray(b)):
-        raise TypingError("Inputs for `np.isclose` must be array-like.")
-
-    def impl(a, b, rtol=1e-05, atol=1e-08, equal_nan=False):
-        xfin = np.asarray(np.isfinite(a))
-        yfin = np.asarray(np.isfinite(b))
-        x, y = np.asarray(a), np.asarray(b)
-        if np.all(xfin) and np.all(yfin):
-            return _within_tol(x, y, rtol, atol)
-        else:
-            finite = xfin & yfin
-            r = np.zeros_like(finite)
-            x = x * np.ones_like(r)
-            y = y * np.ones_like(r)
-
-            r = _within_tol(x, y, rtol, atol)
-            # Negate every element that is not finite
-            r &= (xfin & yfin)
-
-            # Check for equality of infinite values
-            r |= (x == np.asarray(np.inf)) & (y == np.asarray(np.inf))
-            r |= (x == np.asarray(-np.inf)) & (y == np.asarray(-np.inf))
-
-            if equal_nan:
-                xnan = np.asarray(np.isnan(a))
-                ynan = np.asarray(np.isnan(b))
-                return r | (xnan & ynan)
-            else:
-                return r
-    return impl
-
-
 def is_np_inf_impl(x, out, fn):
 
     # if/else branch should be unified after PR #5606 is merged
