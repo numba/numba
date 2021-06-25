d89e73369c756c5a6d9bdf366bd84e7fec901aac
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2021-06-24 19:04:15 -0500

    Fix `inline_closurecall` may not be imported

--- numba/core/ir_utils.py
+++ numba/core/ir_utils.py
@@ -1738,6 +1738,7 @@ def get_ir_of_code(glbls, fcode):
     rewrites.rewrite_registry.apply('before-inference', state)
     # call inline pass to handle cases like stencils and comprehensions
     swapped = {} # TODO: get this from diagnostics store
+    import numba.core.inline_closurecall
     inline_pass = numba.core.inline_closurecall.InlineClosureCallPass(
         ir, numba.core.cpu.ParallelOptions(False), swapped)
     inline_pass.run()
