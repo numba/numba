a3a940d1b65f36c171b8e8dd2d48f7e5b106b115
George Ebberson <george.ebberson@gmail.com>
2021-07-18 17:51:49 +0100

    Add if clause for parfor tests.

--- numba/core/ir_utils.py
+++ numba/core/ir_utils.py
@@ -478,7 +478,13 @@ def add_offset_to_labels(blocks, offset):
     new_blocks = {}
     for l, b in blocks.items():
         # some parfor last blocks might be empty
-        term = b.body[-1] if b.body else None
+        term = None
+        if b.body:
+            term = b.body[-1]
+            for inst in b.body:
+                for T, f in add_offset_to_labels_extensions.items():
+                    if isinstance(inst, T):
+                        f_max = f(inst, offset)
         if isinstance(term, ir.Jump):
             b.body[-1] = ir.Jump(term.target + offset, term.loc)
         if isinstance(term, ir.Branch):
