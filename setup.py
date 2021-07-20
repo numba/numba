a637bc7cde24d0b8174ca6610a4574252b0bcf6c
Valentin Haenel <vhaenel@anaconda.com>
2021-07-20 10:34:57 +0200

    bump llvmlite dependency to 0.38.0dev0 for Numba 0.55.0dev0
    As title

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ min_python_version = "3.7"
 max_python_version = "3.10"  # exclusive
 min_numpy_build_version = "1.11"
 min_numpy_run_version = "1.17"
-min_llvmlite_version = "0.37.0.dev0"
-max_llvmlite_version = "0.38"
+min_llvmlite_version = "0.38.0dev0"
+max_llvmlite_version = "0.39"
 
 if sys.platform.startswith('linux'):
     # Patch for #2555 to make wheels without libpython
