ec01acdbf837795b410bfce0cb238b5b3233a207
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2021-07-21 10:00:33 -0500

    Use loc in warning class instead

--- numba/core/ssa.py
+++ numba/core/ssa.py
@@ -268,11 +268,9 @@ class _FreshVarHandler(_BaseHandler):
                 newtarget = assign.target
                 _logger.debug("first assign: %s", newtarget)
                 if newtarget.name not in scope.localvars:
-                    wmsg = (
-                        f"variable name {newtarget.name!r} not in scope "
-                        f"in {assign} on line {assign.loc}",
-                    )
-                    warnings.warn(errors.NumbaIRAssumptionWarning(wmsg))
+                    wmsg = f"variable name {newtarget.name!r} not in scope."
+                    warnings.warn(errors.NumbaIRAssumptionWarning(wmsg,
+                                  loc=assign.loc))
             else:
                 newtarget = scope.redefine(assign.target.name, loc=assign.loc)
             assign = ir.Assign(
