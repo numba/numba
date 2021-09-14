4719d55d27d9ab5bcab688bd29cdae5e8e17a5ad
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2021-09-14 15:04:19 +0100

    Handle with lift failure mode in >= py38

--- numba/tests/test_withlifting.py
+++ numba/tests/test_withlifting.py
@@ -257,7 +257,8 @@ class TestLiftCall(BaseTestWithLifting):
         self.check_same_semantic(liftcall3)
 
     def test_liftcall4(self):
-        accept = (errors.TypingError, errors.NumbaRuntimeError)
+        accept = (errors.TypingError, errors.NumbaRuntimeError,
+                  errors.NumbaValueError)
         with self.assertRaises(accept) as raises:
             njit(liftcall4)()
         # Known error.  We only support one context manager per function
