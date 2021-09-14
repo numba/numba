99bd664a88a79fd117e6705928c030abd66c2765
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2021-09-14 12:41:38 +0100

    Fix up NumPy support test

--- numba/tests/test_numpy_support.py
+++ numba/tests/test_numpy_support.py
@@ -10,6 +10,7 @@ import numpy as np
 
 import unittest
 from numba.core import types
+from numba.core.errors import NumbaNotImplementedError
 from numba.tests.support import TestCase
 from numba.tests.enum_usecases import Shake, RequestError
 from numba.np import numpy_support
@@ -67,7 +68,7 @@ class TestFromDtype(TestCase):
         # Non-native alignments are unsupported (except for 1-byte types)
         foreign_align = '>' if sys.byteorder == 'little' else '<'
         for letter in 'hHiIlLqQfdFD':
-            self.assertRaises(NotImplementedError, f,
+            self.assertRaises(NumbaNotImplementedError, f,
                               np.dtype(foreign_align + letter))
 
     def test_string_types(self):
