bff18ffd3bf928061f7b68f8bdffe9b7b277946f
Valentin Haenel <vhaenel@anaconda.com>
2021-09-24 14:25:32 +0200

    enable peephole for list_to_tuple in 3.10
    As title

--- numba/core/interpreter.py
+++ numba/core/interpreter.py
@@ -381,7 +381,7 @@ class Interpreter(object):
         # post process the IR to rewrite opcodes/byte sequences that are too
         # involved to risk handling as part of direct interpretation
         peepholes = []
-        if PYVERSION == (3, 9):
+        if PYVERSION in [(3, 9), (3, 10)]:
             peepholes.append(peep_hole_list_to_tuple)
         peepholes.append(peep_hole_delete_with_exit)
 
