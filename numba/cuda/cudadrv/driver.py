eb959b242592224540c8b9a84cf096312f887126
Graham Markall <gmarkall@nvidia.com>
2021-09-28 12:01:21 +0100

    Add file extension map for CUDA Python
    CUDA Python test results are now:
    
    ```
    Ran 1201 tests in 73.284s
    
    FAILED (failures=1, errors=14, skipped=10, expected failures=2)
    ```

--- numba/cuda/cudadrv/driver.py
+++ numba/cuda/cudadrv/driver.py
@@ -2514,15 +2514,25 @@ def launch_kernel(cufunc_handle,
                               extra)
 
 
-# XXX: Need a CUDA Python version of this
-FILE_EXTENSION_MAP = {
-    'o': enums.CU_JIT_INPUT_OBJECT,
-    'ptx': enums.CU_JIT_INPUT_PTX,
-    'a': enums.CU_JIT_INPUT_LIBRARY,
-    'lib': enums.CU_JIT_INPUT_LIBRARY,
-    'cubin': enums.CU_JIT_INPUT_CUBIN,
-    'fatbin': enums.CU_JIT_INPUT_FATBINAR,
-}
+if config.CUDA_USE_CUDA_PYTHON:
+    jitty = cuda_driver.CUjitInputType
+    FILE_EXTENSION_MAP = {
+        'o': jitty.CU_JIT_INPUT_OBJECT,
+        'ptx': jitty.CU_JIT_INPUT_PTX,
+        'a': jitty.CU_JIT_INPUT_LIBRARY,
+        'lib': jitty.CU_JIT_INPUT_LIBRARY,
+        'cubin': jitty.CU_JIT_INPUT_CUBIN,
+        'fatbin': jitty.CU_JIT_INPUT_FATBINARY,
+    }
+else:
+    FILE_EXTENSION_MAP = {
+        'o': enums.CU_JIT_INPUT_OBJECT,
+        'ptx': enums.CU_JIT_INPUT_PTX,
+        'a': enums.CU_JIT_INPUT_LIBRARY,
+        'lib': enums.CU_JIT_INPUT_LIBRARY,
+        'cubin': enums.CU_JIT_INPUT_CUBIN,
+        'fatbin': enums.CU_JIT_INPUT_FATBINAR,
+    }
 
 
 class Linker(metaclass=ABCMeta):
