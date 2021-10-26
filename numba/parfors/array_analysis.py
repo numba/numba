b6835f8a51d630d275131b23b5666006aa863bab
Valentin Haenel <vhaenel@anaconda.com>
2021-10-26 16:21:29 +0200

    satisfy Parfors typemap too
    As title

--- numba/parfors/array_analysis.py
+++ numba/parfors/array_analysis.py
@@ -1297,7 +1297,8 @@ class ArrayAnalysis(object):
             print("analyze_inst:", inst)
         if isinstance(inst, ir.Assign):
             lhs = inst.target
-            typ = self.typemap[lhs.name]
+            typ = (types.intp if lhs.name.startswith("pop_block_info")
+                   else self.typemap[lhs.name])
             shape = None
             if isinstance(typ, types.ArrayCompatible) and typ.ndim == 0:
                 shape = ()
