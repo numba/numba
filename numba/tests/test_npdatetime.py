75b14f6e0ab76598768e2c5e2161956302d54366
Nicholas J Riasanovsky <nick@bodo.ai>
2021-11-29 09:10:54 -0500

    fixed missing import

--- numba/tests/test_npdatetime.py
+++ numba/tests/test_npdatetime.py
@@ -17,7 +17,7 @@ from numba import jit, vectorize, njit
 from numba.np.numpy_support import numpy_version
 from numba.core import types, config
 from numba.core.errors import TypingError
-from numba.tests.support import TestCase, tag
+from numba.tests.support import TestCase, tag, skip_parfors_unsupported
 from numba.np import npdatetime_helpers, numpy_support
 
 TIMEDELTA_M = np.dtype('timedelta64[M]')
