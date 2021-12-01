6b2fb4e98ed310ccee0cba26843657424566b510
esc <esc@users.noreply.github.com>
2021-12-01 18:24:46 +0100

    refactor `_rewrite_return`
    * Cleanup the documentation
    * Use a slice based implementation for block splitting

--- numba/core/transforms.py
+++ numba/core/transforms.py
@@ -623,34 +623,97 @@ def find_setupwiths(func_ir):
 
 
 def _rewrite_return(func_ir, target_block_label):
+    """Rewrite a return block inside a with statement.
+
+    Arguments
+    ---------
+
+    func_ir: Function IR
+      the CFG to transform
+    target_block_label: int
+      the block index/label of the block containing the POP_BLOCK statement
+
+
+    This implements a CFG transformation to insert a block between two other
+    blocks.
+
+    The input situation is:
+
+    ┌───────────────┐
+    │   top         │
+    │   POP_BLOCK   │
+    │   bottom      │
+    └───────┬───────┘
+            │
+    ┌───────▼───────┐
+    │               │
+    │    RETURN     │
+    │               │
+    └───────────────┘
+
+    If such a pattern is detected in IR, it means there is a `return` statement
+    within a `with` context. The basic idea is to rewrite the CFG as follows:
+
+    ┌───────────────┐
+    │   top         │
+    │   POP_BLOCK   │
+    │               │
+    └───────┬───────┘
+            │
+    ┌───────▼───────┐
+    │               │
+    │     bottom    │
+    │               │
+    └───────┬───────┘
+            │
+    ┌───────▼───────┐
+    │               │
+    │    RETURN     │
+    │               │
+    └───────────────┘
+
+    We split the block that contains the `POP_BLOCK` statement into two blocks.
+    Everything from the beginning of the block up to and including the
+    `POP_BLOCK` statement is considered the 'top' and everything below is
+    considered 'bottom'. Finally the jump statements are re-wired to make sure
+    the CFG remains valid.
+
+    """
+    # the block itself from the index
     target_block = func_ir.blocks[target_block_label]
+    # get the index of the block containing the return
     target_block_successor_label = target_block.terminator.get_targets()[0]
+    # the return block
     target_block_successor = func_ir.blocks[target_block_successor_label]
+
     # create the new return block with an appropriate label
     max_label = ir_utils.find_max_label(func_ir.blocks)
-    # use 1000 to make sure we move outside of the range
     new_label = max_label + 1
     # create the new return block
-    new_block_loc = func_ir.blocks[max_label].loc
+    new_block_loc = target_block_successor.loc
     new_block_scope = ir.Scope(None, loc=new_block_loc)
     new_block = ir.Block(new_block_scope, loc=new_block_loc)
 
-    # split the block containing the POP_BLOCK into top and bottom
-    found = False
+    # Split the block containing the POP_BLOCK into top and bottom
+    # Block must be of the form:
+    # -----------------
+    # <some stmts>
+    # POP_BLOCK
+    # <some more stmts>
+    # JUMP
+    # -----------------
     top_body, bottom_body = [], []
-    for stmt in target_block.body:
-        if ir_utils.is_pop_block(stmt):
-            top_body.append(stmt)
-            top_body.append(ir.Jump(target_block_successor_label,
-                                             target_block.loc))
-            found = True
-        elif isinstance(stmt, ir.Jump):
-            bottom_body.append(ir.Jump(new_label, target_block.loc))
-        else:
-            if not found:
-                top_body.append(stmt)
-            else:
-                bottom_body.append(stmt)
+    pop_blocks = [*target_block.find_insts(ir.PopBlock)]
+    assert len(pop_blocks) == 1
+    assert len([*target_block.find_insts(ir.Jump)]) == 1
+    assert isinstance(target_block.body[-1], ir.Jump)
+    pb_marker = pop_blocks[0]
+    pb_is = target_block.body.index(pb_marker)
+    top_body.extend(target_block.body[:pb_is])
+    top_body.append(ir.Jump(target_block_successor_label, target_block.loc))
+    bottom_body.extend(target_block.body[pb_is:-1])
+    bottom_body.append(ir.Jump(new_label, target_block.loc))
+
     # get the contents of the return block
     return_body = func_ir.blocks[target_block_successor_label].body
     # finally, re-assign all blocks
