440724e79423fc9df4bf4dcc5a75b3952a793d29
esc <esc@users.noreply.github.com>
2021-12-10 13:11:54 +0100

    add plutil magic and special case OSX
    As title

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -12,11 +12,14 @@ else
     EXTRA_BUILD_EXT_FLAGS=""
 fi
 
-
-export MACOSX_SDK_VERSION=10.15
-SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
-OSX_SDK_DIR="$(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs"
-USING_SYSTEM_SDK_DIR=1
+if [[ $(uname) == "Darwin" ]]; then
+    export MACOSX_SDK_VERSION=10.15
+    SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+    OSX_SDK_DIR="$(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs"
+    USING_SYSTEM_SDK_DIR=1
+    plutil -replace MinimumSDKVersion -string ${MACOSX_SDK_VERSION} $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
+    plutil -replace DTSDKName -string macosx${MACOSX_SDK_VERSION}internal $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
+fi
 python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS
 # (note we don't install to avoid problems with extra long Windows paths
 #  during distutils-dependent tests -- e.g. test_pycc)
