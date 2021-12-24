25df6460d43001b435bd4e59af4811bbb745abf2
franz <franz.dominik.haas@gmail.com>
2021-12-24 20:41:56 +0100

    fixed flake

--- numba/tests/test_entrypoints.py
+++ numba/tests/test_entrypoints.py
@@ -113,7 +113,7 @@ class TestEntrypoints(TestCase):
                 'entry_points',
                 return_value={'numba_extensions': (my_entrypoint,)},
             ), warnings.catch_warnings(record=True) as w:
-                    entrypoints.init_all()
+                entrypoints.init_all()
             bad_str = "Numba extension '_test_numba_bad_extension:init_func'"
             for x in w:
                 if bad_str in str(x):
