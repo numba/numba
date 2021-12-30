79777a0fe8ccbaa28794e7f1a5243c0f5306dce3
Benjamin Graham <benwilliamgraham@gmail.com>
2021-12-29 20:13:45 -0500

    Added test

--- numba/tests/test_enums.py
+++ numba/tests/test_enums.py
@@ -162,6 +162,19 @@ class TestIntEnum(BaseEnumTest, TestCase):
         for member in IntEnumWithNegatives:
             self.assertPreciseEqual(pyfun(member), cfunc(member))
 
+    def test_int_shape_cast(self):
+        def pyfun_empty(x):
+            return np.empty((x, x), dtype='int64').fill(-1)
+        def pyfun_zeros(x):
+            return np.zeros((x, x), dtype='int64')
+        def pyfun_ones(x):
+            return np.ones((x, x), dtype='int64')
+        for pyfun in [pyfun_empty, pyfun_zeros, pyfun_ones]:
+            cfunc = jit(nopython=True)(pyfun)
+            for member in IntEnumWithNegatives:
+                if member >= 0:
+                    self.assertPreciseEqual(pyfun(member), cfunc(member))
+
 
 if __name__ == '__main__':
     unittest.main()
