d089b866732269cb3f526d4acf54243a11ad425c
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-01-25 11:53:07 +0000

    Add test to exercise numba -g CLI flag
    As title.

--- numba/misc/numba_gdbinfo.py
+++ numba/misc/numba_gdbinfo.py
@@ -1,84 +1,143 @@
 """Module for displaying information about Numba's gdb set up"""
+from collections import namedtuple
+import os
+import re
+import shutil
+import subprocess
+from textwrap import dedent
+from numba import config
+
+# Container for the output of the gdb info data collection
+_fields = ('binary_loc, extension_loc, py_ver, np_ver, supported')
+_gdb_info = namedtuple('_gdb_info', _fields)
 
 
-def _run_cmd(cmdline, env):
-    """Runs cmdline (list of string args) in a subprocess with environment dict
-    env.
-    """
-    import subprocess
-    import threading
-    popen = subprocess.Popen(cmdline,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE,
-                             env=env)
-    # finish in 10 seconds or kill it
-    timeout = threading.Timer(10, popen.kill)
-    try:
-        timeout.start()
-        out, err = popen.communicate()
-        if popen.returncode != 0:
-            raise AssertionError(
-                "process failed with code %s: stderr follows\n%s\n" %
-                (popen.returncode, err.decode()))
-        return out.decode(), err.decode()
-    finally:
-        timeout.cancel()
-    return None, None
+class _GDBTestWrapper():
+    """Wraps the gdb binary and has methods for checking what the gdb binary
+    has support for (Python and NumPy)."""
+
+    def __init__(self,):
+        gdb_binary = shutil.which(config.GDB_BINARY)
+        if gdb_binary is None:
+            msg = ("No valid binary could be found for gdb named: "
+                   f"{config.GDB_BINARY}")
+            raise ValueError(msg)
+        self._gdb_binary = gdb_binary
+
+    def _run_cmd(self, cmd=()):
+        gdb_call = [self.gdb_binary, '-q',]
+        for x in cmd:
+            gdb_call.append('-ex')
+            gdb_call.append(x)
+        gdb_call.extend(['-ex', 'q'])
+        return subprocess.run(gdb_call, capture_output=True, timeout=10,
+                              text=True)
+
+    @property
+    def gdb_binary(self):
+        return self._gdb_binary
+
+    @classmethod
+    def success(cls, status):
+        return status.returncode == 0
+
+    def check_launch(self):
+        """Checks that gdb will launch ok"""
+        return self._run_cmd()
+
+    def check_python(self):
+        cmd = ("python from __future__ import print_function; "
+               "import sys; print(sys.version_info[:2])")
+        return self._run_cmd((cmd,))
+
+    def check_numpy(self):
+        cmd = ("python from __future__ import print_function; "
+               "import types; import numpy; "
+               "print(isinstance(numpy, types.ModuleType))")
+        return self._run_cmd((cmd,))
+
+    def check_numpy_version(self):
+        cmd = ("python from __future__ import print_function; "
+               "import types; import numpy;"
+               "print(numpy.__version__)")
+        return self._run_cmd((cmd,))
 
 
-def display_gdbinfo(sep_pos=45):
+def collect_gdbinfo():
     """Prints information to stdout about the gdb setup that Numba has found"""
-    import os
-    import re
-    from textwrap import dedent
-    from numba import config
 
-    print('-' * 80)
-    fmt = f'%-{sep_pos}s : %-s'
+    # Check gdb exists
+    gdb_wrapper = _GDBTestWrapper()
 
+    # State flags:
+    gdb_has_python = False
+    gdb_has_numpy = False
+    gdb_python_version = 'No Python support'
+    gdb_python_numpy_version = "No NumPy support"
+
+    # Check gdb works
+    status = gdb_wrapper.check_launch()
+    if not gdb_wrapper.success(status):
+        msg = (f"gdb at '{gdb_wrapper.gdb_binary}' does not appear to work."
+               f"\nstdout: {status.stdout}\nstderr: {status.stderr}")
+        raise ValueError(msg)
+
+    # Got this far, so gdb works, start checking what it supports
+    status = gdb_wrapper.check_python()
+    if gdb_wrapper.success(status):
+        version_match = re.match(r'\((\d+),\s+(\d+)\)', status.stdout.strip())
+        if version_match is not None:
+            pymajor, pyminor = version_match.groups()
+            gdb_python_version = f"{pymajor}.{pyminor}"
+            gdb_has_python = True
+
+            status = gdb_wrapper.check_numpy()
+            if gdb_wrapper.success(status):
+                if "Traceback" not in status.stderr.strip():
+                    if status.stdout.strip() == 'True':
+                        gdb_has_numpy = True
+                        gdb_python_numpy_version = "Unknown"
+                        # NumPy is present find the version
+                        status = gdb_wrapper.check_numpy_version()
+                        if gdb_wrapper.success(status):
+                            if "Traceback" not in status.stderr.strip():
+                                gdb_python_numpy_version = status.stdout.strip()
+
+    # Work out what level of print-extension support is present in this gdb
+    if gdb_has_python:
+        if gdb_has_numpy:
+            print_ext_supported = "Full (Python and NumPy supported)"
+        else:
+            print_ext_supported = "Partial (Python only, no NumPy support)"
+    else:
+        print_ext_supported = "None"
+
+    # Work out print ext location
     print_ext_file = "gdb_print_extension.py"
     print_ext_path = os.path.join(os.path.dirname(__file__), print_ext_file)
 
-    gdb_call = [config.GDB_BINARY, "-q", "-ex",]
+    # return!
+    return _gdb_info(gdb_wrapper.gdb_binary, print_ext_path, gdb_python_version,
+                     gdb_python_numpy_version, print_ext_supported)
 
-    cmd = gdb_call + [("python from __future__ import print_function; "
-                       "import sys; print(sys.version_info[:2])"), "-ex", "q"]
-    stderr, stdout = _run_cmd(cmd, os.environ)
-    version_match = re.match(r'\((\d+),\s+(\d+)\)', stderr.strip())
-    if version_match is None:
-        gdb_python_version = 'No Python support'
-        gdb_python_numpy_version = None
-    else:
-        pymajor, pyminor = version_match.groups()
-        gdb_python_version = f"{pymajor}.{pyminor}"
 
-        cmd = gdb_call + [("python from __future__ import print_function; "
-                           "import types; import numpy; "
-                           "print(isinstance(numpy, types.ModuleType))"),
-                          "-ex", "q"]
-
-        stderr, stdout = _run_cmd(cmd, os.environ)
-        if stderr.strip() == 'True':
-            # NumPy is present find the version
-            cmd = gdb_call + [("python from __future__ import print_function; "
-                               "import types; import numpy;"
-                               "print(numpy.__version__)"),
-                              "-ex", "q"]
-            stderr, stdout = _run_cmd(cmd, os.environ)
-            gdb_python_numpy_version = stderr.strip()
-
+def display_gdbinfo(sep_pos=45):
+    """Displays the infomation collected by collect_gdbinfo.
+    """
+    gdb_info = collect_gdbinfo()
+    print('-' * 80)
+    fmt = f'%-{sep_pos}s : %-s'
     # Display the information
-    print(fmt % ("Binary location", config.GDB_BINARY))
-    print(fmt % ("Print extension location", print_ext_path))
-    print(fmt % ("Python version", gdb_python_version))
-    print(fmt % ("NumPy version", gdb_python_numpy_version))
-    print_ext_supported = gdb_python_numpy_version is not None
-    print(fmt % ("Numba printing extension supported", print_ext_supported))
+    print(fmt % ("Binary location", gdb_info.binary_loc))
+    print(fmt % ("Print extension location", gdb_info.extension_loc))
+    print(fmt % ("Python version", gdb_info.py_ver))
+    print(fmt % ("NumPy version", gdb_info.np_ver))
+    print(fmt % ("Numba printing extension support", gdb_info.supported))
 
     print("")
     print("To load the Numba gdb printing extension, execute the following "
           "from the gdb prompt:")
-    print(f"\nsource {print_ext_path}\n")
+    print(f"\nsource {gdb_info.extension_loc}\n")
     print('-' * 80)
     warn = """
     =============================================================
