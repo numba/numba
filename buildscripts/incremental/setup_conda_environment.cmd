87c7695b63aea10acba1d460cf23fb97eb093a31
esc <esc@users.noreply.github.com>
2022-01-26 19:15:05 +0100

    pin for windows too
    As title

--- buildscripts/incremental/setup_conda_environment.cmd
+++ buildscripts/incremental/setup_conda_environment.cmd
@@ -26,7 +26,7 @@ conda create -n %CONDA_ENV% -q -y python=%PYTHON% numpy=%NUMPY% cffi pip scipy j
 
 call activate %CONDA_ENV%
 @rem Install latest llvmlite build
-%CONDA_INSTALL% -c numba/label/dev llvmlite
+%CONDA_INSTALL% -c numba/label/dev llvmlite=0.38
 @rem Install dependencies for building the documentation
 if "%BUILD_DOC%" == "yes" (%CONDA_INSTALL% sphinx sphinx_rtd_theme pygments)
 @rem Install dependencies for code coverage (codecov.io)
