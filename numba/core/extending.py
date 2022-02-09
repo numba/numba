962761b3a0a4925d558d715b5a900741f75bddbd
max9111 <mtauscher@kkv-pc-53.unileoben.domain>
2022-02-09 17:22:11 +0100

    added import

--- numba/core/extending.py
+++ numba/core/extending.py
@@ -17,7 +17,7 @@ from numba.core.imputils import (  # noqa: F401
 from numba.core.datamodel import models   # noqa: F401
 from numba.core.datamodel import register_default as register_model  # noqa: F401, E501
 from numba.core.pythonapi import box, unbox, reflect, NativeValue  # noqa: F401
-from numba._helperlib import _import_cython_function  # noqa: F401
+from numba._helperlib import _import_cython_function,_import_f2py_function  # noqa: F401
 from numba.core.serialize import ReduceMixin
 
 
