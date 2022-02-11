05eac6c680a163c452259784a61be5ff5841fc36
Michael Wang <michaelwang0905@icloud.com>
2022-02-11 10:05:49 -0800

    Revert to static binding with `njit`.

--- numba/cuda/tests/cudapy/test_enums.py
+++ numba/cuda/tests/cudapy/test_enums.py
@@ -5,7 +5,7 @@ Test cases adapted from numba/tests/test_enums.py
 import numpy as np
 
 from numba import int8, int16, int32
-from numba import cuda, vectorize
+from numba import cuda, vectorize, njit
 from numba.cuda.testing import unittest, CUDATestCase, skip_on_cudasim
 from numba.tests.enum_usecases import (
     Color,
@@ -55,20 +55,15 @@ class EnumTest(CUDATestCase):
         self.assertPreciseEqual(expected, got)
 
     def test_return_from_device_func(self):
+        @njit
         def inner(pred):
             return Color.red if pred else Color.green
 
-        def _make_test_func(inner, jitter):
-            inner = jitter(inner)
+        def f(pred, out):
+            out[0] = inner(pred) == Color.red
+            out[1] = inner(not pred) == Color.green
 
-            def f(pred, out):
-                out[0] = inner(pred) == Color.red
-                out[1] = inner(not pred) == Color.green
-            return jitter(f)
-
-        f = _make_test_func(inner, lambda x:x)
-        cuda_f = _make_test_func(inner, cuda.jit())
-
+        cuda_f = cuda.jit(f)
         got = np.zeros((2,), dtype=np.bool_)
         expected = got.copy()
         f(True, expected)
