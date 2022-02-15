2a9c284bd1a48cb7cfc703bdc7e86b4a607d13b5
max9111 <markus.tauscher@gmail.com>
2022-02-15 13:08:14 +0100

    Update stack_arr.py
    flake8

--- numba/cpython/unsafe/stack_arr.py
+++ numba/cpython/unsafe/stack_arr.py
@@ -1,9 +1,10 @@
-#Workaround using stack-allocated arrays, 
+#Workaround using stack-allocated arrays,
 #faster than heap-allocated arrays.
 #Workaround for ctypes byref
 from numba import types,typeof,carray,farray,njit
 from numba.extending import intrinsic
 from numba.core import cgutils,errors
+
 
 @intrinsic
 def val_to_ptr(typingctx, data):
@@ -13,8 +14,8 @@ def val_to_ptr(typingctx, data):
     def impl(context, builder, signature, args):
         ptr = cgutils.alloca_once_value(builder,args[0])
         return ptr
-    sig = types.CPointer(typeof(data).instance_type)\
-        (typeof(data).instance_type)
+    sig = types.CPointer((typeof(data).instance_type)
+                         (typeof(data).instance_type))
     return sig, impl
 
 
@@ -32,26 +33,24 @@ def ptr_to_val(typingctx, data):
 
 @intrinsic
 def intr_stack_empty_alloc(typingctx,shape,dtype):
-    size=1
+    size = 1
     if isinstance(shape,types.scalars.IntegerLiteral):
-        size=shape.literal_value
-        sig = types.CPointer(dtype.dtype)\
-              (types.int64,dtype)
-            
+        size = shape.literal_value
+        sig = types.CPointer(dtype.dtype)(types.int64,dtype)
+
     elif isinstance(shape,(types.containers.Tuple,
                            types.containers.UniTuple)):
         for i in range(len(shape)):
-            size*=shape[i].literal_value
-        
-        sig = types.CPointer(dtype.dtype)\
-              (typeof(shape).instance_type,dtype)
+            size *= shape[i].literal_value
+
+        sig = types.CPointer(dtype.dtype)(typeof(shape).instance_type,dtype)
     else:
         raise errors.TypingError(
-            "Shape must be IntegerLiteral "+ \
+            "Shape must be IntegerLiteral " +
             "or a tuple of IntegerLiterals")
-    
+
     def impl(context, builder, signature, args):
-        ty=context.get_value_type(dtype.dtype)
+        ty = context.get_value_type(dtype.dtype)
         ptr = cgutils.alloca_once(builder, ty,size=size)
         return ptr
     return sig, impl
@@ -65,8 +64,8 @@ def stack_empty(shape,dtype,order="C"):
     Please note: - Arrays allocated on the stack can't be returned.
                  - Stack size is limited
     """
-    arr_ptr=intr_stack_empty_alloc(shape,dtype)
-    if order =="C":
+    arr_ptr = intr_stack_empty_alloc(shape,dtype)
+    if order == "C":
         return carray(arr_ptr,shape)
     elif order == "F":
         return farray(arr_ptr,shape)
