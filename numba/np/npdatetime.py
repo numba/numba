07276e0975a122899b7ebd5be63e0577d4cc3105
Benjamin Graham <benwilliamgraham@gmail.com>
2022-02-18 16:49:45 -0500

    Fixed max

--- numba/np/npdatetime.py
+++ numba/np/npdatetime.py
@@ -851,5 +851,5 @@ def ol_hash_npdatetime(x):
 
 lower_builtin(npdatetime_helpers.datetime_minimum, types.NPDatetime, types.NPDatetime)(datetime_minimum_impl)
 lower_builtin(npdatetime_helpers.datetime_minimum, types.NPTimedelta, types.NPTimedelta)(datetime_minimum_impl)
-lower_builtin(npdatetime_helpers.datetime_minimum, types.NPDatetime, types.NPDatetime)(datetime_maximum_impl)
-lower_builtin(npdatetime_helpers.datetime_minimum, types.NPTimedelta, types.NPTimedelta)(datetime_maximum_impl)
+lower_builtin(npdatetime_helpers.datetime_maximum, types.NPDatetime, types.NPDatetime)(datetime_maximum_impl)
+lower_builtin(npdatetime_helpers.datetime_maximum, types.NPTimedelta, types.NPTimedelta)(datetime_maximum_impl)
