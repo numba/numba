1fea1b42cb12bd0d4ec37c731d9113e5118d0a6b
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-03-08 11:43:18 -0300

    Run DeadLoopElimination after SSA

--- numba/core/compiler.py
+++ numba/core/compiler.py
@@ -624,11 +624,11 @@ class DefaultPassBuilder(object):
 
         pm.add_pass(FindLiterallyCalls, "find literally calls")
         pm.add_pass(LiteralUnroll, "handles literal_unroll")
-        pm.add_pass(DeadLoopElimination, "remove dead loops")
 
         if state.flags.enable_ssa:
             pm.add_pass(ReconstructSSA, "ssa")
 
+        pm.add_pass(DeadLoopElimination, "remove dead loops")
         pm.add_pass(LiteralPropagationSubPipelinePass, "Literal propagation")
 
         pm.finalize()
