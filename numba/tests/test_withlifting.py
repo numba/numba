d1145529aee4614e4f3399d0bd994a5b25170e83
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-03-10 12:42:27 +0000

    Run the no fork on compile test in a subprocess.
    Forces the no fork on compile test to run in a subprocess to
    avoid state from already initialised modules.

--- numba/tests/test_withlifting.py
+++ numba/tests/test_withlifting.py
@@ -20,7 +20,7 @@ from numba import njit, typeof, objmode, types
 from numba.core.extending import overload
 from numba.tests.support import (MemoryLeak, TestCase, captured_stdout,
                                  skip_unless_scipy, needs_strace, linux_only,
-                                 strace)
+                                 strace, needs_subprocess)
 from numba.core.utils import PYVERSION
 from numba.experimental import jitclass
 import unittest
@@ -1227,9 +1227,13 @@ class TestMisc(TestCase):
 
     @linux_only
     @needs_strace
-    def test_no_fork_in_compilation(self):
+    @needs_subprocess
+    def test_no_fork_in_compilation_impl(self):
         # Checks that there is no fork/clone/execve during compilation, see
-        # issue #7881.
+        # issue #7881. This needs running in a subprocess as the offending fork
+        # call that triggered #7881 occurs on the first call to uuid1 as it's
+        # part if the initialisation process for that function (gets hardware
+        # address of machine).
 
         def force_compile():
             @njit('void()') # force compilation
@@ -1243,6 +1247,16 @@ class TestMisc(TestCase):
         # check that compilation does not trigger fork, clone or execve
         strace_data = strace(force_compile, syscalls)
         self.assertFalse(strace_data)
+
+    @linux_only
+    @needs_strace
+    def test_no_fork_in_compilation(self):
+        # Runs the test_no_fork_in_compilation_impl test in a subprocess
+        themod = f'numba.tests.test_withlifting'
+        testname = 'test_no_fork_in_compilation_impl'
+        self.subprocess_test_runner(test_module=themod,
+                                    test_class='TestMisc',
+                                    test_name=testname,)
 
 
 if __name__ == '__main__':
