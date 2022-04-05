1d34c554cb5044c7f466d36dde193bfc92f32792
esc <esc@users.noreply.github.com>
2022-04-05 12:36:04 +0200

    line wrap
    As title

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -55,7 +55,8 @@ set -v
 echo "PYTHON=$PYTHON"
 echo "VANILLA_INSTALL=$VANILLA_INSTALL"
 if [ "${VANILLA_INSTALL}" != "yes" ]; then
-    # Scipy, CFFI, jinja2, IPython, ipython_genutils and pygments are optional dependencies, but exercised in the test suite
+    # Scipy, CFFI, jinja2, IPython, ipython_genutils and pygments are optional
+    # dependencies, but exercised in the test suite
     # pexpect is used to run the gdb tests.
     # ipykernel is used for testing ipython behaviours.
     $CONDA_INSTALL ${EXTRA_CHANNELS} cffi jinja2 ipython ipykernel pygments pexpect
