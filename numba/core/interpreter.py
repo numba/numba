765068c888f7a8495a96e18b17ccf33bc45ab64b
Nicholas J Riasanovsky <nick@bodo.ai>
2022-04-18 11:44:09 -0400

    updated the implementation to account for definitions and simplify the code

--- numba/core/interpreter.py
+++ numba/core/interpreter.py
@@ -6,6 +6,7 @@ import logging
 
 from numba.core import errors, dataflow, controlflow, ir, config
 from numba.core.errors import NotDefinedError, error_extras
+from numba.core.ir_utils import get_definition, guard
 from numba.core.utils import (PYVERSION, BINOPS_TO_OPERATORS,
                               INPLACE_BINOPS_TO_OPERATORS,)
 from numba.core.byteflow import Flow, AdaptDFA, AdaptCFA
@@ -329,146 +330,247 @@ def peep_hole_fuse_dict_add_updates(func_ir):
     expressions with a constant build map.
     """
 
-    def build_new_buildmap(name, blk, old_lineno, new_items):
-        """
-        Create a new buildmap with a new set of items
-        but all the other info the same.
-        """
-        old_assign = blk.body[old_lineno]
-        old_target = old_assign.target
-        old_bm = old_assign.value
-        # Build the literals
-        literal_keys = []
-        # Track the values
-        values = []
-        for k, v in new_items:
-            key_def = func_ir.get_definition(k)
-            if isinstance(key_def, ir.Const):
-                literal_keys.append(key_def.value)
-            value_def = func_ir.get_definition(v)
-            if isinstance(value_def, ir.Const):
-                values.append(value_def.value)
-            else:
-                # Append unknown value if not a literal.
-                values.append(_UNKNOWN_VALUE(value_def))
-
-        value_indexes = {}
-        if len(literal_keys) == len(new_items):
-            # All keys must be literals to have any literal values.
-            literal_value = {
-                x: y for x, y in zip(literal_keys, values)
-            }
-            for i, k in enumerate(literal_keys):
-                value_indexes[k] = i
-        else:
-            literal_value = None
-
-        # Construct a new build map.
-        new_bm = ir.Expr.build_map(items=new_items,
-                                   size=len(new_items),
-                                   literal_value=literal_value,
-                                   value_indexes=value_indexes,
-                                   loc=old_bm.loc)
-
-        # Return a new assign.
-        return ir.Assign(
-            new_bm,
-            ir.Var(old_target.scope, name, old_target.loc),
-            new_bm.loc
-        )
+    # This algorithm fuses build_map expressions into the largest
+    # possible build map before usage. For example, if we have an
+    # IR that looks like this:
+    #
+    #   $d1 = build_map([])
+    #   $key = const("a")
+    #   $value = const(2)
+    #   $setitem_func = getattr($d1, "__setitem__")
+    #   $unused1 = call (setitem_func, ($key, $value))
+    #   $key2 = const("b")
+    #   $value2 = const(3)
+    #   $d2 = build_map([($key2, $value2)])
+    #   $update_func = getattr($d1, "update")
+    #   $unused2 = call ($update_func, ($d2,))
+    #   $othervar = None
+    #   $retvar = cast($othervar)
+    #   return $retvar
+    #
+    # Then the IR is rewritten so any __setitem__ and update operations are
+    # fused into the original buildmap. The new buildmap is then add to the
+    # last location  where it had previously had encountered a __setitem__,
+    # update, or build_map before any other uses.
+    # The new IR would look like:
+    #
+    #   $key = const("a")
+    #   $value = const(2)
+    #   $key2 = const("b")
+    #   $value2 = const(3)
+    #   $d2 = build_map([($key2, $value2)])
+    #   $d1 = build_map([($key, $value), ($key2, $value2)])
+    #   $othervar = None
+    #   $retvar = cast($othervar)
+    #   return $retvar
+    #
+    # Notice how we don't push $d1 to the bottom of the block. This is because
+    # some values may be found below this block (e.g pop_block) that are pattern
+    # matched in other locations, such as objmode handling.
 
     for blk in func_ir.blocks.values():
         new_body = []
-        # dict literal map name -> list of tuples to add to the build
-        lit_maps = dict()
-        # dict literal map name -> list of items to add to the buildmap
-        map_updates = dict()
+        # literal map var name
+        # -> index of build_map assign in the original block body
+        lit_old_idx = {}
+        # literal map var name
+        # -> index of build_map assign in the new block body
+        lit_new_idx = {}
+        # literal map var name -> list of key/value items for build map
+        map_updates = {}
+        blk_changed = False
 
         for i, stmt in enumerate(blk.body):
             # Should we add the current inst to the output
             append_inst = True
-            if (
-                isinstance(stmt, ir.Assign)
-                and isinstance(stmt.value, ir.Expr)
-                and stmt.value.op == "build_map"
+            # Name that shoud be skipped when looking at used
+            # vars.
+            stmt_build_map_out = None
+            if isinstance(stmt, ir.Assign) and isinstance(stmt.value, ir.Expr):
+                if stmt.value.op == "build_map":
+                    # Skip the output build_map when looks for uses.
+                    stmt_build_map_out = stmt.target.name
+                    # If we encounter a build map add it to the
+                    # tracked build_maps.
+                    lit_old_idx[stmt.target.name] = i
+                    lit_new_idx[stmt.target.name] = i
+                    map_updates[stmt.target.name] = stmt.value.items.copy()
+                    append_inst = False
+                elif stmt.value.op == "call" and i > 0:
+                    # If we encounter a call we may need to replace
+                    # the body
+                    func_name = stmt.value.func.name
+                    # If we have an update or a setitem
+                    # it will be the previous expression.
+                    getattr_stmt = blk.body[i - 1]
+                    args = stmt.value.args
+                    if (
+                        isinstance(getattr_stmt, ir.Assign)
+                        and getattr_stmt.target.name == func_name
+                        and isinstance(getattr_stmt.value, ir.Expr)
+                        and getattr_stmt.value.op == "getattr"
+                        and getattr_stmt.value.value.name in lit_old_idx
+                    ):
+                        update_map_name = getattr_stmt.value.value.name
+                        attr = getattr_stmt.value.attr
+                        if attr == "__setitem__":
+                            append_inst = False
+                            # If we have a setitem, update the lists
+                            map_updates[update_map_name].append(args)
+                            # Remove the setitem
+                            new_body[-1] = None
 
-            ):
-                # If we encounter a build map add it to the
-                # constant set.
-                lit_maps[stmt.target.name] = i
-                map_updates[stmt.target.name] = stmt.value.items.copy()
-                append_inst = False
-            elif (
-                isinstance(stmt, ir.Assign)
-                and isinstance(stmt.value, ir.Expr)
-                and stmt.value.op == "call"
-                and i > 0
-            ):
-                # If we encounter a call we may need to replace
-                # the body
-                func_name = stmt.value.func.name
-                # If we have an update or a setitem
-                # it will be the previous expression.
-                getattr_stmt = blk.body[i - 1]
-                args = stmt.value.args
-                if (
-                    isinstance(getattr_stmt, ir.Assign)
-                    and getattr_stmt.target.name == func_name
-                    and isinstance(getattr_stmt.value, ir.Expr)
-                    and getattr_stmt.value.op == "getattr"
-                    and getattr_stmt.value.value.name in lit_maps
-                ):
-                    update_map_name = getattr_stmt.value.value.name
-                    attr = getattr_stmt.value.attr
-                    if attr == "__setitem__":
-                        append_inst = False
-                        # If we have a setitem, update the lists
-                        map_updates[update_map_name].append(args)
-                        # Remove the setitem
-                        new_body.pop()
-
-                    elif attr == "update" and args[0].name in lit_maps:
-                        append_inst = False
-                        # If we have an update and the arg is also
-                        # a literal dictionary, fuse the lists.
-                        map_updates[update_map_name].extend(
-                            map_updates[args[0].name]
-                        )
-                        # Remove the update
-                        new_body.pop()
-
-            if append_inst:
-                # Check if we need to pop any dictionaries from the const map.
-                # Skip the setitem and update that will be used later.
-                if not (
-                    isinstance(stmt, ir.Assign)
-                    and isinstance(stmt.value, ir.Expr)
-                    and stmt.value.op == "getattr"
-                    and stmt.value.value.name in lit_maps
-                    and stmt.value.attr in ("__setitem__", "update")
-                ):
-                    for var in stmt.list_vars():
-                        # If is used pop it from the const map
-                        if var.name in lit_maps:
-                            lineno = lit_maps[var.name]
-                            items = map_updates[var.name]
-                            new_body.append(
-                                build_new_buildmap(var.name, blk, lineno, items)
+                        elif attr == "update" and args[0].name in lit_old_idx:
+                            append_inst = False
+                            # If we have an update and the arg is also
+                            # a literal dictionary, fuse the lists.
+                            map_updates[update_map_name].extend(
+                                map_updates[args[0].name]
                             )
-                            # Delete from the const maps
-                            del lit_maps[var.name]
-                            del map_updates[var.name]
+                            # Remove the update
+                            new_body[-1] = None
+                        if not append_inst:
+                            # The output of __setitem__ and update is now always
+                            # unused so we delete the IR stmtx.
+                            # Update the new insert location
+                            lit_new_idx[update_map_name] = i
+                            getattr_lhs = getattr_stmt.target.name
+                            # Drop the existing definition for this stmt.
+                            func_ir._definitions[getattr_lhs].remove(
+                                getattr_stmt.value
+                            )
+
+            # Check if we need to pop any dictionaries from being tracked.
+            # Skip the setitem/update gettar that will be removed when
+            # handling their call in the next iteration.
+            if not (
+                isinstance(stmt, ir.Assign)
+                and isinstance(stmt.value, ir.Expr)
+                and stmt.value.op == "getattr"
+                and stmt.value.value.name in lit_old_idx
+                and stmt.value.attr in ("__setitem__", "update")
+            ):
+                for var in stmt.list_vars():
+                    # If a dictionary is used it cannot be pushed farther into
+                    # the block. Skip the assign target.
+                    if (
+                        var.name in lit_old_idx
+                        and var.name != stmt_build_map_out
+                    ):
+                        _insert_build_map(
+                            func_ir,
+                            var.name,
+                            blk.body,
+                            new_body,
+                            lit_old_idx,
+                            lit_new_idx,
+                            map_updates,
+                        )
+            if append_inst:
                 new_body.append(stmt)
-        # Update we any dictionaries that weren't used in the block yet.
-        # We pop off the last statement because this needs to remain last
-        # as it might be a jump, branch, or return
-        end = new_body.pop()
-        for var_name, lineno in lit_maps.items():
-            items = map_updates[var_name]
-            new_body.append(build_new_buildmap(var_name, blk, lineno, items))
-        new_body.append(end)
-        blk.body = new_body
+            else:
+                # Drop the existing definition for this stmt.
+                func_ir._definitions[stmt.target.name].remove(stmt.value)
+                blk_changed = True
+                # Append None so the number of instructions remains the same.
+                new_body.append(None)
+
+        # Insert any remaining maps. We make a list of keys because
+        # we modify lit_old_idx in the loop.
+        keys = list(lit_old_idx.keys())
+        for var_name in keys:
+            _insert_build_map(
+                func_ir,
+                var_name,
+                blk.body,
+                new_body,
+                lit_old_idx,
+                lit_new_idx,
+                map_updates,
+            )
+        if blk_changed:
+            blk.body = [x for x in new_body if x is not None]
+
     return func_ir
+
+
+def _insert_build_map(
+    func_ir, name, old_body, new_body, lit_old_idx, lit_new_idx, map_updates
+):
+    """
+    Inserts a an assign with the given name into the new body using the
+    information from dictionaries:
+        lit_old_idx: name -> index in which the original build_map is found
+        lit_new_idx: name -> index in which to insert
+        map_updates: name -> key/value items for the new build map.
+
+    After inserting into new_body, name is deleted from all of the dictionaries.
+    """
+    old_idx = lit_old_idx[name]
+    new_idx = lit_new_idx[name]
+    items = map_updates[name]
+    # Insert each remaining dictionary to the earliest location it combined
+    # its variables. This is to avoid error prone pattern matching in the IR,
+    # especially with nodes expected to fall at the end of blocks.
+    new_body[new_idx] = _build_new_build_map(
+        func_ir, name, old_body, old_idx, items
+    )
+    del lit_old_idx[name]
+    del lit_new_idx[name]
+    del map_updates[name]
+
+
+def _build_new_build_map(func_ir, name, old_body, old_lineno, new_items):
+    """
+    Create a new build_map with a new set of key/value items
+    but all the other info the same.
+    """
+    old_assign = old_body[old_lineno]
+    old_target = old_assign.target
+    old_bm = old_assign.value
+    # Build the literals
+    literal_keys = []
+    # Track the constant key/values to set the literal_value
+    # field of build_map properly
+    values = []
+    for pair in new_items:
+        k, v = pair
+        key_def = guard(get_definition, func_ir, k)
+        if isinstance(key_def, (ir.Const, ir.Global, ir.FreeVar)):
+            literal_keys.append(key_def.value)
+        value_def = guard(get_definition, func_ir, v)
+        if isinstance(value_def, (ir.Const, ir.Global, ir.FreeVar)):
+            values.append(value_def.value)
+        else:
+            # Append unknown value if not a literal.
+            values.append(_UNKNOWN_VALUE(v.name))
+
+    value_indexes = {}
+    if len(literal_keys) == len(new_items):
+        # All keys must be literals to have any literal values.
+        literal_value = {x: y for x, y in zip(literal_keys, values)}
+        for i, k in enumerate(literal_keys):
+            value_indexes[k] = i
+    else:
+        literal_value = None
+
+    # Construct a new build map.
+    new_bm = ir.Expr.build_map(
+        items=new_items,
+        size=len(new_items),
+        literal_value=literal_value,
+        value_indexes=value_indexes,
+        loc=old_bm.loc,
+    )
+
+    # The previous definition has already been removed
+    # when updating the IR in peep_hole_fuse_dict_add_updates
+    func_ir._definitions[name].append(new_bm)
+
+    # Return a new assign.
+    return ir.Assign(
+        new_bm, ir.Var(old_target.scope, name, old_target.loc), new_bm.loc
+    )
 
 
 class Interpreter(object):
