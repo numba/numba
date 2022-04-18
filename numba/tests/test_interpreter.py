974da74fd399fd51b82ad65093fa16eeb0e29a10
Nicholas J Riasanovsky <nick@bodo.ai>
2022-04-18 11:58:00 -0400

    added an objmode test

--- numba/tests/test_interpreter.py
+++ numba/tests/test_interpreter.py
@@ -1,6 +1,7 @@
 """
 Test bytecode fixes provided in interpreter.py
 """
+from numba.extending import objmode
 from numba import njit
 from numba.tests.support import TestCase, MemoryLeakMixin, skip_unless_py10
 
@@ -240,4 +241,94 @@ class TestCallFunctionExPeepHole(TestCase, MemoryLeakMixin):
         cfunc = njit()(f)
         a = py_func(*total_args)
         b = cfunc(*total_args)
+        self.assertEqual(a, b)
+
+    @skip_unless_py10
+    def test_large_kws_objmode(self):
+        """
+        Tests calling an objectmode function with > 15 return values.
+        """
+        def py_func():
+            return (
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+            )
+
+        @njit
+        def objmode_func():
+            """
+            Wrapper to call py_func from objmode. This tests
+            large kws with objmode.
+            """
+            with objmode(
+                a='int64',
+                b='int64',
+                c='int64',
+                d='int64',
+                e='int64',
+                f='int64',
+                g='int64',
+                h='int64',
+                i='int64',
+                j='int64',
+                k='int64',
+                l='int64',
+                m='int64',
+                n='int64',
+                o='int64',
+                p='int64',
+            ):
+                (
+                    a,
+                    b,
+                    c,
+                    d,
+                    e,
+                    f,
+                    g,
+                    h,
+                    i,
+                    j,
+                    k,
+                    l,
+                    m,
+                    n,
+                    o,
+                    p
+                ) = py_func()
+            return (
+                a
+                + b
+                + c
+                + d
+                + e
+                + f
+                + g
+                + h
+                + i
+                + j
+                + k
+                + l
+                + m
+                + n
+                + o
+                + p
+            )
+
+        a = sum(list(py_func()))
+        b = objmode_func()
         self.assertEqual(a, b)
