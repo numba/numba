17a9d9115a77166213bcb509cad31d71513d1cc0
Todd A. Anderson <drtodd13@comcast.net>
2022-04-21 11:09:44 -0700

    add test for get_thread_id outside parallel region

--- numba/tests/test_num_threads.py
+++ numba/tests/test_num_threads.py
@@ -548,6 +548,35 @@ class TestNumThreads(TestCase):
     def tearDown(self):
         set_num_threads(config.NUMBA_NUM_THREADS)
 
+    @skip_parfors_unsupported
+    def _test_get_thread_id_not_parallel(self):
+        python_get_thread_id = get_thread_id()
+        check_array_size = 8
+
+        @njit(parallel=False)
+        def par_false(size):
+            njit_par_false_tid = get_thread_id()
+            res = np.ones(size)
+            for i in prange(size):
+                res[i] = get_thread_id()
+            return njit_par_false_tid, res
+
+        @njit(parallel=True)
+        def par_true(size):
+            njit_par_true_tid = get_thread_id()
+            res = np.ones(size)
+            for i in range(size):
+                res[i] = get_thread_id()
+            return njit_par_true_tid, res
+
+        self.assertEqual(python_get_thread_id, 0)
+        njit_par_false_tid, njit_par_false_arr = par_false(check_array_size)
+        self.assertEqual(njit_par_false_tid, 0)
+        np.testing.assert_equal(njit_par_false_arr, 0)
+        njit_par_true_tid, njit_par_true_arr = par_true(check_array_size)
+        self.assertEqual(njit_par_true_tid, 0)
+        np.testing.assert_equal(njit_par_true_arr, 0)
+
 
 class TestNumThreadsBackends(TestInSubprocess, TestCase):
     _class = TestNumThreads
