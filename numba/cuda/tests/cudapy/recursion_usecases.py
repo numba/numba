f433ebbc624d8266c0b63e0dc12e9fe5978fa2c6
Graham Markall <gmarkall@nvidia.com>
2022-05-10 10:17:55 +0100

    A first test of recursion

---
+++ numba/cuda/tests/cudapy/recursion_usecases.py
@@ -0,0 +1,38 @@
+from numba import cuda
+
+#@cuda.jit("i8(i8)", device=True)
+#def fib1(n):
+#    if n < 2:
+#        return n
+#    # Note the second call uses a named argument
+#    return fib1(n - 1) + fib1(n=n - 2)
+
+
+#def make_fib2():
+#    @jit("i8(i8)", nopython=True)
+#    def fib2(n):
+#        if n < 2:
+#            return n
+#        return fib2(n - 1) + fib2(n=n - 2)
+#
+#    return fib2
+#
+#fib2 = make_fib2()
+#
+#
+#def make_type_change_self(jit=lambda x: x):
+#    @jit
+#    def type_change_self(x, y):
+#        if x > 1 and y > 0:
+#            return x + type_change_self(x - y, y)
+#        else:
+#            return y
+#    return type_change_self
+#
+#
+# Implicit signature
+@cuda.jit
+def fib3(n):
+    if n < 2:
+        return n
+    return fib3(n - 1) + fib3(n - 2)
---
+++ numba/cuda/tests/cudapy/test_recursion.py
@@ -0,0 +1,145 @@
+import math
+import warnings
+
+from numba import cuda, jit
+from numba.core.errors import TypingError, NumbaWarning
+from numba.cuda.testing import CUDATestCase
+import numpy as np
+import unittest
+
+
+class TestSelfRecursion(CUDATestCase):
+
+    def setUp(self):
+        # Avoid importing this module at toplevel, as it triggers compilation
+        # and can therefore fail
+        from numba.cuda.tests.cudapy import recursion_usecases
+        self.mod = recursion_usecases
+        super().setUp()
+
+    def check_fib(self, cfunc):
+        @cuda.jit
+        def kernel(r, x):
+            r[0] = cfunc(x[0])
+
+        x = np.asarray([10], dtype=np.int64)
+        r = np.zeros_like(x)
+        kernel[1, 1](r, x)
+
+        actual = r[0]
+        expected = 55
+        self.assertPreciseEqual(actual, expected)
+
+    @unittest.skip
+    def test_global_explicit_sig(self):
+        breakpoint()
+        self.check_fib(self.mod.fib1)
+
+    @unittest.skip
+    def test_inner_explicit_sig(self):
+        self.check_fib(self.mod.fib2)
+
+    def test_global_implicit_sig(self):
+        self.check_fib(self.mod.fib3)
+
+    @unittest.skip
+    def test_runaway(self):
+        with self.assertRaises(TypingError) as raises:
+            self.mod.runaway_self(123)
+        self.assertIn("cannot type infer runaway recursion",
+                      str(raises.exception))
+
+    @unittest.skip
+    def test_type_change(self):
+        pfunc = self.mod.make_type_change_self()
+        cfunc = self.mod.make_type_change_self(jit(nopython=True))
+        args = 13, 0.125
+        self.assertPreciseEqual(pfunc(*args), cfunc(*args))
+
+    @unittest.skip
+    def test_raise(self):
+        with self.assertRaises(ValueError) as raises:
+            self.mod.raise_self(3)
+
+        self.assertEqual(str(raises.exception), "raise_self")
+
+    @unittest.skip
+    def test_optional_return(self):
+        pfunc = self.mod.make_optional_return_case()
+        cfunc = self.mod.make_optional_return_case(jit(nopython=True))
+        for arg in (0, 5, 10, 15):
+            self.assertEqual(pfunc(arg), cfunc(arg))
+
+    @unittest.skip
+    def test_growing_return_tuple(self):
+        cfunc = self.mod.make_growing_tuple_case(jit(nopython=True))
+        with self.assertRaises(TypingError) as raises:
+            cfunc(100)
+        self.assertIn(
+            "Return type of recursive function does not converge",
+            str(raises.exception),
+        )
+
+
+@unittest.skip
+class TestMutualRecursion(CUDATestCase):
+
+    def setUp(self):
+        from numba.tests import recursion_usecases
+        self.mod = recursion_usecases
+
+    def test_mutual_1(self):
+        expect = math.factorial(10)
+        self.assertPreciseEqual(self.mod.outer_fac(10), expect)
+
+    def test_mutual_2(self):
+        pfoo, pbar = self.mod.make_mutual2()
+        cfoo, cbar = self.mod.make_mutual2(jit(nopython=True))
+        for x in [-1, 0, 1, 3]:
+            self.assertPreciseEqual(pfoo(x=x), cfoo(x=x))
+            self.assertPreciseEqual(pbar(y=x, z=1), cbar(y=x, z=1))
+
+    def test_runaway(self):
+        with self.assertRaises(TypingError) as raises:
+            self.mod.runaway_mutual(123)
+        self.assertIn("cannot type infer runaway recursion",
+                      str(raises.exception))
+
+    def test_type_change(self):
+        pfunc = self.mod.make_type_change_mutual()
+        cfunc = self.mod.make_type_change_mutual(jit(nopython=True))
+        args = 13, 0.125
+        self.assertPreciseEqual(pfunc(*args), cfunc(*args))
+
+    def test_four_level(self):
+        pfunc = self.mod.make_four_level()
+        cfunc = self.mod.make_four_level(jit(nopython=True))
+        arg = 7
+        self.assertPreciseEqual(pfunc(arg), cfunc(arg))
+
+    def test_inner_error(self):
+        # nopython mode
+        cfunc = self.mod.make_inner_error(jit(nopython=True))
+        with self.assertRaises(TypingError) as raises:
+            cfunc(2)
+        errmsg = 'Unknown attribute \'ndim\''
+        self.assertIn(errmsg, str(raises.exception))
+        # objectmode
+        # error is never trigger, function return normally
+        cfunc = self.mod.make_inner_error(jit)
+        pfunc = self.mod.make_inner_error()
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=NumbaWarning)
+            got = cfunc(6)
+        self.assertEqual(got, pfunc(6))
+
+    def test_raise(self):
+        cfunc = self.mod.make_raise_mutual()#jit(nopython=True))
+        with self.assertRaises(ValueError) as raises:
+            cfunc(2)
+
+        self.assertEqual(str(raises.exception), "raise_mutual")
+
+
+if __name__ == '__main__':
+    unittest.main()
