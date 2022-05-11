adb05ce7dd1347a009b8744eec71da01c243ff19
Graham Markall <gmarkall@nvidia.com>
2022-05-11 12:23:58 +0100

    Remove mutual recursion tests

--- numba/cuda/tests/cudapy/test_recursion.py
+++ numba/cuda/tests/cudapy/test_recursion.py
@@ -1,8 +1,5 @@
-import math
-import warnings
-
-from numba import cuda, jit
-from numba.core.errors import TypingError, NumbaWarning
+from numba import cuda
+from numba.core.errors import TypingError
 from numba.cuda.testing import CUDATestCase
 import numpy as np
 import unittest
@@ -118,66 +115,6 @@ class TestSelfRecursion(CUDATestCase):
             "Return type of recursive function does not converge",
             str(raises.exception),
         )
-
-
-@unittest.skip
-class TestMutualRecursion(CUDATestCase):
-
-    def setUp(self):
-        from numba.tests import recursion_usecases
-        self.mod = recursion_usecases
-
-    def test_mutual_1(self):
-        expect = math.factorial(10)
-        self.assertPreciseEqual(self.mod.outer_fac(10), expect)
-
-    def test_mutual_2(self):
-        pfoo, pbar = self.mod.make_mutual2()
-        cfoo, cbar = self.mod.make_mutual2(jit(nopython=True))
-        for x in [-1, 0, 1, 3]:
-            self.assertPreciseEqual(pfoo(x=x), cfoo(x=x))
-            self.assertPreciseEqual(pbar(y=x, z=1), cbar(y=x, z=1))
-
-    def test_runaway(self):
-        with self.assertRaises(TypingError) as raises:
-            self.mod.runaway_mutual(123)
-        self.assertIn("cannot type infer runaway recursion",
-                      str(raises.exception))
-
-    def test_type_change(self):
-        pfunc = self.mod.make_type_change_mutual()
-        cfunc = self.mod.make_type_change_mutual(jit(nopython=True))
-        args = 13, 0.125
-        self.assertPreciseEqual(pfunc(*args), cfunc(*args))
-
-    def test_four_level(self):
-        pfunc = self.mod.make_four_level()
-        cfunc = self.mod.make_four_level(jit(nopython=True))
-        arg = 7
-        self.assertPreciseEqual(pfunc(arg), cfunc(arg))
-
-    def test_inner_error(self):
-        # nopython mode
-        cfunc = self.mod.make_inner_error(jit(nopython=True))
-        with self.assertRaises(TypingError) as raises:
-            cfunc(2)
-        errmsg = 'Unknown attribute \'ndim\''
-        self.assertIn(errmsg, str(raises.exception))
-        # objectmode
-        # error is never trigger, function return normally
-        cfunc = self.mod.make_inner_error(jit)
-        pfunc = self.mod.make_inner_error()
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=NumbaWarning)
-            got = cfunc(6)
-        self.assertEqual(got, pfunc(6))
-
-    def test_raise(self):
-        cfunc = self.mod.make_raise_mutual()#jit(nopython=True))
-        with self.assertRaises(ValueError) as raises:
-            cfunc(2)
-
-        self.assertEqual(str(raises.exception), "raise_mutual")
 
 
 if __name__ == '__main__':
