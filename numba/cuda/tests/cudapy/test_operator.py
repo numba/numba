4a2006d2c55fdf0fede65e4eee61e4e57ee0611a
Michael Collison <collison956@gmail.com>
2022-05-25 13:46:59 -0400

    Add test cases for mixed type comparisions with fp16

--- numba/cuda/tests/cudapy/test_operator.py
+++ numba/cuda/tests/cudapy/test_operator.py
@@ -5,6 +5,8 @@ from numba import cuda
 from numba.core.types import f2,b1
 import operator
 from numba.cuda import compile_ptx
+import itertools
+from numba.np.numpy_support import from_dtype
 
 
 def simple_fp16_gt(ary, a, b):
@@ -174,6 +176,60 @@ class TestOperatorModule(CUDATestCase):
             with self.subTest(op=op):
                 ptx, _ = compile_ptx(fn, args, cc=(5, 3))
                 self.assertIn(s, ptx)
+
+    @skip_on_cudasim('Compilation unsupported in the simulator')
+    def test_mixed_fp16_comparison_ptx(self):
+        functions = (simple_fp16_gt, simple_fp16_ge,
+                     simple_fp16_lt, simple_fp16_le,
+                     simple_fp16_eq, simple_fp16_ne)
+        ops = (operator.gt, operator.ge, operator.lt, operator.le,
+               operator.eq, operator.ne)
+        types_convert = (np.int8,)
+
+        opstring = {operator.gt:'setp.gt.f16',
+                    operator.ge:'setp.ge.f16',
+                    operator.lt:'setp.lt.f16',
+                    operator.le:'setp.le.f16',
+                    operator.eq:'setp.eq.f16',
+                    operator.ne:'setp.ne.f16'}
+        for (fn, op), ty in itertools.product(zip(functions, ops),
+                                              types_convert):
+            with self.subTest(op=op):
+                args = (b1[:], f2, from_dtype(ty))
+                ptx, _ = compile_ptx(fn, args, cc=(5, 3))
+                print(ptx)
+                self.assertIn(opstring[op], ptx)
+
+    @skip_on_cudasim('Compilation unsupported in the simulator')
+    def test_mixed_fp16_comparison_ptx2(self):
+        functions = (simple_fp16_gt, simple_fp16_ge,
+                     simple_fp16_lt, simple_fp16_le,
+                     simple_fp16_eq, simple_fp16_ne)
+        ops = (operator.gt, operator.ge, operator.lt, operator.le,
+               operator.eq, operator.ne)
+
+        types_promote = (np.int16, np.int32, np.int64,
+                         np.float32, np.float64)
+        opstring = {operator.gt:'setp.gt.',
+                    operator.ge:'setp.ge.',
+                    operator.lt:'setp.lt.',
+                    operator.le:'setp.le.',
+                    operator.eq:'setp.eq.',
+                    operator.ne:'setp.neu.'}
+        opsuffix = {np.dtype('int32'): 'f64',
+                    np.dtype('int64'): 'f64',
+                    np.dtype('float32'): 'f32',
+                    np.dtype('float64'): 'f64'}
+
+        for (fn, op), ty in itertools.product(zip(functions, ops),
+                                              types_promote):
+            with self.subTest(op=op):
+                arg2_ty = np.result_type(np.float16, ty)
+                args = (b1[:], f2, from_dtype(arg2_ty))
+                ptx, _ = compile_ptx(fn, args, cc=(5, 3))
+
+                ops = opstring[op] + opsuffix[arg2_ty]
+                self.assertIn(ops, ptx)
 
 
 if __name__ == '__main__':
