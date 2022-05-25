Fixed next_float precision issue depedency on Numpy versions

numba/np/random/generator_core.py
@@ -7,6 +7,7 @@ from numba.core import cgutils, types
 from numba.core.extending import (intrinsic, make_attribute_wrapper, models,
                                   overload, register_jitable,
                                   register_model)
+from numba.np.numpy_support import numpy_version
 
 
 @register_model(types.NumPyRandomBitGeneratorType)
@@ -108,6 +109,11 @@ _generate_next_binding(next_uint32, types.uint32)
 _generate_next_binding(next_uint64, types.uint64)
 
 
-@register_jitable
-def next_float(bitgen):
-    return (next_uint32(bitgen) >> 9) * (1.0 / 8388608.0)
+if numpy_version >= (1, 22):
+    @register_jitable
+    def next_float(bitgen):
+        return (next_uint32(bitgen) >> 8) * (1.0 / 16777216.0)
+else:
+    @register_jitable
+    def next_float(bitgen):
+        return (next_uint32(bitgen) >> 9) * (1.0 / 8388608.0)

numba/np/random/generator_methods.py
@@ -58,8 +58,8 @@ def NumPyRandomGeneratorType_random(inst, size=None, dtype=np.float64):
         assert any([isinstance(size, UniTuple) and
                    isinstance(size.dtype, types.Integer),
                    isinstance(size, Tuple) and size.count == 0,
-                   isinstance(size, types.Integer)
-                   ]), "Size argument either of None,"\
+                   isinstance(size, types.Integer)]
+                   ), "Size argument either of None,"\
             + " an integer, an empty tuple or a tuple of integers"
 
         def impl(inst, size=None, dtype=np.float64):