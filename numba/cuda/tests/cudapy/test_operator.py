527973c94a7e066a45474467c66b3833d00e911f
Michael Collison <collison956@gmail.com>
2022-06-07 11:27:14 -0400

    Add fp16 mixed type test for binary operators

--- numba/cuda/tests/cudapy/test_operator.py
+++ numba/cuda/tests/cudapy/test_operator.py
@@ -5,6 +5,7 @@ from numba import cuda
 from numba.core.types import f2
 from numba.cuda import compile_ptx
 import operator
+import itertools
 
 
 def simple_fp16add(ary, a, b):
@@ -101,6 +102,28 @@ class TestOperatorModule(CUDATestCase):
                 ptx, _ = compile_ptx(fn, args, cc=(5, 3))
                 self.assertIn(s, ptx)
 
+    @skip_unless_cc_53
+    def test_mixed_fp16_binary_arithmetic(self):
+        functions = (simple_fp16add, simple_fp16sub, simple_fp16mul)
+        ops = (operator.add, operator.sub, operator.mul)
+        types = (np.int8, np.int16, np.int32, np.int64,
+                 np.float32, np.float64)
+        for (fn, op), ty in itertools.product(zip(functions, ops), types):
+            with self.subTest(op=op):
+                kernel = cuda.jit(fn)
+
+                
+                arg1 = np.random.random(1).astype(np.float16)
+                arg2 = (np.random.random(1) * 100).astype(ty)
+                arg2_ty = np.result_type(np.float16, ty)
+
+                expected = np.zeros(1, dtype=arg2_ty)
+                got = np.zeros(1, dtype=arg2_ty)
+
+                kernel[1, 1](got, arg1[0], arg2[0])
+                expected = op(arg1, arg2)
+                np.testing.assert_allclose(got[0], expected)
+    
     @skip_on_cudasim('Compilation unsupported in the simulator')
     def test_fp16_inplace_binary_ptx(self):
         functions = (simple_fp16_iadd, simple_fp16_isub, simple_fp16_imul)
