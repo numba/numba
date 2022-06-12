9ef764bc704fd0fd667318db04f4b2ab331561f5
Graham Markall <gmarkall@nvidia.com>
2022-06-12 15:25:56 +0100

    Add module-level docstring to CUDA recursion usecases

--- numba/cuda/tests/cudapy/recursion_usecases.py
+++ numba/cuda/tests/cudapy/recursion_usecases.py
@@ -1,3 +1,10 @@
+"""
+Usecases of recursive functions in the CUDA target, many derived from
+numba/tests/recursion_usecases.py.
+
+Some functions are compiled at import time, hence a separate module.
+"""
+
 from numba import cuda
 
 
