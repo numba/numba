1857a60ea716b2610e1f527aab5b89ada177a15a
kc611 <ckaustubhm06@gmail.com>
2022-06-22 19:48:18 +0530

    WIP-tried setting higher ulps

--- numba/tests/test_np_randomgen.py
+++ numba/tests/test_np_randomgen.py
@@ -67,9 +67,9 @@ class TestRandomGenerators(MemoryLeakMixin, TestCase):
                                               test_size, test_dtype)
 
         if test_dtype == np.float32:
-            self.assertPreciseEqual(numba_res, numpy_res, abs_tol=2**(-22))
+            self.assertPreciseEqual(numba_res, numpy_res, prec="single", ulps=6)
         else:
-            self.assertPreciseEqual(numba_res, numpy_res, abs_tol=2**(-52))
+            self.assertPreciseEqual(numba_res, numpy_res, prec="double", ulps=8)
 
         # Check if the end state of both BitGenerators is same
         # after drawing the distributions
