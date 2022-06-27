a5bec13a1ef520d48a07ae7e64c5b448a4fe8dfb
Kaustubh <ckaustubhm06@gmail.com>
2022-06-27 19:52:50 +0530

    Update numba/np/random/generator_methods.py
    Co-authored-by: stuartarchibald <stuartarchibald@users.noreply.github.com>

--- numba/np/random/generator_methods.py
+++ numba/np/random/generator_methods.py
@@ -56,7 +56,7 @@ def check_size(size):
                 isinstance(size.dtype, types.Integer),
                 isinstance(size, Tuple) and size.count == 0,
                 isinstance(size, types.Integer)]):
-        raise TypingError("Size argument either of None," +
+        raise TypingError("Size argument must be one of None," +
                           " an integer, an empty tuple or a tuple of integers")
 
 
