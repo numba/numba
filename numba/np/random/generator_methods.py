Fixed integer overflow on 32 bit systems

numba/np/random/generator_methods.py
@@ -22,6 +22,8 @@ from numba.np.random.distributions import \
      random_geometric, random_zipf, random_triangular,
      random_poisson, random_negative_binomial)
 from numba.np.random import random_methods
+from numba.core.config import IS_32BITS
+from numba.core.extending import register_jitable
 
 
 def _get_proper_func(func_32, func_64, dtype, dist_name="the given"):
@@ -85,10 +87,10 @@ def check_types(obj, type_list, arg_name):
 @overload_method(types.NumPyRandomGeneratorType, 'integers')
 def NumPyRandomGeneratorType_integers(inst, low, high=None, size=None,
                                       dtype=np.int64, endpoint=False):
-    check_types(low, [types.Float, types.Integer,
-                      types.Boolean, bool, int, float], 'low')
-    check_types(high, [types.Float, types.Integer, types.Boolean,
-                       bool, int, float, type(None)], 'high')
+    check_types(low, [types.Integer,
+                      types.Boolean, bool, int], 'low')
+    check_types(high, [types.Integer, types.Boolean,
+                       bool, int, type(None)], 'high')
     check_types(endpoint, [types.Boolean, bool], 'endpoint')
 
     if isinstance(size, types.Omitted):
@@ -123,6 +125,18 @@ def NumPyRandomGeneratorType_integers(inst, low, high=None, size=None,
                               "np.uint32, np.uint64, np.uint16, np.uint8, "
                               "np.bool_")
 
+    # Hack for treating the arguments as 64 bit integers in 32-bit systems
+    # so they don't overflow during the calculations.
+    # TODO: Remove this logic when we drop support for 32-bit systems.
+    if IS_32BITS and _dtype in [np.int32, np.int64, np.uint32, np.uint64]:
+        @register_jitable
+        def cast_arg_if_32(curr_arg):
+            return np.array(curr_arg, dtype=np.int64)
+    else:
+        @register_jitable
+        def cast_arg_if_32(curr_arg):
+            return curr_arg
+
     if is_nonelike(size):
         def impl(inst, low, high=None, size=None,
                  dtype=np.int64, endpoint=False):
@@ -130,6 +144,8 @@ def NumPyRandomGeneratorType_integers(inst, low, high=None, size=None,
                                                          lower_bound,
                                                          upper_bound)
             mask = None
+            low = cast_arg_if_32(low)
+            rng = cast_arg_if_32(rng)
             return int_func(inst.bit_generator, low, rng, mask, 1, dtype)[0]
         return impl
     else:
@@ -141,6 +157,8 @@ def NumPyRandomGeneratorType_integers(inst, low, high=None, size=None,
                                                          lower_bound,
                                                          upper_bound)
             mask = None
+            low = cast_arg_if_32(low)
+            rng = cast_arg_if_32(rng)
             return int_func(inst.bit_generator, low, rng, mask, size, dtype)
         return impl
 

numba/np/random/random_methods.py
@@ -97,25 +97,22 @@ def bounded_masked_uint64(bitgen, rng, mask):
 
 @register_jitable
 def buffered_bounded_lemire_uint8(bitgen, rng, bcnt, buf):
-    # /*
-    # * Uses Lemire's algorithm - https://arxiv.org/abs/1805.10941
-    # *
-    # * Note: `rng` should not be 0xFF. When this happens `rng_excl` becomes
-    # * zero.
-    # */
+    # Uses Lemire's algorithm - https://arxiv.org/abs/1805.10941
+    # Note: `rng` should not be 0xFF. When this happens `rng_excl` becomes
+    # zero.
     rng_excl = (rng + 1) & 0xFF
 
     assert(rng != 0xFF)
 
-    # /* Generate a scaled random number. */
+    # Generate a scaled random number.
     n, bcnt, buf = buffered_uint8(bitgen, bcnt, buf)
     m = n * rng_excl
 
-    # /* Rejection sampling to remove any bias */
+    # Rejection sampling to remove any bias
     leftover = m & 0xFF
 
     if (leftover < rng_excl):
-        # /* `rng_excl` is a simple upper bound for `threshold`. */
+        # `rng_excl` is a simple upper bound for `threshold`.
         threshold = ((UINT8_MAX - rng) % rng_excl) & 0xFF
 
         while (leftover < threshold):
@@ -128,25 +125,22 @@ def buffered_bounded_lemire_uint8(bitgen, rng, bcnt, buf):
 
 @register_jitable
 def buffered_bounded_lemire_uint16(bitgen, rng, bcnt, buf):
-    # /*
-    # * Uses Lemire's algorithm - https://arxiv.org/abs/1805.10941
-    # *
-    # * Note: `rng` should not be 0xFFFF. When this happens `rng_excl` becomes
-    # * zero.
-    # */
+    # Uses Lemire's algorithm - https://arxiv.org/abs/1805.10941
+    # Note: `rng` should not be 0xFFFF. When this happens `rng_excl` becomes
+    # zero.
     rng_excl = (rng + 1) & 0xFFFF
 
     assert(rng != 0xFFFF)
 
-    # /* Generate a scaled random number. */
+    # Generate a scaled random number.
     n, bcnt, buf = buffered_uint16(bitgen, bcnt, buf)
     m = n * rng_excl
 
-    # /* Rejection sampling to remove any bias */
+    # Rejection sampling to remove any bias
     leftover = m & 0xFFFF
 
     if (leftover < rng_excl):
-        # /* `rng_excl` is a simple upper bound for `threshold`. */
+        # `rng_excl` is a simple upper bound for `threshold`.
         threshold = ((UINT16_MAX - rng) % rng_excl) & 0xFFFF
 
         while (leftover < threshold):
@@ -200,8 +194,8 @@ def bounded_lemire_uint64(bitgen, rng):
     return (m >> 64)
 
 
-#  Fills an array with cnt random npy_uint64 between off and off + rng
-#  inclusive. The numbers wrap if rng is sufficiently large.
+# Fills an array with cnt random npy_uint64 between off and off + rng
+# inclusive. The numbers wrap if rng is sufficiently large.
 @register_jitable
 def random_bounded_uint64_fill(bitgen, low, rng, mask, size, dtype):
     out = np.empty(size, dtype=dtype)
@@ -243,7 +237,7 @@ def random_bounded_uint32_fill(bitgen, low, rng, mask, size, dtype):
         for i in np.ndindex(size):
             out[i] = low
     elif rng == 0xFFFFFFFF:
-        # /* Lemire32 doesn't support rng = 0xFFFFFFFF. */
+        # Lemire32 doesn't support rng = 0xFFFFFFFF.
         for i in np.ndindex(size):
             out[i] = low + next_uint32(bitgen)
     else:
@@ -266,14 +260,14 @@ def random_bounded_uint16_fill(bitgen, low, rng, mask, size, dtype):
         for i in np.ndindex(size):
             out[i] = low
     elif rng == 0xFFFF:
-        # /* Lemire16 doesn't support rng = 0xFFFF. */
+        # Lemire16 doesn't support rng = 0xFFFF.
         for i in np.ndindex(size):
             val, bcnt, buf = buffered_uint16(bitgen, bcnt, buf)
             out[i] = low + val
 
     else:
         if mask is not None:
-            # /* Smallest bit mask >= max */
+            # Smallest bit mask >= max
             for i in np.ndindex(size):
                 val, bcnt, buf = \
                     buffered_bounded_masked_uint16(bitgen, rng,
@@ -298,13 +292,13 @@ def random_bounded_uint8_fill(bitgen, low, rng, mask, size, dtype):
         for i in np.ndindex(size):
             out[i] = low
     elif rng == 0xFF:
-        # /* Lemire8 doesn't support rng = 0xFF. */
+        # Lemire8 doesn't support rng = 0xFF.
         for i in np.ndindex(size):
             val, bcnt, buf = buffered_uint8(bitgen, bcnt, buf)
             out[i] = low + val
     else:
         if mask is not None:
-            # /* Smallest bit mask >= max */
+            # Smallest bit mask >= max
             for i in np.ndindex(size):
                 val, bcnt, buf = \
                     buffered_bounded_masked_uint8(bitgen, rng,

numba/tests/test_np_randomgen.py
@@ -165,7 +165,8 @@ class TestRandomGenerators(MemoryLeakMixin, TestCase):
 
     def test_integers(self):
         test_sizes = [None, (), (100,), (10, 20, 30)]
-        test_dtypes = [np.int64, np.int32, np.int16, np.int8]
+        test_dtypes = [np.int64, np.int32, np.int16, np.int8,
+                       np.uint64, np.uint32, np.uint16, np.uint8]
         bitgen_types = [None, MT19937]
 
         dist_func = lambda x, size, dtype:\