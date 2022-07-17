6368c67cc1ed3119f139ea6437f223ba812e9cc9
Balint Szollosi-Nagy <4211175+perspectivisme@users.noreply.github.com>
2022-07-17 12:23:31 +0200

    Wald distrib: added unit test for issue 8161 to test if keyword arguments have the same names in Numba and Numpy, and same samples if started from the same seed. It fails at present.

--- numba/tests/test_random.py
+++ numba/tests/test_random.py
@@ -881,6 +881,18 @@ class TestRandom(BaseTest):
         self.assertRaises(ValueError, wald, 1.0, 0.0)
         self.assertRaises(ValueError, wald, 1.0, -0.1)
 
+    def test_numpy_wald_kwargs(self):
+        numba_version = jit_with_kwargs("np.random.wald", ['mean', 'scale'])
+        self._check_arbitrary_distrib_kwargs(numba_version,
+                                             get_np_state_ptr(),
+                                             distrib="wald",
+                                             paramlist=[{'mean': 1.0, 'scale': 1.0},
+                                                        {'mean': 2.0, 'scale': 5.0}])
+        self.assertRaises(ValueError, numba_version, 0.0, 1.0)
+        self.assertRaises(ValueError, numba_version, -0.1, 1.0)
+        self.assertRaises(ValueError, numba_version, 1.0, 0.0)
+        self.assertRaises(ValueError, numba_version, 1.0, -0.1)
+
     def test_numpy_zipf(self):
         r = self._follow_numpy(get_np_state_ptr())
         zipf = jit_unary("np.random.zipf")
