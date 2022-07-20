7a44bf6167c37719692ab115074d78e2312b3998
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2022-07-20 17:17:45 -0500

    Merge pull request #8239 from stuartarchibald/wip/subproc_tests
    Add decorator to run a test in a subprocess.

--- numba/tests/support.py
+++ numba/tests/support.py
@@ -558,7 +558,7 @@ class TestCase(unittest.TestCase):
             environment variable name (str) -> value (str)
         It is most convenient to use this method in conjunction with
         @needs_subprocess as the decorator will cause the decorated test to be
-        skipped unless the `SUBPROC_TEST` environment variable is set
+        skipped unless the `SUBPROC_TEST` environment variable is set to 1
         (this special environment variable is set by this method such that the
         specified test(s) will not be skipped in the subprocess).
 
@@ -585,6 +585,32 @@ class TestCase(unittest.TestCase):
         self.assertIn('OK', status.stderr)
         self.assertNotIn('FAIL', status.stderr)
         self.assertNotIn('ERROR', status.stderr)
+
+    def run_test_in_subprocess(maybefunc=None, timeout=60, envvars=None):
+        """Runs the decorated test in a subprocess via invoking numba's test
+        runner. kwargs timeout and envvars are passed through to
+        subprocess_test_runner."""
+        def wrapper(func):
+            def inner(self, *args, **kwargs):
+                if os.environ.get("SUBPROC_TEST", None) != "1":
+                    # Not in a subprocess test env, so stage the call to run the
+                    # test in a subprocess which will set the env var.
+                    class_name = self.__class__.__name__
+                    self.subprocess_test_runner(test_module=self.__module__,
+                                                test_class=class_name,
+                                                test_name=func.__name__,
+                                                timeout=timeout,
+                                                envvars=envvars,)
+                else:
+                    # env var is set, so we're in the subprocess, run the
+                    # actual test.
+                    func(self)
+            return inner
+
+        if isinstance(maybefunc, pytypes.FunctionType):
+            return wrapper(maybefunc)
+        else:
+            return wrapper
 
 
 class SerialMixin(object):
