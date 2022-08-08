ed8eee7ba7bbb3c7ddef128dd292722a05173fa4
Eric Wieser <wieser.eric@gmail.com>
2022-08-08 20:38:44 +0000

    fix

--- numba/core/cgutils.py
+++ numba/core/cgutils.py
@@ -873,7 +873,7 @@ def early_exit_if_null(builder, stack, obj):
     A convenience wrapper for :func:`early_exit_if`, for the common case where the CPython API
     indicates an error by returning ``NULL``.
     """
-    return early_exit_if(builder, stack, cgutils.is_null(builder, obj))
+    return early_exit_if(builder, stack, is_null(builder, obj))
 
 
 def guard_null(context, builder, value, exc_tuple):
