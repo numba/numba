95c760e62c7fadedff18f8c01236bcdd3a900f09
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-08-08 13:12:59 +0100

    Use packages from conda main where possible
    As title

--- buildscripts/incremental/setup_conda_environment.cmd
+++ buildscripts/incremental/setup_conda_environment.cmd
@@ -22,11 +22,9 @@ call deactivate
 @rem Display root environment (for debugging)
 conda list
 @rem Scipy, CFFI, jinja2 and IPython are optional dependencies, but exercised in the test suite
-conda create -n %CONDA_ENV% -q -y python=%PYTHON% cffi pip scipy jinja2 ipython gitpython pyyaml
+conda create -n %CONDA_ENV% -q -y python=%PYTHON% numpy=%NUMPY% cffi pip scipy jinja2 ipython gitpython pyyaml
 
 call activate %CONDA_ENV%
-@rem Install numpy 1.23
-pip install numpy==1.23.0
 @rem Install latest llvmlite build
 %CONDA_INSTALL% -c numba/label/dev llvmlite=0.40
 @rem Install required backports for older Pythons
