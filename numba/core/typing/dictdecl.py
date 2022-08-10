5b3cc6630bc4daf87ffa18e30add7cb79dc0d00d
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-08-10 00:20:08 -0300

    Add dict(*iterable) constructor

--- numba/core/typing/dictdecl.py
+++ numba/core/typing/dictdecl.py
@@ -17,13 +17,26 @@ infer_getattr = registry.register_attr
 
 _message_dict_support = """
 Unsupported use of `dict()` with positional or keyword argument(s). \
-The only supported use is `dict()`.
+The only supported uses are `dict()` or `dict(*iterable)`.
 """.strip()
 
 
 @infer_global(dict)
 class DictBuiltin(AbstractTemplate):
     def generic(self, args, kws):
-        if args or kws:
+        if kws:
             raise errors.TypingError(_message_dict_support)
+        if args:
+            iterable, = args
+            if isinstance(iterable, types.IterableType):
+                dtype = iterable.iterator_type.yield_type
+                if isinstance(dtype, types.UniTuple):
+                    k = v = dtype.key[0]
+                elif isinstance(dtype, types.Tuple):
+                    k, v = dtype.key
+                else:
+                    raise errors.TypingError(_message_dict_support)
+                return signature(types.DictType(k, v), iterable)
+            else:
+                raise errors.TypingError(_message_dict_support)
         return signature(types.DictType(types.undefined, types.undefined))
--- numba/tests/test_dicts.py
+++ numba/tests/test_dicts.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numba import njit
 from numba.core.errors import TypingError
 import unittest
@@ -36,6 +37,30 @@ class TestCompiledDict(TestCase):
 
         d = foo()
         self.assertEqual(d, {1: 2})
+
+    def test_use_dict_iterable_args(self):
+        # Test dict(*iterable)
+        @njit
+        def dict_iterable_1(a, b):
+            d = dict(zip(a, b))
+            return d
+
+        @njit
+        def dict_iterable_2():
+            # from python docs
+            return dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+
+        inps = (
+            ([1, 2, 3], [4, 5, 6]),
+            (np.arange(4), np.arange(4)),
+            ([1, 2, 3], 'abc'),
+            ([1, 2, 3, 4], 'abc'),
+        )
+        for a, b in inps:
+            d = dict_iterable_1(a, b)
+            self.assertEqual(d, dict(zip(a, b)))
+
+        self.assertEqual(dict_iterable_2(), dict_iterable_2.py_func())
 
     def test_unsupported_dict_usage(self):
         # Test dict(dict())
