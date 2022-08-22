c24796d99356aa2988c37c1ae398985b677680a4
Balint Szollosi-Nagy <4211175+bszollosinagy@users.noreply.github.com>
2022-08-22 13:48:57 +0200

    added test to see if the writeable flag is set after ravel() based on whether a copy was necessary or not.

--- numba/tests/test_array_manipulation.py
+++ numba/tests/test_array_manipulation.py
@@ -1287,6 +1287,29 @@ class TestArrayManipulation(MemoryLeakMixin, TestCase):
         # Cannot modify readonly array of type: readonly array(int32, 2d, C)"
         cfunc(np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]]), 2)
 
+    def test_mutability_after_ravel(self):
+        self.disable_leak_check()
+        a_c = np.arange(9).reshape((3, 3)).copy()
+        a_f = a_c.copy(order='F')
+        a_c.flags.writeable = False
+        a_f.flags.writeable = False
+
+        def try_ravel_w_copy(a):
+            result = a.ravel()
+            return result
+
+        pyfunc = try_ravel_w_copy
+        cfunc = jit(nopython=True)(pyfunc)
+
+        ret_c = cfunc(a_c)
+        ret_f = cfunc(a_f)
+
+        msg = 'No copy was performed, so the ' \
+              'resulting array must not be writeable'
+        self.assertTrue(not ret_c.flags.writeable, msg)
+
+        msg = 'A copy was performed, yet the resulting array is not modifiable'
+        self.assertTrue(ret_f.flags.writeable, msg)
 
 
 if __name__ == '__main__':
