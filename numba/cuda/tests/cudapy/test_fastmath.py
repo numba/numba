0210a4dc24d76d07bc58b217d0604747bdaa992f
Michael Collison <collison956@gmail.com>
2022-08-23 00:15:00 -0400

    Add tanh fastmath test case

--- numba/cuda/tests/cudapy/test_fastmath.py
+++ numba/cuda/tests/cudapy/test_fastmath.py
@@ -2,7 +2,7 @@ from typing import List
 from dataclasses import dataclass, field
 from numba import cuda, float32
 from numba.cuda.compiler import compile_ptx_for_current_device
-from math import cos, sin, tan, exp, log, log10, log2, pow
+from math import cos, sin, tan, exp, log, log10, log2, pow, tanh
 from operator import truediv
 import numpy as np
 from numba.cuda.testing import CUDATestCase, skip_on_cudasim
@@ -98,6 +98,15 @@ class TestFastMathOption(CUDATestCase):
                 'cos.approx.ftz.f32 ',
                 'div.approx.ftz.f32 '
             ], prec_unexpected=['sin.approx.ftz.f32 '])
+        )
+
+    def test_tanhf(self):
+        self._test_fast_math_unary(
+            tanh,
+            FastMathCriterion(
+                fast_expected=['tanh.approx.f32 '],
+                prec_unexpected=['tanh.approx.f32 ']
+            )
         )
 
     def test_expf(self):
