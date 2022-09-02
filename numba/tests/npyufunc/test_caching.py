a4e3b2a25525877ef1e1bd4c4b99ddf6a2834bb6
luk-f-a <lucioarjona@hotmail.com>
2022-09-02 21:50:24 +0200

    add ufunc tests

--- numba/tests/npyufunc/test_caching.py
+++ numba/tests/npyufunc/test_caching.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 import os.path
 import re
@@ -5,7 +6,8 @@ import subprocess
 
 import numpy as np
 
-from numba.tests.support import capture_cache_log
+from numba.tests.support import capture_cache_log, temp_directory, \
+    import_dynamic
 from numba.tests.test_caching import BaseCacheTest
 from numba.core import config
 import unittest
@@ -223,6 +225,211 @@ class TestCacheSpecificIssue(UfuncCacheTest):
         # this will fail out if the target context is not init'ed
         self.run_in_separate_process('direct_gufunc_cache_usecase()')
 
+
+class TestCachingModifiedFilesBase(BaseCacheTest):
+    # the file with a main function that will call another one
+    source_text_file1: str = ""
+    # the file with a secondary function which is called from the main
+    source_text_file2: str = ""
+    # an alternative version of the file with the secondary function
+    source_text_file2_alt: str = ""
+
+    def setUp(self):
+        self.tempdir = temp_directory('test_cache_file_modfiles2')
+        self.cache_dir = os.path.join(self.tempdir, "__pycache__")
+
+        self.file1 = os.path.join(self.tempdir, 'file1.py')
+        with open(self.file1, 'w') as fout:
+            print(self.source_text_file1, file=fout)
+
+        self.file2 = os.path.join(self.tempdir, 'file2.py')
+        with open(self.file2, 'w') as fout:
+            print(self.source_text_file2, file=fout)
+
+    def tearDown(self):
+        sys.modules.pop(self.modname, None)
+        sys.modules.pop("file2", None)
+        sys.path.remove(self.tempdir)
+        shutil.rmtree(self.tempdir)
+
+    @staticmethod
+    def import_modules(modnames, modfiles):
+        # Import a fresh version of the test modules.  All jitted functions
+        # in the test module will start anew and load overloads from
+        # the on-disk cache if possible.
+
+        assert len(modnames) == len(modfiles)
+        # all modules must be removed first
+        for modname in modnames:
+            old = sys.modules.pop(modname, None)
+            if old is not None:
+                # Make sure cached bytecode is removed
+                cached = [old.__cached__]
+                for fn in cached:
+                    try:
+                        os.unlink(fn)
+                    except FileNotFoundError:
+                        pass
+
+        mods = []
+        for modname, modfile in zip(modnames, modfiles):
+            mod = import_dynamic(modname)
+            mods.append(mod)
+        return mods
+
+    def run_fc_in_separate_process(self):
+        # Execute file1.py
+        popen = subprocess.Popen([sys.executable, self.file1],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        out, err = popen.communicate()
+        msg = f"stdout:\n{out.decode()}\n\nstderr:\n{err.decode()}"
+        self.assertEqual(popen.returncode, 0, msg=msg)
+
+        # Execute file2.py
+        popen = subprocess.Popen([sys.executable, self.file2],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        out, err = popen.communicate()
+        msg = f"stdout:\n{out.decode()}\n\nstderr:\n{err.decode()}"
+
+    def execute_fc_and_change_it(self, inner_cached):
+        # test cache is invalidated after source file modification
+        # inner_cached: boolean, whether the inner function is cached. This
+        # changes the number of cache files that are expected
+
+        self.modname = "file1"
+        self.modfile = self.file1
+        self.cache_dir = os.path.join(self.tempdir, "__pycache__")
+
+        # 1. execute original files once to populate cache
+        self.run_fc_in_separate_process()
+        # import function and verify cache is being used
+        import sys
+        sys.path.insert(0, self.tempdir)
+        file1 = self.import_module()
+        fc = file1.ufunc
+        # First round of execution to populate cache
+        self.assertPreciseEqual(fc(2), 2)
+        self.assertPreciseEqual(fc(2.5), 2.5)
+        # expected files: 2 index, 2 data for each function
+        self.check_pycache(3 + 3 * inner_cached)
+
+        # 2. Re-import module ane execute again, cached version should be used
+        del fc
+        del file1
+        file1, file2 = self.import_modules(
+            ["file1", "file2"], [self.file1, self.file2]
+        )
+        fc = file1.ufunc
+        self.assertPreciseEqual(fc(2), 2)
+        # expected files: 2 index, 2 data for each function
+        self.check_pycache(3 + 3 * inner_cached)
+        self.assertPreciseEqual(fc(2.5), 2.5)
+        # expected files: 2 index, 2 data for each function
+        self.check_pycache(3 + 3 * inner_cached)
+
+        # 3. modify file and reload
+        self.file2_alt = os.path.join(self.tempdir, 'file2.py')
+        with open(self.file2_alt, 'w') as fout:
+            print(self.source_text_file2_alt, file=fout)
+
+        file1, file2 = self.import_modules(
+            ["file1", "file2"], [self.file1, self.file2]
+        )
+        fc = file1.ufunc
+        # 4. Run again, results should change, cache should not be hit
+        self.assertPreciseEqual(fc(2), 3)
+        # 2 index, 2 data for foo function (2 from previous function2 versions
+        # one of which is overwritten by the new version), 2 for function2.
+        # Function2 has restarted its cache after the change
+        # and it has 2 files (1 new, 1 stale but out of the index
+        # which will be eventually overwriten)
+        self.check_pycache(3 + 3 * inner_cached)
+        self.assertPreciseEqual(fc(2.5), 3.5)
+        # expected files: 2 index, 2 data for foo, 2 for function2
+        self.check_pycache(3 + 3 * inner_cached)
+
+    def check_pycache(self, n):
+        c = self.cache_contents()
+        self.assertEqual(len(c), n, c)
+
+
+class TestCachingModifiedFiles(TestCachingModifiedFilesBase):
+    # This class tests a dispatcher calling another dispatcher which later
+    # changes. Both functions have cache=True
+
+    source_text_file1 = """
+from numba import vectorize
+from file2 import indirect_ufunc_core2
+
+@vectorize(["intp(intp)", "float64(float64)"], 
+    cache=True)
+def ufunc(inp):
+    return indirect_ufunc_core2(inp)
+"""
+    source_text_file2 = """
+from numba import njit
+@njit(cache=True)
+def indirect_ufunc_core1(inp):
+    return inp 
+
+@njit(cache=True)
+def indirect_ufunc_core2(inp):
+    return inp 
+"""
+
+    source_text_file2_alt = """
+from numba import njit
+@njit(cache=True)
+def indirect_ufunc_core1(inp):
+    return inp + 1
+
+@njit(cache=True)
+def indirect_ufunc_core2(inp):
+    return inp + 1
+"""
+
+    def test_invalidation(self, ):
+        self.execute_fc_and_change_it(inner_cached=True)
+
+
+class TestCachingModifiedFiles2(TestCachingModifiedFilesBase):
+    # This class tests a dispatcher calling another dispatcher which later
+    # changes. Only outer function has cache=True
+
+    source_text_file1 = """
+from numba import vectorize
+from file2 import indirect_ufunc_core2
+
+@vectorize(["intp(intp)", "float64(float64)"], cache=True)
+def ufunc(inp):
+    return indirect_ufunc_core2(inp)
+"""
+    source_text_file2 = """
+from numba import njit
+@njit
+def indirect_ufunc_core1(inp):
+    return inp 
+
+@njit
+def indirect_ufunc_core2(inp):
+    return inp 
+"""
+
+    source_text_file2_alt = """
+from numba import njit
+@njit
+def indirect_ufunc_core1(inp):
+    return inp + 1
+
+@nb.njit
+def indirect_ufunc_core2(inp):
+    return inp + 1
+"""
+
+    def test_invalidation(self, ):
+        self.execute_fc_and_change_it(inner_cached=False)
 
 if __name__ == '__main__':
     unittest.main()
