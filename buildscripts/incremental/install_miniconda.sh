684bfa6db7d1873e3e08f392d9db851e85406566
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-09-06 10:42:58 +0100

    Remove 32 bit linux support configurations from build scripts.
    As title.

--- buildscripts/incremental/install_miniconda.sh
+++ buildscripts/incremental/install_miniconda.sh
@@ -5,11 +5,7 @@ set -v -e
 # Install Miniconda
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    if [[ "$BITS32" == "yes" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
-    else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    fi
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 elif [[ "$unamestr" == 'Darwin' ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
 else
