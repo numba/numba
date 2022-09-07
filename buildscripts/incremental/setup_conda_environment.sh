3dfe14a9e5cdcaf6decec8933fb3f9d173db4ec3
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-09-07 16:27:06 +0100

    Try using a single conda solve in Azure CI.
    As title.

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -15,7 +15,6 @@ conda config --show
 CONDA_INSTALL="conda install -q -y"
 PIP_INSTALL="pip install -q"
 
-
 EXTRA_CHANNELS=""
 if [ "${USE_C3I_TEST_CHANNEL}" == "yes" ]; then
     EXTRA_CHANNELS="${EXTRA_CHANNELS} -c c3i_test"
@@ -33,7 +32,7 @@ conda list
 # Create a base env first and then add to it...
 # NOTE: gitpython is needed for CI testing to do the test slicing
 # NOTE: pyyaml is used to ensure that the Azure CI config is valid
-conda create -n $CONDA_ENV -q -y ${EXTRA_CHANNELS} python=$PYTHON numpy=$NUMPY pip gitpython pyyaml "setuptools<60"
+CONDA_INSTALL_ARGS=(python=$PYTHON numpy=$NUMPY pip gitpython pyyaml "\"setuptools<60\"")
 
 # Activate first
 set +v
@@ -48,43 +47,47 @@ if [ "${VANILLA_INSTALL}" != "yes" ]; then
     # dependencies, but exercised in the test suite
     # pexpect is used to run the gdb tests.
     # ipykernel is used for testing ipython behaviours.
-    $CONDA_INSTALL ${EXTRA_CHANNELS} cffi jinja2 ipython ipykernel pygments pexpect
+    CONDA_INSTALL_ARGS+=(cffi jinja2 ipython ipykernel pygments pexpect)
+
     if [[ "$NUMPY" == "1.23" ]] ; then
-        $CONDA_INSTALL ${EXTRA_CHANNELS} conda-forge::scipy
+        CONDA_INSTALL_ARGS+=(conda-forge::scipy)
     else
-        $CONDA_INSTALL ${EXTRA_CHANNELS} scipy
+        CONDA_INSTALL_ARGS+=(scipy)
     fi
 fi
 
 # Install the compiler toolchain and gdb (if available)
 if [[ $(uname) == Linux ]]; then
-    $CONDA_INSTALL gcc_linux-64 gxx_linux-64 gdb gdb-pretty-printer
+    CONDA_INSTALL_ARGS+=(gcc_linux-64 gxx_linux-64 gdb gdb-pretty-printer)
 elif  [[ $(uname) == Darwin ]]; then
-    $CONDA_INSTALL clang_osx-64 clangxx_osx-64
+    CONDA_INSTALL_ARGS+=(clang_osx-64 clangxx_osx-64)
     # Install llvm-openmp on OSX for headers during build and runtime during
     # testing
-    $CONDA_INSTALL llvm-openmp
+    CONDA_INSTALL_ARGS+=(llvm-openmp)
 fi
 
 # Install latest correct build
-$CONDA_INSTALL -c numba/label/dev llvmlite=0.40
+CONDA_INSTALL_ARGS+=(-c numba/label/dev llvmlite=0.40)
 
 # Install importlib-metadata for Python < 3.9
-if [ $PYTHON \< "3.9" ]; then $CONDA_INSTALL importlib_metadata; fi
+if [ $PYTHON \< "3.9" ]; then CONDA_INSTALL_ARGS+=(importlib_metadata); fi
 
 # Install dependencies for building the documentation
-if [ "$BUILD_DOC" == "yes" ]; then $CONDA_INSTALL sphinx=2.4.4 docutils=0.17 sphinx_rtd_theme pygments numpydoc; fi
+if [ "$BUILD_DOC" == "yes" ]; then CONDA_INSTALL_ARGS+=(sphinx=2.4.4 docutils=0.17 sphinx_rtd_theme pygments numpydoc); fi
 if [ "$BUILD_DOC" == "yes" ]; then $PIP_INSTALL rstcheck; fi
 # Install dependencies for code coverage (codecov.io)
 if [ "$RUN_COVERAGE" == "yes" ]; then $PIP_INSTALL codecov; fi
 # Install SVML
-if [ "$TEST_SVML" == "yes" ]; then $CONDA_INSTALL -c numba icc_rt; fi
+if [ "$TEST_SVML" == "yes" ]; then CONDA_INSTALL_ARGS+=(icc_rt); fi
 # Install Intel TBB parallel backend
-if [ "$TEST_THREADING" == "tbb" ]; then $CONDA_INSTALL -c numba tbb=2021 tbb-devel; fi
+if [ "$TEST_THREADING" == "tbb" ]; then CONDA_INSTALL_ARGS+=(tbb=2021 tbb-devel); fi
 # Install pickle5
 if [ "$TEST_PICKLE5" == "yes" ]; then $PIP_INSTALL pickle5; fi
 # Install typeguard
-if [ "$RUN_TYPEGUARD" == "yes" ]; then $CONDA_INSTALL conda-forge::typeguard; fi
+if [ "$RUN_TYPEGUARD" == "yes" ]; then CONDA_INSTALL_ARGS+=(conda-forge::typeguard); fi
+
+CONDA_ARGS_AS_LIST=$(printf "%s " "${CONDA_INSTALL_ARGS[@]}")
+$CONDA_INSTALL $EXTRA_CHANNELS $CONDA_ARGS_AS_LIST
 
 # environment dump for debug
 # echo "DEBUG ENV:"
