ba44f7a6d6e908d93cb34cca508f95013ec1a433
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-09-08 12:02:53 +0100

    Refactoring.
    * Use conda create, not conda install.
    * Push pip commands to last after env activation.
    * Refactor variable names to reflect use.

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -12,7 +12,7 @@ conda config --set show_channel_urls true
 conda info
 conda config --show
 
-CONDA_INSTALL="conda install -q -y"
+CONDA_CREATE="conda create -n ${CONDA_ENV} -q -y"
 PIP_INSTALL="pip install -q"
 
 EXTRA_CHANNELS=""
@@ -32,7 +32,7 @@ conda list
 # Create a base env first and then add to it...
 # NOTE: gitpython is needed for CI testing to do the test slicing
 # NOTE: pyyaml is used to ensure that the Azure CI config is valid
-CONDA_INSTALL_ARGS=(python=$PYTHON numpy=$NUMPY pip gitpython pyyaml "\"setuptools<60\"")
+CONDA_PACKAGES=(python=$PYTHON numpy=$NUMPY pip gitpython pyyaml "\"setuptools<60\"")
 
 # Install optional packages into activated env
 echo "PYTHON=$PYTHON"
@@ -42,46 +42,53 @@ if [ "${VANILLA_INSTALL}" != "yes" ]; then
     # dependencies, but exercised in the test suite
     # pexpect is used to run the gdb tests.
     # ipykernel is used for testing ipython behaviours.
-    CONDA_INSTALL_ARGS+=(cffi jinja2 ipython ipykernel pygments pexpect)
+    CONDA_PACKAGES+=(cffi jinja2 ipython ipykernel pygments pexpect)
 
     if [[ "$NUMPY" == "1.23" ]] ; then
-        CONDA_INSTALL_ARGS+=(conda-forge::scipy)
+        CONDA_PACKAGES+=(conda-forge::scipy)
     else
-        CONDA_INSTALL_ARGS+=(scipy)
+        CONDA_PACKAGES+=(scipy)
     fi
 fi
 
 # Install the compiler toolchain and gdb (if available)
 if [[ $(uname) == Linux ]]; then
-    CONDA_INSTALL_ARGS+=(gcc_linux-64 gxx_linux-64 gdb gdb-pretty-printer)
+    CONDA_PACKAGES+=(gcc_linux-64 gxx_linux-64 gdb gdb-pretty-printer)
 elif  [[ $(uname) == Darwin ]]; then
-    CONDA_INSTALL_ARGS+=(clang_osx-64 clangxx_osx-64)
+    CONDA_PACKAGES+=(clang_osx-64 clangxx_osx-64)
     # Install llvm-openmp on OSX for headers during build and runtime during
     # testing
-    CONDA_INSTALL_ARGS+=(llvm-openmp)
+    CONDA_PACKAGES+=(llvm-openmp)
 fi
 
 # Install latest correct build
 EXTRA_CHANNELS="-c numba/label/dev ${EXTRA_CHANNELS}"
-CONDA_INSTALL_ARGS+=(llvmlite=0.40)
+CONDA_PACKAGES+=(llvmlite=0.40)
 
 # Install importlib-metadata for Python < 3.9
-if [ $PYTHON \< "3.9" ]; then CONDA_INSTALL_ARGS+=(importlib_metadata); fi
+if [ $PYTHON \< "3.9" ]; then CONDA_PACKAGES+=(importlib_metadata); fi
 
 # Install dependencies for building the documentation
-if [ "$BUILD_DOC" == "yes" ]; then CONDA_INSTALL_ARGS+=(sphinx=2.4.4 docutils=0.17 sphinx_rtd_theme pygments numpydoc); fi
+if [ "$BUILD_DOC" == "yes" ]; then CONDA_PACKAGES+=(sphinx=2.4.4 docutils=0.17 sphinx_rtd_theme pygments numpydoc); fi
+# Install SVML
+if [ "$TEST_SVML" == "yes" ]; then CONDA_PACKAGES+=(icc_rt); fi
+# Install Intel TBB parallel backend
+if [ "$TEST_THREADING" == "tbb" ]; then CONDA_PACKAGES+=(tbb=2021 tbb-devel); fi
+# Install typeguard
+if [ "$RUN_TYPEGUARD" == "yes" ]; then CONDA_PACKAGES+=(conda-forge::typeguard); fi
+
+CONDA_PACKAGES_AS_LIST=$(printf "%s " "${CONDA_PACKAGES[@]}")
+echo "Running: $CONDA_CREATE $EXTRA_CHANNELS $CONDA_PACKAGES_AS_LIST"
+$CONDA_CREATE $EXTRA_CHANNELS $CONDA_PACKAGES_AS_LIST
+
+# Anything pip related goes last, conda env solution must be installed first
+# Activate env so pip installs things into the right place.
+set +v
+source activate $CONDA_ENV
+set -v
+# Install rstcheck to check RST file changes
 if [ "$BUILD_DOC" == "yes" ]; then $PIP_INSTALL rstcheck; fi
 # Install dependencies for code coverage (codecov.io)
 if [ "$RUN_COVERAGE" == "yes" ]; then $PIP_INSTALL codecov; fi
-# Install SVML
-if [ "$TEST_SVML" == "yes" ]; then CONDA_INSTALL_ARGS+=(icc_rt); fi
-# Install Intel TBB parallel backend
-if [ "$TEST_THREADING" == "tbb" ]; then CONDA_INSTALL_ARGS+=(tbb=2021 tbb-devel); fi
 # Install pickle5
 if [ "$TEST_PICKLE5" == "yes" ]; then $PIP_INSTALL pickle5; fi
-# Install typeguard
-if [ "$RUN_TYPEGUARD" == "yes" ]; then CONDA_INSTALL_ARGS+=(conda-forge::typeguard); fi
-
-CONDA_ARGS_AS_LIST=$(printf "%s " "${CONDA_INSTALL_ARGS[@]}")
-echo "Running: $CONDA_INSTALL $EXTRA_CHANNELS $CONDA_ARGS_AS_LIST"
-$CONDA_INSTALL $EXTRA_CHANNELS $CONDA_ARGS_AS_LIST
