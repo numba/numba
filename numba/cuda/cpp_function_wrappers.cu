c3dfbe0992dd9d68897629f12f224d1bffd4764b
Michael Collison <collison956@gmail.com>
2022-09-18 23:59:50 -0400

    Share common code across all unary functions

--- numba/cuda/cpp_function_wrappers.cu
+++ numba/cuda/cpp_function_wrappers.cu
@@ -2,6 +2,19 @@
 
 #define FNDEF(fname) __numba_wrapper_ ## fname
 
+#define UNARY_FUNCTION(fname) extern "C" __device__ int\
+  FNDEF(fname)(						\
+    short* return_value,\
+  short x\
+)\
+{\
+    __half retval = fname(__short_as_half (x));\
+\
+  *return_value = __half_as_short (retval);\
+  /* Signal that no Python exception occurred */	\
+  return 0;\
+}\
+
 extern "C" __device__ int
 FNDEF(hdiv)(
   short* return_value,
@@ -16,198 +29,20 @@ FNDEF(hdiv)(
   return 0;
 }
 
-extern "C" __device__ int
-FNDEF(hsin)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hsin(__short_as_half (x));
-  
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
+UNARY_FUNCTION(hsin)
+UNARY_FUNCTION(hcos)
+UNARY_FUNCTION(hlog)
+UNARY_FUNCTION(hlog10)
+UNARY_FUNCTION(hlog2)
+UNARY_FUNCTION(hexp)
+UNARY_FUNCTION(hexp10)
+UNARY_FUNCTION(hexp2)
+UNARY_FUNCTION(hsqrt)
+UNARY_FUNCTION(hrsqrt)
+UNARY_FUNCTION(hfloor)
+UNARY_FUNCTION(hceil)
+UNARY_FUNCTION(hrcp)
+UNARY_FUNCTION(hrint)
+UNARY_FUNCTION(htrunc)
 
-extern "C" __device__ int
-FNDEF(hcos)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hcos(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hlog)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hlog(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hlog10)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hlog10(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hlog2)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hlog2(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hexp)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hexp(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hexp10)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hexp10(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hexp2)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hexp2(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hsqrt)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hsqrt(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hrsqrt)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hrsqrt(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hfloor)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hfloor(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hceil)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hceil(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hrcp)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hrcp(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(hrint)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = hrint(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
-
-extern "C" __device__ int
-FNDEF(htrunc)(
-  short* return_value,
-  short x
-)
-{
-  __half retval = htrunc(__short_as_half (x));
-
-  *return_value = __half_as_short (retval);
-  // Signal that no Python exception occurred
-  return 0;
-}
 
