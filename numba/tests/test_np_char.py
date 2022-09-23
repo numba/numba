a764cc4e78cf4d321b9ba9a0a7cbe821d425df53
Nima Mehrani <ensigntec@gmail.com>
2022-09-22 22:04:35 -0400

    Repartition test suite and reduce test duration
    This update partitions the meta function compare_chararrays into a separate test and segregates the assertRaisesRegex checks into categories based on error type.  In addition, test argument array size is reduced, resulting in a shortened test duration.

--- numba/tests/test_np_char.py
+++ numba/tests/test_np_char.py
@@ -73,24 +73,24 @@ class TestComparisonOperators(TestCase):
 
     @classmethod
     def set_arguments(cls):
-        length = 500
+        length = 300
         np.random.seed(42)
-        # 500 UTF-32 strings of length 1 to 200 in range(1, sys.maxunicode)
+        # 300 UTF-32 strings of length 1 to 200 in range(1, sys.maxunicode)
         # Python 3.7 can not decode unicode in range(55296, 57344)
         q = np.array([''.join([chr(np.random.randint(1, 55295)) if i % 2
                                else chr(np.random.randint(57345, maxunicode))
                                for i in range(np.random.randint(1, 200))])
                       for _ in range(length)])
-        # 500 ASCII strings of length 0 to 50
+        # 300 ASCII strings of length 0 to 50
         r = np.array([''.join([chr(np.random.randint(1, 127))
                                for _ in range(np.random.randint(0, 50))])
                       for _ in range(length)])
-        s: tuple = (np.array(['abc', 'def'] * length),
-                    np.array(['cba', 'fed'] * length))
-        t: tuple = (np.array(['ab', 'bc'] * length),
-                    np.array(['bc', 'ab'] * length))
-        u: tuple = (np.array(['ba', 'cb'] * length),
-                    np.array(['cb', 'ba'] * length))
+        s: tuple = (np.array(['abc', 'def'] * 2),
+                    np.array(['cba', 'fed'] * 2))
+        t: tuple = (np.array(['ab', 'bc'] * 2),
+                    np.array(['bc', 'ab'] * 2))
+        u: tuple = (np.array(['ba', 'cb'] * 2),
+                    np.array(['cb', 'ba'] * 2))
         v = np.random.choice(['abcd', 'abc', 'abcde'], length)
 
         # Whitespace to end of strings & single ASCII characters in range(0, 33)
@@ -133,37 +133,44 @@ class TestComparisonOperators(TestCase):
         setattr(cls, 'byte_args', byte_args)
         setattr(cls, 'string_args', string_args)
 
-    def test_comparison_operators(self):
+    def test_comparisons(self):
 
         def check_output(pyfunc_, cfunc_, x1, x2):
             expected = pyfunc_(x1, x2)
             got = cfunc_(x1, x2)
             self.assertPreciseEqual(expected, got)
 
-        def check_exceptions(cfunc_, x1, x2):
-            em = ".*shape mismatch: objects cannot be broadcast to.*"
-            with self.assertRaisesRegex(ValueError, em):
-                cfunc_(x1, np.empty_like(x1[:1]))
-                cfunc_(x1[:10], x2[:5])
+        def check_shape_exception(cfunc_, x1):
+            error_msg = ".*shape mismatch: objects cannot be broadcast to.*"
+            with self.assertRaisesRegex(ValueError, error_msg):
+                cfunc_(x1, x1[:2])
 
-            em = ".*comparison of non-string arrays.*"
-            with self.assertRaisesRegex((TypingError, TypeError), em):
-                cfunc_(x1[:5], None)
-                cfunc_('abc', 123)
+        def check_comparison_exception(cfunc_, x1):
+            error_msg = ".*comparison of non-string arrays.*"
+            accepted_errors = (TypingError, TypeError)
+            with self.assertRaisesRegex(accepted_errors, error_msg):
+                cfunc_(x1, None)
+            with self.assertRaisesRegex(accepted_errors, error_msg):
+                cfunc_(x1, 123)
 
-            em = ".*NotImplemented.*"
-            with self.assertRaisesRegex((TypingError, NotImplementedError), em):
-                cfunc_(x1.astype('U'), x1.astype('S'))
+        def check_notimplemented_exception(cfunc_, x1):
+            error_msg = ".*NotImplemented.*"
+            accepted_errors = (TypingError, NotImplementedError)
+            with self.assertRaisesRegex(accepted_errors, error_msg):
+                cfunc_(x1.astype('S'), x1.astype('U'))
+            with self.assertRaisesRegex(accepted_errors, error_msg):
                 cfunc_('abc', b'abc')
 
         pyfuncs = (np_char_equal, np_char_not_equal,
                    np_char_greater_equal, np_char_greater,
                    np_char_less_equal, np_char_less)
 
-        args = self.string_args[0]
+        arg = np.array(['abc', 'def', 'hij'], 'S')
         for pyfunc in pyfuncs:
             cfunc = jit(nopython=True)(pyfunc)
-            check_exceptions(cfunc, *args)
+            check_shape_exception(cfunc, arg)
+            check_comparison_exception(cfunc, arg)
+            check_notimplemented_exception(cfunc, arg)
 
         for pyfunc in pyfuncs:
             cfunc = jit(nopython=True)(pyfunc)
@@ -175,31 +182,38 @@ class TestComparisonOperators(TestCase):
                 check_output(pyfunc, cfunc, *args)
                 check_output(pyfunc, cfunc, *args[::-1])
 
-        byte_args = list(_pack_arguments(self.byte_args,
-                                         [('==', '!=', '>=', '>', '<', '<='),
-                                          (True, False)]))
+    def test_compare_chararrays(self):
 
-        string_args = list(_pack_arguments(self.string_args,
-                                           [('==', '!=', '>=', '>', '<', '<='),
-                                            (True, False)]))
-
-        def check_compare_chararrays(a1, a2, cmp, rstrip):
-            pyfunc_ = np_char_compare_chararrays
-            cfunc_ = jit(nopython=True)(pyfunc_)
-
-            expected = pyfunc_(a1, a2, cmp, rstrip)
-            got = cfunc_(a1, a2, cmp, rstrip)
+        def check_output(a1, a2, cmp, rstrip):
+            expected = pyfunc(a1, a2, cmp, rstrip)
+            got = cfunc(a1, a2, cmp, rstrip)
             self.assertPreciseEqual(expected, got)
 
-            em = ".*a bytes-like object is required.*"
-            with self.assertRaisesRegex((TypingError, TypeError), em):
-                cfunc_(a1, a2, 123, rstrip)
+        def check_cmp_exception():
+            cmp = 123
+            error_msg = ".*a bytes-like object is required.*"
+            accepted_errors = (TypingError, TypeError)
+            with self.assertRaisesRegex(accepted_errors, error_msg):
+                cfunc('abc', 'abc', cmp, True)
+
+        pyfunc = np_char_compare_chararrays
+        cfunc = jit(nopython=True)(pyfunc)
+
+        byte_args = _pack_arguments(self.byte_args[:1],
+                                    [('==', '!=', '>=', '>', '<', '<='),
+                                     (True, False)])
+
+        string_args = _pack_arguments(self.string_args[:1],
+                                      [('==', '!=', '>=', '>', '<', '<='),
+                                       (True, False)])
+
+        check_cmp_exception()
 
         for args in byte_args:
-            check_compare_chararrays(*args)
+            check_output(*args)
 
         for args in string_args:
-            check_compare_chararrays(*args)
+            check_output(*args)
 
 
 TestComparisonOperators.set_arguments()
