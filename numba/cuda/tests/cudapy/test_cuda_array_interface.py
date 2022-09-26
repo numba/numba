d4bade8c7947ee8fd8ac4d5532a75ab2ea4df52d
Michael Collison <collison956@gmail.com>
2022-09-26 16:18:05 -0400

    Fix flake8 issue

--- numba/cuda/tests/cudapy/test_cuda_array_interface.py
+++ numba/cuda/tests/cudapy/test_cuda_array_interface.py
@@ -3,7 +3,8 @@ import numpy as np
 from numba import vectorize, guvectorize
 from numba import cuda
 from numba.cuda.cudadrv import driver
-from numba.cuda.testing import skip_unless_cuda_python, unittest, ContextResettingTestCase, ForeignArray
+from numba.cuda.testing import (skip_unless_cuda_python, unittest,
+                                ContextResettingTestCase, ForeignArray)
 from numba.cuda.testing import skip_on_cudasim, skip_if_external_memmgr
 from numba.tests.support import linux_only, override_config
 from unittest.mock import call, patch
