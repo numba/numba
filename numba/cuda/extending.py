d0ea1cd9b2b30da28b04230ffe769a7b224aa66c
Guilherme Leobas <gleobas@quansight.com>
2022-10-19 16:49:34 +0000

    re-enable cmath on CUDA

--- numba/cuda/extending.py
+++ numba/cuda/extending.py
@@ -2,6 +2,13 @@
 Added for symmetry with the core API
 """
 
+from functools import partial
 from numba.core.extending import intrinsic as _intrinsic
+from numba.core.extending import overload as _overload
+from numba.core.extending import overload_method as _overload_method
+from numba.core.extending import overload_attribute as _overload_attribute
 
 intrinsic = _intrinsic(target='cuda')
+overload = partial(_overload, target='cuda')
+overload_method = partial(_overload_method, target='cuda')
+overload_attribute = partial(_overload_attribute, target='cuda')
