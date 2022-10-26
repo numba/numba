b16198a570c96372f0b5e89ba472f80b2803e812
kc611 <ckaustubhm06@gmail.com>
2022-10-26 16:55:55 +0530

    Added advanced indexing support for multiple subspaces

--- numba/core/typing/arraydecl.py
+++ numba/core/typing/arraydecl.py
@@ -102,12 +102,6 @@ def get_array_index_type(ary, idx):
 
     if advanced:
         ndim += max(array_indices)
-        if num_subspaces > 1:
-            # Advanced indexing limitation # 3
-            msg = ("Using more than one indexing subspace is unsupported."
-                   " An indexing subspace is a group of one or more"
-                   " consecutive indices comprising integer or array types.")
-            raise NumbaTypeError(msg)
 
     # Only Numpy arrays support advanced indexing
     if advanced and not isinstance(ary, types.Array):
