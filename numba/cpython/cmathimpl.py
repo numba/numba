c0a6fe318cb3c3f97d1fd89c8f8a4b5bbfe0f039
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-11-02 13:29:28 -0300

    import errors

--- numba/cpython/cmathimpl.py
+++ numba/cpython/cmathimpl.py
@@ -7,7 +7,7 @@ import cmath
 import math
 
 from numba.core.imputils import Registry, impl_ret_untracked
-from numba.core import types, cgutils
+from numba.core import types, cgutils, errors
 from numba.core.typing import signature
 from numba.cpython import builtins, mathimpl
 from numba.core.extending import overload, intrinsic
