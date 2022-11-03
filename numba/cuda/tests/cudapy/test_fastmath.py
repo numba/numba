6fc34745a8abd76ad55cfe10d3284361d3e77822
Michael Collison <collison956@gmail.com>
2022-11-03 14:49:55 -0400

    New test cases for tanhf for compute capability 7.0 and 7.5

--- numba/cuda/tests/cudapy/test_fastmath.py
+++ numba/cuda/tests/cudapy/test_fastmath.py
@@ -1,7 +1,7 @@
 from typing import List
 from dataclasses import dataclass, field
 from numba import cuda, float32
-from numba.cuda.compiler import compile_ptx_for_current_device
+from numba.cuda.compiler import compile_ptx_for_current_device, compile_ptx
 from math import cos, sin, tan, exp, log, log10, log2, pow, tanh
 from operator import truediv
 import numpy as np
@@ -114,16 +114,27 @@ class TestFastMathOption(CUDATestCase):
             )
         )
 
-    @skip_unless_less_cc_75
-    def test_tanhf_no_fastmath(self):
-        self._test_fast_math_unary(
-            tanh,
-            FastMathCriterion(
-                fast_expected=['ex2.approx.ftz.f32 ',
-                               'rcp.approx.ftz.f32 '],
+    @skip_unless_cc_75
+    def test_tanhf_compile_ptx(self):
+        def tanh_kernel(r, x):
+            r[0] = tanh(x)
+
+        def tanh_common_test(cc, criterion):
+            fastptx, _ = compile_ptx(tanh_kernel, (float32[::1], float32), fastmath=True, cc=cc)
+            precptx, _ = compile_ptx(tanh_kernel, (float32[::1], float32), cc=cc)
+            criterion.check(self, fastptx, precptx)
+
+        tanh_common_test(cc=(7,5), criterion=FastMathCriterion(
+                fast_expected=['tanh.approx.f32 '],
                 prec_unexpected=['tanh.approx.f32 ']
-            )
-        )
+            ))
+        
+        tanh_common_test(cc=(7, 0),
+                         criterion=FastMathCriterion(
+            fast_expected=['ex2.approx.ftz.f32 ',
+                           'rcp.approx.ftz.f32 '],
+            prec_unexpected=['tanh.approx.f32 ']))
+
 
     def test_expf(self):
         self._test_fast_math_unary(
