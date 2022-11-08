a1dad1fd8a0894c1816da277d5fd58215736121c
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2022-11-08 09:28:10 -0600

    Apply suggestions from code review
    Co-authored-by: stuartarchibald <stuartarchibald@users.noreply.github.com>

--- numba/tests/support.py
+++ numba/tests/support.py
@@ -126,8 +126,8 @@ skip_ppc64le_issue6465 = unittest.skipIf(platform.machine() == 'ppc64le',
 _uname = platform.uname()
 IS_OSX_ARM64 = _uname.system == 'Darwin' and _uname.machine == 'arm64'
 skip_m1_llvm_rtdyld_failure  = unittest.skipIf(IS_OSX_ARM64,
-    "skip tests that contributes to trigger LLVM RuntimeDyLd AssertionError on "
-    "OSX arm64. (numba#8567)")
+    "skip tests that contribute to triggering an AssertionError in LLVM's "
+    "RuntimeDyLd on OSX arm64. (see: numba#8567)")
 
 skip_m1_fenv_errors = unittest.skipIf(IS_OSX_ARM64,
     "fenv.h-like functionality unreliable on OSX arm64")
