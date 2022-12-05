25a89afa5e1c86687f7b87965a5ae0bcb9f49db3
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2022-12-05 13:14:33 +0000

    Update `test_extended_arg` to use CodeType.replace()
    As title. This also permits the removal of
    `numba.tests.support.tweak_code`.
    
    The `test_extended_arg.py` file is made flake8 compliant.

--- numba/tests/support.py
+++ numba/tests/support.py
@@ -674,40 +674,6 @@ def compile_function(name, code, globs):
     eval(co, globs, ns)
     return ns[name]
 
-def tweak_code(func, codestring=None, consts=None):
-    """
-    Tweak the code object of the given function by replacing its
-    *codestring* (a bytes object) and *consts* tuple, optionally.
-    """
-    co = func.__code__
-    tp = type(co)
-    if codestring is None:
-        codestring = co.co_code
-    if consts is None:
-        consts = co.co_consts
-    if utils.PYVERSION >= (3, 11):
-        new_code = tp(co.co_argcount, co.co_posonlyargcount,
-                      co.co_kwonlyargcount, co.co_nlocals,
-                      co.co_stacksize, co.co_flags, codestring,
-                      consts, co.co_names, co.co_varnames,
-                      co.co_filename, co.co_name, co.co_qualname,
-                      co.co_firstlineno, co.co_lnotab, co.co_exceptiontable,
-                      co.co_freevars, co.co_cellvars)
-    elif utils.PYVERSION >= (3, 8):
-        new_code = tp(co.co_argcount, co.co_posonlyargcount,
-                      co.co_kwonlyargcount, co.co_nlocals,
-                      co.co_stacksize, co.co_flags, codestring,
-                      consts, co.co_names, co.co_varnames,
-                      co.co_filename, co.co_name, co.co_firstlineno,
-                      co.co_lnotab)
-    else:
-        new_code = tp(co.co_argcount, co.co_kwonlyargcount, co.co_nlocals,
-                      co.co_stacksize, co.co_flags, codestring,
-                      consts, co.co_names, co.co_varnames,
-                      co.co_filename, co.co_name, co.co_firstlineno,
-                      co.co_lnotab)
-    func.__code__ = new_code
-
 
 _trashcan_dir = 'numba-tests'
 
