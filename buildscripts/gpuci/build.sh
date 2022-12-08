843cb751370c625433e26e9a8b4f617dc52d879e
Graham Markall <gmarkall@nvidia.com>
2022-12-08 11:02:34 +0000

    Switch back to rapidsai channel for cubinlinker
    The latest cubinlinker packages have now been copied from the gmarkall
    channel to the rapidsai channel.

--- buildscripts/gpuci/build.sh
+++ buildscripts/gpuci/build.sh
@@ -54,7 +54,7 @@ gpuci_logger "Create testing env"
 gpuci_mamba_retry create -n numba_ci -y \
                   "python=${PYTHON_VER}" \
                   "cudatoolkit=${CUDA_TOOLKIT_VER}" \
-                  "gmarkall::cubinlinker" \
+                  "rapidsai::cubinlinker" \
                   "conda-forge::ptxcompiler" \
                   "numba/label/dev::llvmlite" \
                   "numpy=${NUMPY_VER}" \
