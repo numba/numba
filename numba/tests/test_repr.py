ca04af8d16a4685fb237b30f1117c50b2eb73d24
luk-f-a <lucioarjona@hotmail.com>
2023-01-16 21:59:46 +0100

    applying review suggestions

--- numba/tests/test_repr.py
+++ numba/tests/test_repr.py
@@ -7,19 +7,30 @@ from numba.core import types
 from numba.typed import List, Dict
 
 NB_TYPES = [
-    types.Array, types.NestedArray, types.int64, types.float64,
-    types.unicode_type, types.Record, types.UnicodeCharSeq, types.UniTuple,
-    types.List, types.Tuple, types.DictType, types.ListType, types.Set,
-    types.bool_
-]
+    types.Array,
+    types.NestedArray,
+    types.bool_,
+    types.unicode_type,
+    types.Record,
+    types.UnicodeCharSeq,
+    types.UniTuple,
+    types.List,
+    types.Tuple,
+    types.DictType,
+    types.ListType,
+    types.Set,
+] + list(types.number_domain)
 
 
 class TestRepr(TestCase):
-    def check_repr(self, val):
-        ty = typeof(val)
+    def setUp(self) -> None:
         tys_ns = {ty.__name__: ty for ty in NB_TYPES if hasattr(ty, "__name__")}
         tys_ns.update({ty.name: ty for ty in NB_TYPES if hasattr(ty, "name")})
-        ty2 = eval(repr(ty), tys_ns)
+        self.tys_ns = tys_ns
+
+    def check_repr(self, val):
+        ty = typeof(val)
+        ty2 = eval(repr(ty), self.tys_ns)
         self.assertEqual(ty, ty2)
 
     def test_types(self):
@@ -27,10 +38,8 @@ class TestRepr(TestCase):
         rec_dtype = [("a", "f8"), ("b", "U8"), ("c", "i8", (2, 3))]
         nb_dict = Dict()
         nb_dict['a'] = 1
-        # tests cases
+        # tests cases: list of different types + list comp of number types
         val_types_cases = [
-            1,
-            1.2,
             True,
             "a",
             (1, 2),
@@ -44,7 +53,8 @@ class TestRepr(TestCase):
             nb_dict,
             List([1, 2]),
             {1, 2},
-        ]
+        ] + [number(1.1) for number in types.number_domain]
+
         for val in val_types_cases:
             self.check_repr(val)
 
