a9ffa8c3f151c3286f5793f72b0b7c25f24b9c5c
Graham Markall <gmarkall@nvidia.com>
2023-01-18 16:58:12 +0000

    CUDA: Warn when requesting debug and lineinfo
    Also we add lineinfo as an explicit kwarg to the `@cuda.jit` decorator.

--- numba/cuda/tests/cudapy/test_lineinfo.py
+++ numba/cuda/tests/cudapy/test_lineinfo.py
@@ -1,9 +1,10 @@
-from numba.cuda.testing import skip_on_cudasim
 from numba import cuda, float32, int32
-from numba.cuda.testing import CUDATestCase
+from numba.core.errors import NumbaInvalidConfigWarning
+from numba.cuda.testing import CUDATestCase, skip_on_cudasim
 from numba.cuda.cudadrv.nvvm import NVVM
 import re
 import unittest
+import warnings
 
 
 @skip_on_cudasim('Simulator does not produce lineinfo')
@@ -202,6 +203,19 @@ class TestCudaLineInfo(CUDATestCase):
         self.assertEqual(subprograms, expected_subprograms,
                          f'"Expected {expected_subprograms} DISubprograms; '
                          f'got {subprograms}')
+
+    def test_debug_and_lineinfo_warning(self):
+        with warnings.catch_warnings(record=True) as w:
+            # We pass opt=False to prevent the warning about opt and debug
+            # occurring as well
+            @cuda.jit(debug=True, lineinfo=True, opt=False)
+            def f():
+                pass
+
+        self.assertEqual(len(w), 1)
+        self.assertEqual(w[0].category, NumbaInvalidConfigWarning)
+        self.assertIn('debug and lineinfo are mutually exclusive',
+                      str(w[0].message))
 
 
 if __name__ == '__main__':
