f0e9adbbfd200a2dc88392ba87cfd51dcdc7252c
Andre Masella <andre@masella.name>
2023-02-02 13:57:40 -0500

    DO NOT MERGE: LLVM14 TESTING INFRASTRUCTURE

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -62,7 +62,7 @@ elif  [[ $(uname) == Darwin ]]; then
 fi
 
 # Install latest correct build
-$CONDA_INSTALL -c numba/label/dev llvmlite=0.40
+$CONDA_INSTALL -c numba/label/dev "llvmlite=0.40.0dev0llvm14*"
 
 # Install importlib-metadata for Python < 3.9
 if [ $PYTHON \< "3.9" ]; then $CONDA_INSTALL importlib_metadata; fi
