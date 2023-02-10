5b45803cefb4724046b2a7e10becba35a3fb90a1
Andre Masella <andre@masella.name>
2023-02-10 16:40:46 -0500

    Add additional refprune test
    As titled

--- numba/tests/test_refop_pruning.py
+++ numba/tests/test_refop_pruning.py
@@ -122,6 +122,22 @@ class TestRefOpPruning(TestCase):
             self.check(func, (types.intp), basicblock=True, diamond=False,
                        fanout=True, fanout_raise=False)
 
+    def test_fanout_3(self):
+        # fanout with raise
+        def func(n):
+            ary = np.arange(n)
+            # basically an impl of array.sum
+            c = 0
+            # The raise is from StopIteration of next(iterator) implicit in
+            # the for loop
+            for v in np.nditer(ary):
+                c += v.item()
+            return 1
+
+        with set_refprune_flags('per_bb,fanout_raise'):
+            self.check(func, (types.intp), basicblock=True, diamond=False,
+                       fanout=False, fanout_raise=True)
+
 
 class TestRefPruneFlags(TestCase):
     def setUp(self):
