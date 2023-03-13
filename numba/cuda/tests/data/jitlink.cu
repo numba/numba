ccf2415c170792b186b9e780722676e3ce02bdc5
Graham Markall <gmarkall@nvidia.com>
2023-03-13 13:33:36 +0000

    CUDA jitlink.cu / .ptx: Re-target to sm_50
    This makes the test easier to run on some devices that are still in use.
    
    Noted in out-of-band discussions on PR #8167.

--- numba/cuda/tests/data/jitlink.cu
+++ numba/cuda/tests/data/jitlink.cu
@@ -1,6 +1,6 @@
 // Compile with:
 //
-//   nvcc -gencode arch=compute_53,code=compute_53 -rdc true -ptx jitlink.cu
+//   nvcc -gencode arch=compute_50,code=compute_50 -rdc true -ptx jitlink.cu
 //
 // using the oldest supported toolkit version (10.2 at the time of writing).
 
