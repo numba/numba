2c917db64086aa3abca81ebf9aec6525c1b150bf
Andre Masella <andre@masella.name>
2023-03-13 17:03:52 -0400

    Add GitHub Action to build a release
    As titled.

---
+++ buildscripts/github/install_numpy.ps1
@@ -0,0 +1,11 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
+
+& pip install numpy
---
+++ buildscripts/github/install_numpy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pip install numpy
