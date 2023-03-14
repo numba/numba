5633a1b832b52465f9929ad4841c4edd49ca3cb6
Kyan Cheung <kyancheung@protonmail.com>
2023-03-14 19:26:33 +0800

    Update test_libdevice.py

--- numba/cuda/tests/cudapy/test_libdevice.py
+++ numba/cuda/tests/cudapy/test_libdevice.py
@@ -152,7 +152,7 @@ def make_test_call(libname):
             pyargs = pyreturns + pyargs
         else:
             pyargs.insert(0, sig.return_type[::1])
-        
+
         pyargs = tuple(pyargs)
         ptx, resty = compile_ptx(pyfunc, pyargs)
 
