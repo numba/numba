d2d13056f0188909a194e42b9c6d844c59c5fd81
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2023-03-22 11:49:20 +0000

    Fix failing typeguard test.
    `typeguard` 3+ uses its own exception classes to report type
    violations opposed to `TypeError`, the patch accommodates this.
    
    Closes #8835

--- numba/tests/test_typeguard.py
+++ numba/tests/test_typeguard.py
@@ -16,12 +16,19 @@ def guard_ret(val) -> int:
 
 @skip_unless_typeguard
 class TestTypeGuard(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        import typeguard
+        # typeguard 3+ uses typeguard.TypeCheckError, 2.x uses TypeError
+        cls._exception_type = getattr(typeguard, 'TypeCheckError', TypeError)
+
     def test_check_args(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(self._exception_type):
             guard_args(float(1.2))
 
     def test_check_ret(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(self._exception_type):
             guard_ret(float(1.2))
 
     def test_check_does_not_work_with_inner_func(self):
