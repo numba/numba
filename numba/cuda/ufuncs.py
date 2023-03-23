37a74faeac24c43665afa924305f4a0e73b74023
Graham Markall <gmarkall@nvidia.com>
2023-03-23 12:19:26 +0000

    Add module docstring to numba.cuda.ufuncs

--- numba/cuda/ufuncs.py
+++ numba/cuda/ufuncs.py
@@ -1,3 +1,11 @@
+"""Contains information on how to translate different ufuncs for the CUDA
+target. It is a database of different ufuncs and how each of its loops maps to
+a function that implements the inner kernel of that ufunc (the inner kernel
+being the per-element function).
+
+Use get_ufunc_info() to get the information related to a ufunc.
+"""
+
 import math
 import numpy as np
 from functools import lru_cache
