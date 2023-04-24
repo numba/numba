1dd1d836cf4fb6d369e5e78c19f3d618076fedc1
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2023-04-24 12:43:58 +0100

    Remove `run_nullary_func` from numba/tests/test_sets.py
    As title.

--- numba/tests/test_sets.py
+++ numba/tests/test_sets.py
@@ -8,7 +8,7 @@ from numba.core.errors import TypingError
 
 import numpy as np
 
-from numba import jit
+from numba import jit, njit
 from numba.tests.support import (TestCase, enable_pyobj_flags, MemoryLeakMixin,
                                  compile_function)
 
@@ -304,16 +304,22 @@ class BaseTest(MemoryLeakMixin, TestCase):
 
 class TestSetLiterals(BaseTest):
 
-    def test_build_set(self, flags=enable_pyobj_flags):
+    def check(self, pyfunc):
+        cfunc = njit(pyfunc)
+        expected = pyfunc()
+        got = cfunc()
+        self.assertPreciseEqual(expected, got)
+        return got, expected
+
+    def test_build_set(self):
         pyfunc = set_literal_return_usecase((1, 2, 3, 2))
-        self.run_nullary_func(pyfunc, flags=flags)
+        self.check(pyfunc)
 
     def test_build_heterogeneous_set(self, flags=enable_pyobj_flags):
         pyfunc = set_literal_return_usecase((1, 2.0, 3j, 2))
-        self.run_nullary_func(pyfunc, flags=flags)
+        self.check(pyfunc)
         pyfunc = set_literal_return_usecase((2.0, 2))
-        got, expected = self.run_nullary_func(pyfunc, flags=flags)
-
+        got, expected = self.check(pyfunc)
         self.assertIs(type(got.pop()), type(expected.pop()))
 
     def test_build_set_nopython(self):
