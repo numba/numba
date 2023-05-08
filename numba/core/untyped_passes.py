33378fe59d3ac4ec2bc1e557504ca98fc6c1a672
Guilherme Leobas <guilhermeleobas@gmail.com>
2023-05-08 10:52:56 -0300

    Fix issue 8939

--- numba/core/untyped_passes.py
+++ numba/core/untyped_passes.py
@@ -906,8 +906,12 @@ class MixedContainerUnroller(FunctionPass):
             new_var_dict = {}
             for name, var in var_table.items():
                 scope = switch_ir.blocks[lbl].scope
-                new_var_dict[name] = scope.define(
-                    f"v{branch_ty}_{name}", var.loc).name
+                if isinstance(branch_ty, types.Literal):
+                    new_var_dict[name] = scope.define(
+                        f"v{branch_ty.literal_value}_{name}", var.loc).name
+                else:
+                    new_var_dict[name] = scope.define(
+                        f"v{branch_ty}_{name}", var.loc).name
             replace_var_names(loop_blocks, new_var_dict)
 
             # clobber the sentinel body and then stuff in the rest
