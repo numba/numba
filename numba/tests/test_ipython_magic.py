2ddf6683f2d76c8ec6df9a584d81719171fd13fb
Matthew Murray <matthewmurray711@gmail.com>
2023-05-15 20:10:48 -0400

    Dont set ip attribute

--- numba/tests/test_ipython_magic.py
+++ numba/tests/test_ipython_magic.py
@@ -14,12 +14,9 @@ x = f(41.0)
 
 class IPythonMagic(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.ip = TerminalInteractiveShell()
-        cls.ip.extension_manager.load_extension('numba')
-
     def test_numba(self):
-        self.ip.run_cell_magic("numba", "", code)
-        self.ip.ex(f'{code}')
-        self.assertEqual(self.ip.user_ns['x'], 82.0)
+        ip = TerminalInteractiveShell()
+        ip.extension_manager.load_extension('numba')
+        ip.run_cell_magic("numba", "", code)
+        ip.ex(f'{code}')
+        self.assertEqual(ip.user_ns['x'], 82.0)
