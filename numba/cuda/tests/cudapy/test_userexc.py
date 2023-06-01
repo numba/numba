d1deff2fa4b08ad6dc73246390579fa1ecd1ecb5
Michael Collison <collison956@gmail.com>
2023-05-31 22:27:46 -0400

    Add test cases for exceptions/asserts in device functions

--- numba/cuda/tests/cudapy/test_userexc.py
+++ numba/cuda/tests/cudapy/test_userexc.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numba.cuda.testing import unittest, CUDATestCase
 from numba import cuda
 from numba.core import config
@@ -10,6 +11,18 @@ class MyError(Exception):
 regex_pattern = (
     r'In function [\'"]test_exc[\'"], file [\:\.\/\\\-a-zA-Z_0-9]+, line \d+'
 )
+
+
+@cuda.jit(device=True)
+def device_function(x):
+    if x == 1:
+        raise ValueError('Error')
+    elif x == 2:
+        raise TypeError('Error')
+    elif x == 3:
+        raise RuntimeError('Error')
+    elif x == 4:
+        assert False, 'Error'
 
 
 class TestUserExc(CUDATestCase):
@@ -34,6 +47,33 @@ class TestUserExc(CUDATestCase):
             self.assertRegexpMatches(str(cm.exception), regex_pattern)
             self.assertRegexpMatches(str(cm.exception), regex_pattern)
         self.assertIn("tid=[0, 0, 0] ctaid=[0, 0, 0]: foo", str(cm.exception))
+
+
+@cuda.jit(debug=True, opt=False)
+def kernel(d_array):
+    device_function(d_array[cuda.threadIdx.x])
+
+
+class TestDeviceExceptions(CUDATestCase):
+    def test_value_error(self):
+        d_array = cuda.to_device(np.array([1], dtype=np.int32))
+        with self.assertRaises(ValueError):
+            kernel[1, 1](d_array)
+
+    def test_type_error(self):
+        d_array = cuda.to_device(np.array([2], dtype=np.int32))
+        with self.assertRaises(TypeError):
+            kernel[1, 1](d_array)
+
+    def test_runtime_error(self):
+        d_array = cuda.to_device(np.array([3], dtype=np.int32))
+        with self.assertRaises(RuntimeError):
+            kernel[1, 1](d_array)
+
+    def test_assertion_error(self):
+        d_array = cuda.to_device(np.array([4], dtype=np.int32))
+        with self.assertRaises(AssertionError):
+            kernel[1, 1](d_array)
 
 
 if __name__ == '__main__':
