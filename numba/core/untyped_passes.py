2c9d4eb034ee3c97e66704b574e020463693158c
esc <esc@users.noreply.github.com>
2023-06-02 15:44:43 +0200

    add comment about variable redefine
    As title

--- numba/core/untyped_passes.py
+++ numba/core/untyped_passes.py
@@ -909,9 +909,12 @@ class MixedContainerUnroller(FunctionPass):
                 try:
                     scope.get_exact(name)
                 except errors.NotDefinedError:
-                    # is this correct? In case the scope doesn't have the
+                    # In case the scope doesn't have the
                     # variable, we need to define it prior creating new
-                    # copies of it!
+                    # copies of it! This is because the scope of the function
+                    # and the scope of the function and the scope of the loop
+                    # are different and the variable needs to be redefined
+                    # within the scope of the loop.
                     scope.define(name, var.loc)
                 new_var_dict[name] = scope.redefine(name, var.loc).name
             replace_var_names(loop_blocks, new_var_dict)
