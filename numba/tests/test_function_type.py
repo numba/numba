830785ffe8b25f47189b9ced8115778f1615d50d
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2023-07-12 10:22:00 -0500

    Fix #9071. Conversion of Dispatcher to FunctionType fail to handle compiler error.

--- numba/tests/test_function_type.py
+++ numba/tests/test_function_type.py
@@ -1,12 +1,14 @@
 import unittest
 import types as pytypes
 from numba import jit, njit, cfunc, types, int64, float64, float32, errors
-from numba import literal_unroll
+from numba import literal_unroll, typeof
 from numba.core.config import IS_32BITS, IS_WIN32
 import ctypes
 import warnings
 
-from .support import TestCase
+from .support import TestCase, MemoryLeakMixin
+
+import numpy as np
 
 
 def dump(foo):  # FOR DEBUGGING, TO BE REMOVED
@@ -1255,6 +1257,35 @@ class TestBasicSubtyping(TestCase):
         got = bar(tup, tup_bar)
         expected = foo1(a) + foo2(a) + bar1(a) + bar2(a)
         self.assertEqual(got, expected)
+
+
+class TestMultiFunctionType(MemoryLeakMixin, TestCase):
+
+    def test_base(self):
+        # The test is adapted from https://github.com/numba/numba/issues/9071
+        nb_array = typeof(np.ones(2))
+        callee_int_type = types.FunctionType(int64(int64))
+        sig_int = int64(callee_int_type, int64)
+        callee_array_type = types.FunctionType(float64(nb_array))
+        sig_array = float64(callee_array_type, nb_array)
+
+        @njit([sig_int, sig_array])
+        def caller(callee, a):
+            return callee(a)
+
+        @njit
+        def callee_int(b):
+            return b
+
+        @njit
+        def callee_array(c):
+            return c.sum()
+
+        b = 1
+        c = np.ones(2)
+
+        self.assertEqual(caller(callee_int, b), b)
+        self.assertEqual(caller(callee_array, c), c.sum())
 
 
 if __name__ == '__main__':
