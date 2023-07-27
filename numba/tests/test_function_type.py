7604f0ea91125d04bd1932fbe98c07dae0783c30
kc611 <ckaustubhm06@gmail.com>
2023-07-27 16:49:26 +0530

    Added release notes and fixed flake8 issues

--- numba/tests/test_function_type.py
+++ numba/tests/test_function_type.py
@@ -2,7 +2,7 @@ import unittest
 import types as pytypes
 from numba import jit, njit, cfunc, types, int64, float64, float32, errors
 from numba import literal_unroll
-from numba.core.config import IS_32BITS, IS_WIN32
+from numba.core.config import IS_WIN32
 import ctypes
 import warnings
 
