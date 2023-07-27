fe3d209b8c3683c01b32d9106d9c8547873aa0ad
Graham Markall <gmarkall@nvidia.com>
2023-07-27 07:17:46 +0100

    Use an IntEnum for NVRTC error codes

--- numba/cuda/cudadrv/nvrtc.py
+++ numba/cuda/cudadrv/nvrtc.py
@@ -1,10 +1,10 @@
 from ctypes import byref, c_char, c_char_p, c_int, c_size_t, c_void_p, POINTER
+from enum import IntEnum
 from numba.core import config
 from numba.cuda.cudadrv.error import (NvrtcError, NvrtcCompilationError,
                                       NvrtcSupportError)
 
 import functools
-import sys
 import threading
 import warnings
 
@@ -14,23 +14,21 @@ nvrtc_program = c_void_p
 # Result code
 nvrtc_result = c_int
 
-RESULT_CODE_NAMES = """
-NVRTC_SUCCESS
-NVRTC_ERROR_OUT_OF_MEMORY
-NVRTC_ERROR_PROGRAM_CREATION_FAILURE
-NVRTC_ERROR_INVALID_INPUT
-NVRTC_ERROR_INVALID_PROGRAM
-NVRTC_ERROR_INVALID_OPTION
-NVRTC_ERROR_COMPILATION
-NVRTC_ERROR_BUILTIN_OPERATION_FAILURE
-NVRTC_ERROR_NO_NAME_EXPRESSIONS_AFTER_COMPILATION
-NVRTC_ERROR_NO_LOWERED_NAMES_BEFORE_COMPILATION
-NVRTC_ERROR_NAME_EXPRESSION_NOT_VALID
-NVRTC_ERROR_INTERNAL_ERROR
-""".split()
 
-for i, k in enumerate(RESULT_CODE_NAMES):
-    setattr(sys.modules[__name__], k, i)
+class NvrtcResult(IntEnum):
+    NVRTC_SUCCESS = 0
+    NVRTC_ERROR_OUT_OF_MEMORY = 1
+    NVRTC_ERROR_PROGRAM_CREATION_FAILURE = 2
+    NVRTC_ERROR_INVALID_INPUT = 3
+    NVRTC_ERROR_INVALID_PROGRAM = 4
+    NVRTC_ERROR_INVALID_OPTION = 5
+    NVRTC_ERROR_COMPILATION = 6
+    NVRTC_ERROR_BUILTIN_OPERATION_FAILURE = 7
+    NVRTC_ERROR_NO_NAME_EXPRESSIONS_AFTER_COMPILATION = 8
+    NVRTC_ERROR_NO_LOWERED_NAMES_BEFORE_COMPILATION = 9
+    NVRTC_ERROR_NAME_EXPRESSION_NOT_VALID = 10
+    NVRTC_ERROR_INTERNAL_ERROR = 11
+
 
 _nvrtc_lock = threading.Lock()
 
@@ -121,10 +119,13 @@ class NVRTC:
                     @functools.wraps(func)
                     def checked_call(*args, func=func, name=name):
                         error = func(*args)
-                        if error == NVRTC_ERROR_COMPILATION: # noqa: F821
+                        if error == NvrtcResult.NVRTC_ERROR_COMPILATION:
                             raise NvrtcCompilationError()
-                        elif error != NVRTC_SUCCESS: # noqa: F821
-                            error_name = RESULT_CODE_NAMES[error]
+                        elif error != NvrtcResult.NVRTC_SUCCESS:
+                            try:
+                                error_name = NvrtcResult(error).name
+                            except ValueError:
+                                error_name = f'Unknown nvrtc_result {error}'
                             msg = f'Failed to call {name}: {error_name}'
                             raise NvrtcError(msg)
 
