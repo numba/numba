5e6663fa8a971d0b0c23032477cd08ac50b74cf3
Da Li <da.li@radix-trading.com>
2023-08-28 13:50:33 +0200

    fix overload method template

--- numba/core/typing/templates.py
+++ numba/core/typing/templates.py
@@ -1141,6 +1141,22 @@ class _OverloadMethodTemplate(_OverloadAttributeTemplate):
                 if sig is not None:
                     return sig.as_method()
 
+            def get_template_info(self):
+                basepath = os.path.dirname(os.path.dirname(numba.__file__))
+                impl = self._overload_func
+                code, firstlineno, path = self.get_source_code_info(impl)
+                sig = str(utils.pysignature(impl))
+                info = {
+                    'kind': "overload_method",
+                    'name': getattr(impl, '__qualname__', impl.__name__),
+                    'sig': sig,
+                    'filename': utils.safe_relpath(path, start=basepath),
+                    'lines': (firstlineno, firstlineno + len(code) - 1),
+                    'docstring': impl.__doc__
+                }
+
+                return info
+
         return types.BoundFunction(MethodTemplate, typ)
 
 
