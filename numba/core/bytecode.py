84ac5c847a780ed5381c883c1b412df9352703b8
esc <esc@users.noreply.github.com>
2023-09-28 14:10:40 +0200

    [WIP] [py312] [bytecode] LOAD_GLOBAL
    As title

--- numba/core/bytecode.py
+++ numba/core/bytecode.py
@@ -322,7 +322,7 @@ class _ByteCode(object):
 
 def _fix_LOAD_GLOBAL_arg(arg):
     if PYVERSION >= (3, 11):
-        assert PYVERSION == (3, 11) # reminder to check newer versions
+        assert PYVERSION >= (3, 11) # reminder to check newer versions
         return arg >> 1
     return arg
 
