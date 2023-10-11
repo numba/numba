be7be7778c229de02b6330f984db7a7260d3c9c5
esc <esc@users.noreply.github.com>
2023-10-11 23:05:25 +0200

    Merge pull request #9227 from gmarkall/np126-r2
    Support NumPy 1.26 (PR aimed at review / merge)

--- numba/tests/test_np_functions.py
+++ numba/tests/test_np_functions.py
@@ -5056,7 +5056,8 @@ class TestNPFunctions(MemoryLeakMixin, TestCase):
                     self.assertPreciseEqual(expected,
                                             got, prec='double', ulps=2)
                 else:
-                    self.assertPreciseEqual(expected, got, prec='exact')
+                    self.assertPreciseEqual(expected, got, prec='double',
+                                            ulps=2)
 
         for M in ['a', 1.1, 1j]:
             with self.assertRaises(TypingError) as raises:
