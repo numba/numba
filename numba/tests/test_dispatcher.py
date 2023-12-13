ddd40cd1e85d517844e79494c3689356780012f0
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2023-12-13 10:01:42 +0000

    Adjust test to use expectedFailure opposed to skip.
    As title.

--- numba/tests/test_dispatcher.py
+++ numba/tests/test_dispatcher.py
@@ -1052,9 +1052,9 @@ class TestNoRetryFailedSignature(unittest.TestCase):
 
         self.run_test(foo)
 
-    @unittest.skip(("Unconditionally skip. @overload does not have an error "
-                    "cache. See PR #9259 for this feature and revert the skip "
-                    "once this is merged."))
+    @unittest.expectedFailure
+    # NOTE: @overload does not have an error cache. See PR #9259 for this
+    # feature and remove the xfail once this is merged.
     def test_error_count(self):
         def check(field, would_fail):
             # Slightly modified from the reproducer in issue #4117.
