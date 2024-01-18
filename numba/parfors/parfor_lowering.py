163d585f5b616162fe200cd0f68d2632cab79fa6
Todd A. Anderson <drtodd13@comcast.net>
2024-01-18 10:40:01 -0800

    Set mutable=True flag in MutableSequence type.  Forbid hoisting of mutable types in parfor lowering.

--- numba/parfors/parfor_lowering.py
+++ numba/parfors/parfor_lowering.py
@@ -871,30 +871,38 @@ def _hoist_internal(inst, dep_on_param, call_table, hoisted, not_hoisted,
     if inst.target.name in stored_arrays:
         not_hoisted.append((inst, "stored array"))
         if config.DEBUG_ARRAY_OPT >= 1:
-            print("Instruction", inst, " could not be hoisted because the created array is stored.")
+            print("Instruction", inst, "could not be hoisted because the created array is stored.")
+        return False
+
+    target_type = typemap[inst.target.name]
+    if getattr(target_type, "mutable", False):
+        if config.DEBUG_ARRAY_OPT >= 1:
+            print("Instruction", inst, "could not be hoisted because objects of this type are mutable.")
         return False
 
     uses = set()
+    # Get vars used by this statement.
     visit_vars_inner(inst.value, find_vars, uses)
+    # Filter out input parameters from the set of variable usages.
     diff = uses.difference(dep_on_param)
     if config.DEBUG_ARRAY_OPT >= 1:
         print("_hoist_internal:", inst, "uses:", uses, "diff:", diff)
     if len(diff) == 0 and is_pure(inst.value, None, call_table):
         if config.DEBUG_ARRAY_OPT >= 1:
-            print("Will hoist instruction", inst, typemap[inst.target.name])
+            print("Will hoist instruction", inst, target_type)
         hoisted.append(inst)
-        if not isinstance(typemap[inst.target.name], types.npytypes.Array):
+        if not isinstance(target_type, types.npytypes.Array):
             dep_on_param += [inst.target.name]
         return True
     else:
         if len(diff) > 0:
             not_hoisted.append((inst, "dependency"))
             if config.DEBUG_ARRAY_OPT >= 1:
-                print("Instruction", inst, " could not be hoisted because of a dependency.")
+                print("Instruction", inst, "could not be hoisted because of a dependency.")
         else:
             not_hoisted.append((inst, "not pure"))
             if config.DEBUG_ARRAY_OPT >= 1:
-                print("Instruction", inst, " could not be hoisted because it isn't pure.")
+                print("Instruction", inst, "could not be hoisted because it isn't pure.")
     return False
 
 def find_setitems_block(setitems, itemsset, block, typemap):
