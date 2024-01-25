2c2a44d028e4e5fe6fd650f5a952112ae616d227
Todd A. Anderson <drtodd13@comcast.net>
2024-01-24 16:37:20 -0800

    Undo.

--- numba/core/analysis.py
+++ numba/core/analysis.py
@@ -238,12 +238,9 @@ def compute_cfg_from_blocks(blocks):
         cfg.add_node(k)
 
     for k, b in blocks.items():
-        try:
-            term = b.terminator
-            for target in term.get_targets():
-                cfg.add_edge(k, target)
-        except Exception:
-            pass # Allow blocks without a terminator like last block in parfor.
+        term = b.terminator
+        for target in term.get_targets():
+            cfg.add_edge(k, target)
 
     cfg.set_entry_point(min(blocks))
     cfg.process()
