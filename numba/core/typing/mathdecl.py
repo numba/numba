8ca3bd2a58e83598124faf23ca0e035d88e485f2
Ryan Grout <ryan@ryangrout.org>
2024-02-14 14:58:26 -0700

    Add typing for math.nextafter

--- numba/core/typing/mathdecl.py
+++ numba/core/typing/mathdecl.py
@@ -1,4 +1,5 @@
 import math
+import sys
 from numba.core import types, utils
 from numba.core.typing.templates import (AttributeTemplate, ConcreteTemplate,
                                          signature, Registry)
@@ -84,6 +85,17 @@ class Math_hypot(ConcreteTemplate):
         signature(types.float32, types.float32, types.float32),
         signature(types.float64, types.float64, types.float64),
     ]
+
+
+if sys.hexversion >= 0x03090000:
+    @infer_global(math.nextafter)
+    class Math_nextafter(ConcreteTemplate):
+        cases = [
+            signature(types.float64, types.float64, types.float64),
+            signature(types.float32, types.float32, types.float32),
+            signature(types.float64, types.int64, types.int64),
+            signature(types.float64, types.uint64, types.uint64)
+        ]
 
 
 @infer_global(math.isinf)
