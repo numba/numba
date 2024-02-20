0dcf6bdae1f3387737f506d92fe57355bbfb5d8d
kc611 <ckaustubhm06@gmail.com>
2024-02-20 16:41:12 +0530

    Added new types by modifying numba.core.types.new_scalar module

---
+++ numba/core/types/new_scalars/__init__.py
@@ -0,0 +1,4 @@
+from numba.core.types.new_scalars.scalars import *
+from numba.core.types.new_scalars.python_types import *
+from numba.core.types.new_scalars.machine_types import *
+from numba.core.types.new_scalars.numpy_types import *
