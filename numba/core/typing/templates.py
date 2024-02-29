017b630601ae646d509899aaf1cd8ba3512ed998
dlee992 <lidanuaa@gmail.com>
2024-02-28 18:30:38 -0600

    sig could be None, why?

--- numba/core/typing/templates.py
+++ numba/core/typing/templates.py
@@ -843,10 +843,12 @@ class _OverloadFunctionTemplate(AbstractTemplate):
         # Make sure that the implementation can be fully compiled
         disp_type = types.Dispatcher(disp)
         sig = disp_type.get_call_type(self.context, args, kws)
-        flatten_args = sig.args
 
-        if cache_key is not None:
-            self._impl_cache[cache_key] = disp, flatten_args
+        if sig is not None:
+            flatten_args = sig.args
+
+            if cache_key is not None:
+                self._impl_cache[cache_key] = disp, flatten_args
 
         return disp, args
 
