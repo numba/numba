f407462d84603462585ae2655f8691687267ed89
kc611 <ckaustubhm06@gmail.com>
2024-03-06 15:56:28 +0530

    Fixed np.arange implementation template

--- numba/np/arrayobj.py
+++ numba/np/arrayobj.py
@@ -4782,7 +4782,7 @@ def np_arange(start, / ,stop=None, step=None, dtype=None):
     stop_value = getattr(stop, "literal_value", None)
     step_value = getattr(step, "literal_value", None)
 
-    def impl(start, stop=None, step=None, dtype=None):
+    def impl(start, /, stop=None, step=None, dtype=None):
         # Allow for improved performance if given literal arguments.
         lit_start = start_value if start_value is not None else start
         lit_stop = stop_value if stop_value is not None else stop
