db03908778713c5ec8c7bc027ae52fa3f409a764
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-03-15 18:52:02 -0500

    Skip test_issue9490_non_det_ssa_problem for PPC

--- numba/tests/test_parfors.py
+++ numba/tests/test_parfors.py
@@ -44,7 +44,8 @@ from numba.tests.support import (TestCase, captured_stdout, MemoryLeakMixin,
                                  override_env_config, linux_only, tag,
                                  skip_parfors_unsupported, _32bit, needs_blas,
                                  needs_lapack, disabled_test, skip_unless_scipy,
-                                 needs_subprocess)
+                                 needs_subprocess,
+                                 skip_ppc64le_invalid_ctr_loop)
 from numba.core.extending import register_jitable
 from numba.core.bytecode import _fix_LOAD_GLOBAL_arg
 from numba.core import utils
@@ -2369,6 +2370,7 @@ class TestParfors(TestParforsBase):
         self.assertEqual(expected, njit(parallel=True)(def_in_loop)(4))
 
     @needs_lapack  # use of np.linalg.solve
+    @skip_ppc64le_invalid_ctr_loop
     def test_issue9490_non_det_ssa_problem(self):
         cmd = [
             sys.executable,
