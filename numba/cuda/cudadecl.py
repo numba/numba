fdc423fa210317092c5db1233bc3b237f9f84ef8
Guilherme Leobas <guilhermeleobas@gmail.com>
2024-04-08 16:16:11 -0300

    address reviewer comments

--- numba/cuda/cudadecl.py
+++ numba/cuda/cudadecl.py
@@ -801,6 +801,6 @@ for func in comparison_functions:
 for func in bit_twiddling_functions:
     register_numpy_ufunc(func, register_global)
 
-for func in ('log', 'log2', 'log10'):
-    if func in math_operations:
+for func in math_operations:
+    if func in ('log', 'log2', 'log10'):
         register_numpy_ufunc(func, register_global)
