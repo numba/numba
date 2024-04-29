8e8180260a357c9c9eb9dd2d529d6a09afcfbb71
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-04-29 10:48:31 -0500

    Make new_style the default error capturing mode

--- numba/tests/test_config.py
+++ numba/tests/test_config.py
@@ -124,6 +124,19 @@ class TestConfig(TestCase):
         self.assertIn(ex_expected, err_msg)
         self.assertIn(source_compiled, out.decode('utf-8'))
 
+    def test_default_error_style_handling(self):
+        # ensure that the default is new_style
+        new_env = os.environ.copy()
+        new_env['NUMBA_CAPTURED_ERRORS'] = 'default'
+        code = ("from numba.core import config\n"
+                "print('---->', config.CAPTURED_ERRORS)\n"
+                "assert config.CAPTURED_ERRORS == 'new_style'")
+        out, err = run_in_subprocess(dedent(code), env=new_env)
+        err_msg = err.decode('utf-8')
+        out_msg = out.decode('utf-8')
+        ex_expected = "----> new_style"
+        self.assertIn(ex_expected, out_msg, msg=err_msg)
+
 
 class TestNumbaOptLevel(TestCase):
     # Tests that the setting of NUMBA_OPT influences the "cheap" module pass.
