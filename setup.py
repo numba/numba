eb0d620c8e4f0dfb08df66ce051c66a2c4b556b4
esc <esc@users.noreply.github.com>
2024-05-08 11:03:17 +0200

    NumPy pin for 2.0
    As title

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ except ImportError:
 
 min_python_version = "3.9"
 max_python_version = "3.13"  # exclusive
-min_numpy_build_version = "1.11"
+min_numpy_build_version = "2.0.0rc1"
 min_numpy_run_version = "1.22"
+max_numpy_run_version = "2.1"
 min_llvmlite_version = "0.43.0dev0"
 max_llvmlite_version = "0.44"
 
@@ -363,10 +364,11 @@ def get_ext_modules():
 
 packages = find_packages(include=["numba", "numba.*"])
 
-build_requires = ['numpy >={}'.format(min_numpy_build_version)]
+build_requires = ['numpy >={},{}'.format(min_numpy_build_version,
+                                         max_numpy_run_version)]
 install_requires = [
     'llvmlite >={},<{}'.format(min_llvmlite_version, max_llvmlite_version),
-    'numpy >={}'.format(min_numpy_run_version),
+    'numpy >={},<{}'.format(min_numpy_run_version, max_numpy_run_version),
 ]
 
 metadata = dict(
