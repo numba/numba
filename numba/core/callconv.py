bd4664203cb2ce01ec744b4802b3750b4400f4be
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-05-21 16:07:40 -0500

    Sentry callcov return type against function type spec. And, fix violation in typedobjectutils.py

--- numba/core/callconv.py
+++ numba/core/callconv.py
@@ -877,6 +877,10 @@ class CPUCallConv(BaseCallConv):
                Equivalent iterable: ("noinline", "fast")
         """
         retty = self.get_return_type(resty).pointee
+        actual_retty = self._get_return_argument(callee.function_type).pointee
+        if retty != actual_retty:
+            m = f"Function type returns {actual_retty} but resty={retty}"
+            raise ValueError(m)
 
         retvaltmp = cgutils.alloca_once(builder, retty)
         # initialize return value to zeros
