5d47c5711e16de327a656dd14f5fbc71b447d865
JadeyGraham96 <total.trance.network@gmail.com>
2024-06-06 07:06:14 +0100

    Refactor & fix generator code

--- numba/core/generators.py
+++ numba/core/generators.py
@@ -4,7 +4,6 @@ Support for lowering generators.
 
 import llvmlite.ir
 from llvmlite.ir import Constant, IRBuilder
-
 from numba.core import types, config, cgutils
 from numba.core.funcdesc import FunctionDescriptor
 
@@ -22,7 +21,7 @@ class GeneratorDescriptor(FunctionDescriptor):
         function described by *fndesc*, with type *gentype*.
 
         The generator inherits the env_name from the *fndesc*.
-        All emitted functions for the generator shares the same Env.
+        All emitted functions for the generator share the same Env.
         """
         assert isinstance(gentype, types.Generator)
         restype = gentype.yield_type
@@ -30,11 +29,10 @@ class GeneratorDescriptor(FunctionDescriptor):
         argtypes = (gentype,)
         qualname = fndesc.qualname + '.next'
         unique_name = fndesc.unique_name + '.next'
-        self = cls(fndesc.native, fndesc.modname, qualname, unique_name,
+        return cls(fndesc.native, fndesc.modname, qualname, unique_name,
                    fndesc.doc, fndesc.typemap, restype, fndesc.calltypes,
                    args, fndesc.kws, argtypes=argtypes, mangler=mangler,
                    inline=False, env_name=fndesc.env_name)
-        return self
 
     @property
     def llvm_finalizer_name(self):
@@ -45,7 +43,7 @@ class GeneratorDescriptor(FunctionDescriptor):
         return 'finalize_' + self.mangled_name
 
 
-class BaseGeneratorLower(object):
+class BaseGeneratorLower:
     """
     Base support class for lowering generators.
     """
@@ -61,7 +59,6 @@ class BaseGeneratorLower(object):
         self.gentype = self.get_generator_type()
         self.gendesc = GeneratorDescriptor.from_generator_fndesc(
             lower.func_ir, self.fndesc, self.gentype, self.context.mangler)
-        # Helps packing non-omitted arguments into a structure
         self.arg_packer = self.context.get_data_packer(self.fndesc.argtypes)
 
         self.resume_blocks = {}
@@ -70,16 +67,23 @@ class BaseGeneratorLower(object):
     def call_conv(self):
         return self.lower.call_conv
 
-    def get_args_ptr(self, builder, genptr):
-        return cgutils.gep_inbounds(builder, genptr, 0, 1)
+    def get_pointer(self, builder, genptr, *indices):
+        """
+        Get a pointer to a specific location in the generator structure.
 
-    def get_resume_index_ptr(self, builder, genptr):
-        return cgutils.gep_inbounds(builder, genptr, 0, 0,
-                                    name='gen.resume_index')
+        Parameters:
+        builder : IRBuilder
+            The LLVM IR builder.
+        genptr : Value
+            The pointer to the generator structure.
+        indices : tuple
+            The indices to access the desired field in the structure.
 
-    def get_state_ptr(self, builder, genptr):
-        return cgutils.gep_inbounds(builder, genptr, 0, 2,
-                                    name='gen.state')
+        Returns:
+        Value
+            The pointer to the specified field.
+        """
+        return cgutils.gep_inbounds(builder, genptr, 0, *indices)
 
     def lower_init_func(self, lower):
         """
@@ -87,48 +91,34 @@ class BaseGeneratorLower(object):
         the passed-by-reference generator structure).
         """
         lower.setup_function(self.fndesc)
-
         builder = lower.builder
 
-        # Insert the generator into the target context in order to allow
-        # calling from other Numba-compiled functions.
-        lower.context.insert_generator(self.gentype, self.gendesc,
-                                       [self.library])
-
-        # Init argument values
+        lower.context.insert_generator(self.gentype, self.gendesc, [self.library])
         lower.extract_function_arguments()
-
         lower.pre_lower()
 
-        # Initialize the return structure (i.e. the generator structure).
         retty = self.context.get_return_type(self.gentype)
-        # Structure index #0: the initial resume index (0 == start of generator)
         resume_index = self.context.get_constant(types.int32, 0)
-        # Structure index #1: the function arguments
         argsty = retty.elements[1]
         statesty = retty.elements[2]
 
-        lower.debug_print("# low_init_func incref")
-        # Incref all NRT arguments before storing into generator states
+        self.incref_arguments(lower, builder)
+
+        argsval = self.arg_packer.as_data(builder, lower.fnargs)
+        statesval = Constant(statesty, None)
+        gen_struct = cgutils.make_anonymous_struct(builder, [resume_index, argsval, statesval], retty)
+        retval = self.box_generator_struct(lower, gen_struct)
+
+        self.call_conv.return_value(builder, retval)
+        lower.post_lower()
+
+    def incref_arguments(self, lower, builder):
+        """
+        Increment the reference count for all NRT arguments before storing into generator states.
+        """
         if self.context.enable_nrt:
             for argty, argval in zip(self.fndesc.argtypes, lower.fnargs):
                 self.context.nrt.incref(builder, argty, argval)
-
-        # Filter out omitted arguments
-        argsval = self.arg_packer.as_data(builder, lower.fnargs)
-
-        # Zero initialize states
-        statesval = Constant(statesty, None)
-        gen_struct = cgutils.make_anonymous_struct(builder,
-                                                   [resume_index, argsval,
-                                                    statesval],
-                                                   retty)
-
-        retval = self.box_generator_struct(lower, gen_struct)
-
-        lower.debug_print("# low_init_func before return")
-        self.call_conv.return_value(builder, retval)
-        lower.post_lower()
 
     def lower_next_func(self, lower):
         """
@@ -137,44 +127,49 @@ class BaseGeneratorLower(object):
         yielded value).
         """
         lower.setup_function(self.gendesc)
-        lower.debug_print("# lower_next_func: {0}".format(self.gendesc.unique_name))
-        assert self.gendesc.argtypes[0] == self.gentype
         builder = lower.builder
         function = lower.function
 
-        # Extract argument values and other information from generator struct
         genptr, = self.call_conv.get_arguments(function)
-        self.arg_packer.load_into(builder,
-                                  self.get_args_ptr(builder, genptr),
-                                  lower.fnargs)
+        self.arg_packer.load_into(builder, self.get_pointer(builder, genptr, 1), lower.fnargs)
 
-        self.resume_index_ptr = self.get_resume_index_ptr(builder, genptr)
-        self.gen_state_ptr = self.get_state_ptr(builder, genptr)
+        self.resume_index_ptr = self.get_pointer(builder, genptr, 0, 0)
+        self.gen_state_ptr = self.get_pointer(builder, genptr, 0, 2)
 
         prologue = function.append_basic_block("generator_prologue")
 
-        # Lower the generator's Python code
         entry_block_tail = lower.lower_function_body()
 
-        # Add block for StopIteration on entry
-        stop_block = function.append_basic_block("stop_iteration")
-        builder.position_at_end(stop_block)
-        self.call_conv.return_stop_iteration(builder)
+        stop_block = self.add_stop_iteration_block(builder, function)
 
-        # Add prologue switch to resume blocks
         builder.position_at_end(prologue)
-        # First Python block is also the resume point on first next() call
         first_block = self.resume_blocks[0] = lower.blkmap[lower.firstblk]
 
-        # Create front switch to resume points
-        switch = builder.switch(builder.load(self.resume_index_ptr),
-                                stop_block)
+        switch = builder.switch(builder.load(self.resume_index_ptr), stop_block)
         for index, block in self.resume_blocks.items():
             switch.add_case(index, block)
 
-        # Close tail of entry block
         builder.position_at_end(entry_block_tail)
         builder.branch(prologue)
+
+    def add_stop_iteration_block(self, builder, function):
+        """
+        Add a block to handle StopIteration at the entry point of the generator.
+
+        Parameters:
+        builder : IRBuilder
+            The LLVM IR builder.
+        function : Function
+            The LLVM function.
+
+        Returns:
+        BasicBlock
+            The StopIteration block.
+        """
+        stop_block = function.append_basic_block("stop_iteration")
+        builder.position_at_end(stop_block)
+        self.call_conv.return_stop_iteration(builder)
+        return stop_block
 
     def lower_finalize_func(self, lower):
         """
@@ -200,14 +195,33 @@ class BaseGeneratorLower(object):
         self.call_conv.return_stop_iteration(lower.builder)
 
     def create_resumption_block(self, lower, index):
-        block_name = "generator_resume%d" % (index,)
+        """
+        Create a block for resuming the generator at a specific index.
+
+        Parameters:
+        lower : Lower
+            The lower object containing the function context.
+        index : int
+            The index at which to resume the generator.
+        """
+        block_name = f"generator_resume{index}"
         block = lower.function.append_basic_block(block_name)
         lower.builder.position_at_end(block)
         self.resume_blocks[index] = block
 
     def debug_print(self, builder, msg):
+        """
+        Print a debug message if DEBUG_JIT is enabled.
+
+        Parameters:
+        builder : IRBuilder
+            The LLVM IR builder.
+        msg : str
+            The message to print.
+        """
         if config.DEBUG_JIT:
-            self.context.debug_print(builder, "DEBUGJIT: {0}".format(msg))
+            self.context.debug_print(builder, f"DEBUGJIT: {msg}")
+
 
 class GeneratorLower(BaseGeneratorLower):
     """
@@ -227,15 +241,13 @@ class GeneratorLower(BaseGeneratorLower):
         """
         self.debug_print(builder, "# generator: finalize")
         if self.context.enable_nrt:
-
-            # Always dereference all arguments
-            # self.debug_print(builder, "# generator: clear args")
-            args_ptr = self.get_args_ptr(builder, genptr)
+            args_ptr = self.get_pointer(builder, genptr, 1)
             for ty, val in self.arg_packer.load(builder, args_ptr):
                 self.context.nrt.decref(builder, ty, val)
 
         self.debug_print(builder, "# generator: finalize end")
         builder.ret_void()
+
 
 class PyGeneratorLower(BaseGeneratorLower):
     """
@@ -253,7 +265,7 @@ class PyGeneratorLower(BaseGeneratorLower):
             arg_types=(types.pyobject,) * self.func_ir.arg_count,
             state_types=(types.pyobject,) * len(self.geninfo.state_vars),
             has_finalizer=True,
-            )
+        )
 
     def box_generator_struct(self, lower, gen_struct):
         """
@@ -267,8 +279,7 @@ class PyGeneratorLower(BaseGeneratorLower):
         NULL-initialize all generator state variables, to avoid spurious
         decref's on cleanup.
         """
-        lower.builder.store(Constant(self.gen_state_ptr.type.pointee, None),
-                            self.gen_state_ptr)
+        lower.builder.store(Constant(self.gen_state_ptr.type.pointee, None), self.gen_state_ptr)
 
     def lower_finalize_func_body(self, builder, genptr):
         """
@@ -276,21 +287,15 @@ class PyGeneratorLower(BaseGeneratorLower):
         state variables.
         """
         pyapi = self.context.get_python_api(builder)
-        resume_index_ptr = self.get_resume_index_ptr(builder, genptr)
+        resume_index_ptr = self.get_pointer(builder, genptr, 0, 0)
         resume_index = builder.load(resume_index_ptr)
-        # If resume_index is 0, next() was never called
-        # If resume_index is -1, generator terminated cleanly
-        # (note function arguments are saved in state variables,
-        #  so they don't need a separate cleanup step)
-        need_cleanup = builder.icmp_signed(
-            '>', resume_index, Constant(resume_index.type, 0))
+
+        need_cleanup = builder.icmp_signed('>', resume_index, Constant(resume_index.type, 0))
 
         with cgutils.if_unlikely(builder, need_cleanup):
-            # Decref all live vars (some may be NULL)
-            gen_state_ptr = self.get_state_ptr(builder, genptr)
+            gen_state_ptr = self.get_pointer(builder, genptr, 2)
             for state_index in range(len(self.gentype.state_types)):
-                state_slot = cgutils.gep_inbounds(builder, gen_state_ptr,
-                                                  0, state_index)
+                state_slot = cgutils.gep_inbounds(builder, gen_state_ptr, 0, state_index)
                 ty = self.gentype.state_types[state_index]
                 val = self.context.unpack_value(builder, ty, state_slot)
                 pyapi.decref(val)
@@ -298,7 +303,7 @@ class PyGeneratorLower(BaseGeneratorLower):
         builder.ret_void()
 
 
-class LowerYield(object):
+class LowerYield:
     """
     Support class for lowering a particular yield point.
     """
@@ -315,46 +320,48 @@ class LowerYield(object):
         self.yp = yield_point
         self.inst = self.yp.inst
         self.live_vars = live_vars
-        self.live_var_indices = [lower.generator_info.state_vars.index(v)
-                                 for v in live_vars]
+        self.live_var_indices = [lower.generator_info.state_vars.index(v) for v in live_vars]
 
     def lower_yield_suspend(self):
         self.lower.debug_print("# generator suspend")
-        # Save live vars in state
-        for state_index, name in zip(self.live_var_indices, self.live_vars):
-            state_slot = cgutils.gep_inbounds(self.builder, self.gen_state_ptr,
-                                              0, state_index)
-            ty = self.gentype.state_types[state_index]
-            # The yield might be in a loop, in which case the state might
-            # contain a predicate var that branches back to the loop head, in
-            # this case the var is live but in sequential lowering won't have
-            # been alloca'd yet, so do this here.
-            fetype = self.lower.typeof(name)
-            self.lower._alloca_var(name, fetype)
-            val = self.lower.loadvar(name)
-            # IncRef newly stored value
-            if self.context.enable_nrt:
-                self.context.nrt.incref(self.builder, ty, val)
-
-            self.context.pack_value(self.builder, ty, val, state_slot)
-        # Save resume index
-        indexval = Constant(self.resume_index_ptr.type.pointee,
-                            self.inst.index)
-        self.builder.store(indexval, self.resume_index_ptr)
+        self.save_live_vars()
+        self.save_resume_index()
         self.lower.debug_print("# generator suspend end")
 
     def lower_yield_resume(self):
-        # Emit resumption point
         self.genlower.create_resumption_block(self.lower, self.inst.index)
         self.lower.debug_print("# generator resume")
-        # Reload live vars from state
+        self.reload_live_vars()
+        self.lower.debug_print("# generator resume end")
+
+    def save_live_vars(self):
+        """
+        Save live variables in the generator state.
+        """
         for state_index, name in zip(self.live_var_indices, self.live_vars):
-            state_slot = cgutils.gep_inbounds(self.builder, self.gen_state_ptr,
-                                              0, state_index)
+            state_slot = cgutils.gep_inbounds(self.builder, self.gen_state_ptr, 0, state_index)
+            ty = self.gentype.state_types[state_index]
+            self.lower._alloca_var(name, self.lower.typeof(name))
+            val = self.lower.loadvar(name)
+            if self.context.enable_nrt:
+                self.context.nrt.incref(self.builder, ty, val)
+            self.context.pack_value(self.builder, ty, val, state_slot)
+
+    def save_resume_index(self):
+        """
+        Save the current resume index in the generator state.
+        """
+        indexval = Constant(self.resume_index_ptr.type.pointee, self.inst.index)
+        self.builder.store(indexval, self.resume_index_ptr)
+
+    def reload_live_vars(self):
+        """
+        Reload live variables from the generator state.
+        """
+        for state_index, name in zip(self.live_var_indices, self.live_vars):
+            state_slot = cgutils.gep_inbounds(self.builder, self.gen_state_ptr, 0, state_index)
             ty = self.gentype.state_types[state_index]
             val = self.context.unpack_value(self.builder, ty, state_slot)
             self.lower.storevar(val, name)
-            # Previous storevar is making an extra incref
             if self.context.enable_nrt:
                 self.context.nrt.decref(self.builder, ty, val)
-        self.lower.debug_print("# generator resume end")
