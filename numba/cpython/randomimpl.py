547560ebcd6f363b5fef83fd670039c368934d14
NSchiffmacher <nathan.schiffmacher@gmail.com>
2024-07-03 21:56:29 +0200

    fix np.random.logistic

--- numba/cpython/randomimpl.py
+++ numba/cpython/randomimpl.py
@@ -1467,6 +1467,9 @@ def laplace_impl2(loc, scale):
 def laplace_impl3(loc, scale, size):
     if is_nonelike(size):
         return lambda loc, scale, size: np.random.laplace(loc, scale)
+    if is_empty_tuple(size):
+        # Handle size = ()
+        return lambda loc, scale, size: np.array(np.random.laplace(loc, scale))
     if isinstance(size, types.Integer) or (isinstance(size, types.UniTuple) and
                                            isinstance(size.dtype,
                                                       types.Integer)):
