61ba9df1f15256ed5ad8d3e902bea487d7d662cb
kc611 <ckaustubhm06@gmail.com>
2024-07-26 18:16:16 +0530

    Fixed flake8 issues

--- numba/cuda/ufuncs.py
+++ numba/cuda/ufuncs.py
@@ -11,6 +11,7 @@ import numpy as np
 from functools import lru_cache
 from numba.core import typing
 
+
 def get_ufunc_info(ufunc_key):
     return ufunc_db()[ufunc_key]
 
@@ -23,7 +24,6 @@ def ufunc_db():
     from numba.np.numpy_support import numpy_version
     from numba.cuda.mathimpl import (get_unary_impl_for_fn_and_ty,
                                      get_binary_impl_for_fn_and_ty)
-
 
     def np_unary_impl(fn, context, builder, sig, args):
         npyfuncs._check_arity_and_homogeneity(sig, args, 1)
