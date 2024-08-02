519f8e29193563121f880875a55af47d83a5df42
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-08-02 10:52:45 -0500

    Fix C99 `I` not working with NumPy 2.0.1

--- numba/_helperlib.c
+++ numba/_helperlib.c
@@ -19,7 +19,7 @@
 #else
     #include <stdint.h>
     #define _complex_float_t complex float
-    #define _complex_float_ctor(r, i) (r + I * i)
+    #define _complex_float_ctor(r, i) (r + _Complex_I * i)
     #define _complex_double_t complex double
 #endif
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
