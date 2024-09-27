c27f0d79d524a04feeb6a74788e5b3ff157420b3
kc611 <ckaustubhm06@gmail.com>
2024-09-27 18:12:56 +0530

    Fixed flake8 issues

--- numba/tests/test_try_except.py
+++ numba/tests/test_try_except.py
@@ -5,7 +5,6 @@ from itertools import product
 import numpy as np
 
 from numba import njit, typed, objmode, prange
-from numba.core.utils import PYVERSION
 from numba.core import ir_utils, ir
 from numba.core.errors import (
     UnsupportedError, CompilerError, NumbaPerformanceWarning, TypingError,
