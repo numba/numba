af7063c4d364a579b51f355e638fc039627797dd
kc611 <ckaustubhm06@gmail.com>
2024-10-10 14:38:30 +0530

    Fix windows buildscripts

--- buildscripts/incremental/setup_conda_environment.cmd
+++ buildscripts/incremental/setup_conda_environment.cmd
@@ -23,7 +23,7 @@ conda list
 @rem CFFI, jinja2 and IPython are optional dependencies, but exercised in the test suite
 conda create -n %CONDA_ENV% -q -y python=%PYTHON% numpy=%NUMPY% cffi pip jinja2 ipython gitpython pyyaml
 @rem Install SciPy only if NumPy is not 2.1
-if "%NUMPY%" != "2.1" (%CONDA_INSTALL% scipy)
+if "%NUMPY%" neq "2.1" (%CONDA_INSTALL% scipy)
 
 call activate %CONDA_ENV%
 @rem Install latest llvmlite build
