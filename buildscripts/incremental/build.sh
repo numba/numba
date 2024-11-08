8c8588307149604adf71501d6b55acad407d68c6
swap357 <swapnilpatel357@gmail.com>
2024-11-07 16:09:39 -0800

    remove macos sdk dependency, use xcode 14.2

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -14,11 +14,7 @@ else
 fi
 
 if [[ $(uname) == "Darwin" ]]; then
-    # The following is suggested in https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html?highlight=SDK#macos-sdk
-    wget -q https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.10.sdk.tar.xz
-    shasum -c ./buildscripts/incremental/MacOSX10.10.sdk.checksum
-    tar -xf ./MacOSX10.10.sdk.tar.xz
-    export SDKROOT=`pwd`/MacOSX10.10.sdk
+    export MACOSX_DEPLOYMENT_TARGET='11.0'
 fi
 python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS --verbose
 # (note we don't install to avoid problems with extra long Windows paths
