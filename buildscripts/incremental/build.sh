e9c742c7b66c6c2f28a1e4c5d435212db436ccd4
swap357 <swapnilpatel357@gmail.com>
2024-11-07 19:02:58 -0800

    update build.sh conditions - arch specific

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -15,8 +15,38 @@ fi
 
 if [[ $(uname) == "Darwin" ]]; then
     export MACOSX_DEPLOYMENT_TARGET='11.0'
-    export SDKROOT=$(xcrun --show-sdk-path)
+    # Determine architecture
+    ARCH=$(uname -m)
+    if [[ "$ARCH" == "arm64" ]]; then
+        CONDA_SUBDIR=osx-arm64
+        # Bootstrap with ARM64 compiler
+        conda create -y -p ${PWD}/bootstrap clangxx_osx-arm64
+    else
+        CONDA_SUBDIR=osx-64
+        # Bootstrap with x86_64 compiler
+        conda create -y -p ${PWD}/bootstrap clangxx_osx-64
+    fi
+    # Use explicit SDK path if set, otherwise detect
+    if [ -z "$SDKROOT" ]; then
+        SDKPATH=$(xcrun --show-sdk-path)
+    else
+        SDKPATH=$SDKROOT
+    fi
+    # Set minimum deployment target if not already set
+    if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
+        export MACOSX_DEPLOYMENT_TARGET=11.0
+    fi
+    # Set Darwin target based on architecture
+    if [[ "$ARCH" == "arm64" ]]; then
+        DARWIN_TARGET=arm64-apple-darwin20.0.0
+    else
+        DARWIN_TARGET=x86_64-apple-darwin13.4.0
+    fi
 fi
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    export MACOSX_DEPLOYMENT_TARGET  # Keep existing value
+fi
+
 python setup.py build_ext -q --inplace --debug $EXTRA_BUILD_EXT_FLAGS --verbose
 # (note we don't install to avoid problems with extra long Windows paths
 #  during distutils-dependent tests -- e.g. test_pycc)
