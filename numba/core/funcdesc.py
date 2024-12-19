12afbc8c65024a3df4eb7f7adf61a862ab423876
= <=>
2024-12-19 15:47:03 -0500

    only use func globals when cloudpicke

--- numba/core/funcdesc.py
+++ numba/core/funcdesc.py
@@ -150,13 +150,20 @@ class FunctionDescriptor(object):
         doc = func.__doc__ or ''
         args = tuple(func_ir.arg_names)
         kws = ()        # TODO
+        global_dict = None
+
         # issue #9786: In cases where the module globals mismatch the
         # function's global dictionary (e.g. when using cloudpicke),
         # we want to use the function's globals.
-        global_dict = func_ir.func_id.func.__globals__
-
-        # Dynamically generated function.
-        modname = _dynamic_modname if modname is None else modname
+        if modname is None or (
+            func_ir.func_id.module is not None
+            and func_ir.func_id.module.__dict__ != global_dict
+        ):
+            # Dynamically generated function.
+            modname = _dynamic_modname
+            # Retain a reference to the dictionary of the function.
+            # This disables caching, serialization and pickling.
+            global_dict = func_ir.func_id.func.__globals__
 
         unique_name = func_ir.func_id.unique_name
 
