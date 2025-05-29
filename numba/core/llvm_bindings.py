55b79539221492e73ccd6dc3064fa0aeb0e0a483
Yashwant Singh <yashwants@nvidia.com>
2025-05-29 01:45:23 -0700

    Prepare Numba for LLVM20 changes

--- numba/core/llvm_bindings.py
+++ numba/core/llvm_bindings.py
@@ -32,20 +32,6 @@ def _inlining_threshold(optlevel, sizelevel=0):
 
     return 225
 
-
-def create_pass_manager_builder(opt=2, loop_vectorize=False,
-                                slp_vectorize=False):
-    """
-    Create an LLVM pass manager with the desired optimisation level and options.
-    """
-    pmb = llvm.create_pass_manager_builder()
-    pmb.opt_level = opt
-    pmb.loop_vectorize = loop_vectorize
-    pmb.slp_vectorize = slp_vectorize
-    pmb.inlining_threshold = _inlining_threshold(opt)
-    return pmb
-
-
 def create_pass_builder(tm, opt=2, loop_vectorize=False,
                         slp_vectorize=False):
     """
