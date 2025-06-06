Fix error for unsupported axis kwarg in average

numba/np/old_arraymath.py
@@ -917,22 +917,20 @@ def np_any(a):
 
 @overload(np.average)
 def np_average(a, axis=None, weights=None):
-
-    if weights is None or isinstance(weights, types.NoneType):
+    if axis is not None or not isinstance(axis, types.NoneType):
         def np_average_impl(a, axis=None, weights=None):
-            arr = np.asarray(a)
-            return np.mean(arr)
+            raise TypeError("Numba does not support average with axis.")
     else:
-        if axis is None or isinstance(axis, types.NoneType):
+        if weights is None or isinstance(weights, types.NoneType):
+            def np_average_impl(a, axis=None, weights=None):
+                arr = np.asarray(a)
+                return np.mean(arr)
+        else:
             def np_average_impl(a, axis=None, weights=None):
                 arr = np.asarray(a)
                 weights = np.asarray(weights)
 
                 if arr.shape != weights.shape:
-                    if axis is None:
-                        raise TypeError(
-                            "Numba does not support average when shapes of "
-                            "a and weights differ.")
                     if weights.ndim != 1:
                         raise TypeError(
                             "1D weights expected when shapes of "
@@ -945,9 +943,6 @@ def np_average(a, axis=None, weights=None):
 
                 avg = np.sum(np.multiply(arr, weights)) / scl
                 return avg
-        else:
-            def np_average_impl(a, axis=None, weights=None):
-                raise TypeError("Numba does not support average with axis.")
 
     return np_average_impl
 

numba/tests/test_np_functions.py
@@ -4332,12 +4332,19 @@ class TestNPFunctions(MemoryLeakMixin, TestCase):
                 cfunc(data, weights=weights)
             err = e.exception
             self.assertEqual(str(err),
-                             "Numba does not support average when shapes of "
+                             "1D weights expected when shapes of "
                              "a and weights differ.")
 
         def test_1D_weights_axis(data, axis, weights):
             with self.assertRaises(TypeError) as e:
-                cfunc(data,axis=axis, weights=weights)
+                cfunc(data, axis=axis, weights=weights)
+            err = e.exception
+            self.assertEqual(str(err),
+                             "Numba does not support average with axis.")
+
+        def test_axis(data, axis):
+            with self.assertRaises(TypeError) as e:
+                cfunc(data, axis=axis)
             err = e.exception
             self.assertEqual(str(err),
                              "Numba does not support average with axis.")
@@ -4346,10 +4353,13 @@ class TestNPFunctions(MemoryLeakMixin, TestCase):
         data = np.arange(6).reshape((3,2,1))
         w = np.asarray([1. / 4, 3. / 4])
 
-        #test without axis argument
+        #test axis
+        test_axis(data, axis=1)
+
+        #test shape mismatch
         test_1D_weights(data, weights=w)
 
-        #test with axis argument
+        #test axis and with weights provided
         test_1D_weights_axis(data, axis=1, weights=w)
 
     def test_allclose(self):