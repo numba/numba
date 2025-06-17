4ae1ade8e1dda62ba92f5b25fa5edf0128f73ada
swap357 <swapnilpatel357@gmail.com>
2025-06-17 00:20:47 -0700

    refactor: move VS2022 environment setup back to bld.bat from GHA workflow

--- buildscripts/condarecipe.local/bld.bat
+++ buildscripts/condarecipe.local/bld.bat
@@ -1,3 +1,15 @@
+REM First, setup the VS2022 environment
+for /F "usebackq tokens=*" %%i in (`vswhere.exe -nologo -products * -version "[17.0,18.0)" -property installationPath`) do (
+  set "VSINSTALLDIR=%%i\\"
+)
+if not exist "%VSINSTALLDIR%" (
+  echo "Could not find VS 2022"
+  exit /B 1
+)
+
+echo "Found VS 2022 in %VSINSTALLDIR%"
+call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" x64
+
 %PYTHON% setup.py build install --single-version-externally-managed --record=record.txt
 
 exit /b %errorlevel%
