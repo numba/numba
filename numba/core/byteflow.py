7112cba487e1c6fc18f1be0fa68684431414438f
esc <esc@users.noreply.github.com>
2025-06-24 15:59:17 +0200

    [314] [py] new bytecode: `POP_ITER`
    As title

--- numba/core/byteflow.py
+++ numba/core/byteflow.py
@@ -445,6 +445,14 @@ class TraceRunner(object):
     def op_POP_TOP(self, state, inst):
         state.pop()
 
+    if PYVERSION in ((3, 14), ):
+        # New in 3.14
+        op_POP_ITER = op_POP_TOP
+    elif PYVERSION in ((3, 10), (3, 11), (3, 12), (3, 13)):
+        pass
+    else:
+        raise NotImplementedError(PYVERSION)
+
     if PYVERSION in ((3, 13),):
         def op_TO_BOOL(self, state, inst):
             res = state.make_temp()
