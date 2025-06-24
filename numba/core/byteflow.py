a71e1640cb31f2007ee8f58409626a9c9fbf6e2c
esc <esc@users.noreply.github.com>
2025-06-24 19:05:32 +0200

    [314] [py] new bytecode: `NOT_TAKEN`
    As title

--- numba/core/byteflow.py
+++ numba/core/byteflow.py
@@ -372,6 +372,14 @@ class TraceRunner(object):
     def op_NOP(self, state, inst):
         state.append(inst)
 
+    if PYVERSION in ((3,14), ):
+        # New in 3.14
+        op_NOT_TAKEN = op_NOP
+    elif PYVERSION in ((3, 10), (3, 11), (3, 12), (3, 13)):
+        pass
+    else:
+        raise NotImplementedError(PYVERSION)
+
     def op_RESUME(self, state, inst):
         state.append(inst)
 
