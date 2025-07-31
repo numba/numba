e691de88d705070a02bd8276fdb9652032c461a1
KevinMai0202 <129697366+KevinMai0202@users.noreply.github.com>
2025-07-31 07:45:48 +0000

    Fix flake8 error

--- numba/experimental/structref.py
+++ numba/experimental/structref.py
@@ -385,6 +385,7 @@ class StructRefProxy:
         """
         return self._type
 
+
 @extending.lower_builtin(operator.is_, types.StructRef, types.StructRef)
 def structref_is(context, builder, sig, args):
     """
