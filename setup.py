320daf4d9fbefbcf6f1273b445d6d557d583f634
esc <esc@users.noreply.github.com>
2025-08-27 21:16:22 +0200

    pin NumPy and llvmlite
    As title

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ except ImportError:
 
 min_python_version = "3.10"
 max_python_version = "3.14"  # exclusive
-min_numpy_build_version = "1.11"
+min_numpy_build_version = "2.0.0rc1"
 min_numpy_run_version = "1.22"
+max_numpy_run_version = "2.4"
 min_llvmlite_version = "0.45.0dev0"
 max_llvmlite_version = "0.46"
 
@@ -365,11 +366,11 @@ def get_ext_modules():
 
 
 packages = find_packages(include=["numba", "numba.*"])
-
-build_requires = ['numpy >={}'.format(min_numpy_build_version)]
+build_requires = ['numpy >={},<{}'.format(min_numpy_build_version,
+                                          max_numpy_run_version)]
 install_requires = [
     'llvmlite >={},<{}'.format(min_llvmlite_version, max_llvmlite_version),
-    'numpy >={}'.format(min_numpy_run_version),
+    'numpy >={},<{}'.format(min_numpy_run_version, max_numpy_run_version),
 ]
 
 metadata = dict(
