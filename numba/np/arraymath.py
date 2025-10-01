5e6c99895c690fbea52383f386e3b91140c123af
swap357 <swapnilpatel357@gmail.com>
2025-10-01 14:24:11 -0700

    refactor np_all overload and adjust test cases for scalar inputs

--- numba/np/arraymath.py
+++ numba/np/arraymath.py
@@ -802,9 +802,6 @@ def array_argmax(a, axis=None):
 @overload_method(types.Array, "all")
 def np_all(a):
 
-    if not type_can_asarray(a):
-        raise TypingError('The argument "a" must be array-like')
-
     # for scalar
     if isinstance(a, (types.Number, types.Boolean)):
         def scalar_all(a):
@@ -812,13 +809,15 @@ def np_all(a):
         return scalar_all
 
     # for array
-    def flat_all(a):
-        for v in np.nditer(a):
-            if not v.item():
-                return False
-        return True
+    if isinstance(a, types.Array):
+        def flat_all(a):
+            for v in np.nditer(a):
+                if not v.item():
+                    return False
+            return True
+        return flat_all
 
-    return flat_all
+    return None
 
 
 @register_jitable
