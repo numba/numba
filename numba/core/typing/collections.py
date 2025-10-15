459243e73d63851d8582b20433742a5de7b7dca9
Graham Markall <gmarkall@nvidia.com>
2025-10-15 11:33:05 +0100

    Is collections' operator.contains typing used?
    It may be buggy as it returns the container type forcing a cast. If this
    were used for e.g. `1.5 in (1, 2)` then the signature would likely be
    along the lines of `bool(UniTuple(int64, 2), int64)`, so I am wondering
    if it is used at all.

--- numba/core/typing/collections.py
+++ numba/core/typing/collections.py
@@ -6,15 +6,6 @@ from .templates import (AttributeTemplate, ConcreteTemplate, AbstractTemplate,
 from .builtins import normalize_1d_index
 
 
-@infer_global(operator.contains)
-class InContainer(AbstractTemplate):
-    key = operator.contains
-
-    def generic(self, args, kws):
-        cont, item = args
-        if isinstance(cont, types.Container):
-            return signature(types.boolean, cont, cont.dtype)
-
 @infer_global(len)
 class ContainerLen(AbstractTemplate):
 
