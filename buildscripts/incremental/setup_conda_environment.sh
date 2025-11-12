11080f732ff28942f7510af4238254c9eca1646f
swap357 <swapnilpatel357@gmail.com>
2025-11-13 01:10:57 +0530

    pin gcc_linux-64 and gxx_linux-64 to v11

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -66,11 +66,11 @@ fi
 # Install the compiler toolchain and gdb (if available)
 if [[ $(uname) == Linux ]]; then
     if [ $PYTHON \< "3.12" ]; then
-        $CONDA_INSTALL gcc_linux-64 gxx_linux-64 gdb gdb-pretty-printer
+        $CONDA_INSTALL gcc_linux-64=11 gxx_linux-64=11 gdb gdb-pretty-printer
     else
         # At the time of writing gdb and gdb-pretty-printer were not available
         # for 3.12.
-        $CONDA_INSTALL gcc_linux-64 gxx_linux-64
+        $CONDA_INSTALL gcc_linux-64=11 gxx_linux-64=11
     fi
 elif  [[ $(uname) == Darwin ]]; then
     $CONDA_INSTALL clang_osx-64 clangxx_osx-64
