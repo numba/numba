c3363cd493350c014ddd9951fdc8007bf1f3f890
swap357 <swapnilpatel357@gmail.com>
2025-11-14 03:16:30 +0530

    fix quotes on win-64 run_test.bat numpy AVX512_SKX detection command

--- buildscripts/condarecipe.local/run_test.bat
+++ buildscripts/condarecipe.local/run_test.bat
@@ -8,10 +8,14 @@ set _NUMBA_REDUCED_TESTING=1
 @rem Disable NumPy dispatching to AVX512_SKX feature extensions if the chip is
 @rem reported to support the feature and NumPy >= 1.22 as this results in the use
 @rem of low accuracy SVML libm replacements in ufunc loops.
-set "_NPY_CMD=from numba.misc import numba_sysinfo;sysinfo=numba_sysinfo.get_sysinfo();print(sysinfo['NumPy AVX512_SKX detected'] and sysinfo['NumPy Version']^>='1.22')"
-for /f %%i in ('python -c %_NPY_CMD%') do set NUMPY_DETECTS_AVX512_SKX_NP_GT_122=%%i
+set "_NPY_CMD=from numba.misc import numba_sysinfo; "
+set "_NPY_CMD=%_NPY_CMD%sysinfo=numba_sysinfo.get_sysinfo(); "
+set "_NPY_CMD=%_NPY_CMD%print(sysinfo['NumPy AVX512_SKX detected'] "
+set "_NPY_CMD=%_NPY_CMD%and sysinfo['NumPy Version']>='1.22')"
+for /f %%i in ('python -c "%_NPY_CMD%"') do (
+    set NUMPY_DETECTS_AVX512_SKX_NP_GT_122=%%i
+)
 echo NumPy ^>= 1.22 with AVX512_SKX detected: %NUMPY_DETECTS_AVX512_SKX_NP_GT_122%
-
 if "%NUMPY_DETECTS_AVX512_SKX_NP_GT_122%"=="True" (
     set NPY_DISABLE_CPU_FEATURES=AVX512_SKX
 )
