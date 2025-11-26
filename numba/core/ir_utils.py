e2ab03c7306a6f30a59625b2a582b03dfa54b4bb
Todd A. Anderson <toddanderson@bodo.ai>
2025-11-25 17:11:33 -0800

    Make calls to type names create aliasing variables.

--- numba/core/ir_utils.py
+++ numba/core/ir_utils.py
@@ -5,6 +5,7 @@
 
 import numpy
 import math
+import sys
 
 import types as pytypes
 import collections
@@ -885,6 +886,22 @@ def find_potential_aliases(blocks, args, typemap, func_ir, alias_map=None,
                         _add_alias(lhs, expr.args[0].name, alias_map, arg_aliases)
                     if isinstance(fmod, ir.Var) and fname in np_alias_funcs:
                         _add_alias(lhs, fmod.name, alias_map, arg_aliases)
+
+                    # If we use an arg to create a type then they alias.
+                    fmod_mod = sys.modules[fmod]
+                    fmod_obj = getattr(fmod_mod, fname)
+                    if isinstance(fmod_obj, type):
+                        for earg in expr.args:
+                            if isinstance(earg, ir.Var):
+                                _add_alias(lhs, earg.name, alias_map, arg_aliases)
+                        for earg_tup in expr.kws:
+                            _, earg = earg_tup
+                            if isinstance(earg, ir.Var):
+                                _add_alias(lhs, earg.name, alias_map, arg_aliases)
+                    # FIX ME: Technically this should be an denylist instead
+                    # of a allowlist.  Any call to potentially create an alias
+                    # and we should exclude calls known not to create aliases.
+
 
     # copy to avoid changing size during iteration
     old_alias_map = copy.deepcopy(alias_map)
