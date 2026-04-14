a5cc9ebbaf87cba95206dff9c55cfede9a8896aa
kc611 <>
2026-04-14 22:09:11 +0530

    Updated intp_ to map to np_intp and uint to map to np_unintp

--- numba/tests/test_typenames.py
+++ numba/tests/test_typenames.py
@@ -1,16 +1,11 @@
-import numpy as np
-
 from numba.core import types
 import unittest
 
 
 class TestTypeNames(unittest.TestCase):
     def test_numpy_integers(self):
-        expect = getattr(types, "int%d" % (np.dtype("int").itemsize * 8))
-        self.assertEqual(types.int_, expect)
-
-        expect = getattr(types, "uint%d" % (np.dtype("uint").itemsize * 8))
-        self.assertEqual(types.uint, expect)
+        self.assertEqual(types.int_, types.np_intp)
+        self.assertEqual(types.uint, types.np_uintp)
 
 
 if __name__ == '__main__':
