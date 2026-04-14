fc82c3975a247106373f5205b42eb9d16b2ec1e7
Nikita Syschikov <>
2026-04-14 20:06:24 +0300

    Add ddof support to np.nanvar and np.nanstd (fixes #6611)

--- numba/np/arraymath.py
+++ numba/np/arraymath.py
@@ -1290,37 +1290,57 @@ def np_nanmean(a):
 
 
 @overload(np.nanvar)
-def np_nanvar(a):
+def np_nanvar(a, ddof=0):
     if not isinstance(a, types.Array):
         return
+    if not isinstance(ddof, (types.Integer, types.Omitted, int)):
+        return
+
     isnan = get_isnan(a.dtype)
 
-    def nanvar_impl(a):
-        # Compute the mean
-        m = np.nanmean(a)
-
-        # Compute the sum of square diffs
-        ssd = 0.0
-        count = 0
-        for view in np.nditer(a):
-            v = view.item()
-            if not isnan(v):
-                val = (v.item() - m)
-                ssd += np.real(val * np.conj(val))
-                count += 1
-        # np.divide() doesn't raise ZeroDivisionError
-        return np.divide(ssd, count)
+    if isinstance(a.dtype, (types.Complex,)):
+        def nanvar_impl(a, ddof=0):
+            # For complex input NumPy uses abs(v - m)**2 (always real)
+            m = np.nanmean(a)
+            n = 0
+            s = 0.0
+            for view in np.nditer(a):
+                v = view.item()
+                if not isnan(v):
+                    d = v - m
+                    s += (d.real ** 2 + d.imag ** 2)
+                    n += 1
+            # When all elements are NaN, n==0; return nan to match NumPy
+            if n <= ddof:
+                return np.nan
+            return s / (n - ddof)
+    else:
+        def nanvar_impl(a, ddof=0):
+            m = np.nanmean(a)
+            n = 0
+            s = 0.0
+            for view in np.nditer(a):
+                v = view.item()
+                if not isnan(v):
+                    s += (v - m) ** 2
+                    n += 1
+            # When all elements are NaN, n==0; return nan to match NumPy
+            if n <= ddof:
+                return np.nan
+            return s / (n - ddof)
 
     return nanvar_impl
 
 
 @overload(np.nanstd)
-def np_nanstd(a):
+def np_nanstd(a, ddof=0):
     if not isinstance(a, types.Array):
         return
+    if not isinstance(ddof, (types.Integer, types.Omitted, int)):
+        return
 
-    def nanstd_impl(a):
-        return np.nanvar(a) ** 0.5
+    def nanstd_impl(a, ddof=0):
+        return np.nanvar(a, ddof) ** 0.5
 
     return nanstd_impl
 
