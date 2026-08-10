8c17dfaf2be014da7ba314622bd7de550f07dd7f
kc611 <ckaustubhm06@gmail.com>
2026-08-10 21:21:29 +0530

    Fix imports in various tests

--- numba/np/types/__init__.py
+++ numba/np/types/__init__.py
@@ -7,7 +7,7 @@ numpy_version = tuple(map(int, np.__version__.split('.')[:2]))
 
 if numpy_version >= (2, 0):
     types.bool = types.bool_
-    types.__all__.append('bool')
+    # types.__all__.append('bool')
 
 if numpy_version < (2, 0):
     types.float_ = types.float32
