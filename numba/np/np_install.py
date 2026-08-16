20fa1e1e63e60fa6977479f31f3725bc836e4108
kc611 <ckaustubhm06@gmail.com>
2026-08-16 16:45:16 +0530

    Patch in NumPy based casting while installing NumPy module

--- numba/np/np_install.py
+++ numba/np/np_install.py
@@ -1,5 +1,7 @@
 from numba.core.typing.context import Context
 from numba.np import boxing   # noqa: F401
+from numba.core import types
+import numpy as np
 
 # Patch core registries to load additional npy registries
 load_additional_core_registries = Context.load_additional_registries
@@ -15,3 +17,11 @@ def load_additional_npy_registries(self):
 
 
 Context.load_additional_registries = load_additional_npy_registries
+
+# Patch the cast_python_value methods of Integer, Float, and Complex
+# to use NumPy's casting functions instead of Python's built-in types.
+npy_scalar_cast = lambda self, value: getattr(np, self.name)(value)
+
+types.Integer.cast_python_value = npy_scalar_cast
+types.Float.cast_python_value = npy_scalar_cast
+types.Complex.cast_python_value = npy_scalar_cast
